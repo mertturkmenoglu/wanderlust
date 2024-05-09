6662bb1716d3557a99971723776035130f75bbcd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-09 20:42:37 +0300

    add webhook event handlers

--- apps/api/src/routes/webhooks.ts
+++ apps/api/src/routes/webhooks.ts
@@ -2,7 +2,10 @@ import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { Webhook } from "svix";
 import env from "../env";
-import { WebhookEvent } from "@clerk/backend";
+import { DeletedObjectJSON, UserJSON, WebhookEvent } from "@clerk/backend";
+import { db } from "../db";
+import { auths, users } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 const app = new Hono();
 
@@ -41,19 +44,103 @@ app.post("/", async (c) => {
   }
 
   // Do something with the payload
-  // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", evt.data);
 
-  return c.json(
-    {
-      success: true,
-      message: "Webhook received",
-    },
-    200
-  );
+  try {
+    if (eventType === "user.created") {
+      await handleUserCreate(evt.data);
+    } else if (eventType === "user.updated") {
+      await handleUserUpdate(evt.data);
+    } else if (eventType === "user.deleted") {
+      await handleUserDelete(evt.data);
+    } else {
+      console.log(`Different event: ${id} - ${eventType}`);
+      console.log(evt.data);
+    }
+
+    return c.json(
+      {
+        success: true,
+        message: "Webhook received",
+      },
+      200
+    );
+  } catch (e) {
+    console.error(e);
+    return c.json(
+      {
+        success: false,
+        message: "Something went wrong",
+      },
+      500
+    );
+  }
 });
+
+async function handleUserCreate(data: UserJSON): Promise<void> {
+  try {
+    await db.transaction(async (tx) => {
+      const [user] = await tx.insert(users).values({}).returning();
+      await tx.insert(auths).values({
+        clerkId: data.id,
+        firstName: data.first_name,
+        lastName: data.last_name,
+        email: data.email_addresses[0]?.email_address,
+        emailId: data.email_addresses[0]?.id,
+        emailVerification: data.email_addresses[0]?.verification?.status,
+        lastSignInAt: data.last_sign_in_at,
+        createdAt: data.created_at,
+        updatedAt: data.updated_at,
+        username: data.username,
+        image: data.image_url,
+        userId: user.id,
+      });
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function handleUserUpdate(data: UserJSON): Promise<void> {
+  try {
+    await db
+      .update(auths)
+      .set({
+        clerkId: data.id,
+        firstName: data.first_name,
+        lastName: data.last_name,
+        email: data.email_addresses[0]?.email_address,
+        emailId: data.email_addresses[0]?.id,
+        emailVerification: data.email_addresses[0]?.verification?.status,
+        lastSignInAt: data.last_sign_in_at,
+        createdAt: data.created_at,
+        updatedAt: data.updated_at,
+        username: data.username,
+        image: data.image_url,
+      })
+      .where(eq(auths.clerkId, data.id));
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+async function handleUserDelete(data: DeletedObjectJSON): Promise<void> {
+  try {
+    await db.transaction(async (tx) => {
+      const clerkId = data.id;
+      if (clerkId === undefined) {
+        throw new Error("Clerk ID is undefined");
+      }
+      const [authObj] = await tx
+        .delete(auths)
+        .where(eq(auths.clerkId, clerkId))
+        .returning();
+      await tx.delete(users).where(eq(users.id, authObj.userId));
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 export default app;
