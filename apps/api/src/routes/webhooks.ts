2e9c9fb248f6c116ac61985b0e06b10383350558
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-09 17:17:15 +0300

    add webhooks route

---
+++ apps/api/src/routes/webhooks.ts
@@ -0,0 +1,59 @@
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+import { Webhook } from "svix";
+import env from "../env";
+import { WebhookEvent } from "@clerk/backend";
+
+const app = new Hono();
+
+app.post("/", async (c) => {
+  // Get the Svix headers for verification
+  const svix_id = c.req.header("svix-id");
+  const svix_timestamp = c.req.header("svix-timestamp");
+  const svix_signature = c.req.header("svix-signature");
+
+  // If there are no Svix headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    throw new HTTPException(400, {
+      message: "Error occured -- no svix headers",
+    });
+  }
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(env.CLERK_WEBHOOK_SECRET);
+  let evt: WebhookEvent;
+
+  // Attempt to verify the incoming webhook
+  // If successful, the payload will be available from 'evt'
+  // If the verification fails, error out and  return error code
+  try {
+    const payload = await c.req.raw.text();
+    evt = wh.verify(payload, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err: any) {
+    console.log("Error verifying webhook:", err.message);
+    throw new HTTPException(400, {
+      message: err.message,
+    });
+  }
+
+  // Do something with the payload
+  // For this guide, you simply log the payload to the console
+  const { id } = evt.data;
+  const eventType = evt.type;
+  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", evt.data);
+
+  return c.json(
+    {
+      success: true,
+      message: "Webhook received",
+    },
+    200
+  );
+});
+
+export default app;
