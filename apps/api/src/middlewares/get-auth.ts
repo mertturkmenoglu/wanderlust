8e62fa83effea9738b62cc9afd5bc2ef719d6fe5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-12 17:12:15 +0300

    add new middleware and add docs

--- apps/api/src/middlewares/get-auth.ts
+++ apps/api/src/middlewares/get-auth.ts
@@ -6,6 +6,17 @@ import { auths } from "../db/schema";
 import { eq } from "drizzle-orm";
 import { Env } from "..";
 
+/**
+ * Enforces authentication.
+ *
+ * User must be authenticated. Else, returns 401 Unauthorized exception.
+ *
+ * If user is authenticated, `auth` environment variable is set as the database user.
+ *
+ * Use it after the `clerkMiddleware` from the `@hono/clerk-auth` package
+ *
+ * @throws HTTPException 401 if the user is not authenticated.
+ */
 export const getAuth = createMiddleware<Env>(async (c, next) => {
   const auth = getAuthClerk(c);
 
---
+++ apps/api/src/middlewares/with-auth.ts
@@ -0,0 +1,33 @@
+import { createMiddleware } from "hono/factory";
+import { Env } from "..";
+import { getAuth as getAuthClerk } from "@hono/clerk-auth";
+import { db } from "../db";
+import { auths } from "../db/schema";
+import { eq } from "drizzle-orm";
+
+/**
+ * Check the auth status of user.
+ *
+ * If user is authenticated, `withAuth` environment variable is set as the database user.
+ *
+ * Else, `withAuth` is undefined.
+ *
+ * Use it after the `clerkMiddleware` from the `@hono/clerk-auth` package
+ *
+ */
+export const withAuth = createMiddleware<Env>(async (c, next) => {
+  const auth = getAuthClerk(c);
+
+  if (!auth || !auth.userId) {
+    c.set("withAuth", undefined);
+  } else {
+    const [authUser] = await db
+      .select()
+      .from(auths)
+      .where(eq(auths.clerkId, auth.userId));
+
+    c.set("auth", authUser);
+  }
+
+  await next();
+});
