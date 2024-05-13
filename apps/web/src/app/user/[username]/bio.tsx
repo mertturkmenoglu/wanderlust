119e2a438214cc5be982a4e15f80b44d94b05193
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-13 06:41:58 +0300

    update dummy bio component

--- apps/web/src/app/user/[username]/bio.tsx
+++ apps/web/src/app/user/[username]/bio.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useIsThisUser } from "@/hooks/useIsThisUser";
 import { getUserProfile } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 function Bio() {
   const params = useParams<{ username: string }>();
+  const isThisUser = useIsThisUser(params.username);
 
   const query = useQuery({
     queryKey: ["user", params.username],
@@ -16,6 +19,7 @@ function Bio() {
     <div>
       <div>
         <pre>{JSON.stringify(query.data, null, 2)}</pre>
+        {isThisUser && <Link href="/settings">Settings</Link>}
       </div>
     </div>
   );
