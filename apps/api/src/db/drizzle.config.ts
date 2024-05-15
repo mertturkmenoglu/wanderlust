f8c77df59bb8186a8b92fdad5c01b1ef635f1507
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-15 23:53:02 +0300

    update drizzle config

--- apps/api/src/db/drizzle.config.ts
+++ apps/api/src/db/drizzle.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "drizzle-kit";
+import env from "../env";
 export default {
   schema: "./src/db/schema.ts",
+  driver: "pg",
   out: "./drizzle",
+  dbCredentials: {
+    connectionString: env.DB_URL,
+  },
 } satisfies Config;
