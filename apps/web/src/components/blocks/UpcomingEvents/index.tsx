5cb295511f951b5050b1afd185c67ff94e4675ff
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-15 04:27:18 +0300

    add dummy upcoming events component

---
+++ apps/web/src/components/blocks/UpcomingEvents/index.tsx
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { dummydata } from "@/lib/dummydata";
+import Image from "next/image";
+
+function UpcomingEvents() {
+  return (
+    <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-6 gap-4 place-content-center">
+      {dummydata.map((d) => (
+        <Card key={d.image} className="group">
+          <Image
+            src={d.image}
+            alt=""
+            className="aspect-video rounded-t-xl w-full object-cover"
+            width={512}
+            height={288}
+          />
+
+          <CardHeader>
+            <CardTitle>{d.name}</CardTitle>
+            <CardDescription>By {d.organizer}</CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <div className="flex-1 space-y-1">
+              <p className="text-sm font-medium leading-none">{d.location}</p>
+              <p className="text-sm text-muted-foreground">{d.startsAt}</p>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default UpcomingEvents;
