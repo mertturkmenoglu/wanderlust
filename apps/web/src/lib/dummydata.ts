09ac6d7665aea6de2b1be08967f416f8d3e3a73f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-15 04:26:53 +0300

    add dummy data

---
+++ apps/web/src/lib/dummydata.ts
@@ -0,0 +1,44 @@
+export const dummydata = [
+  {
+    name: "Evolution Conference",
+    organizer: "Aalto University",
+    location: "Espoo, Finland",
+    startsAt: "12/12/2024 8:00 PM",
+    image: "http://source.unsplash.com/YRMWVcdyhmI",
+  },
+  {
+    name: "Keepers Market",
+    organizer: "Frankie Magazine",
+    location: "Royal Exhibition Building, Carlton",
+    startsAt: "12/12/2024 8:00 PM",
+    image: "http://source.unsplash.com/hSAlu33padA",
+  },
+  {
+    name: "DJ Brogx - Party All Night",
+    organizer: "NYC Summit Inc.",
+    location: "Bryant Park",
+    startsAt: "12/12/2024 8:00 PM",
+    image: "http://source.unsplash.com/ZhQCZjr9fHo",
+  },
+  {
+    name: "Career Development Panel",
+    organizer: "Jane Merrywether",
+    location: "Oxford League Club",
+    startsAt: "12/12/2024 8:00 PM",
+    image: "http://source.unsplash.com/cw-cj_nFa14",
+  },
+  {
+    name: "World Economical Forum",
+    organizer: "WTC",
+    location: "Zorlu Center",
+    startsAt: "12/12/2024 8:00 PM",
+    image: "http://source.unsplash.com/nwLTVwb7DbU",
+  },
+  {
+    name: "Las Vegas Startups",
+    organizer: "NeONBRAND Digital Marketing",
+    location: "Las Vegas",
+    startsAt: "12/12/2024 8:00 PM",
+    image: "http://source.unsplash.com/1-aA2Fadydc",
+  },
+];
