d42ba0f38d74c1b26f9151cea76081d6c9dc727f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-15 08:05:27 +0300

    add rate limiter

--- apps/api/src/routes/users.ts
+++ apps/api/src/routes/users.ts
@@ -2,13 +2,23 @@ import { clerkMiddleware } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { rateLimiter } from "hono-rate-limiter";
 import { HTTPException } from "hono/http-exception";
 import { z } from "zod";
+import { Env } from "..";
 import { db } from "../db";
 import { users } from "../db/schema";
 import { getAuth } from "../middlewares/get-auth";
 
+const limiter = rateLimiter<Env>({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-6", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  keyGenerator: (c) => c.get("clerkAuth")?.userId ?? c.env.ip.address,
+});
+
 const app = new Hono()
+  .use(limiter)
   .get("/me", clerkMiddleware(), getAuth, async (c) => {
     const auth = c.get("auth");
 
