c2f13b54573912aa2947b949d44f978a6f290df1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-17 08:37:05 +0300

    move profile page

--- apps/web/src/app/settings/profile/[[...rest]]/page.tsx
+++ apps/web/src/app/settings/profile/[[...rest]]/page.tsx
@@ -1,21 +1,22 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { UserProfile, ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
-import Org from "./org";
-import { auth } from "@clerk/nextjs/server";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  OrganizationSwitcher,
+  UserProfile,
+} from "@clerk/nextjs";
 
 function Page() {
   return (
     <div>
-      <div>Settings</div>
       <div className="container mx-auto flex justify-center">
         <ClerkLoading>
           <Skeleton className="w-full h-96 bg-muted" />
         </ClerkLoading>
         <ClerkLoaded>
-          <div>
+          <div className="space-y-4">
             <UserProfile />
-            <div className="my-12" />
-            {auth().orgId === process.env.CLERK_ADMIN_ORG_ID && <Org />}
+            <OrganizationSwitcher />
           </div>
         </ClerkLoaded>
       </div>
