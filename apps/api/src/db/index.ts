809e9772d2747996e7effb466335f595c21249f8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-19 15:43:02 +0300

    update database connection

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -3,17 +3,21 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import env from "../env";
 
-const connectionString = env.DB_URL;
+/**
+ * Cache the database connection in development. This avoids creating a new connection on every HMR
+ * update.
+ */
+const globalForDb = globalThis as unknown as {
+  conn: postgres.Sql | undefined;
+};
 
-export const sql = postgres(connectionString, {
-  onnotice: () => {},
-  connect_timeout: 1_000_000,
-});
+const conn = globalForDb.conn ?? postgres(env.DB_URL);
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(sql);
+export const db = drizzle(conn);
 
 export async function runDrizzleMigrations() {
-  const migrationPostgres = postgres(connectionString, { onnotice: () => {} });
+  const migrationPostgres = postgres(env.DB_URL, { onnotice: () => {} });
   const migrationConnection = drizzle(migrationPostgres);
   await migrate(migrationConnection, { migrationsFolder: "drizzle" });
 }
