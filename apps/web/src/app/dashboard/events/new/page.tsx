106e03e4629c2441c93c815dafe76914e45d4485
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-19 15:43:18 +0300

    update new event page

--- apps/web/src/app/dashboard/events/new/page.tsx
+++ apps/web/src/app/dashboard/events/new/page.tsx
@@ -1,9 +1,14 @@
+import NewEventForm from "./_components/form";
+
 function Page() {
   return (
     <div>
       <div>
         <div>
-          <div>New Event</div>
+          <h3 className="text-lg font-bold tracking-tight my-8">
+            Create New Event
+          </h3>
+          <NewEventForm />
         </div>
       </div>
     </div>
