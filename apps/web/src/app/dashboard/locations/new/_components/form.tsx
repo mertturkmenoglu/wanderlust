22d9257b62ce8c465c15a896395dbae12c37ec67
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-19 10:49:13 +0300

    add locations form

--- apps/web/src/app/dashboard/locations/new/_components/form.tsx
+++ apps/web/src/app/dashboard/locations/new/_components/form.tsx
@@ -6,6 +6,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
   FormControl,
@@ -16,16 +17,50 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { createNewLocation, getCategories } from "@/lib/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 type FormInput = z.infer<typeof createLocationSchema>;
 
 function NewLocationForm() {
+  const router = useRouter();
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => getCategories(),
+    staleTime: 10 * 60 * 1000,
+  });
+
+  const mutation = useMutation({
+    mutationKey: ["new-location"],
+    mutationFn: async (payload: FormInput) => createNewLocation(payload),
+    onSuccess: () => {
+      router.push("/dashboard");
+    },
+  });
+
   const form = useForm<FormInput>({
     resolver: zodResolver(createLocationSchema),
+    defaultValues: {
+      phone: null,
+      website: null,
+      priceLevel: null,
+      accessibilityLevel: null,
+      hasWifi: false,
+      categoryId: 1,
+    },
   });
 
   const onSubmit: SubmitHandler<FormInput> = (data) => {
-    console.log(data);
+    mutation.mutate(data);
   };
 
   return (
@@ -43,14 +78,158 @@ function NewLocationForm() {
               <FormControl>
                 <Input placeholder="Name of the location" {...field} />
               </FormControl>
+              <FormDescription>Name of the location</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone Number</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="+1 (000) 000 0000"
+                  {...field}
+                  value={field.value ?? undefined}
+                />
+              </FormControl>
+              <FormDescription>Optional</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="website"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Website</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="https://example.com"
+                  {...field}
+                  value={field.value ?? undefined}
+                />
+              </FormControl>
+              <FormDescription>Optional</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="priceLevel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Price Level</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="1"
+                  {...field}
+                  type="number"
+                  value={`${field.value}`}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    field.onChange(e.target.valueAsNumber);
+                  }}
+                />
+              </FormControl>
               <FormDescription>
-                This is your public display name.
+                1 is cheap, 5 is expensive (Optional)
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+
+        <FormField
+          control={form.control}
+          name="accessibilityLevel"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Accessibility Level</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="5"
+                  {...field}
+                  type="number"
+                  value={`${field.value}`}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    field.onChange(e.target.valueAsNumber);
+                  }}
+                />
+              </FormControl>
+              <FormDescription>
+                1 is low 5 is highly accessible (Optional)
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="hasWifi"
+          render={({ field }) => (
+            <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+              <FormControl>
+                <Checkbox
+                  checked={field.value ?? false}
+                  onCheckedChange={field.onChange}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                <FormLabel>Has WiFi connection</FormLabel>
+                <FormDescription>
+                  Indicates there is an option to connect to WiFi (Optional,
+                  defaults to false)
+                </FormDescription>
+              </div>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="categoryId"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <Select
+                onValueChange={(v) => {
+                  field.onChange(+v);
+                }}
+                defaultValue={
+                  field.value === undefined ? undefined : `${field.value}`
+                }
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {(categories ?? []).map((c) => (
+                    <SelectItem value={`${c.id}`} key={c.id}>
+                      {c.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormDescription></FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Create</Button>
       </form>
     </Form>
   );
