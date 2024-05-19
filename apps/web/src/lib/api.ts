2cde5634582c6f8cf4ce9f55a8d6a875aac5165f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-19 10:48:54 +0300

    add new api function and update hono client config

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,7 +1,13 @@
+import { createLocationSchema } from "#/routes/dto/create-location";
 import { hc } from "hono/client";
+import { z } from "zod";
 import type { AppType } from "../../../api/src";
 
-export const { api } = hc<AppType>(process.env.API_URL!);
+export const { api } = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
+  init: {
+    credentials: "include",
+  },
+});
 
 export async function getUserProfile(username: string) {
   const res = await api.users[":username"].profile.$get({
@@ -20,6 +26,21 @@ export async function getUserProfile(username: string) {
 
 export async function getCategories() {
   const res = await api.categories.$get();
+
+  if (!res.ok) {
+    throw new Error("Error");
+  }
+
+  const { data } = await res.json();
+  return data;
+}
+
+type CreateNewLocationPayload = z.infer<typeof createLocationSchema>;
+
+export async function createNewLocation(payload: CreateNewLocationPayload) {
+  const res = await api.locations.$post({
+    json: payload,
+  });
 
   if (!res.ok) {
     throw new Error("Error");
