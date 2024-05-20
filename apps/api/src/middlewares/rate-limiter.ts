0e161981986a3a3f993b17c9b3106b45263dfd22
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-20 09:05:24 +0300

    extract rate limiter middleware

---
+++ apps/api/src/middlewares/rate-limiter.ts
@@ -0,0 +1,22 @@
+import { MiddlewareHandler } from "hono";
+import { rateLimiter as honoRateLimiter } from "hono-rate-limiter";
+import { createMiddleware } from "hono/factory";
+import { Env } from "..";
+
+type Options = {
+  window?: number;
+  limit?: number;
+};
+
+export const rateLimiter = (options?: Options): MiddlewareHandler => {
+  const { window = 10 * 60 * 1000, limit = 100 } = options ?? {};
+
+  return createMiddleware<Env>(
+    honoRateLimiter({
+      windowMs: window,
+      limit: limit,
+      standardHeaders: "draft-6",
+      keyGenerator: (c) => c.get("clerkAuth")?.userId ?? c.env.ip.address,
+    })
+  );
+};
