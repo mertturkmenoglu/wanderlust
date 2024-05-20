6f67d7540fb1543afe1eb9cd6435cb40e3934688
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-20 21:55:27 +0300

    add data fetching to dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,31 +1,32 @@
+"use client";
+
+import { getAddresses, getEvents, getLocations } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import {
-  Location,
-  addressCols,
-  eventCols,
-  locationsCols,
-} from "./_components/columns";
+import { addressCols, eventCols, locationsCols } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 
-export const points: Location[] = [
-  {
-    id: "728ed52f",
-    name: "Location 1",
-    categoryId: 1,
-  },
-  {
-    id: "489e1d42",
-    name: "Location 1",
-    categoryId: 2,
-  },
-];
-
 function Page() {
+  const qLocations = useQuery({
+    queryKey: ["locations"],
+    queryFn: async () => getLocations(),
+  });
+
+  const qEvents = useQuery({
+    queryKey: ["events"],
+    queryFn: async () => getEvents(),
+  });
+
+  const qAddresses = useQuery({
+    queryKey: ["addresses"],
+    queryFn: async () => getAddresses(),
+  });
+
   return (
     <div>
       <div>
         <h3 className="text-lg font-bold tracking-tight my-4">Points</h3>
-        <DataTable columns={locationsCols} data={points} />
+        <DataTable columns={locationsCols} data={qLocations.data ?? []} />
         <Link href="/dashboard/locations/new" className="mt-4 block">
           Create new location
         </Link>
@@ -33,7 +34,7 @@ function Page() {
 
       <div className="mt-16">
         <h3 className="text-lg font-bold tracking-tight my-4">Events</h3>
-        <DataTable columns={eventCols} data={[]} />
+        <DataTable columns={eventCols} data={qEvents.data ?? []} />
         <Link href="/dashboard/events/new" className="mt-4 block">
           Create new event
         </Link>
@@ -41,7 +42,7 @@ function Page() {
 
       <div className="mt-16">
         <h3 className="text-lg font-bold tracking-tight my-4">Addresses</h3>
-        <DataTable columns={addressCols} data={[]} />
+        <DataTable columns={addressCols} data={qAddresses.data ?? []} />
         <Link href="/dashboard/addresses/new" className="mt-4 block">
           Create new address
         </Link>
