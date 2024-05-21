8641d3189e7db1e372f17a376db320d3e85ff625
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-21 11:50:25 +0300

    add username validation schema

--- apps/api/src/routes/dto.ts
+++ apps/api/src/routes/dto.ts
@@ -3,3 +3,7 @@ import { z } from "zod";
 export const validateId = z.object({
   id: z.string().min(1).uuid(),
 });
+
+export const validateUsername = z.object({
+  username: z.string().min(1),
+});
