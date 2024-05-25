dcf91a1f961ea12750b5502368d8bd4d5af17031
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-25 23:51:27 +0300

    add drizzle eslint config

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,4 +1,5 @@
 import pluginJs from "@eslint/js";
+import eslintConfigDrizzle from "eslint-config-drizzle";
 import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -8,4 +9,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
+  eslintConfigDrizzle,
 ];
