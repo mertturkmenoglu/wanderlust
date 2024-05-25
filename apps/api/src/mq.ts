019fc6e6d42561ee52e69df729e477fcc15a5956
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-25 16:39:10 +0300

    update mq client

--- apps/api/src/mq.ts
+++ apps/api/src/mq.ts
@@ -1,5 +1,10 @@
 import amqplib from "amqplib";
-import { MQEventPayload, MQQueue, SendWelcomeEmailPayload } from "../../common";
+import {
+  MQEventPayload,
+  MQEventType,
+  MQQueue,
+  SendWelcomeEmailPayload,
+} from "../../common";
 
 export const mq = await amqplib.connect("amqp://localhost");
 
@@ -7,20 +12,37 @@ function serialize(payload: MQEventPayload) {
   return Buffer.from(JSON.stringify(payload));
 }
 
-async function getEmailChannel() {
-  const queue: MQQueue = "email";
+async function getChannel(q: MQQueue) {
   const ch = await mq.createChannel();
-  await ch.assertQueue(queue);
-  return [queue, ch] as const;
+  await ch.assertQueue(q);
+  return [q, ch] as const;
 }
 
 export async function sendWelcomeEmail(payload: SendWelcomeEmailPayload) {
-  const [queue, ch] = await getEmailChannel();
+  const [q, ch] = await getChannel("email");
 
   return ch.sendToQueue(
-    queue,
+    q,
     serialize({
       type: "send-welcome-email",
+      payload,
+    })
+  );
+}
+
+export type UserEventType = Extract<
+  MQEventType,
+  "user-created" | "user-updated" | "user-deleted"
+>;
+type UserPayload = (MQEventPayload & { type: UserEventType })["payload"];
+
+export async function sendUserEvent(type: UserEventType, payload: UserPayload) {
+  const [q, ch] = await getChannel("user");
+
+  return ch.sendToQueue(
+    q,
+    serialize({
+      type,
       payload,
     })
   );
