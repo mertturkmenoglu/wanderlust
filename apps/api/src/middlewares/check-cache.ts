f65ebc8812517326af167927b5c1accdb8662325
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-25 17:32:55 +0300

    add caching middleware

---
+++ apps/api/src/middlewares/check-cache.ts
@@ -0,0 +1,18 @@
+import { CacheKey, cacheRead } from "@/cache";
+import { Env } from "@/start";
+import { MiddlewareHandler } from "hono";
+import { createMiddleware } from "hono/factory";
+
+export const checkCache = <T>(key: CacheKey): MiddlewareHandler => {
+  return createMiddleware<Env>(async (c, next) => {
+    const cacheResult = await cacheRead<T>(key);
+
+    if (cacheResult === null) {
+      await next();
+    } else {
+      c.json({
+        data: cacheResult,
+      });
+    }
+  });
+};
