7c3f74d76e1ec3da9afb42699d7b0f177de8d602
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-25 16:40:07 +0300

    add mq consumer to core api

---
+++ apps/api/src/events/index.ts
@@ -0,0 +1,40 @@
+import amqplib from "amqplib";
+import { MQEventPayload, MQQueue } from "../../../common";
+import { handleUserCreate, handleUserDelete, handleUserUpdate } from "./users";
+
+async function consumer() {
+  const conn = await amqplib.connect("amqp://localhost");
+  const queue: MQQueue = "user";
+  const ch = await conn.createChannel();
+  await ch.assertQueue(queue);
+
+  await ch.consume(queue, (msg) => {
+    if (msg !== null) {
+      ch.ack(msg);
+      router(msg);
+    } else {
+      console.log("Consumer cancelled by server");
+    }
+  });
+}
+
+function router(msg: amqplib.ConsumeMessage) {
+  const str = msg.content.toString();
+  const payload = JSON.parse(str) as MQEventPayload;
+
+  switch (payload.type) {
+    case "user-created":
+      handleUserCreate(payload.payload);
+      break;
+    case "user-updated":
+      handleUserUpdate(payload.payload);
+      break;
+    case "user-deleted":
+      handleUserDelete(payload.payload);
+      break;
+    default:
+      break;
+  }
+}
+
+consumer();
---
+++ apps/api/src/events/users.ts
@@ -0,0 +1,17 @@
+import {
+  UserCreatedPayload,
+  UserDeletedPayload,
+  UserUpdatedPayload,
+} from "../../../common";
+
+export async function handleUserCreate(payload: UserCreatedPayload) {
+  console.log("User created", payload);
+}
+
+export async function handleUserUpdate(payload: UserUpdatedPayload) {
+  console.log("User updated", payload);
+}
+
+export async function handleUserDelete(payload: UserDeletedPayload) {
+  console.log("User deleted", payload);
+}
