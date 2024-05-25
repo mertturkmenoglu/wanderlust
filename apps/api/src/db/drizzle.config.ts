18e8294e1f985a75430a4284be51811565b9454a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-25 19:18:29 +0300

    update drizzle config db credentials

--- apps/api/src/db/drizzle.config.ts
+++ apps/api/src/db/drizzle.config.ts
@@ -7,6 +7,10 @@ export default {
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DB_URL,
+    database: env.DB_NAME,
+    host: env.DB_HOST,
+    password: env.DB_PASSWORD,
+    port: env.DB_PORT,
+    user: env.DB_USER,
   },
 } satisfies Config;
