905e33fb810e11dea6274a6f26cd5c1304732dc3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-28 16:59:34 +0300

    update dummy data

--- apps/web/src/lib/dummydata.ts
+++ apps/web/src/lib/dummydata.ts
@@ -4,41 +4,39 @@ export const dummydata = [
     organizer: "Aalto University",
     location: "Espoo, Finland",
     startsAt: "12/12/2024 8:00 PM",
-    image: "http://source.unsplash.com/YRMWVcdyhmI",
+    image:
+      "https://fastly.picsum.photos/id/210/1024/576.jpg?hmac=oZlAniQmO0LVGtHsHX_V4ruBXNK9agzC4Hmvqsk1BLE",
   },
   {
     name: "Keepers Market",
     organizer: "Frankie Magazine",
     location: "Royal Exhibition Building, Carlton",
     startsAt: "12/12/2024 8:00 PM",
-    image: "http://source.unsplash.com/hSAlu33padA",
+    image:
+      "https://fastly.picsum.photos/id/1048/1024/576.jpg?hmac=RYHoSTG0oc4nuvKUr3pJ1JpZArBH6HKaPWj6e238jCs",
   },
   {
     name: "DJ Brogx - Party All Night",
     organizer: "NYC Summit Inc.",
     location: "Bryant Park",
     startsAt: "12/12/2024 8:00 PM",
-    image: "http://source.unsplash.com/ZhQCZjr9fHo",
+    image:
+      "https://fastly.picsum.photos/id/570/1024/576.jpg?hmac=Qj9AgqU4mlD9IBpLdpUzC0pgR0Hy0Gx9axHv1cpOrZo",
   },
   {
     name: "Career Development Panel",
     organizer: "Jane Merrywether",
     location: "Oxford League Club",
     startsAt: "12/12/2024 8:00 PM",
-    image: "http://source.unsplash.com/cw-cj_nFa14",
+    image:
+      "https://fastly.picsum.photos/id/788/1024/576.jpg?hmac=LDvyC-PqYmGlJHNERKBLuRlzroZFinVZ8ydtYGGTSQA",
   },
   {
     name: "World Economical Forum",
     organizer: "WTC",
     location: "Zorlu Center",
     startsAt: "12/12/2024 8:00 PM",
-    image: "http://source.unsplash.com/nwLTVwb7DbU",
-  },
-  {
-    name: "Las Vegas Startups",
-    organizer: "NeONBRAND Digital Marketing",
-    location: "Las Vegas",
-    startsAt: "12/12/2024 8:00 PM",
-    image: "http://source.unsplash.com/1-aA2Fadydc",
+    image:
+      "https://fastly.picsum.photos/id/657/1024/576.jpg?hmac=vURSRDbsozwTJScz91cIC3vPqo67w9zw2wxWTy72NDI",
   },
 ];
