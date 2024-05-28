ff00fa3c2145eb68fc016cef180ae0a508791eee
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-28 17:16:49 +0300

    add latest locations section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,27 @@
 import CategoryNavigation from "@/components/blocks/CategoryNavigation";
 import UpcomingEvents from "@/components/blocks/UpcomingEvents";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { api, rpc } from "@/lib/api";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+export const revalidate = 600;
+
+async function peekLocations() {
+  return await rpc(() => api.locations.peek.$get());
+}
+
+export default async function Home() {
+  const data = await peekLocations();
+
   return (
     <main className="">
       <nav className="mx-auto flex justify-center my-12 items-center space-x-4">
@@ -24,6 +42,44 @@ export default function Home() {
       </h2>
 
       <UpcomingEvents />
+
+      <h2 className="scroll-m-20 text-2xl font-semibold tracking-tighter lg:text-3xl text-accent-foreground mt-12">
+        New Locations
+      </h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 my-8">
+        {data.map((location) => (
+          <Card key={location.id} className="group">
+            <img
+              src={location.media.slice(0, 1)[0].url}
+              alt={location.media.slice(0, 1)[0].alt}
+              className="aspect-video rounded-t-xl w-full object-cover"
+              width={512}
+              height={288}
+            />
+
+            <CardHeader>
+              <CardTitle>{location.name}</CardTitle>
+              <CardDescription>
+                {location.address.city} / {location.address.state}
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <div className="flex-1 space-y-1">
+                <p className="text-sm leading-none">{location.category.name}</p>
+                <div className="flex items-center gap-2 pt-4 flex-wrap">
+                  {location.tags.map((tag) => (
+                    <Badge key={tag} className="text-xs capitalize">
+                      {tag}
+                    </Badge>
+                  ))}
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
