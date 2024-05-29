134bd981f784f6c5241116e6d1d8e07dc145afae
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-29 21:36:12 +0300

    update addresses script

--- apps/api/scripts/addresses.ts
+++ apps/api/scripts/addresses.ts
@@ -1,5 +1,6 @@
 import { parse } from 'csv-parse';
 import fs from 'node:fs';
+import { cities, countries, db, states } from '../src/db';
 import { logger } from '../src/logger';
 
 type Country = {
@@ -50,15 +51,21 @@ type City = {
   wikiDataId: string;
 };
 
-const citiesPathname = 'src/scripts/data/cities.csv';
-const statesPathname = 'src/scripts/data/states.csv';
-const countriesPathname = 'src/scripts/data/countries.csv';
+const citiesPathname = 'cities.csv';
+const statesPathname = 'states.csv';
+const countriesPathname = 'countries.csv';
 
-function checkFilesExist() {
-  const files = [citiesPathname, statesPathname, countriesPathname];
+async function checkFilesExist() {
+  const files = [citiesPathname, statesPathname, countriesPathname].map(
+    (file) => {
+      return [import.meta.dir, 'data', file].join('/');
+    }
+  );
 
   for (const file of files) {
-    if (!fs.existsSync(file)) {
+    const ok = await Bun.file(file).exists();
+
+    if (!ok) {
       logger.error(`File ${file} not found`);
       return false;
     }
@@ -92,27 +99,107 @@ function readCsv<T>(path: string) {
       resolve(items);
     });
 
-    fs.createReadStream(path).pipe(parser);
+    const fullPath = [import.meta.dir, 'data', path].join('/');
+
+    fs.createReadStream(fullPath).pipe(parser);
   });
 }
 
 export async function syncLocations() {
   logger.info('Syncing locations...');
 
-  if (!checkFilesExist()) {
+  const ok = await checkFilesExist();
+
+  if (!ok) {
     process.exit(1);
   }
 
-  const [_countries, _states, _cities] = await Promise.all([
+  const [countriesData, statesData, citiesData] = await Promise.all([
     readCsv<Country>(countriesPathname),
     readCsv<State>(statesPathname),
     readCsv<City>(citiesPathname),
   ]);
 
-  // TODO: Implement the logic to add the locations to the database
-  // ui.logger.success("Locations synced successfully!");
+  let i = 1;
 
-  logger.error('Not implemented yet!');
+  for (const country of countriesData) {
+    logger.info(`Processing country ${i}/${countriesData.length}`);
+    try {
+      await db.insert(countries).values({
+        id: country.id,
+        name: country.name,
+        iso2: country.iso2,
+        numericCode: country.numeric_code,
+        phoneCode: country.phone_code,
+        capital: country.capital,
+        currency: country.currency,
+        currencyName: country.currency_name,
+        currencySymbol: country.currency_symbol,
+        tld: country.tld,
+        native: country.native,
+        region: country.region,
+        subregion: country.subregion,
+        timezones: country.timezones,
+        latitude: country.latitude,
+        longitude: country.longitude,
+      });
+      logger.info(`Country ${i} inserted successfully!`);
+    } catch (error) {
+      logger.error(`Error processing country ${i}`);
+    }
+    i++;
+  }
+
+  i = 1;
+
+  for (const state of statesData) {
+    logger.info(`Processing state ${i}/${statesData.length}`);
+    try {
+      await db.insert(states).values({
+        id: state.id,
+        name: state.name,
+        countryId: state.country_id,
+        countryCode: state.country_code,
+        countryName: state.country_name,
+        stateCode: state.state_code,
+        type: state.type,
+        latitude: state.latitude,
+        longitude: state.longitude,
+      });
+      logger.info(`State ${i} inserted successfully!`);
+    } catch (error) {
+      logger.error(`Error processing state ${i}`);
+    }
+    i++;
+  }
+
+  i = 1;
+
+  for (const city of citiesData) {
+    logger.info(`Processing city ${i}/${citiesData.length}`);
+    try {
+      await db.insert(cities).values({
+        id: city.id,
+        name: city.name,
+        stateId: city.state_id,
+        stateCode: city.state_code,
+        stateName: city.state_name,
+        countryId: city.country_id,
+        countryCode: city.country_code,
+        countryName: city.country_name,
+        latitude: city.latitude,
+        longitude: city.longitude,
+        wikiDataId: city.wikiDataId,
+      });
+      logger.info(`City ${i} inserted successfully!`);
+    } catch (error) {
+      logger.error(`Error processing city ${i}`);
+    }
+    i++;
+  }
+
+  logger.info('Script ended');
+  process.exit(0);
 }
 
 syncLocations();
