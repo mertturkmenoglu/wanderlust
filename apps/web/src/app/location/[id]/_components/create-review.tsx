5d7709795d27e59e1522aa3f9f1430fca0a643a8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-31 23:23:51 +0300

    update reviews components

---
+++ apps/web/src/app/location/[id]/_components/create-review.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Rating } from '@/components/ui/rating';
+import { Textarea } from '@/components/ui/textarea';
+import { useState } from 'react';
+
+export default function CreateReview() {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
+  const [error, setError] = useState<string[]>([]);
+
+  function createReview() {
+    setError([]);
+    if (!comment) {
+      setError((prev) => [...prev, 'Please enter a comment']);
+    }
+    if (rating === 0) {
+      setError((prev) => [...prev, 'Please select a rating']);
+    }
+
+    if (error.length > 0) {
+      return;
+    }
+
+    console.log('Creating review', { rating, comment });
+  }
+
+  return (
+    <DialogContent className="sm:max-w-xl">
+      <DialogHeader>
+        <DialogTitle>Your Review</DialogTitle>
+        <DialogDescription>Share your experience with others</DialogDescription>
+      </DialogHeader>
+      <div className="grid gap-4 py-4">
+        <div className="">
+          <Textarea
+            id="comment"
+            placeholder="Write your review here..."
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            rows={4}
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <Rating
+            id="review"
+            onChange={(v) => setRating(v.value)}
+          />
+        </div>
+
+        <div className="flex justify-center text-center">
+          {error.length > 0 && (
+            <ul className="space-y-1 text-sm text-red-500">
+              {error.map((err) => (
+                <li key={err}>{err}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+      <DialogFooter className="flex !justify-center">
+        <Button
+          type="button"
+          className="block"
+          onClick={createReview}
+        >
+          Create review
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  );
+}
---
+++ apps/web/src/app/location/[id]/_components/review-list.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import EmptyContent from '@/components/blocks/EmptyContent';
+import { Skeleton } from '@/components/ui/skeleton';
+import { api, rpcPaginated } from '@/lib/api';
+import { useQuery } from '@tanstack/react-query';
+
+type Props = {
+  locationId: string;
+};
+
+export default function ReviewList({ locationId }: Props) {
+  const query = useQuery({
+    queryKey: ['reviews', locationId],
+    queryFn: async () => {
+      return rpcPaginated(() =>
+        api.reviews.location[':id'].$get({
+          param: {
+            id: locationId,
+          },
+          query: {
+            pageSize: '10',
+            page: '1',
+          },
+        })
+      );
+    },
+  });
+
+  if (query.isLoading) {
+    return (
+      <div className="mt-16 flex w-full justify-center">
+        <Skeleton className="h-32 w-full rounded" />
+      </div>
+    );
+  }
+
+  if (query.isError) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <>
+      {query.data && query.data.data.length === 0 && (
+        <EmptyContent
+          className="my-16"
+          showBackButton={false}
+        />
+      )}
+      {query.data &&
+        query.data.data.map((review) => (
+          <div key={review.id}>
+            <h3>{review.comment}</h3>
+            <p>{review.user.username}</p>
+          </div>
+        ))}
+    </>
+  );
+}
