44aff9764899bdce6d0beeeb4da8e4896ada44d1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-31 08:37:30 +0300

    use infinite query in bookmarks page

--- apps/web/src/app/bookmarks/page.tsx
+++ apps/web/src/app/bookmarks/page.tsx
@@ -1,43 +1,56 @@
 'use client';
 
-import { api, rpc } from '@/lib/api';
-import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { Button } from '@/components/ui/button';
+import { api, rpcPaginated } from '@/lib/api';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import React from 'react';
 import BookmarkCard from './_components/bookmark-card';
 
-export default function Page() {
-  const searchParams = useSearchParams();
-  const [page, setPage] = useState(searchParams.get('page') ?? '1');
+function getBookmarks(page: number) {
+  return rpcPaginated(() => api.bookmarks.$get({ query: { page: `${page}` } }));
+}
 
-  const query = useQuery({
-    queryKey: ['bookmarks', page],
-    queryFn: async () => {
-      return rpc(() =>
-        api.bookmarks.$get({
-          query: {
-            page,
-          },
-        })
-      );
-    },
-    placeholderData: keepPreviousData,
+export default function Page() {
+  const query = useInfiniteQuery({
+    queryKey: ['bookmarks'],
+    queryFn: ({ pageParam }) => getBookmarks(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : null,
   });
 
   return (
     <div className="mt-8">
       {query.data && (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
-          {query.data.map((bookmark) => (
-            <Link
-              href={`/location/${bookmark.locationId}`}
-              key={bookmark.locationId}
-              className="block"
-            >
-              <BookmarkCard bookmark={bookmark} />
-            </Link>
+          {query.data.pages.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.data.map((bookmark) => (
+                <Link
+                  href={`/location/${bookmark.locationId}`}
+                  key={bookmark.locationId}
+                  className="block"
+                >
+                  <BookmarkCard bookmark={bookmark} />
+                </Link>
+              ))}
+            </React.Fragment>
           ))}
+        </div>
+      )}
+      {query.hasNextPage && (
+        <div className="mt-4 flex justify-center">
+          <Button
+            onClick={() => query.fetchNextPage()}
+            disabled={!query.hasNextPage || query.isFetchingNextPage}
+          >
+            {query.isFetchingNextPage
+              ? 'Loading more...'
+              : query.hasNextPage
+                ? 'Load More'
+                : 'Nothing more to load'}
+          </Button>
         </div>
       )}
     </div>
