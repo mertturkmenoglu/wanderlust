9b3dca2ae01e104b31d3f6670dbdd778b5f725af
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-05-31 08:37:01 +0300

    add paginated api call helper

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,4 +1,5 @@
 import { AppType } from '#/index';
+import type { Pagination } from '#/pagination';
 import { ClientResponse, hc } from 'hono/client';
 
 export const { api } = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
@@ -18,4 +19,18 @@ export async function rpc<T>(
 
   const { data } = await res.json();
   return data;
+}
+
+export async function rpcPaginated<T>(
+  fn: () => Promise<
+    ClientResponse<{ data: T; pagination: Pagination }, number, 'json'>
+  >
+) {
+  const res = await fn();
+
+  if (!res.ok) {
+    throw new Error('Error');
+  }
+
+  return res.json();
 }
