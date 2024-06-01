097dd7664bd937abdeaf15c989e6f9b43d895e5a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-01 08:19:12 +0300

    update api helpers

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,5 +1,4 @@
 import { AppType } from '#/index';
-import type { Pagination } from '#/pagination';
 import { ClientResponse, hc } from 'hono/client';
 
 export const { api } = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
@@ -9,22 +8,7 @@ export const { api } = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
 });
 
 export async function rpc<T>(
-  fn: () => Promise<ClientResponse<{ data: T }, number, 'json'>>
-) {
-  const res = await fn();
-
-  if (!res.ok) {
-    throw new Error('Error');
-  }
-
-  const { data } = await res.json();
-  return data;
-}
-
-export async function rpcPaginated<T>(
-  fn: () => Promise<
-    ClientResponse<{ data: T; pagination: Pagination }, number, 'json'>
-  >
+  fn: () => Promise<ClientResponse<T, number, 'json'>>
 ) {
   const res = await fn();
 
