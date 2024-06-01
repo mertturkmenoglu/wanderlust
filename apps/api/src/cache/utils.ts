487cf9d58ddf0ad899ee8d3dfb01cb613f2fa71e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-01 09:49:33 +0300

    update cache utils

--- apps/api/src/cache/utils.ts
+++ apps/api/src/cache/utils.ts
@@ -1,7 +1,7 @@
 import { cache } from './init';
 import { CacheKey } from './keys';
 
-export async function cacheRead<T>(key: CacheKey) {
+export async function cacheRead<T>(key: CacheKey | string) {
   const cacheResult = await cache.get(key);
 
   if (cacheResult === null) {
@@ -15,6 +15,10 @@ export async function cacheRead<T>(key: CacheKey) {
   }
 }
 
-export async function cacheWrite<T>(key: CacheKey, value: T, ttl: number) {
+export async function cacheWrite<T>(
+  key: CacheKey | string,
+  value: T,
+  ttl: number
+) {
   await cache.set(key, JSON.stringify(value), 'EX', ttl);
 }
