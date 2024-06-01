e5080147283c816cd8c51dd0a423674a065d86c8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-02 00:45:55 +0300

    coerce query parameter

--- apps/api/src/routes/uploads/dto.ts
+++ apps/api/src/routes/uploads/dto.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const getNewUploadUrlSchema = z.object({
   type: z.enum(['reviews']),
-  count: z.number().int().min(1).max(4),
+  count: z.coerce.number().int().min(1).max(4),
 });
 
 export type GetNewUploadUrlQuery = z.infer<typeof getNewUploadUrlSchema>;
