f83c0b1892853d7eb7637526378396c15e3821f9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-04 11:28:50 +0300

    update upload dto

--- apps/api/src/routes/uploads/dto.ts
+++ apps/api/src/routes/uploads/dto.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const getNewUploadUrlSchema = z.object({
-  type: z.enum(['reviews']),
+  type: z.enum(['reviews', 'locations']),
   count: z.coerce.number().int().min(1).max(4),
   mime: z.enum([
     'image/jpeg',
