2ebce0f881e44c075008447f6d7d63f6d4eadd78
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-04 11:29:04 +0300

    add image upload function

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,5 +1,6 @@
 import { AppType } from '#/index';
 import { ClientResponse, hc } from 'hono/client';
+import { UploadImageType } from './types';
 
 export const { api } = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!, {
   init: {
@@ -17,4 +18,41 @@ export async function rpc<T>(
   }
 
   return res.json();
+}
+
+export async function uploadImages(
+  files: File[],
+  type: UploadImageType
+): Promise<string[]> {
+  const urls: string[] = [];
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const url = await rpc(() =>
+      api.uploads['new-url'].$get({
+        query: {
+          type,
+          count: `1`,
+          mime: file.type,
+        },
+      })
+    );
+
+    try {
+      const r = await fetch(url.data[0].url, {
+        method: 'PUT',
+        body: file,
+      });
+      if (r.ok) {
+        urls.push(url.data[0].url);
+      }
+    } catch (err) {
+      console.error('Failed to upload file', err);
+    }
+  }
+
+  for (let i = 0; i < urls.length; i++) {
+    urls[i] = urls[i].split('?')[0];
+  }
+
+  return urls;
 }
