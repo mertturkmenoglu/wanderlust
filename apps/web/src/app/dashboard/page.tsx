f0ef7386c100e2295b1d4ee7648e0f14d0f1f1d3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-04 10:59:16 +0300

    feat: Improve locations table in dashboard page

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { api, rpc } from '@/lib/api';
 import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
@@ -20,17 +21,27 @@ function Page() {
   return (
     <div>
       <div>
-        <h3 className="my-4 text-lg font-bold tracking-tight">Points</h3>
-        <DataTable
-          columns={locationsCols}
-          data={qLocations.data?.data ?? []}
-        />
-        <Link
-          href="/dashboard/locations/new"
-          className="mt-4 block"
-        >
-          Create new location
-        </Link>
+        <h3 className="my-4 text-lg font-bold tracking-tight">Locations</h3>
+        {qLocations.data?.data && (
+          <DataTable
+            columns={locationsCols}
+            data={qLocations.data.data.map((loc) => ({
+              ...loc,
+              category: loc.category.name,
+              city: loc.address.city,
+              state: loc.address.state ?? '-',
+              country: loc.address.country,
+            }))}
+          />
+        )}
+        <Button asChild>
+          <Link
+            href="/dashboard/locations/new"
+            className="mt-4 block"
+          >
+            Create new location
+          </Link>
+        </Button>
       </div>
 
       <div className="mt-16">
