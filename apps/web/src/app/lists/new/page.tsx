a96941f67dcb2921d4753937a7f6f5610999278f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-06 00:08:36 +0300

    protect route

--- apps/web/src/app/lists/new/page.tsx
+++ apps/web/src/app/lists/new/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from '@clerk/nextjs/server';
 import NewListForm from './_components/form';
 
 export default function Page() {
+  auth().protect();
+
   return (
     <div>
       <NewListForm />
