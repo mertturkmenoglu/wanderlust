92247fb5a4e3aaf69ae0bd55bd02737cf51824f5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-06 00:09:01 +0300

    add dropdown menu to list page

--- apps/web/src/app/lists/[id]/page.tsx
+++ apps/web/src/app/lists/[id]/page.tsx
@@ -1,7 +1,29 @@
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { api, rpc } from '@/lib/api';
-import { ChevronLeft } from 'lucide-react';
+import { currentUser } from '@clerk/nextjs/server';
+import {
+  ChevronLeft,
+  EllipsisVertical,
+  FlagIcon,
+  TrashIcon,
+} from 'lucide-react';
 import Link from 'next/link';
+import DeleteButton from './_components/delete-button';
 
 type Props = {
   params: {
@@ -21,23 +43,86 @@ async function getList(id: string) {
 
 export default async function Page({ params: { id } }: Props) {
   const list = await getList(id);
+  const user = await currentUser();
+  const belongsToCurrentUser = list.data.user.username === user?.username;
 
   return (
     <div>
-      <div className="flex flex-col justify-start">
-        <Button
-          asChild
-          variant="link"
-          className="w-min px-0"
-        >
-          <Link href="/lists">
-            <ChevronLeft className="mr-2 size-4" />
-            <span>Lists</span>
-          </Link>
-        </Button>
-        <h2 className="text-2xl font-semibold tracking-tight">
-          {list.data.name}
-        </h2>
+      <div className="flex items-end justify-between">
+        <div className="flex flex-col justify-start">
+          <Button
+            asChild
+            variant="link"
+            className="w-min px-0"
+          >
+            <Link href="/lists">
+              <ChevronLeft className="mr-2 size-4" />
+              <span>Lists</span>
+            </Link>
+          </Button>
+          <h2 className="text-2xl font-semibold tracking-tight">
+            {list.data.name}
+          </h2>
+        </div>
+
+        <Dialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              asChild
+              className="block"
+            >
+              <Button
+                className="block"
+                variant="ghost"
+              >
+                <EllipsisVertical className="size-6" />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent
+              className="mr-4 w-32 space-y-2 p-2"
+              align="end"
+            >
+              <DropdownMenuItem className="cursor-pointer p-0">
+                <Button
+                  className="flex w-full justify-start hover:no-underline"
+                  variant="link"
+                  size="sm"
+                >
+                  <FlagIcon className="mr-2 size-4" />
+                  Report
+                </Button>
+              </DropdownMenuItem>
+              {belongsToCurrentUser && (
+                <DialogTrigger asChild>
+                  <DropdownMenuItem className="cursor-pointer p-0">
+                    <Button
+                      className="flex w-full justify-start text-destructive hover:no-underline"
+                      variant="link"
+                      size="sm"
+                      type="button"
+                    >
+                      <TrashIcon className="mr-2 size-4" />
+                      Delete
+                    </Button>
+                  </DropdownMenuItem>
+                </DialogTrigger>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. Are you sure you want to
+                permanently delete this list?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <DeleteButton listId={id} />
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <hr className="my-2" />
       <div className="my-16"></div>
