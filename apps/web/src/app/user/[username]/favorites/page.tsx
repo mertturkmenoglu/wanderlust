c29cadccf88a152486c08fd3e573f66d6a386acf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-05 08:02:59 +0300

    add dummy pages

---
+++ apps/web/src/app/user/[username]/favorites/page.tsx
@@ -0,0 +1,9 @@
+import EmptyContent from '@/components/blocks/EmptyContent';
+
+export default async function Page() {
+  return (
+    <div>
+      <EmptyContent className="mt-16" />
+    </div>
+  );
+}
