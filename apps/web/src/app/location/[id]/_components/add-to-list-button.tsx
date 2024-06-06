b6e38a49ce863b9d9de5e2182df85cbbcaac4102
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-06 16:35:17 +0300

    add location menu

--- apps/web/src/app/location/[id]/_components/add-to-list-button.tsx
+++ apps/web/src/app/location/[id]/_components/add-to-list-button.tsx
@@ -1,24 +1,51 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { api } from '@/lib/api';
 import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 type Props = {
   locationId: string;
+  listId: string | null;
 };
 
-export default function AddToListButton({ locationId }: Props) {
+export default function AddToListButton({ locationId, listId }: Props) {
   const mutation = useMutation({
     mutationKey: ['add-to-list'],
-    mutationFn: async (id: string) => {},
-    onSuccess: async () => {},
-    onError: () => {},
+    mutationFn: async () => {
+      if (listId === null) {
+        throw new Error('Cannot create list');
+      }
+
+      const res = await api.lists[':id'].items.$post({
+        param: {
+          id: listId,
+        },
+        json: {
+          locationId,
+        },
+      });
+
+      if (!res.ok) {
+        const msg = await res.text();
+        throw new Error(msg);
+      }
+    },
+    onSuccess: async () => {
+      toast.success('Added to the list');
+    },
+    onError: (e) => {
+      toast.error(`Cannot add to the list: ${e.message}`);
+    },
   });
+
   return (
     <Button
       type="button"
       variant="default"
-      onClick={() => mutation.mutate(locationId)}
+      onClick={() => mutation.mutate()}
+      disabled={listId === null}
     >
       Add to list
     </Button>
---
+++ apps/web/src/app/location/[id]/_components/menu.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { api, rpc } from '@/lib/api';
+import { useAuth } from '@clerk/nextjs';
+import { useQuery } from '@tanstack/react-query';
+import { EllipsisVertical, FlagIcon, Plus } from 'lucide-react';
+import { useState } from 'react';
+import AddToListButton from './add-to-list-button';
+
+type Props = {
+  locationId: string;
+};
+
+export default function Menu({ locationId }: Props) {
+  const { isSignedIn } = useAuth();
+  const [listId, setListId] = useState<string | null>(null);
+
+  const query = useQuery({
+    queryKey: ['lists'],
+    queryFn: async () => {
+      const res = await rpc(() =>
+        api.lists.my.$get({
+          query: {
+            page: '1',
+            pageSize: '100',
+          },
+        })
+      );
+
+      return res.data;
+    },
+  });
+
+  return (
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          asChild
+          className="block"
+        >
+          <Button
+            className="flex items-center justify-center rounded-full"
+            variant="ghost"
+            size="icon"
+          >
+            <EllipsisVertical className="size-6" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent
+          className=" w-32 space-y-2 p-2"
+          align="end"
+        >
+          <DropdownMenuItem className="cursor-pointer p-0">
+            <Button
+              className="flex w-full justify-start hover:no-underline"
+              variant="link"
+              size="sm"
+            >
+              <FlagIcon className="mr-2 size-4" />
+              Report
+            </Button>
+          </DropdownMenuItem>
+
+          {isSignedIn && (
+            <DialogTrigger asChild>
+              <DropdownMenuItem className="cursor-pointer p-0">
+                <Button
+                  className="flex w-full justify-start hover:no-underline"
+                  variant="link"
+                  size="sm"
+                  type="button"
+                >
+                  <Plus className="mr-2 size-4" />
+                  Add to list
+                </Button>
+              </DropdownMenuItem>
+            </DialogTrigger>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Select a list</DialogTitle>
+          <DialogDescription>
+            {query.data && (
+              <Select onValueChange={(v) => setListId(v)}>
+                <SelectTrigger className="mt-4">
+                  <SelectValue placeholder="Select a list" />
+                </SelectTrigger>
+                <SelectContent>
+                  {query.data.map((list) => (
+                    <SelectItem
+                      value={list.id}
+                      key={list.id}
+                    >
+                      {list.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <AddToListButton
+            locationId={locationId}
+            listId={listId}
+          />
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
