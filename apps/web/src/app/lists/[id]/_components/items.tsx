b1185a324728f84f875ed90ec8498a5ddb3588e1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-07 09:41:24 +0300

    cleanup list items component

--- apps/web/src/app/lists/[id]/_components/items.tsx
+++ apps/web/src/app/lists/[id]/_components/items.tsx
@@ -1,48 +1,17 @@
 'use client';
 
-import { api, rpc } from '@/lib/api';
-import { useInfiniteQuery } from '@tanstack/react-query';
-import React from 'react';
+import { ListItem } from '@/lib/types';
 
 type Props = {
-  listId: string;
+  items: ListItem[];
 };
 
-async function getListItems(listId: string, page: number) {
-  return rpc(() => {
-    return api.lists[':id'].items.$get({
-      param: {
-        id: listId,
-      },
-      query: {
-        page: `${page}`,
-      },
-    });
-  });
-}
-
-export default function Items({ listId }: Props) {
-  const query = useInfiniteQuery({
-    queryKey: ['list', 'items', listId],
-    queryFn: ({ pageParam }) => getListItems(listId, pageParam),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage) =>
-      lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : null,
-  });
-
+export default function Items({ items }: Props) {
   return (
     <div>
-      {query.data && (
-        <div>
-          {query.data.pages.map((page, i) => (
-            <React.Fragment key={i}>
-              {page.data.map((loc) => (
-                <pre key={loc.locationId}>{JSON.stringify(loc, null, 2)}</pre>
-              ))}
-            </React.Fragment>
-          ))}
-        </div>
-      )}
+      {items.map((item) => (
+        <pre key={item.locationId}>{JSON.stringify(item, null, 2)}</pre>
+      ))}
     </div>
   );
 }
