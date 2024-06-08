329437a3ce090764f7a99e8594aad155255a684d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-08 20:31:58 +0300

    add reviews section to user page

--- apps/web/src/app/user/[username]/reviews/page.tsx
+++ apps/web/src/app/user/[username]/reviews/page.tsx
@@ -1,9 +1,78 @@
-import EmptyContent from '@/components/blocks/EmptyContent';
+'use client';
 
-export default async function Page() {
+import EmptyContent from '@/components/blocks/EmptyContent';
+import ReviewCard from '@/components/blocks/ReviewCard';
+import { Button } from '@/components/ui/button';
+import { api, rpc } from '@/lib/api';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import React from 'react';
+
+async function getReviews(username: string, page: number) {
+  return rpc(() =>
+    api.reviews.user[':username'].$get({
+      param: {
+        username,
+      },
+      query: {
+        page: `${page}`,
+      },
+    })
+  );
+}
+
+export default function Page() {
+  const { username } = useParams();
+  const query = useInfiniteQuery({
+    queryKey: ['reviews', username],
+    queryFn: ({ pageParam }) => {
+      if (typeof username !== 'string') {
+        throw new Error('Invalid username');
+      }
+
+      return getReviews(username, pageParam);
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : null,
+  });
+
   return (
-    <div>
-      <EmptyContent className="mt-16" />
+    <div className="my-8">
+      {query.data && (
+        <div className="grid grid-cols-1 gap-4">
+          {query.data.pages.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.data.map((review) => (
+                <Link
+                  href={`/location/${review.locationId}`}
+                  key={review.id}
+                  className="block"
+                >
+                  <ReviewCard review={review} />
+                </Link>
+              ))}
+            </React.Fragment>
+          ))}
+        </div>
+      )}
+      {query.hasNextPage && (
+        <div className="mt-4 flex justify-center">
+          <Button
+            onClick={() => query.fetchNextPage()}
+            disabled={!query.hasNextPage || query.isFetchingNextPage}
+          >
+            {query.isFetchingNextPage
+              ? 'Loading more...'
+              : query.hasNextPage
+                ? 'Load More'
+                : 'Nothing more to load'}
+          </Button>
+        </div>
+      )}
+
+      {query.data && query.data.pages.length === 0 && <EmptyContent />}
     </div>
   );
 }
