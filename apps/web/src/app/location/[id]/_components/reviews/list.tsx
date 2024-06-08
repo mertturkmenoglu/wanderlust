cd00ee15fa0abdea644586086fc06861eb959885
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-08 20:31:43 +0300

    use infinite query on location page review section

--- apps/web/src/app/location/[id]/_components/reviews/list.tsx
+++ apps/web/src/app/location/[id]/_components/reviews/list.tsx
@@ -2,30 +2,36 @@
 
 import EmptyContent from '@/components/blocks/EmptyContent';
 import ReviewCard from '@/components/blocks/ReviewCard';
+import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
 import { api, rpc } from '@/lib/api';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React from 'react';
 
 type Props = {
   locationId: string;
 };
 
+async function getReviews(locationId: string, page: number) {
+  return rpc(() =>
+    api.reviews.location[':id'].$get({
+      param: {
+        id: locationId,
+      },
+      query: {
+        page: `${page}`,
+      },
+    })
+  );
+}
+
 export default function ReviewList({ locationId }: Props) {
-  const query = useQuery({
+  const query = useInfiniteQuery({
     queryKey: ['reviews', locationId],
-    queryFn: async () => {
-      return rpc(() =>
-        api.reviews.location[':id'].$get({
-          param: {
-            id: locationId,
-          },
-          query: {
-            pageSize: '10',
-            page: '1',
-          },
-        })
-      );
-    },
+    queryFn: ({ pageParam }) => getReviews(locationId, pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : null,
   });
 
   if (query.isLoading) {
@@ -42,17 +48,35 @@ export default function ReviewList({ locationId }: Props) {
 
   return (
     <div className="my-16 space-y-4">
-      {query.data && query.data.data.length === 0 && (
+      {query.data && query.data.pages.length === 0 && (
         <EmptyContent showBackButton={false} />
       )}
       {query.data && (
         <div className="mx-auto grid max-w-[1000px] grid-cols-1 gap-4">
-          {query.data.data.map((review) => (
-            <ReviewCard
-              key={review.id}
-              review={review}
-            />
+          {query.data.pages.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.data.map((review) => (
+                <ReviewCard
+                  key={review.id}
+                  review={review}
+                />
+              ))}
+            </React.Fragment>
           ))}
+        </div>
+      )}
+      {query.hasNextPage && (
+        <div className="mt-4 flex justify-center">
+          <Button
+            onClick={() => query.fetchNextPage()}
+            disabled={!query.hasNextPage || query.isFetchingNextPage}
+          >
+            {query.isFetchingNextPage
+              ? 'Loading more...'
+              : query.hasNextPage
+                ? 'Load More'
+                : 'Nothing more to load'}
+          </Button>
         </div>
       )}
     </div>
