6b5d851c97a25054cb716d5f5c797d4f2de3a611
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-08 06:41:28 +0300

    update list dtos

--- apps/api/src/routes/lists/dto.ts
+++ apps/api/src/routes/lists/dto.ts
@@ -12,8 +12,15 @@ export const createListSchema = createInsertSchema(lists).omit({
 export type CreateListDto = z.infer<typeof createListSchema>;
 
 export const createListItemSchema = createInsertSchema(listItems).omit({
+  id: true,
   createdAt: true,
   listId: true,
+  index: true,
 });
 
 export type CreateListItemDto = z.infer<typeof createListItemSchema>;
+
+export const validateDeleteListItemParams = z.object({
+  id: z.string().min(1).uuid(),
+  itemId: z.string().min(1).uuid(),
+});
