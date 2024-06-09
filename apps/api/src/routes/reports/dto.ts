bb3622988496f80ff9d56b22f2c0cab897d74659
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-10 00:39:19 +0300

    update reports dtos

--- apps/api/src/routes/reports/dto.ts
+++ apps/api/src/routes/reports/dto.ts
@@ -1,6 +1,7 @@
 import { createInsertSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { reports } from '../../db';
+import { validatePagination } from '../dto';
 
 export const createReportSchema = createInsertSchema(reports).omit({
   id: true,
@@ -25,3 +26,11 @@ export const updateReportSchema = createInsertSchema(reports).omit({
 });
 
 export type UpdateReportDto = z.infer<typeof updateReportSchema>;
+
+export const getReportsQueryParamsSchema = validatePagination.merge(
+  z.object({
+    status: z.enum(['pending', 'resolved', 'in_progress']).optional(),
+  })
+);
+
+export type GetReportsQueryParams = z.infer<typeof getReportsQueryParamsSchema>;
