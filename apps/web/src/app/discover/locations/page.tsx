5c84a6f46f4948d02fc8aec2869b3d809c224702
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-09 21:51:46 +0300

    update discover locations page

--- apps/web/src/app/discover/locations/page.tsx
+++ apps/web/src/app/discover/locations/page.tsx
@@ -1,9 +1,43 @@
-function Page(): React.ReactElement {
+import { api, rpc } from '@/lib/api';
+import { categories } from '@/lib/categories';
+import Link from 'next/link';
+
+async function getCategoriesWithCount() {
+  return rpc(() => api.categories.count.$get());
+}
+
+export default async function Page(): Promise<React.ReactElement> {
+  const { data } = await getCategoriesWithCount();
+
   return (
     <main>
-      <div>Discover locations page</div>
+      <h2 className="mt-16 text-3xl font-bold tracking-tight">Categories</h2>
+      <div className="my-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        {data.map((c) => (
+          <Link
+            key={c.category.id}
+            href={`/search?c=${c.category.id}`}
+          >
+            <div className="rounded-xl border border-border">
+              <img
+                src={
+                  categories.find((cat) => cat.id === c.category.id)?.url ?? ''
+                }
+                alt={c.category.name}
+                className="aspect-video h-48 w-full rounded-t-lg object-cover"
+              />
+              <div className="p-3">
+                <div className="text-xl font-bold tracking-tight">
+                  {c.category.name}
+                </div>
+                <div className="text-sm text-muted-foreground">
+                  {c.count} locations
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
-
-export default Page;
