2d0bdccc865e2089aa7b600346678eeadb65e29c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-12 15:58:03 +0300

    update search page

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,7 +1,71 @@
-function Page(): React.ReactElement {
+'use client';
+
+import { Address } from '#/db';
+import CustomSearchBox from '@/components/blocks/CustomSearchBox';
+import { Button } from '@/components/ui/button';
+import { useSearchClient } from '@/hooks/useSearchClient';
+import { Media } from '@/lib/types';
+import 'instantsearch.css/themes/reset.css';
+import {
+  ClearRefinements,
+  CurrentRefinements,
+  Hits,
+  InstantSearch,
+  Pagination,
+  RefinementList,
+} from 'react-instantsearch';
+
+type Props = {
+  hit: {
+    id: string;
+    name: string;
+    media: Media[];
+    categoryId: number;
+    address: Address;
+  };
+};
+
+function HitComponent({ hit }: Props) {
   return (
-    <main>
-      <div>Search page</div>
+    <div className="my-4 border border-border p-4">
+      <div>{hit.name}</div>
+    </div>
+  );
+}
+
+function Page(): React.ReactElement {
+  const searchClient = useSearchClient();
+
+  return (
+    <main className="container my-16">
+      <InstantSearch
+        indexName="locations"
+        searchClient={searchClient}
+        routing={false}
+        insights
+        future={{
+          preserveSharedStateOnUnmount: true,
+        }}
+      >
+        <CustomSearchBox />
+
+        <div className="my-8 flex gap-8">
+          <RefinementList attribute="tags" />
+
+          <div>
+            <Button
+              asChild
+              variant="link"
+              className="cursor-pointer"
+            >
+              <ClearRefinements />
+            </Button>
+            <CurrentRefinements />
+            <Hits hitComponent={HitComponent} />
+          </div>
+        </div>
+        <Pagination />
+      </InstantSearch>
     </main>
   );
 }
