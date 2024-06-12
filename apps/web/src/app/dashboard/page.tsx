f28e183f732def107038e5a0e3f5e4f9d1986125
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-12 10:50:45 +0300

    update dashboard page

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,64 +1,19 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { api, rpc } from '@/lib/api';
-import { useQuery } from '@tanstack/react-query';
-import Link from 'next/link';
-import { eventCols, locationsCols } from './_components/columns';
-import { DataTable } from './_components/data-table';
+import AppMessage from '@/components/blocks/AppMessage';
 
-function Page() {
-  const qLocations = useQuery({
-    queryKey: ['locations'],
-    queryFn: async () => rpc(() => api.locations.peek.$get()),
-  });
-
-  const qEvents = useQuery({
-    queryKey: ['events'],
-    queryFn: async () => rpc(() => api.events.peek.$get()),
-  });
-
+export default function Page() {
   return (
-    <div>
-      <div>
-        <h3 className="my-4 text-lg font-bold tracking-tight">Locations</h3>
-        {qLocations.data?.data && (
-          <DataTable
-            columns={locationsCols}
-            data={qLocations.data.data.map((loc) => ({
-              ...loc,
-              category: loc.category.name,
-              city: loc.address.city,
-              state: loc.address.state ?? '-',
-              country: loc.address.country,
-            }))}
-          />
-        )}
-        <Button asChild>
-          <Link
-            href="/dashboard/locations/new"
-            className="mt-4 block"
-          >
-            Create new location
-          </Link>
-        </Button>
-      </div>
-
-      <div className="mt-16">
-        <h3 className="my-4 text-lg font-bold tracking-tight">Events</h3>
-        <DataTable
-          columns={eventCols}
-          data={qEvents.data?.data ?? []}
-        />
-        <Link
-          href="/dashboard/events/new"
-          className="mt-4 block"
-        >
-          Create new event
-        </Link>
-      </div>
+    <div className="flex justify-center">
+      <AppMessage
+        showBackButton={false}
+        emptyMessage={
+          <div className="text-center">
+            <h2>Your Admin Dashboard</h2>
+            <div className="text-sm">Select an action from sidebar</div>
+          </div>
+        }
+      />
     </div>
   );
 }
-
-export default Page;
