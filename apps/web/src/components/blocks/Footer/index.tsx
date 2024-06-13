d9b3bccdcdcd41d9db848085bce207e02230babb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-13 09:56:06 +0300

    update footer

--- apps/web/src/components/blocks/Footer/index.tsx
+++ apps/web/src/components/blocks/Footer/index.tsx
@@ -1,65 +1,62 @@
-import { GithubIcon, FacebookIcon, TwitterIcon, YoutubeIcon, InstagramIcon } from "lucide-react";
-import Link from "next/link";
+import {
+  GitHubLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 function Footer() {
-
   return (
     <footer className="my-20">
-      <ul className="flex justify-center items-center gap-8 mb-8">
+      <ul className="mb-8 flex items-center justify-center gap-8 text-sm text-muted-foreground">
         <li>
-          <Link href="/about">
-            About
-          </Link>
+          <Link href="/about">About</Link>
         </li>
         <li>
-          <Link href="/blog">
-            Blog
-          </Link>
+          <Link href="/blog">Blog</Link>
         </li>
         <li>
-          <Link href="/help">
-            FAQ
-          </Link>
+          <Link href="/help">FAQ</Link>
         </li>
         <li>
-          <Link href="/contact-us">
-            Contact Us
-          </Link>
+          <Link href="/contact-us">Contact Us</Link>
         </li>
         <li>
-          <Link href="/privacy">
-            Privacy
-          </Link>
+          <Link href="/privacy">Privacy</Link>
         </li>
       </ul>
-      <ul className="flex justify-center items-center gap-8 mb-8">
+      <ul className="mb-8 flex items-center justify-center gap-8 text-muted-foreground">
         <li>
-          <a href="https://github.com/">
-            <GithubIcon className="size-6" />
+          <a
+            href="https://github.com/mertturkmenoglu/wanderlust"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubLogoIcon className="size-6" />
           </a>
         </li>
         <li>
-          <a href="https://www.facebook.com/">
-            <FacebookIcon className="size-6" />
+          <a
+            href="https://twitter.com/capreaee"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TwitterLogoIcon className="size-6" />
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/home">
-            <TwitterIcon className="size-6" />
-          </a>
-        </li>
-        <li>
-          <a href="https://youtube.com/">
-            <YoutubeIcon className="size-6" />
-          </a>
-        </li>
-        <li>
-          <a href="https://instagram.com/">
-            <InstagramIcon className="size-6" />
+          <a
+            href="https://www.linkedin.com/in/mert-turkmenoglu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInLogoIcon className="size-6" />
           </a>
         </li>
       </ul>
-      <div className="text-center">© 2024 Wanderlust, Inc. All rights reserved.</div>
+      <div className="text-center text-xs text-muted-foreground">
+        © 2024 Wanderlust, Inc. All rights reserved.
+      </div>
     </footer>
   );
 }
