34d3ac1f1c86cc2601fd3f9e095005d7f20d32dc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-13 18:15:27 +0300

    add dummy cities page

---
+++ apps/web/src/app/cities/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="container my-16">
+      <div className="text-2xl font-bold leading-3 tracking-tight">
+        Discover locations in London
+      </div>
+    </main>
+  );
+}
