dcaf436525d1038d523c1b6863323184104241ad
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-13 12:34:09 +0300

    add hits per page component

---
+++ apps/web/src/app/search/_components/hits-per-page.tsx
@@ -0,0 +1,49 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { useHitsPerPage } from 'react-instantsearch';
+
+export default function HitsPerPage() {
+  const { items, refine } = useHitsPerPage({
+    items: [
+      { label: '10 items', value: 10, default: true },
+      { label: '25 items', value: 25 },
+      { label: '50 items', value: 50 },
+    ],
+  });
+
+  const { value: currentValue } =
+    items.find(({ isRefined }) => isRefined)! || {};
+
+  return (
+    <Select
+      onValueChange={(v) => {
+        refine(+v);
+      }}
+      value={String(currentValue)}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Items per page" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Items per page</SelectLabel>
+          {items.map((item) => (
+            <SelectItem
+              key={item.value}
+              value={String(item.value)}
+            >
+              {item.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
