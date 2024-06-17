26eff944198add9a31b1d42d96401a1a5b105a15
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-17 15:22:29 +0300

    change new location item count

--- apps/web/src/app/_components/new-locations.tsx
+++ apps/web/src/app/_components/new-locations.tsx
@@ -18,8 +18,8 @@ export default async function NewLocations() {
         New Locations
       </h2>
 
-      <div className="my-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-        {data.slice(0, 5).map((location) => (
+      <div className="my-8 grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+        {data.slice(0, 12).map((location) => (
           <Link
             key={location.id}
             href={`/location/${location.id}`}
