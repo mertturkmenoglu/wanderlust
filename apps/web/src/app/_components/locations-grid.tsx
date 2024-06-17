5d7daf86ec071e96a4b2f5491d4037705ecdf64e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-17 18:24:44 +0300

    update locations grid component

--- apps/web/src/app/_components/locations-grid.tsx
+++ apps/web/src/app/_components/locations-grid.tsx
@@ -5,17 +5,41 @@ import Link from 'next/link';
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-async function peekLocations() {
-  return rpc(() => api.locations.peek.$get());
+async function peekLocations(type: Props['type']) {
+  return rpc(() =>
+    api.locations.peek.$get({
+      query: {
+        type,
+      },
+    })
+  );
 }
 
-export default async function NewLocations() {
-  const { data } = await peekLocations();
+type Props = {
+  type: 'new' | 'popular' | 'featured' | 'favorite';
+};
+
+function getTitle(type: Props['type']) {
+  switch (type) {
+    case 'new':
+      return 'New Locations';
+    case 'popular':
+      return 'Popular Locations';
+    case 'featured':
+      return 'Featured Locations';
+    case 'favorite':
+      return 'Favorite Locations';
+  }
+}
+
+export default async function LocationsGrid({ type }: Props) {
+  const { data } = await peekLocations(type);
+  const title = getTitle(type);
 
   return (
     <div className="mx-auto">
       <h2 className="mt-12 scroll-m-20 text-2xl font-semibold tracking-tighter text-accent-foreground lg:text-3xl">
-        New Locations
+        {title}
       </h2>
 
       <div className="my-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
