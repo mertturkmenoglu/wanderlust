e113befcf1c67a763fea42179bce36707b7c1235
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-18 16:23:24 +0300

    cleanup

--- apps/web/src/components/blocks/TagNavigation/data.ts
+++ apps/web/src/components/blocks/TagNavigation/data.ts
@@ -22,7 +22,6 @@ const base = 'search?locations[refinementList][tags][0]=';
 
 const data = [
   { href: `${base}trending`, text: 'Trending', icon: FlameIcon },
-  // { href: `${base}Family Friendly`, text: 'Family Friendly', icon: BabyIcon },
   { href: `${base}coffee`, text: 'Coffee', icon: CoffeeIcon },
   { href: `${base}bar`, text: 'Bars', icon: BeerIcon },
   { href: `${base}landmark`, text: 'Landmarks', icon: LandmarkIcon },
@@ -32,17 +31,11 @@ const data = [
   { href: `${base}vegan`, text: 'Vegan', icon: SaladIcon },
   { href: `${base}camping`, text: 'Camping', icon: TentIcon },
   { href: `${base}music`, text: 'Music', icon: Music2Icon },
-  // { href: `${base}Fine Dining`, text: 'Fine Dining', icon: UtensilsIcon },
-  // { href: `${base}City Night`, text: 'City Night', icon: MoonIcon },
   { href: `${base}steakhouse`, text: 'Steakhouse', icon: BeefIcon },
   { href: `${base}pattisery`, text: 'Pattiseries', icon: CakeSliceIcon },
-  // { href: `${base}Street Food`, text: 'Street Food', icon: DrumstickIcon },
   { href: `${base}yoga`, text: 'Yoga', icon: LeafIcon },
-  // { href: `${base}Cocktail Bar`, text: 'Cocktail Bars', icon: MartiniIcon },
   { href: `${base}wine`, text: 'Wine', icon: WineIcon },
-  // { href: `${base}Movie Theater`, text: 'Movie Theaters', icon: PopcornIcon },
   { href: `${base}photography`, text: 'Photography', icon: CameraIcon },
-  // { href: `${base}Budget`, text: 'On a Budget', icon: DollarSignIcon },
 ] satisfies NavItemProps[];
 
 export default data;
