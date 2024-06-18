a3668a18c908215ff37ec91705b55968dc3825ee
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-18 17:09:30 +0300

    refactor search page

--- apps/web/src/app/search/page.tsx
+++ apps/web/src/app/search/page.tsx
@@ -1,43 +1,12 @@
 'use client';
 
-import { Address } from '#/db';
-import Card from '@/components/blocks/Autocomplete/card';
-import CustomSearchBox from '@/components/blocks/CustomSearchBox';
-import { useCategories } from '@/hooks/use-categories';
 import { useSearchClient } from '@/hooks/useSearchClient';
-import { Media } from '@/lib/types';
 import 'instantsearch.css/themes/reset.css';
-import { Hits, InstantSearch } from 'react-instantsearch';
-import HitsPerPage from './_components/hits-per-page';
-import Pagination from './_components/pagination';
-import RefinementList from './_components/refinement-list';
+import { InstantSearch } from 'react-instantsearch';
+import Container from './_components/container';
 
-type Props = {
-  hit: {
-    id: string;
-    name: string;
-    media: Media[];
-    categoryId: number;
-    address: Address;
-  };
-};
-
-function HitComponent({ hit }: Props) {
-  return (
-    <Card
-      id={hit.id}
-      categoryName=""
-      city={hit.address.city}
-      image={hit.media[0].url}
-      name={hit.name}
-      state={hit.address.state ?? ''}
-    />
-  );
-}
-
-function Page(): React.ReactElement {
+export default function Page() {
   const searchClient = useSearchClient();
-  const query = useCategories();
 
   return (
     <main className="container my-16">
@@ -49,50 +18,8 @@ function Page(): React.ReactElement {
           preserveSharedStateOnUnmount: true,
         }}
       >
-        <CustomSearchBox />
-
-        <div className="my-8 flex gap-8">
-          <div className="min-w-[256px]">
-            <div className="text-lg font-semibold tracking-tight underline">
-              Filters
-            </div>
-
-            <RefinementList attribute="tags" />
-
-            <RefinementList attribute="address.city" />
-
-            <RefinementList attribute="address.state" />
-
-            {query.data && (
-              <RefinementList
-                attribute="categoryId"
-                categories={query.data.data}
-              />
-            )}
-
-            <RefinementList attribute="priceLevel" />
-
-            <RefinementList
-              attribute="accessibilityLevel"
-              className="mt-4"
-            />
-          </div>
-
-          <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-              <div className="text-2xl font-semibold tracking-tight">
-                Results
-              </div>
-              <HitsPerPage />
-            </div>
-            <hr className="my-2" />
-            <Hits hitComponent={HitComponent} />
-            <Pagination />
-          </div>
-        </div>
+        <Container />
       </InstantSearch>
     </main>
   );
 }
-
-export default Page;
