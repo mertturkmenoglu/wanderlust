1ba33e841d7b44ed7d69b38a1ee98c6e331e27b5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-18 17:09:11 +0300

    create search container component

---
+++ apps/web/src/app/search/_components/container.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import AppMessage from '@/components/blocks/AppMessage';
+import CustomSearchBox from '@/components/blocks/CustomSearchBox';
+import { useEmptyResult } from '../_hooks/use-empty-result';
+import Filters from './filters';
+import Results from './results';
+
+export default function Container() {
+  const { isEmptyResult, isEmptyQuery } = useEmptyResult();
+
+  return (
+    <>
+      <CustomSearchBox />
+
+      {isEmptyQuery && (
+        <AppMessage
+          emptyMessage="Start typing to search."
+          showBackButton={false}
+          className="my-16"
+        />
+      )}
+
+      {isEmptyResult && (
+        <AppMessage
+          emptyMessage="No results found."
+          showBackButton={false}
+          className="my-16"
+        />
+      )}
+
+      {!(isEmptyResult || isEmptyQuery) && (
+        <div className="my-8 flex gap-8">
+          <div className="min-w-[256px]">
+            <Filters />
+          </div>
+
+          <div className="w-full">
+            <Results />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
