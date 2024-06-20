a8343dab128470448f8fb01e02d5cb0aab7aad1a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-06-20 12:10:53 +0300

    extract components

---
+++ apps/web/src/app/user/[username]/_components/verified.tsx
@@ -0,0 +1,10 @@
+import { BadgeCheckIcon } from "lucide-react";
+
+export default function Verified() {
+  return (
+    <div className="mt-2 flex items-center gap-2">
+      <BadgeCheckIcon className="size-6 text-primary" />
+      <span className="text-sm text-gray-500">Verified</span>
+    </div>
+  );
+}
