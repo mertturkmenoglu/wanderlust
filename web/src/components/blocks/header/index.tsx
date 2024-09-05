63566de3dc8900aac1f2c4203f717ec448f666a3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-05 15:53:54 +0300

    add header links

--- web/src/components/blocks/header/index.tsx
+++ web/src/components/blocks/header/index.tsx
@@ -1,6 +1,7 @@
 import Logo from '@/app/icon.png';
 import { getAuth } from '@/lib/auth';
 import { cn } from '@/lib/utils';
+import { Calendar, MapPin } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Menu from './menu';
@@ -30,9 +31,30 @@ export default async function Header({ className, ...props }: Readonly<Props>) {
           alt="Wanderlust"
           className="size-12 min-h-12 min-w-12"
         />
-
-        <div className="text-xl">Wanderlust</div>
       </Link>
+
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link
+              href="/discover/locations"
+              className="flex items-center gap-2 rounded-full px-4 py-2 hover:bg-muted"
+            >
+              <MapPin className="size-6" />
+              <span className="hidden sm:block">Locations</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/discover/events"
+              className="flex items-center gap-2 rounded-full px-4 py-2 hover:bg-muted"
+            >
+              <Calendar className="size-6" />
+              <span className="hidden sm:block">Events</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
       {!isSignedIn && <SignInButton />}
 
