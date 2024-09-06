21afd0a394f37d7447ef793d4ad57b20251a4687
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-06 13:18:46 +0300

    update users module errors

--- api/internal/app/users/errors.go
+++ api/internal/app/users/errors.go
@@ -2,10 +2,11 @@ package users
 
 import (
 	"errors"
+	"net/http"
 	"wanderlust/internal/app/api"
 )
 
 var (
-	ErrUserNotFound        = api.NewApiError("0100", errors.New("user not found"))
-	ErrUsernameNotProvided = api.NewApiError("0101", errors.New("username not provided"))
+	ErrUserNotFound        = api.NewApiError(http.StatusNotFound, "0100", errors.New("user not found"))
+	ErrUsernameNotProvided = api.NewApiError(http.StatusBadRequest, "0101", errors.New("username not provided"))
 )
