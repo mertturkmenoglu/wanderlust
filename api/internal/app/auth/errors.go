5997e445dae7da3558bf286734daf78389864613
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-06 13:18:20 +0300

    update auth errors

--- api/internal/app/auth/errors.go
+++ api/internal/app/auth/errors.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"errors"
+	"net/http"
 	"wanderlust/internal/app/api"
 )
 
@@ -12,22 +13,22 @@ import (
 // errPrevCodeNotExpired                = errors.New("previous code hasn't expired")
 
 var (
-	ErrSessionGet                        = api.NewApiError("0001", errors.New("failed to get session"))
-	ErrCreateOAuthState                  = api.NewApiError("0002", errors.New("cannot create oauth state parameter"))
-	ErrInvalidProvider                   = api.NewApiError("0003", errors.New("invalid oauth provider"))
-	ErrInvalidSessionState               = api.NewApiError("0004", errors.New("invalid session state"))
-	ErrInvalidStateParameter             = api.NewApiError("0005", errors.New("invalid state parameter"))
-	ErrOAuthTokenExchange                = api.NewApiError("0006", errors.New("failed to exchange token"))
-	ErrOAuthFailedUserFetch              = api.NewApiError("0007", errors.New("failed to fetch user"))
-	ErrOAuthInvalidUserResp              = api.NewApiError("0008", errors.New("failed to parse user"))
-	ErrInvalidEmailOrPassword            = api.NewApiError("0009", errors.New("invalid email or password"))
-	ErrEmailTaken                        = api.NewApiError("0010", errors.New("email has already been taken"))
-	ErrUsernameTaken                     = api.NewApiError("0011", errors.New("username has already been taken"))
-	ErrUsernameChars                     = api.NewApiError("0012", errors.New("username must include only alphanumeric characters or underscore"))
-	ErrHash                              = api.NewApiError("0013", errors.New("cannot hash"))
-	ErrInvalidEmail                      = api.NewApiError("0014", errors.New("invalid email"))
-	ErrEmailAlreadyVerified              = api.NewApiError("0015", errors.New("email has already been verified"))
-	ErrMalformedOrMissingVerifyToken     = api.NewApiError("0016", errors.New("verification token is missing or malformed"))
-	ErrInvalidOrExpiredVerifyCode        = api.NewApiError("0017", errors.New("verification code is invalid or expired"))
-	ErrPasswordResetCodeExpiredOrInvalid = api.NewApiError("0018", errors.New("password reset code is invalid or expired"))
+	ErrSessionGet                        = api.NewApiError(http.StatusInternalServerError, "0001", errors.New("failed to get session"))
+	ErrCreateOAuthState                  = api.NewApiError(http.StatusInternalServerError, "0002", errors.New("cannot create oauth state parameter"))
+	ErrInvalidProvider                   = api.NewApiError(http.StatusBadRequest, "0003", errors.New("invalid oauth provider"))
+	ErrInvalidSessionState               = api.NewApiError(http.StatusBadRequest, "0004", errors.New("invalid session state"))
+	ErrInvalidStateParameter             = api.NewApiError(http.StatusBadRequest, "0005", errors.New("invalid state parameter"))
+	ErrOAuthTokenExchange                = api.NewApiError(http.StatusInternalServerError, "0006", errors.New("failed to exchange token"))
+	ErrOAuthFailedUserFetch              = api.NewApiError(http.StatusInternalServerError, "0007", errors.New("failed to fetch user"))
+	ErrOAuthInvalidUserResp              = api.NewApiError(http.StatusInternalServerError, "0008", errors.New("failed to parse user"))
+	ErrInvalidEmailOrPassword            = api.NewApiError(http.StatusBadRequest, "0009", errors.New("invalid email or password"))
+	ErrEmailTaken                        = api.NewApiError(http.StatusUnprocessableEntity, "0010", errors.New("email has already been taken"))
+	ErrUsernameTaken                     = api.NewApiError(http.StatusUnprocessableEntity, "0011", errors.New("username has already been taken"))
+	ErrUsernameChars                     = api.NewApiError(http.StatusUnprocessableEntity, "0012", errors.New("username must include only alphanumeric characters or underscore"))
+	ErrHash                              = api.NewApiError(http.StatusInternalServerError, "0013", errors.New("cannot hash"))
+	ErrInvalidEmail                      = api.NewApiError(http.StatusUnprocessableEntity, "0014", errors.New("invalid email"))
+	ErrEmailAlreadyVerified              = api.NewApiError(http.StatusUnprocessableEntity, "0015", errors.New("email has already been verified"))
+	ErrMalformedOrMissingVerifyToken     = api.NewApiError(http.StatusBadRequest, "0016", errors.New("verification token is missing or malformed"))
+	ErrInvalidOrExpiredVerifyCode        = api.NewApiError(http.StatusUnprocessableEntity, "0017", errors.New("verification code is invalid or expired"))
+	ErrPasswordResetCodeExpiredOrInvalid = api.NewApiError(http.StatusUnprocessableEntity, "0018", errors.New("password reset code is invalid or expired"))
 )
