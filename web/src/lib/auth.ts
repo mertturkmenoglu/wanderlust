e047a532990c644fe485ea89a7a99c19b7f60f5e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-07 05:02:20 +0300

    refactor auth handling to utilize DTOs and add navigation guards for onboarding and email verification

--- web/src/lib/auth.ts
+++ web/src/lib/auth.ts
@@ -1,8 +1,9 @@
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { cache } from 'react';
 import api from './api';
 
-export type Auth = {
+export type AuthDto = {
   data: GetMeResponseDto;
 };
 
@@ -13,6 +14,7 @@ export type GetMeResponseDto = {
   fullName: string;
   googleId: string | null;
   isEmailVerified: boolean;
+  isOnboardingCompleted: boolean;
   isActive: boolean;
   role: string;
   gender: string | null;
@@ -24,7 +26,7 @@ export type GetMeResponseDto = {
 
 const cookieName = '__wanderlust_auth';
 
-export const getAuth = cache(async (): Promise<Auth | null> => {
+export const getAuth = cache(async (): Promise<AuthDto | null> => {
   const sessionCookie = cookies().get(cookieName);
 
   if (!sessionCookie) {
@@ -43,8 +45,30 @@ export const getAuth = cache(async (): Promise<Auth | null> => {
       return null;
     }
 
-    return await res.json<Auth>();
+    return await res.json<AuthDto>();
   } catch (e) {
     return null;
   }
 });
+
+export function protect(auth: AuthDto | null) {
+  if (auth === null) {
+    redirect('/sign-in');
+  }
+
+  protectOnboard(auth);
+}
+
+export function protectOnboard(auth: AuthDto | null) {
+  if (auth === null) {
+    return;
+  }
+
+  if (!auth.data.isOnboardingCompleted) {
+    redirect('/onboarding');
+  }
+
+  if (!auth.data.isEmailVerified) {
+    redirect('/verify-email');
+  }
+}
