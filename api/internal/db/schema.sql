2a1520b507906af6d66a17e2158a61a96adff56b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-09 21:46:52 +0300

    update schema

--- api/internal/db/schema.sql
+++ api/internal/db/schema.sql
@@ -108,6 +108,7 @@ CREATE TABLE IF NOT EXISTS cities (
   name VARCHAR(64) NOT NULL,
   state_id INT NOT NULL,
   state_code VARCHAR(16) NOT NULL,
+  state_name VARCHAR(64) NOT NULL,
   country_id INT NOT NULL,
   country_code CHAR(2) NOT NULL,
   country_name VARCHAR(64) NOT NULL,
