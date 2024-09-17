6ef573096c8b457db9fd26cc1f67418118cc5daf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-17 09:38:52 +0300

    update custom http error handler

--- api/internal/app/api/error_handler.go
+++ api/internal/app/api/error_handler.go
@@ -1,40 +1,24 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-	"wanderlust/config"
-
 	"github.com/labstack/echo/v4"
-	"github.com/pterm/pterm"
-	"github.com/spf13/viper"
 )
 
 func CustomHTTPErrorHandler(err error, c echo.Context) {
-	pterm.DefaultLogger.WithLevel(pterm.LogLevelError).Error(err.Error())
-
-	code := http.StatusInternalServerError
-	ae, ok := err.(ApiError)
-	env := viper.GetString(config.ENV)
-	isDev := env == "dev"
-	shouldLog := !(!isDev && ae.Status >= http.StatusInternalServerError)
-
-	if ok && shouldLog {
-		code = ae.Status
-
-		c.JSON(code, ErrorResponse{
-			Errors: []ErrorDto{
-				{
-					Status: fmt.Sprintf("%d", code),
-					Code:   ae.Code,
-					Title:  ae.Err.Error(),
-					Detail: ae.Err.Error(),
-				},
-			},
-		})
-	} else {
-		c.JSON(ae.Status, echo.Map{
-			"message": http.StatusText(ae.Status),
-		})
+	if c.Response().Committed {
+		return
 	}
+
+	_, isEchoErr := err.(*echo.HTTPError)
+
+	if !isEchoErr {
+		ae, ok := err.(ApiError)
+
+		if ok {
+			NewErr(c, ae.Status, ae)
+			return
+		}
+	}
+
+	c.Echo().DefaultHTTPErrorHandler(err, c)
 }
