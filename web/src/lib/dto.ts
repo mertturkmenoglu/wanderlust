b706c441c06fbbc510b936e60d5bbd46041f881e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-18 11:48:14 +0300

    add dto

---
+++ web/src/lib/dto.ts
@@ -0,0 +1,12 @@
+export type GetCityByIdResponseDto = {
+  id: number;
+  name: string;
+  stateCode: string;
+  stateName: string;
+  countryCode: string;
+  countryName: string;
+  imageUrl: string;
+  latitude: number;
+  longitude: number;
+  description: string;
+};
