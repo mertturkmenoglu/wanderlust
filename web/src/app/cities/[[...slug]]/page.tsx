0665d1047aa4965121b69a79f24de45059ca2675
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-19 18:30:44 +0300

    use collection component with dummy data

--- web/src/app/cities/[[...slug]]/page.tsx
+++ web/src/app/cities/[[...slug]]/page.tsx
@@ -1,3 +1,5 @@
+import Collection from '@/components/blocks/collection';
+import OverlayBanner from '@/components/blocks/overlay-banner';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,8 +8,10 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
 import api from '@/lib/api';
 import { GetCityByIdResponseDto } from '@/lib/dto';
+import Link from 'next/link';
 
 type Props = {
   params: {
@@ -45,11 +49,11 @@ export default async function Page({ params: { slug } }: Readonly<Props>) {
         </BreadcrumbList>
       </Breadcrumb>
 
-      <div className="flex gap-8">
+      <div className="flex flex-col md:flex-row md:gap-8">
         <img
           src={city.data.imageUrl}
           alt=""
-          className="mt-8 max-w-md rounded-md object-cover"
+          className="mt-8 w-full rounded-md object-cover md:max-w-md"
         />
 
         <div>
@@ -62,6 +66,141 @@ export default async function Page({ params: { slug } }: Readonly<Props>) {
           </div>
         </div>
       </div>
+
+      <Collection
+        className="my-8"
+        title="Curated Locations"
+        actions={
+          <Button
+            asChild
+            variant="link"
+          >
+            <Link href={`/collections/city/curated/${cityId}`}>See more</Link>
+          </Button>
+        }
+        items={[
+          {
+            id: '0',
+            title: 'Squirrels Out',
+            category: 'Historical Landmark',
+            favorite: true,
+            image:
+              'https://a.ltrbxd.com/resized/film-poster/4/7/5/3/7/0/475370-knives-out-0-1000-0-1500-crop.jpg?v=7da76d742c',
+          },
+          {
+            id: '1',
+            title: 'Anatomy of a Squirrel',
+            category: 'Restaurant',
+            favorite: true,
+            image: 'https://i.imgur.com/5F9jdqG.jpg',
+          },
+          {
+            id: '2',
+            title: 'All Quiet on the Western Squirrels',
+            category: 'Bookstores',
+            favorite: false,
+            image: 'https://i.imgur.com/SnP70MO.jpg',
+          },
+          {
+            id: '3',
+            title: 'Sincap Coffee',
+            category: 'Coffee Shops',
+            favorite: true,
+            image: 'https://i.imgur.com/JYnxkQM.jpg',
+          },
+          {
+            id: '4',
+            title: 'No Time to Squirrel',
+            category: 'Photography Spots',
+            favorite: false,
+            image: 'https://i.imgur.com/XFG5Q7R.jpg',
+          },
+          {
+            id: '5',
+            title: "Squirrel's Gambit",
+            category: 'Restaurant',
+            favorite: true,
+            image: 'https://i.imgur.com/FKlIkC5.jpg',
+          },
+        ]}
+      />
+
+      <OverlayBanner
+        image="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Categories Banner Image"
+        message={
+          <div className="flex items-center gap-4">
+            <div>Plan a trip to {city.data.name}</div>
+            <Button
+              asChild
+              variant="default"
+            >
+              <Link href="/trips/planner">Start Planning</Link>
+            </Button>
+          </div>
+        }
+        className="my-8"
+        imgClassName="aspect-[3]"
+      />
+
+      <Collection
+        className="my-8"
+        title="Popular Tourist Attractions"
+        actions={
+          <Button
+            asChild
+            variant="link"
+          >
+            <Link href={`/collections/city/curated/${cityId}`}>See more</Link>
+          </Button>
+        }
+        items={[
+          {
+            id: '0',
+            title: 'Squirrel Runner 2049',
+            category: 'Historical Landmark',
+            favorite: true,
+            image:
+              'https://a.ltrbxd.com/resized/film-poster/2/6/5/4/3/9/265439-blade-runner-2049-0-1000-0-1500-crop.jpg?v=86735e0bb8',
+          },
+          {
+            id: '1',
+            title: "The Squirrel's Speech",
+            category: 'Breweries',
+            favorite: true,
+            image:
+              'https://a.ltrbxd.com/resized/film-poster/5/3/6/3/5363-the-king-s-speech-0-1000-0-1500-crop.jpg?v=334f06fd89',
+          },
+          {
+            id: '2',
+            title: 'The Squirrel from Earth',
+            category: 'Museums',
+            favorite: false,
+            image: 'https://i.imgur.com/1mn0i08.jpg',
+          },
+          {
+            id: '3',
+            title: 'Squirrel Gump',
+            category: 'Famous Filming Locations',
+            favorite: true,
+            image: 'https://i.imgur.com/f6VKpRj.jpg',
+          },
+          {
+            id: '4',
+            title: 'The Grand Squirrel Hotel',
+            category: 'Hotels',
+            favorite: false,
+            image: 'https://i.imgur.com/Ivsxi5b.jpg',
+          },
+          {
+            id: '5',
+            title: 'The Squirrel of The Opera',
+            category: 'Theaters',
+            favorite: true,
+            image: 'https://i.imgur.com/FKlIkC5.jpg',
+          },
+        ]}
+      />
     </div>
   );
 }
