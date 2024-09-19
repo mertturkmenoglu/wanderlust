f3eec1eebe69164de53af62a76f8d456ac650ee7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-19 15:17:56 +0300

    update cities page

--- web/src/app/cities/page.tsx
+++ web/src/app/cities/page.tsx
@@ -1,5 +1,8 @@
+import OverlayBanner from '@/components/blocks/overlay-banner';
+import { Button } from '@/components/ui/button';
 import api from '@/lib/api';
 import { GetCitiesResponseDto } from '@/lib/dto';
+import Link from 'next/link';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
@@ -31,14 +34,35 @@ export default async function Page() {
 
   return (
     <div className="container">
+      <OverlayBanner
+        image="https://images.unsplash.com/photo-1607388510015-c632e99da586?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Categories Banner Image"
+        message={
+          <div className="flex items-center gap-4">
+            <div>Discover the world around you</div>
+            <Button
+              asChild
+              variant="default"
+            >
+              <Link href="/discover/locations">See categories</Link>
+            </Button>
+          </div>
+        }
+        className="my-8"
+        imgClassName="aspect-[3]"
+      />
+
       <div className="flex items-baseline">
-        <h2 className="mt-16 text-4xl font-bold">Browse by country</h2>
+        <h2 className="text-4xl font-bold">Browse by country</h2>
       </div>
 
-      <div>
+      <div className="my-8">
         {groups.map((group) => (
-          <div key={group[0]}>
-            <h3 className="mt-8 text-2xl font-bold">{group[0]}</h3>
+          <div
+            key={group[0]}
+            className="my-8"
+          >
+            <h3 className="text-2xl font-bold">{group[0]}</h3>
             <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
               {group[1].map((city) => (
                 <a
