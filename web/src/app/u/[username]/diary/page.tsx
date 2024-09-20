51175f799c2f349a80b30cde247ade0562fa6d63
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-20 08:59:21 +0300

    add dummy pages

---
+++ web/src/app/u/[username]/diary/page.tsx
@@ -0,0 +1,13 @@
+import AppMessage from '@/components/blocks/app-message';
+
+export default async function Page() {
+  return (
+    <div>
+      <AppMessage
+        className="my-16"
+        emptyMessage="This user has no diary entries"
+        showBackButton={false}
+      />
+    </div>
+  );
+}
