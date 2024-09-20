507f37726471bc5cca28295a2a631eca79e0f084
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-20 09:00:45 +0300

    add user image component

---
+++ web/src/components/blocks/user-image/index.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { cn } from '@/lib/utils';
+import { useEffect, useRef } from 'react';
+
+type Props = {
+  className?: string;
+  imgClassName?: string;
+  fallbackClassName?: string;
+  src: string | null;
+  initials?: string | null;
+};
+
+export default function UserImage({
+  className,
+  imgClassName,
+  fallbackClassName,
+  src,
+  initials = '',
+}: Readonly<Props>) {
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (imgRef.current) {
+      imgRef.current.setAttribute('referrerpolicy', 'no-referrer');
+    }
+  }, [imgRef]);
+
+  return (
+    <Avatar className={cn('rounded-xl', className)}>
+      <AvatarImage
+        src={src ?? ''}
+        referrerPolicy="no-referrer"
+        className={cn(imgClassName)}
+      />
+      <AvatarFallback
+        delayMs={1000}
+        className={cn(
+          'rounded-xl bg-gradient-to-br from-sky-600 via-cyan-500 to-teal-600 text-2xl font-semibold text-white',
+          fallbackClassName
+        )}
+      >
+        {initials ?? ''}
+      </AvatarFallback>
+    </Avatar>
+  );
+}
