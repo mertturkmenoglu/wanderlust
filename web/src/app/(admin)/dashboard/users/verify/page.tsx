b7459ef05d4049f0860d3da7d4d76ce033f1c908
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-21 07:02:47 +0300

    add users dashboard pages

---
+++ web/src/app/(admin)/dashboard/users/verify/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+export default function Page() {
+  const [username, setUsername] = useState('');
+  const mutation = useMutation({
+    mutationKey: ['user-verify'],
+    mutationFn: async () => {
+      // TODO: Implement later
+    },
+    onSuccess: () => {
+      toast.success('User is verified');
+    },
+    onError: (e) => {
+      toast.error(`An error happened: ${e.message}`);
+    },
+  });
+
+  return (
+    <div>
+      <h3 className="mb-4 text-lg font-bold tracking-tight">
+        Make User Verified
+      </h3>
+
+      <Input
+        type="text"
+        value={username}
+        className="max-w-[512px]"
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <Button
+        onClick={() => {
+          mutation.mutate();
+        }}
+        className="mt-4"
+      >
+        Make User Verified
+      </Button>
+    </div>
+  );
+}
