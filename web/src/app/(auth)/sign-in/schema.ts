5e8ced40c50668f5415beeaec4d3e73fc67abacd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-22 12:09:18 +0300

    start migrating from next to remix because fuck next

--- web/src/app/(auth)/sign-in/schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const FormSchema = z.object({
-  email: z.string().min(1, { message: 'Email is required' }).email(),
-  password: z.string().min(1, { message: 'Password is required' }),
-});
-
-export type FormInput = z.infer<typeof FormSchema>;
