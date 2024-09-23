f031ea17df324941c15e01d68df1190378feed4a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-23 10:00:30 +0300

    add sign in page

---
+++ web/app/routes/_auth.sign-in/schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const FormSchema = z.object({
+  email: z.string().min(1, { message: "Email is required" }).email(),
+  password: z.string().min(1, { message: "Password is required" }),
+});
+
+export type FormInput = z.infer<typeof FormSchema>;
