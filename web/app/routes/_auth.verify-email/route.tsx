328fec6b5f2b90f8bb8398ccad95c8097db7d292
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-23 10:00:54 +0300

    update email verify page

--- web/app/routes/_auth.verify-email/route.tsx
+++ web/app/routes/_auth.verify-email/route.tsx
@@ -1,7 +1,10 @@
 export default function Page() {
   return (
-    <div>
-      <div>This is the verify email page</div>
+    <div className="container mx-auto my-32 flex max-w-lg flex-col py-8">
+      <h2 className="mt-4 text-xl font-bold">Verify Email</h2>
+      <div className="text-sm text-muted-foreground">
+        Please verify your email address to start using Wanderlust.
+      </div>
     </div>
   );
 }
