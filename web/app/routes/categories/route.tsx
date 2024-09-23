7234d5f072b288d60757064e01560330ce5f8020
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-23 10:01:22 +0300

    update categories page

--- web/app/routes/categories/route.tsx
+++ web/app/routes/categories/route.tsx
@@ -1,7 +1,39 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getCategories } from "~/lib/api";
+
+export const loader = async () => {
+  const res = await getCategories();
+  return json({ categories: res.data.categories });
+};
+
 export default function Page() {
+  const { categories } = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <div>This is the categories page</div>
+    <div className="container mx-auto">
+      <div className="flex items-baseline">
+        <h2 className="mt-8 text-4xl font-bold">Browse by category</h2>
+      </div>
+
+      <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+        {categories.map((category) => (
+          <a
+            href={`/categories/${category.id}/${category.name}`}
+            key={category.id}
+            className="rounded-md"
+          >
+            <img
+              src={category.image}
+              alt=""
+              className="aspect-video w-full rounded-md object-cover"
+            />
+            <div className="mt-2 text-xl font-bold lg:text-base">
+              {category.name}
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
