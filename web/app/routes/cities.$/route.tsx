3db185956d603bb0e8d0ebe129bc6cbd0aee5970
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-23 10:01:35 +0300

    update cities page

--- web/app/routes/cities.$/route.tsx
+++ web/app/routes/cities.$/route.tsx
@@ -1,7 +1,26 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useParams } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const slug = params;
+  console.log(slug);
+  return null;
+};
+
 export default function Page() {
+  const params = useParams();
+  const slug = params["*"];
+
+  if (!slug || slug.length < 1) {
+    return <div>This is the cities page</div>;
+  }
+
+  const p = slug.split("/")[0];
+
   return (
     <div>
       <div>This is the cities page</div>
+      <div>{JSON.stringify(p)}</div>
     </div>
   );
 }
