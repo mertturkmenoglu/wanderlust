2c1beb6407f410dff248f88bd82492a0ca364ec2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-24 08:13:21 +0300

    update queries

--- api/internal/db/pois.sql.go
+++ api/internal/db/pois.sql.go
@@ -27,6 +27,141 @@ type BatchCreatePoisParams struct {
 	OpenTimes          []byte
 }
 
+const getPoiAmenities = `-- name: GetPoiAmenities :many
+SELECT amenities_pois.amenity_id, amenities_pois.poi_id, amenities.id, amenities.name FROM amenities_pois
+LEFT JOIN amenities ON amenities.id = amenities_pois.amenity_id
+WHERE poi_id = $1
+ORDER BY amenity_id
+`
+
+type GetPoiAmenitiesRow struct {
+	AmenitiesPoi AmenitiesPoi
+	Amenity      Amenity
+}
+
+func (q *Queries) GetPoiAmenities(ctx context.Context, poiID string) ([]GetPoiAmenitiesRow, error) {
+	rows, err := q.db.Query(ctx, getPoiAmenities, poiID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetPoiAmenitiesRow
+	for rows.Next() {
+		var i GetPoiAmenitiesRow
+		if err := rows.Scan(
+			&i.AmenitiesPoi.AmenityID,
+			&i.AmenitiesPoi.PoiID,
+			&i.Amenity.ID,
+			&i.Amenity.Name,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getPoiById = `-- name: GetPoiById :one
+SELECT pois.id, pois.name, pois.phone, pois.description, pois.address_id, pois.website, pois.price_level, pois.accessibility_level, pois.total_votes, pois.total_points, pois.total_favorites, pois.category_id, pois.open_times, pois.created_at, pois.updated_at, addresses.id, addresses.city_id, addresses.line1, addresses.line2, addresses.postal_code, addresses.lat, addresses.lng, categories.id, categories.name, categories.image, cities.id, cities.name, cities.state_code, cities.state_name, cities.country_code, cities.country_name, cities.image_url, cities.latitude, cities.longitude, cities.description FROM pois
+LEFT JOIN addresses ON addresses.id = pois.address_id
+LEFT JOIN categories ON categories.id = pois.category_id
+LEFT JOIN cities ON cities.id = addresses.city_id
+WHERE pois.id = $1 LIMIT 1
+`
+
+type GetPoiByIdRow struct {
+	Poi      Poi
+	Address  Address
+	Category Category
+	City     City
+}
+
+func (q *Queries) GetPoiById(ctx context.Context, id string) (GetPoiByIdRow, error) {
+	row := q.db.QueryRow(ctx, getPoiById, id)
+	var i GetPoiByIdRow
+	err := row.Scan(
+		&i.Poi.ID,
+		&i.Poi.Name,
+		&i.Poi.Phone,
+		&i.Poi.Description,
+		&i.Poi.AddressID,
+		&i.Poi.Website,
+		&i.Poi.PriceLevel,
+		&i.Poi.AccessibilityLevel,
+		&i.Poi.TotalVotes,
+		&i.Poi.TotalPoints,
+		&i.Poi.TotalFavorites,
+		&i.Poi.CategoryID,
+		&i.Poi.OpenTimes,
+		&i.Poi.CreatedAt,
+		&i.Poi.UpdatedAt,
+		&i.Address.ID,
+		&i.Address.CityID,
+		&i.Address.Line1,
+		&i.Address.Line2,
+		&i.Address.PostalCode,
+		&i.Address.Lat,
+		&i.Address.Lng,
+		&i.Category.ID,
+		&i.Category.Name,
+		&i.Category.Image,
+		&i.City.ID,
+		&i.City.Name,
+		&i.City.StateCode,
+		&i.City.StateName,
+		&i.City.CountryCode,
+		&i.City.CountryName,
+		&i.City.ImageUrl,
+		&i.City.Latitude,
+		&i.City.Longitude,
+		&i.City.Description,
+	)
+	return i, err
+}
+
+const getPoiMedia = `-- name: GetPoiMedia :many
+SELECT id, poi_id, url, thumbnail, alt, caption, width, height, media_order, extension, mime_type, file_size, created_at FROM media
+WHERE poi_id = $1
+ORDER BY media_order
+`
+
+func (q *Queries) GetPoiMedia(ctx context.Context, poiID string) ([]Medium, error) {
+	rows, err := q.db.Query(ctx, getPoiMedia, poiID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Medium
+	for rows.Next() {
+		var i Medium
+		if err := rows.Scan(
+			&i.ID,
+			&i.PoiID,
+			&i.Url,
+			&i.Thumbnail,
+			&i.Alt,
+			&i.Caption,
+			&i.Width,
+			&i.Height,
+			&i.MediaOrder,
+			&i.Extension,
+			&i.MimeType,
+			&i.FileSize,
+			&i.CreatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const peekPois = `-- name: PeekPois :many
 SELECT id, name, phone, description, address_id, website, price_level, accessibility_level, total_votes, total_points, total_favorites, category_id, open_times, created_at, updated_at FROM pois
 LIMIT 25
--- api/internal/db/queries/pois.sql
+++ api/internal/db/queries/pois.sql
@@ -38,3 +38,21 @@ LIMIT $1;
 -- name: PeekPois :many
 SELECT * FROM pois
 LIMIT 25;
+
+-- name: GetPoiById :one
+SELECT sqlc.embed(pois), sqlc.embed(addresses), sqlc.embed(categories), sqlc.embed(cities) FROM pois
+LEFT JOIN addresses ON addresses.id = pois.address_id
+LEFT JOIN categories ON categories.id = pois.category_id
+LEFT JOIN cities ON cities.id = addresses.city_id
+WHERE pois.id = $1 LIMIT 1;
+
+-- name: GetPoiMedia :many
+SELECT * FROM media
+WHERE poi_id = $1
+ORDER BY media_order;
+
+-- name: GetPoiAmenities :many
+SELECT sqlc.embed(amenities_pois), sqlc.embed(amenities) FROM amenities_pois
+LEFT JOIN amenities ON amenities.id = amenities_pois.amenity_id
+WHERE poi_id = $1
+ORDER BY amenity_id;
