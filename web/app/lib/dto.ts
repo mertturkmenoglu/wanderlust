42fefe6ab581c2b7c53f9121329fc09df614d89c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-24 08:13:49 +0300

    update dtos

--- web/app/lib/dto.ts
+++ web/app/lib/dto.ts
@@ -1,3 +1,5 @@
+/// AUTH
+
 export type ResetPasswordRequestDto = {
   email: string;
   code: string;
@@ -25,6 +27,8 @@ export type GetMeResponseDto = {
   updatedAt: string;
 };
 
+// CITIES
+
 export type GetCityByIdResponseDto = {
   id: number;
   name: string;
@@ -42,6 +46,8 @@ export type GetCitiesResponseDto = {
   cities: GetCityByIdResponseDto[];
 };
 
+/// CATEGORIES
+
 export type GetCategoriesResponseDto = {
   categories: GetCategoryByIdResponseDto[];
 };
@@ -51,6 +57,8 @@ export type GetCategoryByIdResponseDto = {
   name: string;
   image: string;
 };
+
+/// USERS
 
 export type GetUserProfileResponseDto = {
   id: string;
@@ -69,6 +77,8 @@ export type GetUserProfileResponseDto = {
   followingCount: number;
   createdAt: string;
 };
+
+/// POIS
 
 export type OpenTimesSingle = {
   day: string;
@@ -109,4 +119,77 @@ export type CreateMediaDto = {
   caption: string;
   width: number;
   height: number;
+};
+
+export type GetPoiByIdResponseDto = {
+  id: string;
+  name: string;
+  phone: string | null;
+  description: string;
+  addressId: number;
+  website: string | null;
+  priceLevel: number;
+  accessibilityLevel: number;
+  totalVotes: number;
+  totalPoints: number;
+  totalFavorites: number;
+  categoryId: number;
+  openTimes: OpenTimesSingle[];
+  createdAt: string;
+  updatedAt: string;
+  category: Category;
+  amenities: Amenity[];
+  media: Media[];
+  address: Address;
+};
+
+export type Category = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+export type Media = {
+  id: number;
+  poiId: string;
+  url: string;
+  thumbnail: string;
+  alt: string;
+  caption: string | null;
+  width: number;
+  height: number;
+  mediaOrder: number;
+  extension: string;
+  mimeType: string;
+  fileSize: number;
+  createdAt: string;
+};
+
+export type Amenity = {
+  id: number;
+  name: string;
+};
+
+export type Address = {
+  id: number;
+  cityId: number;
+  city: City;
+  line1: string;
+  line2: string | null;
+  postalCode: string | null;
+  lat: number;
+  lng: number;
+};
+
+export type City = {
+  id: number;
+  name: string;
+  stateCode: string;
+  stateName: string;
+  countryCode: string;
+  countryName: string;
+  imageUrl: string;
+  latitude: number;
+  longitude: number;
+  description: string;
 };
