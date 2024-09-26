194364be8785bfc5d1ea7185ceb640725ac361c7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-26 20:37:49 +0300

    update authorization controls

--- api/internal/authz/common.go
+++ api/internal/authz/common.go
@@ -5,6 +5,7 @@ import "github.com/labstack/echo/v4"
 type AuthzAct string
 
 const (
+	ActAmenityCreate  AuthzAct = "amenity-create"
 	ActAmenityUpdate  AuthzAct = "amenity-update"
 	ActBookmarkCreate AuthzAct = "bookmark-create"
 	ActBookmarkRead   AuthzAct = "bookmark-read"
@@ -14,6 +15,7 @@ const (
 type AuthzFn func(s *Authz, c echo.Context) (bool, error)
 
 var Fns = map[AuthzAct]AuthzFn{
+	ActAmenityCreate:  IsAdmin,
 	ActAmenityUpdate:  IsAdmin,
 	ActBookmarkCreate: Identity,
 	ActBookmarkRead:   Identity,
