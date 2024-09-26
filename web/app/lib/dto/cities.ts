09368de6d14d6987a83985fa05c6e79b15b59646
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-27 02:28:15 +0300

    update dtos

--- web/app/lib/dto/cities.ts
+++ web/app/lib/dto/cities.ts
@@ -14,3 +14,32 @@ export type GetCityByIdResponseDto = {
 export type GetCitiesResponseDto = {
   cities: GetCityByIdResponseDto[];
 };
+
+export type CreateCityRequestDto = {
+  id: number;
+  name: string;
+  stateCode: string;
+  stateName: string;
+  countryCode: string;
+  countryName: string;
+  imageUrl: string;
+  latitude: number;
+  longitude: number;
+  description: string;
+};
+
+export type CreateCityResponseDto = GetCityByIdResponseDto;
+
+export type UpdateCityRequestDto = {
+  name: string;
+  stateCode: string;
+  stateName: string;
+  countryCode: string;
+  countryName: string;
+  imageUrl: string;
+  latitude: number;
+  longitude: number;
+  description: string;
+};
+
+export type UpdateCityResponseDto = GetCityByIdResponseDto;
