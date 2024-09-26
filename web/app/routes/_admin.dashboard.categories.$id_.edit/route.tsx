0b22cec8138c4de1665527cbddecd94e8c7d0503
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-26 10:54:19 +0300

    implement admin dashboard category edit page

--- web/app/routes/_admin.dashboard.categories.$id_.edit/route.tsx
+++ web/app/routes/_admin.dashboard.categories.$id_.edit/route.tsx
@@ -1,7 +1,39 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import BackLink from "~/components/blocks/back-link";
+import { getCategories } from "~/lib/api";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.id, "id is required");
+  const id = +params.id;
+  const categories = await getCategories();
+  const category = categories.data.categories.find(
+    (category) => category.id === id
+  );
+
+  if (!category) {
+    throw new Response("Category not found", { status: 404 });
+  }
+
+  return json({ category });
+}
+
 export default function Page() {
+  const { category } = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <div>This is the admin dashboard category edit page</div>
+      <BackLink
+        href={`/dashboard/categories/${category.id}`}
+        text="Go back to category details"
+      />
+      <div>This is the edit page for category {category.id}</div>
+      <div>
+        <pre className="max-w-xl break-words flex-wrap text-wrap whitespace-pre-wrap">
+          {JSON.stringify(category, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 }
