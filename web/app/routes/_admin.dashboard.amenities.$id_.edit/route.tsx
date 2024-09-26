1b77247e389999bac9006ed2782257c6b8d95489
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-26 10:39:57 +0300

    implement admin dashboard amenity edit page

--- web/app/routes/_admin.dashboard.amenities.$id_.edit/route.tsx
+++ web/app/routes/_admin.dashboard.amenities.$id_.edit/route.tsx
@@ -1,7 +1,37 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import BackLink from "~/components/blocks/back-link";
+import { getAmenities } from "~/lib/api";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.id, "id is required");
+  const id = +params.id;
+  const amenities = await getAmenities();
+  const amenity = amenities.data.amenities.find((amenity) => amenity.id === id);
+
+  if (!amenity) {
+    throw new Response("Amenity not found", { status: 404 });
+  }
+
+  return json({ amenity });
+}
+
 export default function Page() {
+  const { amenity } = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <div>This is the admin dashboard amenity edit page</div>
+      <BackLink
+        href={`/dashboard/amenities/${amenity.id}`}
+        text="Go back to amenity details"
+      />
+      <div>This is the edit page for amenty {amenity.id}</div>
+      <div>
+        <pre className="max-w-xl break-words flex-wrap text-wrap whitespace-pre-wrap">
+          {JSON.stringify(amenity, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 }
--- web/app/routes/_admin.dashboard.amenities.new/route.tsx
+++ web/app/routes/_admin.dashboard.amenities.new/route.tsx
@@ -1,7 +1,68 @@
+import InputError from "~/components/kit/input-error";
+import InputInfo from "~/components/kit/input-info";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "~/components/ui/button";
+
+export const schema = z.object({
+  id: z.number().min(1),
+  name: z.string().min(1),
+});
+
+export type FormInput = z.infer<typeof schema>;
+
+export function useNewAmenityForm() {
+  return useForm<FormInput>({
+    resolver: zodResolver(schema),
+  });
+}
+
 export default function Page() {
+  const form = useNewAmenityForm();
+
   return (
     <div>
-      <div>This is the admin dashboard amenities new page</div>
+      <h3 className="mb-8 text-lg font-bold tracking-tight">
+        Create New Amenity
+      </h3>
+      <form
+        className="container mx-0 mt-8 grid grid-cols-1 gap-4 px-0 md:grid-cols-2"
+        onSubmit={form.handleSubmit(console.log)}
+      >
+        <div className="">
+          <Label htmlFor="id">ID</Label>
+          <Input
+            type="number"
+            id="id"
+            placeholder="ID of the amenity"
+            autoComplete="off"
+            {...form.register("id")}
+          />
+          <InputInfo text="ID of the amenity" />
+          <InputError error={form.formState.errors.id} />
+        </div>
+
+        <div className="">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            id="name"
+            placeholder="Name"
+            autoComplete="off"
+            {...form.register("name")}
+          />
+          <InputInfo text="Name" />
+          <InputError error={form.formState.errors.name} />
+        </div>
+
+        <div>
+          <Button type="submit">Create</Button>
+        </div>
+      </form>
     </div>
   );
 }
