8d004fc1487c595711484a05a4c42a0654beec78
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-26 10:53:47 +0300

    implement admin dashboard categories listing page

--- web/app/routes/_admin.dashboard.categories._index/route.tsx
+++ web/app/routes/_admin.dashboard.categories._index/route.tsx
@@ -1,7 +1,28 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import { getCategories } from "~/lib/api";
+
+export async function loader() {
+  const categories = await getCategories();
+  return json({ categories: categories.data.categories });
+}
+
 export default function Page() {
+  const { categories } = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <div>This is the admin dashboard categories page</div>
+      <h2 className="text-2xl font-bold tracking-tight">Categories</h2>
+      <div className="grid grid-cols-4 gap-4 mt-8">
+        {categories.map((category) => (
+          <Link to={`/dashboard/categories/${category.id}`} key={category.id}>
+            <Button asChild variant="link" className="p-0">
+              <div className="font-bold text-wrap">{category.name}</div>
+            </Button>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
