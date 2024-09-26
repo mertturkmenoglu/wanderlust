0bf26afcfff48534c51428d29d8e0f997490dc1b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-27 02:43:40 +0300

    update authorization rules

--- api/internal/authz/common.go
+++ api/internal/authz/common.go
@@ -5,33 +5,35 @@ import "github.com/labstack/echo/v4"
 type AuthzAct string
 
 const (
-	ActAmenityCreate  AuthzAct = "amenity-create"
-	ActAmenityUpdate  AuthzAct = "amenity-update"
-	ActAmenityDelete  AuthzAct = "amenity-delete"
-	ActBookmarkCreate AuthzAct = "bookmark-create"
-	ActBookmarkRead   AuthzAct = "bookmark-read"
-	ActBookmarkDelete AuthzAct = "bookmark-delete"
-	ActCategoryCreate AuthzAct = "category-create"
-	ActCategoryUpdate AuthzAct = "category-update"
-	ActCategoryDelete AuthzAct = "category-delete"
-	ActCityCreate     AuthzAct = "city-create"
-	ActCityUpdate     AuthzAct = "city-update"
-	ActCityDelete     AuthzAct = "city-delete"
+	ActAmenityCreate    AuthzAct = "amenity-create"
+	ActAmenityUpdate    AuthzAct = "amenity-update"
+	ActAmenityDelete    AuthzAct = "amenity-delete"
+	ActBookmarkCreate   AuthzAct = "bookmark-create"
+	ActBookmarkRead     AuthzAct = "bookmark-read"
+	ActBookmarkDelete   AuthzAct = "bookmark-delete"
+	ActCategoryCreate   AuthzAct = "category-create"
+	ActCategoryUpdate   AuthzAct = "category-update"
+	ActCategoryDelete   AuthzAct = "category-delete"
+	ActCityCreate       AuthzAct = "city-create"
+	ActCityUpdate       AuthzAct = "city-update"
+	ActCityDelete       AuthzAct = "city-delete"
+	ActMakeUserVerified AuthzAct = "make-user-verified"
 )
 
 type AuthzFn func(s *Authz, c echo.Context) (bool, error)
 
 var Fns = map[AuthzAct]AuthzFn{
-	ActAmenityCreate:  IsAdmin,
-	ActAmenityUpdate:  IsAdmin,
-	ActAmenityDelete:  IsAdmin,
-	ActBookmarkCreate: Identity,
-	ActBookmarkRead:   Identity,
-	ActBookmarkDelete: Identity,
-	ActCategoryCreate: IsAdmin,
-	ActCategoryUpdate: IsAdmin,
-	ActCategoryDelete: IsAdmin,
-	ActCityCreate:     IsAdmin,
-	ActCityUpdate:     IsAdmin,
-	ActCityDelete:     IsAdmin,
+	ActAmenityCreate:    IsAdmin,
+	ActAmenityUpdate:    IsAdmin,
+	ActAmenityDelete:    IsAdmin,
+	ActBookmarkCreate:   Identity,
+	ActBookmarkRead:     Identity,
+	ActBookmarkDelete:   Identity,
+	ActCategoryCreate:   IsAdmin,
+	ActCategoryUpdate:   IsAdmin,
+	ActCategoryDelete:   IsAdmin,
+	ActCityCreate:       IsAdmin,
+	ActCityUpdate:       IsAdmin,
+	ActCityDelete:       IsAdmin,
+	ActMakeUserVerified: IsAdmin,
 }
