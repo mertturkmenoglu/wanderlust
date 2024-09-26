1e14235e1ad5748f89c59a688a56c97a71f4c4ab
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-26 19:40:06 +0300

    update is admin authz function

--- api/internal/authz/fn.go
+++ api/internal/authz/fn.go
@@ -1,11 +1,25 @@
 package authz
 
 import (
+	"context"
+
 	"github.com/labstack/echo/v4"
 )
 
 func IsAdmin(s *Authz, c echo.Context) (bool, error) {
-	return false, nil
+	userId, ok := c.Get("user_id").(string)
+
+	if !ok {
+		return false, echo.ErrInternalServerError
+	}
+
+	res, err := s.Db.Queries.IsAdmin(context.Background(), userId)
+
+	if err != nil {
+		return false, err
+	}
+
+	return res, nil
 }
 
 func Identity(s *Authz, c echo.Context) (bool, error) {
