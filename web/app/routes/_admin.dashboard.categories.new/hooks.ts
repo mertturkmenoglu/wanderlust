ab6faec02b6dd82ddfb4bb2f80c67897f20dda3d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-27 01:52:36 +0300

    implement new category page

--- web/app/routes/_admin.dashboard.categories.new/hooks.ts
+++ web/app/routes/_admin.dashboard.categories.new/hooks.ts
@@ -1,9 +1,29 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "@remix-run/react";
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { createCategory } from "~/lib/api";
 import { FormInput, schema } from "./schema";
 
 export function useNewCategoryForm() {
   return useForm<FormInput>({
     resolver: zodResolver(schema),
+  });
+}
+
+export function useNewCategoryMutation() {
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationKey: ["category-create"],
+    mutationFn: (data: FormInput) => createCategory(data),
+    onSuccess: async () => {
+      toast.success("Category created");
+      navigate("/dashboard/categories");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
   });
 }
--- web/app/routes/_admin.dashboard.categories.new/route.tsx
+++ web/app/routes/_admin.dashboard.categories.new/route.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import { SubmitHandler } from "react-hook-form";
 import InputError from "~/components/kit/input-error";
 import InputInfo from "~/components/kit/input-info";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
-import { useNewCategoryForm } from "./hooks";
+import { useNewCategoryForm, useNewCategoryMutation } from "./hooks";
+import { FormInput } from "./schema";
 
 export default function Page() {
   const [previewUrl, setPreviewUrl] = useState("");
   const form = useNewCategoryForm();
+  const mutation = useNewCategoryMutation();
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    mutation.mutate(data);
+  };
 
   return (
     <div>
@@ -26,7 +33,10 @@ export default function Page() {
 
       <form
         className="container mx-0 mt-8 grid grid-cols-1 gap-4 px-0 md:grid-cols-2"
-        onSubmit={form.handleSubmit(console.log)}
+        onSubmit={form.handleSubmit(onSubmit, (errors) => {
+          console.log(errors);
+          console.log(form.getValues().id);
+        })}
       >
         <div className="">
           <Label htmlFor="id">ID</Label>
@@ -35,7 +45,7 @@ export default function Page() {
             id="id"
             placeholder="ID of the category"
             autoComplete="off"
-            {...form.register("id")}
+            {...form.register("id", { valueAsNumber: true })}
           />
           <InputInfo text="ID of the category" />
           <InputError error={form.formState.errors.id} />
