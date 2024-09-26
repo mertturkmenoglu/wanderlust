f79b253fcb1da15f01f0d71f26976d3bd207f582
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-26 10:39:44 +0300

    implement admin dashboard amenity details page

--- web/app/routes/_admin.dashboard.amenities.$id/route.tsx
+++ web/app/routes/_admin.dashboard.amenities.$id/route.tsx
@@ -1,7 +1,43 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import BackLink from "~/components/blocks/back-link";
+import { Button } from "~/components/ui/button";
+import { getAmenities } from "~/lib/api";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.id, "id is required");
+  const amenities = await getAmenities();
+  const id = +params.id;
+  const amenity = amenities.data.amenities.find((amenity) => amenity.id === id);
+
+  if (!amenity) {
+    throw new Response("Amenity not found", { status: 404 });
+  }
+
+  return json({ amenity });
+}
+
 export default function Page() {
+  const { amenity } = useLoaderData<typeof loader>();
+
   return (
     <div>
-      <div>This is the admin dashboard amenity details page</div>
+      <BackLink href="/dashboard/amenities" text="Go back to amenities page" />
+      <div className="flex items-end gap-4">
+        <h2 className="text-4xl font-bold mt-8">{amenity.name}</h2>
+        <Button variant="link" className="px-0" asChild>
+          <Link to={`/dashboard/amenities/${amenity.id}/edit`}>Edit</Link>
+        </Button>
+      </div>
+      <div className="flex gap-2 mt-4">
+        <div className="font-semibold">ID:</div>
+        <div>{amenity.id}</div>
+      </div>
+      <div className="flex gap-2 mt-2">
+        <div className="font-semibold">Name:</div>
+        <div>{amenity.name}</div>
+      </div>
     </div>
   );
 }
