700078aa86b884c92016af1b16542c202cd3fad3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-27 02:28:32 +0300

    implement new city page

---
+++ web/app/routes/_admin.dashboard.cities.new/hooks.ts
@@ -0,0 +1,31 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "@remix-run/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { createCity } from "~/lib/api";
+import { FormInput, schema } from "./schema";
+
+export function useNewCityForm() {
+  return useForm<FormInput>({
+    resolver: zodResolver(schema),
+  });
+}
+
+export function useNewCityMutation() {
+  const qc = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationKey: ["city-create"],
+    mutationFn: async (data: FormInput) => createCity(data),
+    onSuccess: async () => {
+      qc.invalidateQueries({ queryKey: ["cities"] });
+      navigate(`/dashboard/cities`);
+      toast.success("City created");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+}
