d5eb600e0ca243181bac7fb85dddd2c97321ac81
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-27 12:14:35 +0300

    update schema rules

--- web/app/routes/settings.profile/schema.ts
+++ web/app/routes/settings.profile/schema.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const schema = z.object({
   fullName: z.string().min(1).max(128),
   gender: z.string().min(1).max(32).optional(),
-  bio: z.string().min(1).max(160).optional(),
+  bio: z.string().min(1).max(255).optional(),
   pronouns: z.string().min(1).max(32).optional(),
-  website: z.string().min(1).max(256).url().optional(),
+  website: z.string().min(1).max(255).url().optional(),
   phone: z.string().min(1).max(32).optional(),
 });
 
