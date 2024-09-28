714bbbd476ec6c3cd888a0da1515cdf03a6f561e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-28 17:21:20 +0300

    implement bookmarks page

--- web/app/routes/bookmarks/route.tsx
+++ web/app/routes/bookmarks/route.tsx
@@ -1,6 +1,11 @@
 import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
-import { useQuery } from "@tanstack/react-query";
+import { Link } from "@remix-run/react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React from "react";
+import AppMessage from "~/components/blocks/app-message";
+import { Button } from "~/components/ui/button";
 import { getMe, getUserBookmarks } from "~/lib/api";
+import BookmarkCard from "./components/bookmark-card";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   try {
@@ -22,15 +27,58 @@ export function meta() {
 }
 
 export default function Page() {
-  const query = useQuery({
+  const query = useInfiniteQuery({
     queryKey: ["bookmarks"],
-    queryFn: async () => getUserBookmarks(1, 10),
+    queryFn: ({ pageParam }) => getUserBookmarks(pageParam, 25),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : null,
   });
 
   return (
     <div className="container mx-auto my-16">
-      <div>This is the bookmarks page</div>
-      <pre>{JSON.stringify(query.data ?? {}, null, 2)}</pre>
+      <h2 className="text-2xl font-bold capitalize tracking-tight">
+        Your Bookmarks
+      </h2>
+      <div className="my-8">
+        {query.isLoading && (
+          <AppMessage emptyMessage="Loading..." showBackButton={false} />
+        )}
+        {query.data && query.data.pages[0].data.bookmarks.length === 0 && (
+          <AppMessage emptyMessage="You have no bookmarks yet" />
+        )}
+        {query.data && (
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+            {query.data.pages.map((page, i) => (
+              <React.Fragment key={i}>
+                {page.data.bookmarks.map((bookmark) => (
+                  <Link
+                    to={`/p/${bookmark.poiId}`}
+                    key={bookmark.poiId}
+                    className="block"
+                  >
+                    <BookmarkCard bookmark={bookmark} />
+                  </Link>
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        )}
+        {query.hasNextPage && (
+          <div className="mt-4 flex justify-center">
+            <Button
+              onClick={() => query.fetchNextPage()}
+              disabled={!query.hasNextPage || query.isFetchingNextPage}
+            >
+              {query.isFetchingNextPage
+                ? "Loading more..."
+                : query.hasNextPage
+                ? "Load More"
+                : "Nothing more to load"}
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
