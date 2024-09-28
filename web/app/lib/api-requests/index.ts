dcd8bfd963e55f528e8b301bdaaccdf8cb378deb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-28 17:06:11 +0300

    add bookmarks export to api requests index file

--- web/app/lib/api-requests/index.ts
+++ web/app/lib/api-requests/index.ts
@@ -1,6 +1,7 @@
 export * from "./admin";
 export * from "./amenities";
 export * from "./auth";
+export * from "./bookmarks";
 export * from "./categories";
 export * from "./cities";
 export * from "./pois";
