fd48ede58a749666353a9475b174deca928deb8a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-28 17:21:06 +0300

    add bookmark card component

---
+++ web/app/routes/bookmarks/components/bookmark-card.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { GetBookmarkByIdResponseDto } from "~/lib/dto";
+
+type Props = {
+  bookmark: GetBookmarkByIdResponseDto;
+};
+
+export default function BookmarkCard({ bookmark: { poi } }: Props) {
+  return (
+    <Card className="group flex flex-col md:flex-row">
+      <img
+        src={poi.media.url}
+        alt={poi.media.alt}
+        className="aspect-video w-full rounded-t-md object-cover md:w-32 md:rounded-none md:rounded-l-md lg:w-32 2xl:w-64"
+      />
+
+      <div>
+        <CardHeader>
+          <CardTitle className="line-clamp-1 capitalize">{poi.name}</CardTitle>
+          <CardDescription className="line-clamp-1">
+            {poi.address.city.name} / {poi.address.city.countryName}
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p className="line-clamp-1 text-sm leading-none">
+            {poi.category.name}
+          </p>
+        </CardContent>
+      </div>
+    </Card>
+  );
+}
