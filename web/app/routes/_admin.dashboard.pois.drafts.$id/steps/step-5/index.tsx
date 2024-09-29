850a873fe05c2bbb6c07de66b75673ddeaa7631e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-30 01:59:32 +0300

    add dummy step 5

---
+++ web/app/routes/_admin.dashboard.pois.drafts.$id/steps/step-5/index.tsx
@@ -0,0 +1,52 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import Dnd from "~/components/blocks/file-upload-dnd";
+import { Button, buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+import { loader } from "../../route";
+import { useUpload } from "./hooks";
+
+export default function Step5() {
+  const { draft } = useLoaderData<typeof loader>();
+  const [fileError, setFileError] = useState<string[]>([]);
+  const [collapseApi, fileApi] = useUpload();
+
+  return (
+    <div>
+      <h3 className="mt-8 text-lg font-bold tracking-tight">Upload Media</h3>
+
+      <div className="container mx-0 mt-8 grid grid-cols-1 gap-4 px-0 md:grid-cols-2">
+        <div className="col-span-2">
+          <Dnd capi={collapseApi} fapi={fileApi} />
+
+          {fileError.length > 0 && (
+            <div>
+              {fileError.map((error, i) => (
+                <div key={i} className="text-destructive">
+                  {error}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div>
+          <Link
+            to={`/dashboard/pois/drafts/${draft.id}?step=4`}
+            className={cn(
+              "block w-full",
+              buttonVariants({ variant: "default" })
+            )}
+          >
+            Previous
+          </Link>
+        </div>
+
+        <div>
+          <Button type="button" className="block w-full">
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
