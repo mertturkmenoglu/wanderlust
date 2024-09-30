0a30d775be8fb4bf2ae68cd2fba9b700756f32c1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-01 00:25:57 +0300

    add auth legal text component

---
+++ web/app/components/blocks/auth/legal-text.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@remix-run/react";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+type Props = {
+  type: "signin" | "signup";
+};
+
+export default function AuthLegalText({ type }: Props) {
+  return (
+    <div className="text-xs text-muted-foreground">
+      {type === "signin"
+        ? "By signing in, you agree to our"
+        : "By signing up, you agree to our"}{" "}
+      <Link
+        to="/terms"
+        className={cn(buttonVariants({ variant: "link", size: "sm" }), "px-0")}
+      >
+        Terms of Service
+      </Link>{" "}
+      and{" "}
+      <Link
+        to="/privacy"
+        className={cn(buttonVariants({ variant: "link", size: "sm" }), "px-0")}
+      >
+        Privacy Policy
+      </Link>
+      {"."}
+    </div>
+  );
+}
