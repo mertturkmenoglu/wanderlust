c8603c7abddefb85403be1e7f71f533f9b141603
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-01 16:08:11 +0300

    add amenities component

---
+++ web/app/routes/p.$id/components/amenities.tsx
@@ -0,0 +1,23 @@
+import { CheckIcon } from "lucide-react";
+
+type Props = {
+  amenities: Array<{ id: number; name: string }>;
+};
+
+export default function Amenities({ amenities }: Props) {
+  return (
+    <div className="mt-8">
+      <h3 className="text-lg font-bold tracking-tight">Amenities</h3>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
+        {amenities.map((amenity) => (
+          <div key={amenity.id} className="flex items-center gap-2">
+            <CheckIcon className="size-4 text-primary" />
+            <span className="text-muted-foreground text-sm">
+              {amenity.name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
