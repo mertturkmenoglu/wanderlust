a3ebd8f910446902b746f243db8748332068e4f6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-01 11:54:53 +0300

    update http error handler

--- api/internal/app/api/error_handler.go
+++ api/internal/app/api/error_handler.go
@@ -1,12 +1,22 @@
 package api
 
 import (
+	"wanderlust/config"
+	"wanderlust/internal/logs"
+
 	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
 )
+
+var logger = logs.NewPTermLogger()
 
 func CustomHTTPErrorHandler(err error, c echo.Context) {
 	if c.Response().Committed {
 		return
+	}
+
+	if viper.GetString(config.ENV) == "dev" {
+		logger.Error("An error happened:", logger.Args("method", c.Request().Method, "path", c.Path(), "error", err.Error()))
 	}
 
 	_, isEchoErr := err.(*echo.HTTPError)
