8cfcb16bada49b1addc84687cd6f263bf458a589
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-01 16:07:52 +0300

    add formatted rating component

---
+++ web/app/components/kit/formatted-rating.tsx
@@ -0,0 +1,39 @@
+import { useId } from "react";
+import { Rating } from "./rating";
+
+type Props = {
+  rating: number;
+  votes: number;
+  starsClassName?: string;
+};
+
+export default function FormattedRating({
+  rating,
+  votes,
+  starsClassName,
+}: Props) {
+  const id = useId();
+  const fmt = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    compactDisplay: "short",
+    notation: "compact",
+  });
+
+  const formattedRating = fmt.format(votes);
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Rating
+        id={id}
+        onChange={() => {}}
+        defaultValue={rating}
+        disabled={true}
+        starsClassName={starsClassName}
+      />
+      <div className="flex items-center space-x-1">
+        <span className="font-bold">{rating.toFixed(1)}</span>
+        <span>({formattedRating})</span>
+      </div>
+    </div>
+  );
+}
