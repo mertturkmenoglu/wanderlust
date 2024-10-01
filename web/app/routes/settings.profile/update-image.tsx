b758709fb91172a5018773eb5978096497d561a4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-01 12:09:42 +0300

    merge update profile and banner components

--- web/app/routes/settings.profile/update-image.tsx
+++ web/app/routes/settings.profile/update-image.tsx
@@ -11,22 +11,18 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
-import { updateUserProfileImage } from "~/lib/api-requests";
+import { updateBannerImage, updateUserProfileImage } from "~/lib/api";
 import { cn } from "~/lib/utils";
 
 type Props = {
   image: string | null;
+  fallbackImage: string;
   fullName: string;
+  action: "profile" | "banner";
 };
 
-export default function UpdateProfileImage({
-  image = "/profile.png",
-  fullName,
-}: Props) {
-  const [preview, setPreview] = useState(image ?? "");
-  const [file, setFile] = useState<File | null>(null);
-
-  const mutation = useMutation({
+function useProfileMutation() {
+  return useMutation({
     mutationKey: ["update-profile-image"],
     mutationFn: async (formData: FormData) => updateUserProfileImage(formData),
     onSuccess: () => {
@@ -36,6 +32,33 @@ export default function UpdateProfileImage({
       toast.error("Something went wrong");
     },
   });
+}
+
+function useBannerMutation() {
+  return useMutation({
+    mutationKey: ["update-banner-image"],
+    mutationFn: async (formData: FormData) => updateBannerImage(formData),
+    onSuccess: () => {
+      toast.success("Banner image updated");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+}
+
+export default function UpdateImage({
+  image,
+  fallbackImage,
+  fullName,
+  action,
+}: Props) {
+  const [preview, setPreview] = useState(() =>
+    image === null ? fallbackImage : image
+  );
+  const [file, setFile] = useState<File | null>(null);
+  const profile = useProfileMutation();
+  const banner = useBannerMutation();
 
   return (
     <div className="mt-4 max-w-xl flex gap-4">
@@ -45,7 +68,10 @@ export default function UpdateProfileImage({
             <img
               src={preview}
               alt={fullName}
-              className="mt-4 w-32 rounded-md aspect-square object-cover"
+              className={cn("mt-4 rounded-md object-cover", {
+                "w-32 aspect-square": action === "profile",
+                "w-64 aspect-video": action === "banner",
+              })}
             />
             <div
               className={cn(
@@ -60,19 +86,24 @@ export default function UpdateProfileImage({
         </DialogTrigger>
         <DialogContent className="sm:max-w-xl">
           <DialogHeader>
-            <DialogTitle>Select a new profile image</DialogTitle>
+            <DialogTitle>
+              Select a new {action === "profile" ? "profile" : "banner"} image
+            </DialogTitle>
           </DialogHeader>
           <div className="flex flex-col items-center justify-center space-x-2 text-sm gap-8">
             <div className="flex flex-col items-center gap-4">
               <img
                 src={preview}
                 alt={fullName}
-                className="mt-4 w-48 rounded-md aspect-square object-cover"
+                className={cn("mt-4 rounded-md object-cover", {
+                  "w-48 aspect-square": action === "profile",
+                  "w-80 aspect-video": action === "banner",
+                })}
               />
             </div>
             <div className="flex flex-col">
               <label
-                htmlFor="profile-image"
+                htmlFor={`${action}-image`}
                 className={cn(
                   "mr-4 py-2 px-4 rounded-md border-0 text-sm font-semibold bg-primary/10",
                   "text-primary hover:bg-primary/20 cursor-pointer",
@@ -80,17 +111,17 @@ export default function UpdateProfileImage({
                 )}
               >
                 <UploadIcon className="size-4 text-primary" />
-                {!file ? "Upload a profile image" : "Change selection"}
+                {!file ? `Upload a ${action} image` : "Change selection"}
               </label>
               <div className="text-xs text-muted-foreground mt-2">
                 PNG, JPEG, GIF, and WebP are supported. Maximum 5MB.
               </div>
               <input
-                id="profile-image"
+                id={`${action}-image`}
                 type="file"
                 name="files"
                 accept="image/jpeg,image/png,image/jpg,image/webp,image/gif"
-                placeholder="Upload a profile image"
+                placeholder={`Upload a ${action} image`}
                 className="hidden"
                 onChange={(e) => {
                   const file = e.target.files?.[0];
@@ -114,7 +145,12 @@ export default function UpdateProfileImage({
                 const form = new FormData();
 
                 form.append("files", file);
-                mutation.mutate(form);
+
+                if (action === "profile") {
+                  profile.mutate(form);
+                } else {
+                  banner.mutate(form);
+                }
               }}
               disabled={!file}
             >
