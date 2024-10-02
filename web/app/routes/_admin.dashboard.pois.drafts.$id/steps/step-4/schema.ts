2ee3d3e0ec489cef7ceced1822f5852e30dabbd4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-02 19:46:45 +0300

    update step 4 schema

--- web/app/routes/_admin.dashboard.pois.drafts.$id/steps/step-4/schema.ts
+++ web/app/routes/_admin.dashboard.pois.drafts.$id/steps/step-4/schema.ts
@@ -1,43 +1,13 @@
 import { z } from "zod";
 
 export const schema = z.object({
-  openTimes: z.object({
-    mon: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-    tue: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-    wed: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-    thu: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-    fri: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-    sat: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-    sun: z.object({
-      open: z.string().optional(),
-      close: z.string().optional(),
-      closed: z.boolean().optional(),
-    }),
-  }),
+  openHours: z.array(
+    z.object({
+      day: z.string(),
+      opensAt: z.string(),
+      closesAt: z.string(),
+    })
+  ),
 });
 
 export type FormInput = z.infer<typeof schema>;
