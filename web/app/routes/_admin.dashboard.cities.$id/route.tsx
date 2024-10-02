f2312aa9fcff694bb961bbd42c71a67cd976e8c9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-02 12:28:44 +0300

    update admin dashboard city details layout

--- web/app/routes/_admin.dashboard.cities.$id/route.tsx
+++ web/app/routes/_admin.dashboard.cities.$id/route.tsx
@@ -3,6 +3,7 @@ import { Link, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import BackLink from "~/components/blocks/back-link";
 import { Button } from "~/components/ui/button";
+import { Separator } from "~/components/ui/separator";
 import { getCityById } from "~/lib/api";
 import DeleteDialog from "./delete-dialog";
 
@@ -17,22 +18,34 @@ export default function Page() {
   const { city } = useLoaderData<typeof loader>();
 
   return (
-    <div>
+    <>
       <BackLink href="/dashboard/cities" text="Go back to cities page" />
-      <div className="flex items-end gap-4">
-        <h2 className="text-4xl font-bold mt-8">{city.name}</h2>
-        <Button variant="link" className="px-0" asChild>
-          <Link to={`/dashboard/cities/${city.id}/edit`}>Edit</Link>
-        </Button>
-        <DeleteDialog id={city.id} />
-      </div>
+
       <img
         src={city.imageUrl}
         alt={city.name}
-        className="mt-8 w-64 rounded-md aspect-video object-cover"
+        className="mt-4 w-64 rounded-md aspect-video object-cover"
       />
 
-      <div className="flex gap-2 mt-4">
+      <h2 className="text-4xl font-bold mt-4">{city.name}</h2>
+
+      <div className="flex flex-row gap-2 w-min items-start mt-4">
+        <Button variant="outline" asChild>
+          <Link to={`/cities/${city.id}/${city.name}`}>Visit Page</Link>
+        </Button>
+
+        <Button variant="outline" asChild>
+          <Link to={`/dashboard/cities/${city.id}/edit`}>Edit</Link>
+        </Button>
+
+        <DeleteDialog id={city.id} />
+      </div>
+
+      <Separator className="my-4 max-w-md" />
+
+      <h3 className="mt-4 text-lg font-bold tracking-tight">Short Info</h3>
+
+      <div className="flex gap-2 mt-2">
         <div className="font-semibold">City Id:</div>
         <div>{city.id}</div>
       </div>
@@ -56,22 +69,6 @@ export default function Page() {
         <div className="font-semibold">Country Name:</div>
         <div>{city.countryName}</div>
       </div>
-      <div className="flex gap-2 mt-2">
-        <div className="font-semibold">Image URL:</div>
-        <div>{city.imageUrl}</div>
-      </div>
-      <div className="flex gap-2 mt-2">
-        <div className="font-semibold">Latitude:</div>
-        <div>{city.latitude}</div>
-      </div>
-      <div className="flex gap-2 mt-2">
-        <div className="font-semibold">Longitude:</div>
-        <div>{city.longitude}</div>
-      </div>
-      <div className="flex gap-2 mt-2 max-w-xl">
-        <div className="font-semibold">Description:</div>
-        <div className="max-w-xl">{city.description}</div>
-      </div>
-    </div>
+    </>
   );
 }
