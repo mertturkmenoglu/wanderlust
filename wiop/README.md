31454b9dd24da7fbf3397f6a591d4857bd8eedc3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-03 16:32:52 +0300

    add wanderlust image optimization proxy server

---
+++ wiop/README.md
@@ -0,0 +1,1 @@
+# Wanderlust Image Optimization Proxy Server
---
+++ wiop/index.mjs
@@ -0,0 +1,22 @@
+import { createApp, toNodeListener } from "h3";
+import {
+  createIPX,
+  createIPXH3Handler,
+  ipxFSStorage,
+  ipxHttpStorage,
+} from "ipx";
+import { listen } from "listhen";
+
+const ipx = createIPX({
+  storage: ipxFSStorage({ dir: "./public" }),
+  httpStorage: ipxHttpStorage({
+    domains: ["localhost:9000"],
+    allowAllDomains: true,
+  }),
+});
+
+const app = createApp().use("/", createIPXH3Handler(ipx));
+
+listen(toNodeListener(app), {
+  port: 3002,
+});
