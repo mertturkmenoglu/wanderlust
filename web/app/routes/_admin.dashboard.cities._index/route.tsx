5614ebb197cd25e46d538d182628ad27c2b767ab
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-04 19:41:56 +0300

    add new city link

--- web/app/routes/_admin.dashboard.cities._index/route.tsx
+++ web/app/routes/_admin.dashboard.cities._index/route.tsx
@@ -1,7 +1,8 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { citiesCols } from "~/components/blocks/dashboard/columns";
 import { DataTable } from "~/components/blocks/dashboard/data-table";
+import { Button } from "~/components/ui/button";
 import { getCities } from "~/lib/api";
 
 export async function loader() {
@@ -14,7 +15,13 @@ export default function Page() {
 
   return (
     <div>
-      <h3 className="mb-8 text-lg font-bold tracking-tight">Cities</h3>
+      <div className="flex items-baseline gap-8 mb-8">
+        <h3 className="text-lg font-bold tracking-tight">Cities</h3>
+        <Button asChild variant="link" className="px-0">
+          <Link to="/dashboard/cities/new">New City</Link>
+        </Button>
+      </div>
+
       <DataTable
         columns={citiesCols}
         data={cities.map((city) => ({
