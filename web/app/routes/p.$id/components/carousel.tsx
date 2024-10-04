a01cb7c7f900f758b24deb8f52a05c512f2b158f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-04 19:42:40 +0300

    show media caption

--- web/app/routes/p.$id/components/carousel.tsx
+++ web/app/routes/p.$id/components/carousel.tsx
@@ -4,8 +4,8 @@ import {
   CarouselNext,
   CarouselPrevious,
   Carousel as ShadcnCarousel,
-} from '~/components/ui/carousel';
-import { Media } from '~/lib/dto';
+} from "~/components/ui/carousel";
+import { Media } from "~/lib/dto";
 
 type Props = {
   media: Media[];
@@ -16,16 +16,18 @@ export default function Carousel({ media }: Props) {
     <ShadcnCarousel className="mx-auto flex h-min w-11/12 justify-center lg:w-full">
       <CarouselContent>
         {media.map((media, index) => (
-          <CarouselItem
-            key={index}
-            className="mx-auto flex justify-center"
-          >
-            <div className="flex h-[512px] w-11/12 items-center justify-center overflow-hidden rounded-lg lg:w-full">
+          <CarouselItem key={index} className="mx-auto flex justify-center">
+            <div className="flex h-[512px] flex-col items-center justify-center overflow-hidden rounded-lg lg:w-full">
               <img
-                className="rounded-lg object-cover"
+                className="rounded-lg object-cover h-[96%]"
                 src={media.url}
                 alt={media.alt}
               />
+              {media.caption !== null && (
+                <div className="text-center text-xs text-muted-foreground mt-2 tracking-tighter">
+                  {media.caption}
+                </div>
+              )}
             </div>
           </CarouselItem>
         ))}
