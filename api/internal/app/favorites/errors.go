a0b7a6fb9e2872bca44643795677898f749d44ab
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-05 02:20:10 +0300

    add username required error

--- api/internal/app/favorites/errors.go
+++ api/internal/app/favorites/errors.go
@@ -1,1 +1,11 @@
 package favorites
+
+import (
+	"errors"
+	"net/http"
+	"wanderlust/internal/app/api"
+)
+
+var (
+	ErrUsernameRequired = api.NewApiError(http.StatusBadRequest, "0500", errors.New("username is required"))
+)
