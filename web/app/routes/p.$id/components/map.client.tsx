9c3d7646ddbd15a630bf8663db2b5a9c28373185
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-04 19:42:50 +0300

    add top margin

--- web/app/routes/p.$id/components/map.client.tsx
+++ web/app/routes/p.$id/components/map.client.tsx
@@ -12,7 +12,7 @@ export function Map({ lat, lng }: Props) {
   const url = `https://mt0.google.com/vt/scale=${window.devicePixelRatio}&hl=en&x={x}&y={y}&z={z}`;
 
   return (
-    <div>
+    <div className="mt-8">
       <h3 className="text-2xl font-bold">Location</h3>
       <MapContainer
         center={[lat, lng]}
