6e5d3f009d25d69d6bdd8764061d4737e24b70dc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-05 06:28:27 +0300

    change grid cols

--- web/app/routes/poi-grid.tsx
+++ web/app/routes/poi-grid.tsx
@@ -31,7 +31,7 @@ export default function PoiGrid({ dataKey: key, data }: Props) {
         {title}
       </h2>
 
-      <div className="my-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-5">
+      <div className="my-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {isEmpty && <div>No data available.</div>}
         {!isEmpty &&
           data.slice(0, 6).map((poi) => (
