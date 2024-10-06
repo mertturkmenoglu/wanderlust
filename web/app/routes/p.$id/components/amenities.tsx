416d51977c46b9e0c55b2ad92a74b1e3de53d683
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-06 17:02:11 +0300

    handle empty amenities state

--- web/app/routes/p.$id/components/amenities.tsx
+++ web/app/routes/p.$id/components/amenities.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Amenities({ amenities }: Props) {
   return (
     <div className="mt-8">
-      <h3 className="text-lg font-bold tracking-tight">Amenities</h3>
+      <h3 className="text-2xl font-bold tracking-tight">Amenities</h3>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
         {amenities.map((amenity) => (
           <div key={amenity.id} className="flex items-center gap-2">
@@ -17,6 +17,13 @@ export default function Amenities({ amenities }: Props) {
             </span>
           </div>
         ))}
+        {amenities.length === 0 && (
+          <div className="col-span-full">
+            <div className="text-muted-foreground text-sm">
+              There are no amenities available for this location.
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
