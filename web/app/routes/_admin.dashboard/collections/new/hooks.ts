aa28eb461364edb7badba9ace376d6f90f71a625
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-07 12:39:37 +0300

    implement new collection page

---
+++ web/app/routes/_admin.dashboard/collections/new/hooks.ts
@@ -0,0 +1,31 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "@remix-run/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { createCollection } from "~/lib/api";
+import { FormInput, schema } from "./schema";
+
+export function useNewCollectionForm() {
+  return useForm<FormInput>({
+    resolver: zodResolver(schema),
+  });
+}
+
+export function useNewCollectionMutation() {
+  const qc = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationKey: ["collection-create"],
+    mutationFn: async (data: FormInput) => createCollection(data),
+    onSuccess: async () => {
+      qc.invalidateQueries({ queryKey: ["collections"] });
+      navigate(`/dashboard/collections`);
+      toast.success("Collection created");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+}
--- web/app/routes/_admin.dashboard/collections/new/route.tsx
+++ web/app/routes/_admin.dashboard/collections/new/route.tsx
@@ -1,3 +1,68 @@
+import { SubmitHandler } from "react-hook-form";
+import BackLink from "~/components/blocks/back-link";
+import InputError from "~/components/kit/input-error";
+import InputInfo from "~/components/kit/input-info";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
+import { useNewCollectionForm, useNewCollectionMutation } from "./hooks";
+import { FormInput } from "./schema";
+
 export default function Page() {
-  return <div>New collection page</div>;
+  const form = useNewCollectionForm();
+  const mutation = useNewCollectionMutation();
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    mutation.mutate(data);
+  };
+
+  return (
+    <div>
+      <BackLink
+        href="/dashboard/collections"
+        text="Go back to collections page"
+      />
+      <h3 className="mb-8 text-lg font-bold tracking-tight">
+        Create New Collection
+      </h3>
+
+      <form
+        className="container mx-0 mt-8 grid grid-cols-1 gap-4 px-0 md:grid-cols-2"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="">
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            id="name"
+            placeholder="Name"
+            autoComplete="off"
+            {...form.register("name")}
+          />
+          <InputInfo text={(form.watch("name")?.length ?? 0) + "/128"} />
+          <InputError error={form.formState.errors.name} />
+        </div>
+
+        <div className="col-span-2">
+          <Label htmlFor="desc">Description</Label>
+          <Textarea
+            id="desc"
+            placeholder="Description of the collection"
+            autoComplete="off"
+            rows={6}
+            {...form.register("description")}
+          />
+          <InputInfo
+            text={(form.watch("description")?.length ?? 0) + "/1024"}
+          />
+          <InputError error={form.formState.errors.description} />
+        </div>
+
+        <div>
+          <Button type="submit">Create</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
