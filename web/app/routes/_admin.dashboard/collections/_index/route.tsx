364da601bed7bf256226ea1457b4602bd2829482
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-07 12:31:58 +0300

    implement collections page

--- web/app/routes/_admin.dashboard/collections/_index/route.tsx
+++ web/app/routes/_admin.dashboard/collections/_index/route.tsx
@@ -1,3 +1,42 @@
+import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { collectionsCols } from "~/components/blocks/dashboard/columns";
+import { DataTable } from "~/components/blocks/dashboard/data-table";
+import { Button } from "~/components/ui/button";
+import { getCollections } from "~/lib/api-requests";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  try {
+    const Cookie = request.headers.get("Cookie") ?? "";
+    const collections = await getCollections(1, 25, { headers: { Cookie } });
+    return json({ collections: collections.data.collections });
+  } catch (e) {
+    throw redirect("/");
+  }
+}
+
 export default function Page() {
-  return <div>Collections page</div>;
+  const { collections } = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <div className="flex items-baseline gap-8 mb-8">
+        <h3 className="text-lg font-bold tracking-tight">Collections</h3>
+        <Button asChild variant="link" className="px-0">
+          <Link to="/dashboard/collections/new">New Collection</Link>
+        </Button>
+      </div>
+
+      <DataTable
+        columns={collectionsCols}
+        data={collections.map((collection) => ({
+          id: collection.id,
+          name: collection.name,
+          description: collection.description,
+          createdAt: collection.createdAt,
+        }))}
+        hrefPrefix="/dashboard/collections"
+      />
+    </div>
+  );
 }
