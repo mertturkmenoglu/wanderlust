c3f417f7cc0b61401a0c30eee8fd6f448d3b40f6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-07 10:55:46 +0300

    define collections module

---
+++ api/internal/app/collections/module.go
@@ -0,0 +1,46 @@
+package collections
+
+import (
+	"wanderlust/internal/app/api"
+	"wanderlust/internal/db"
+
+	"github.com/sony/sonyflake"
+)
+
+type Module struct {
+	handlers *handlers
+}
+
+var _ api.IModule = (*Module)(nil)
+
+type handlers struct {
+	service *service
+}
+
+type service struct {
+	repository *repository
+}
+
+type repository struct {
+	db    *db.Db
+	flake *sonyflake.Sonyflake
+}
+
+func New(db *db.Db, flake *sonyflake.Sonyflake) *Module {
+	repository := repository{
+		db:    db,
+		flake: flake,
+	}
+
+	service := service{
+		repository: &repository,
+	}
+
+	handlers := &handlers{
+		service: &service,
+	}
+
+	return &Module{
+		handlers: handlers,
+	}
+}
