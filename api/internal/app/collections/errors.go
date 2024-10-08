08873b3e3fea2d09495ab4d741074ccb81bc6042
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-08 22:27:11 +0300

    makes sense

--- api/internal/app/collections/errors.go
+++ api/internal/app/collections/errors.go
@@ -10,4 +10,5 @@ var (
 	ErrIdRequired          = api.NewApiError(http.StatusBadRequest, "0550", errors.New("id is required"))
 	ErrCollectionNotFound  = api.NewApiError(http.StatusNotFound, "0551", errors.New("collection not found"))
 	ErrCollectionIndexCast = api.NewApiError(http.StatusInternalServerError, "0552", errors.New("cannot cast index value"))
+	ErrInvalidOrder        = api.NewApiError(http.StatusBadRequest, "0553", errors.New("invalid order"))
 )
