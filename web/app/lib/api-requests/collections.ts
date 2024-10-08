e4b497c8bf40e9f0cf0a9cf0d2105c18126ef44a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-08 16:09:46 +0300

    add api requests

--- web/app/lib/api-requests/collections.ts
+++ web/app/lib/api-requests/collections.ts
@@ -1,9 +1,13 @@
 import { Options } from "ky";
 import api from "../api";
 import {
+  CreateCollectionItemRequestDto,
+  CreateCollectionItemResponseDto,
   CreateCollectionRequestDto,
   GetCollectionByIdResponseDto,
+  GetCollectionItemsResponseDto,
   GetCollectionsResponseDto,
+  UpdateCollectionItemsRequestDto,
   UpdateCollectionRequestDto,
 } from "../dto";
 
@@ -37,6 +41,36 @@ export function deleteCollection(id: string) {
 
 export function updateCollection(id: string, dto: UpdateCollectionRequestDto) {
   return api.patch(`collections/${id}`, {
+    json: dto,
+  });
+}
+
+export function getCollectionItems(collectionId: string, options?: Options) {
+  return api.get(`collections/${collectionId}/items`, options).json<{
+    data: GetCollectionItemsResponseDto;
+  }>();
+}
+
+export function createCollectionItem(
+  collectionId: string,
+  dto: CreateCollectionItemRequestDto
+) {
+  return api
+    .post(`collections/${collectionId}/items`, {
+      json: dto,
+    })
+    .json<{ data: CreateCollectionItemResponseDto }>();
+}
+
+export function deleteCollectionItem(collectionId: string, poiId: string) {
+  return api.delete(`collections/${collectionId}/items/${poiId}`);
+}
+
+export function updateCollectionItems(
+  collectionId: string,
+  dto: UpdateCollectionItemsRequestDto
+) {
+  return api.patch(`collections/${collectionId}/items`, {
     json: dto,
   });
 }
