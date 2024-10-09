4a36264ab5db40fa51c3eaadf49eb1644fd75204
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-09 20:01:55 +0300

    create handlers for each route (wip)

---
+++ api/internal/app/lists/handlers.go
@@ -0,0 +1,39 @@
+package lists
+
+import "github.com/labstack/echo/v4"
+
+func (h *handlers) createList(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) getAllListOfUser(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) getListById(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) getPublicListsOfUser(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) updateList(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) deleteList(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) createListItem(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) deleteListItem(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) updateListItems(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
--- api/internal/app/lists/routes.go
+++ api/internal/app/lists/routes.go
@@ -2,22 +2,18 @@ package lists
 
 import "github.com/labstack/echo/v4"
 
-func (h *handlers) DummyHandler(c echo.Context) error {
-	return echo.ErrNotImplemented
-}
-
 func (m *Module) RegisterRoutes(e *echo.Group) {
 	routes := e.Group("/lists")
 	{
-		routes.GET("/", m.handlers.DummyHandler) // Get all lists of current user
-		routes.GET("/:id", m.handlers.DummyHandler) // Get list by id
-		routes.GET("/user/:username", m.handlers.DummyHandler) // Get public lists of user
-		routes.POST("/", m.handlers.DummyHandler) // Create list
-		routes.PATCH("/:id", m.handlers.DummyHandler) // Update list
-		routes.DELETE("/:id", m.handlers.DummyHandler) // Delete list
+		routes.GET("/", m.handlers.getAllListOfUser)
+		routes.GET("/:id", m.handlers.getListById)
+		routes.GET("/user/:username", m.handlers.getPublicListsOfUser)
+		routes.POST("/", m.handlers.createList)
+		routes.PATCH("/:id", m.handlers.updateList)
+		routes.DELETE("/:id", m.handlers.deleteList)
 
-		routes.POST("/:id/items", m.handlers.DummyHandler) // Create list item
-		routes.DELETE("/:id/items/:poiId", m.handlers.DummyHandler) // Delete list item
-		routes.PATCH("/:id/items", m.handlers.DummyHandler) // Update list items
+		routes.POST("/:id/items", m.handlers.createListItem)
+		routes.DELETE("/:id/items/:poiId", m.handlers.deleteListItem)
+		routes.PATCH("/:id/items", m.handlers.updateListItems)
 	}
 }
