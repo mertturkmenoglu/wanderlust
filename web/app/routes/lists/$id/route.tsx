ec9b222c4ffd1332ba1a83170afeff8e71008d1e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-12 21:55:18 +0300

    implement dropdown and delete list functionality

--- web/app/routes/lists/$id/route.tsx
+++ web/app/routes/lists/$id/route.tsx
@@ -1,14 +1,44 @@
 import { json, LoaderFunctionArgs, MetaArgs } from "@remix-run/node";
 import {
   isRouteErrorResponse,
+  Link,
   useLoaderData,
+  useNavigate,
   useRouteError,
 } from "@remix-run/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  EllipsisVerticalIcon,
+  FlagIcon,
+  PencilIcon,
+  TrashIcon,
+} from "lucide-react";
+import { useContext } from "react";
+import { toast } from "sonner";
 import invariant from "tiny-invariant";
 import AppMessage from "~/components/blocks/app-message";
 import BackLink from "~/components/blocks/back-link";
-import { getListById } from "~/lib/api-requests";
+import { Button, buttonVariants } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { Separator } from "~/components/ui/separator";
+import { deleteListById, getListById } from "~/lib/api-requests";
 import { getCookiesFromRequest } from "~/lib/cookies";
+import { AuthContext } from "~/providers/auth-provider";
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
   invariant(params.id, "id is required");
@@ -45,10 +75,106 @@ export function meta({ data, error }: MetaArgs<typeof loader>) {
 
 export default function Page() {
   const { list } = useLoaderData<typeof loader>();
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const qc = useQueryClient();
+  const isOwner = auth.user?.data?.id === list.userId;
+
+  const deleteMutation = useMutation({
+    mutationKey: ["delete-list", list.id],
+    mutationFn: () => deleteListById(list.id),
+    onSuccess: () => {
+      toast.success("List is deleted.");
+      qc.invalidateQueries({ queryKey: ["lists"] });
+      navigate("/lists");
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
 
   return (
     <div className="container mx-auto my-8">
       <BackLink href="/lists" text="Go back to lists" />
+      <div className="flex justify-between items-center">
+        <div>
+          <h2 className="text-2xl font-bold tracking-tighter">{list.name}</h2>
+          <div className="text-xs text-muted-foreground mt-1">
+            {new Date(list.createdAt).toLocaleDateString()}
+          </div>
+        </div>
+        <Dialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger
+              className={buttonVariants({ variant: "ghost", size: "icon" })}
+            >
+              <EllipsisVerticalIcon className="" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>
+                <Link
+                  to={`/report?type=list&resourceId=${list.id}`}
+                  className="flex items-center gap-2 w-full"
+                >
+                  <FlagIcon className="size-3" />
+                  <div className="text-sm">Report</div>
+                </Link>
+              </DropdownMenuItem>
+
+              {isOwner && (
+                <>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link
+                      to={`/lists/${list.id}/edit`}
+                      className="flex items-center gap-2 w-full"
+                    >
+                      <PencilIcon className="size-3" />
+                      <div className="text-sm">Edit</div>
+                    </Link>
+                  </DropdownMenuItem>
+
+                  <DialogTrigger asChild>
+                    <DropdownMenuItem>
+                      <button className="flex items-center gap-2 w-full text-destructive">
+                        <TrashIcon className="size-3" />
+                        <div className="text-sm">Delete</div>
+                      </button>
+                    </DropdownMenuItem>
+                  </DialogTrigger>
+                </>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Are you sure?</DialogTitle>
+            </DialogHeader>
+            <div className="flex items-center space-x-2 text-sm">
+              Are you sure you want to delete this list? This action cannot be
+              undone and all data will be permanently deleted.
+            </div>
+            <DialogFooter className="">
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button
+                type="button"
+                variant="destructive"
+                onClick={() => deleteMutation.mutate()}
+              >
+                Delete
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+
+      <Separator className="my-2" />
+
       <div>List details page</div>
       <pre>{JSON.stringify(list, null, 2)}</pre>
     </div>
