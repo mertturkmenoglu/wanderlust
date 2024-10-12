7baaf451cf0f62f823af9b44f92f1b7a3e3ff79f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-12 18:52:54 +0300

    add list not found error

--- api/internal/app/lists/errors.go
+++ api/internal/app/lists/errors.go
@@ -7,5 +7,6 @@ import (
 )
 
 var (
-	ErrIdRequired = api.NewApiError(http.StatusBadRequest, "0600", errors.New("id is required"))
+	ErrIdRequired   = api.NewApiError(http.StatusBadRequest, "0600", errors.New("id is required"))
+	ErrListNotFound = api.NewApiError(http.StatusNotFound, "0601", errors.New("list not found"))
 )
