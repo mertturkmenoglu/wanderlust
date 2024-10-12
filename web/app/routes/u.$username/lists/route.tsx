ee5d6a170000fb422b20d42d9eae6dce9fab4f34
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-12 23:35:38 +0300

    implement users public lists page

--- web/app/routes/u.$username/lists/route.tsx
+++ web/app/routes/u.$username/lists/route.tsx
@@ -1,7 +1,73 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React from "react";
+import invariant from "tiny-invariant";
+import AppMessage from "~/components/blocks/app-message";
+import { Button } from "~/components/ui/button";
+import { getPublicListsOfUser } from "~/lib/api-requests";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.username, "username is required");
+  return json({ username: params.username });
+}
+
 export default function Page() {
+  const { username } = useLoaderData<typeof loader>();
+  const query = useInfiniteQuery({
+    queryKey: ["public-lists", username],
+    queryFn: ({ pageParam }) => getPublicListsOfUser(username, pageParam, 25),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : null,
+  });
+
   return (
     <div>
-      <div>This is the user lists page</div>
+      {query.isLoading && (
+        <AppMessage emptyMessage="Loading..." showBackButton={false} />
+      )}
+      {query.data && query.data.pages[0].data.lists.length === 0 && (
+        <AppMessage emptyMessage="You have no lists yet" />
+      )}
+      {query.data && (
+        <div className="grid grid-cols-1 gap-4">
+          {query.data.pages.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.data.lists.map((list) => (
+                <Link to={`/lists/${list.id}`} key={list.id} className="block">
+                  <div className="flex items-center gap-4">
+                    <div>
+                      <div className="text-primary hover:underline">
+                        {list.name}
+                      </div>
+                      <div className="text-sm text-muted-foreground">
+                        Created at:{" "}
+                        {new Date(list.createdAt).toLocaleDateString()}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </React.Fragment>
+          ))}
+        </div>
+      )}
+
+      {query.hasNextPage && (
+        <div className="mt-4 flex justify-center">
+          <Button
+            onClick={() => query.fetchNextPage()}
+            disabled={!query.hasNextPage || query.isFetchingNextPage}
+          >
+            {query.isFetchingNextPage
+              ? "Loading more..."
+              : query.hasNextPage
+              ? "Load More"
+              : "Nothing more to load"}
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
