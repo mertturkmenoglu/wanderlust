772cb225e60eaae985cb98f49d8f29f2e2762dda
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 06:52:13 +0300

    define steps

---
+++ web/app/routes/diary/new/steps.ts
@@ -0,0 +1,26 @@
+export const steps = [
+  {
+    step: 1,
+    content: "Title",
+  },
+  {
+    step: 2,
+    content: "Add Locations",
+  },
+  {
+    step: 3,
+    content: "Description",
+  },
+  {
+    step: 4,
+    content: "Add Media",
+  },
+  {
+    step: 5,
+    content: "Share with Friends",
+  },
+  {
+    step: 6,
+    content: "Review and Complete",
+  },
+];
