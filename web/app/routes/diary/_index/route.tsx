41bf436841cefd1084106860b1ac62faf835e381
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 06:31:32 +0300

    add header to diary index page

--- web/app/routes/diary/_index/route.tsx
+++ web/app/routes/diary/_index/route.tsx
@@ -1,3 +1,10 @@
+import { Link } from "@remix-run/react";
+import { BookMarkedIcon, PlusIcon } from "lucide-react";
+import AppMessage from "~/components/blocks/app-message";
+import { buttonVariants } from "~/components/ui/button";
+import { Separator } from "~/components/ui/separator";
+import { cn } from "~/lib/utils";
+
 export function meta() {
   return [
     {
@@ -9,8 +16,31 @@ export function meta() {
 export default function Page() {
   return (
     <div className="container mx-auto my-8">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <BookMarkedIcon className="size-8 text-primary " />
+          <h2 className="text-2xl">My Diary</h2>
+        </div>
+        <Link
+          to="/diary/new"
+          className={cn(
+            "flex items-center gap-2",
+            buttonVariants({ variant: "default" })
+          )}
+        >
+          <PlusIcon className="size-4" />
+          <div>New Entry</div>
+        </Link>
+      </div>
+
+      <Separator className="my-4" />
+
       <div>
-        <div>This is the diary page</div>
+        <AppMessage
+          emptyMessage="This user has no diary entries yet"
+          className="my-16"
+          showBackButton={false}
+        />
       </div>
     </div>
   );
