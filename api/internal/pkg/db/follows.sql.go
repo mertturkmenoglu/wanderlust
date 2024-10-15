7298ca9cf7c69981564afcb083794ac4987ceb28
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 04:10:13 +0300

    update queries

--- api/internal/pkg/db/follows.sql.go
+++ api/internal/pkg/db/follows.sql.go
@@ -29,6 +29,130 @@ func (q *Queries) Follow(ctx context.Context, arg FollowParams) error {
 	return err
 }
 
+const getUserFollowers = `-- name: GetUserFollowers :many
+SELECT
+  users.id, users.email, users.username, users.full_name, users.password_hash, users.google_id, users.fb_id, users.is_email_verified, users.is_onboarding_completed, users.is_active, users.is_business_account, users.is_verified, users.role, users.password_reset_token, users.password_reset_expires, users.login_attempts, users.lockout_until, users.bio, users.pronouns, users.website, users.phone, users.profile_image, users.banner_image, users.followers_count, users.following_count, users.last_login, users.created_at, users.updated_at
+FROM follows
+  LEFT JOIN users ON users.id = follows.follower_id
+WHERE follows.following_id = $1
+ORDER BY follows.created_at DESC
+`
+
+type GetUserFollowersRow struct {
+	User User
+}
+
+func (q *Queries) GetUserFollowers(ctx context.Context, followingID string) ([]GetUserFollowersRow, error) {
+	rows, err := q.db.Query(ctx, getUserFollowers, followingID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetUserFollowersRow
+	for rows.Next() {
+		var i GetUserFollowersRow
+		if err := rows.Scan(
+			&i.User.ID,
+			&i.User.Email,
+			&i.User.Username,
+			&i.User.FullName,
+			&i.User.PasswordHash,
+			&i.User.GoogleID,
+			&i.User.FbID,
+			&i.User.IsEmailVerified,
+			&i.User.IsOnboardingCompleted,
+			&i.User.IsActive,
+			&i.User.IsBusinessAccount,
+			&i.User.IsVerified,
+			&i.User.Role,
+			&i.User.PasswordResetToken,
+			&i.User.PasswordResetExpires,
+			&i.User.LoginAttempts,
+			&i.User.LockoutUntil,
+			&i.User.Bio,
+			&i.User.Pronouns,
+			&i.User.Website,
+			&i.User.Phone,
+			&i.User.ProfileImage,
+			&i.User.BannerImage,
+			&i.User.FollowersCount,
+			&i.User.FollowingCount,
+			&i.User.LastLogin,
+			&i.User.CreatedAt,
+			&i.User.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getUserFollowing = `-- name: GetUserFollowing :many
+SELECT
+  users.id, users.email, users.username, users.full_name, users.password_hash, users.google_id, users.fb_id, users.is_email_verified, users.is_onboarding_completed, users.is_active, users.is_business_account, users.is_verified, users.role, users.password_reset_token, users.password_reset_expires, users.login_attempts, users.lockout_until, users.bio, users.pronouns, users.website, users.phone, users.profile_image, users.banner_image, users.followers_count, users.following_count, users.last_login, users.created_at, users.updated_at
+FROM follows
+  LEFT JOIN users ON users.id = follows.following_id
+WHERE follows.follower_id = $1
+ORDER BY follows.created_at DESC
+`
+
+type GetUserFollowingRow struct {
+	User User
+}
+
+func (q *Queries) GetUserFollowing(ctx context.Context, followerID string) ([]GetUserFollowingRow, error) {
+	rows, err := q.db.Query(ctx, getUserFollowing, followerID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetUserFollowingRow
+	for rows.Next() {
+		var i GetUserFollowingRow
+		if err := rows.Scan(
+			&i.User.ID,
+			&i.User.Email,
+			&i.User.Username,
+			&i.User.FullName,
+			&i.User.PasswordHash,
+			&i.User.GoogleID,
+			&i.User.FbID,
+			&i.User.IsEmailVerified,
+			&i.User.IsOnboardingCompleted,
+			&i.User.IsActive,
+			&i.User.IsBusinessAccount,
+			&i.User.IsVerified,
+			&i.User.Role,
+			&i.User.PasswordResetToken,
+			&i.User.PasswordResetExpires,
+			&i.User.LoginAttempts,
+			&i.User.LockoutUntil,
+			&i.User.Bio,
+			&i.User.Pronouns,
+			&i.User.Website,
+			&i.User.Phone,
+			&i.User.ProfileImage,
+			&i.User.BannerImage,
+			&i.User.FollowersCount,
+			&i.User.FollowingCount,
+			&i.User.LastLogin,
+			&i.User.CreatedAt,
+			&i.User.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const isUserFollowing = `-- name: IsUserFollowing :one
 SELECT EXISTS (
   SELECT 1
--- api/internal/pkg/db/queries/follows.sql
+++ api/internal/pkg/db/queries/follows.sql
@@ -17,3 +17,19 @@ INSERT INTO follows (
 -- name: Unfollow :exec
 DELETE FROM follows
 WHERE follower_id = $1 AND following_id = $2;
+
+-- name: GetUserFollowers :many
+SELECT
+  sqlc.embed(users)
+FROM follows
+  LEFT JOIN users ON users.id = follows.follower_id
+WHERE follows.following_id = $1
+ORDER BY follows.created_at DESC;
+
+-- name: GetUserFollowing :many
+SELECT
+  sqlc.embed(users)
+FROM follows
+  LEFT JOIN users ON users.id = follows.following_id
+WHERE follows.follower_id = $1
+ORDER BY follows.created_at DESC;
