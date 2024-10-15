af24371b0428bbef2dc341211ae611ba85fed39c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 03:44:00 +0300

    add dummy diary page

---
+++ web/app/routes/diary/_index/route.tsx
@@ -0,0 +1,17 @@
+export function meta() {
+  return [
+    {
+      title: "Diary | Wanderlust",
+    },
+  ];
+}
+
+export default function Page() {
+  return (
+    <div className="container mx-auto my-8">
+      <div>
+        <div>This is the diary page</div>
+      </div>
+    </div>
+  );
+}
