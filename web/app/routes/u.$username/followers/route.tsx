b87e835e5900549e82f54cf15e6c45d82b694953
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 04:10:47 +0300

    implement followers and following pages

--- web/app/routes/u.$username/followers/route.tsx
+++ web/app/routes/u.$username/followers/route.tsx
@@ -1,7 +1,50 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import AppMessage from "~/components/blocks/app-message";
+import UserImage from "~/components/blocks/user-image";
+import { getUserFollowers } from "~/lib/api-requests";
+import { ipx } from "~/lib/img-proxy";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.username, "username is required");
+  const followers = await getUserFollowers(params.username);
+  return json({ followers: followers.data.followers });
+}
 export default function Page() {
+  const { followers } = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <div>This is the user followers page</div>
+    <div className="my-8">
+      {followers.length === 0 && (
+        <AppMessage
+          emptyMessage="This user has no followers yet"
+          showBackButton={false}
+        />
+      )}
+
+      {followers.length > 0 && (
+        <div className="grid grid-cols-1 gap-4">
+          {followers.map((follower) => (
+            <Link
+              to={`/u/${follower.username}`}
+              key={follower.id}
+              className="flex gap-4 items-center hover:bg-muted p-2 rounded-md"
+            >
+              <UserImage
+                src={ipx(`http://${follower.profileImage ?? ""}`, "w_512")}
+                className="size-32"
+              />
+              <div>
+                <div className="text-lg">{follower.fullName}</div>
+                <div className="text-muted-foreground">
+                  @{follower.username}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- web/app/routes/u.$username/following/route.tsx
+++ web/app/routes/u.$username/following/route.tsx
@@ -1,7 +1,51 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import AppMessage from "~/components/blocks/app-message";
+import UserImage from "~/components/blocks/user-image";
+import { getUserFollowing } from "~/lib/api";
+import { ipx } from "~/lib/img-proxy";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.username, "username is required");
+  const following = await getUserFollowing(params.username);
+  return json({ following: following.data.following });
+}
+
 export default function Page() {
+  const { following } = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <div>This is the user following page</div>
+    <div className="my-8">
+      {following.length === 0 && (
+        <AppMessage
+          emptyMessage="This user has no followers yet"
+          showBackButton={false}
+        />
+      )}
+
+      {following.length > 0 && (
+        <div className="grid grid-cols-1 gap-4">
+          {following.map((following) => (
+            <Link
+              to={`/u/${following.username}`}
+              key={following.id}
+              className="flex gap-4 items-center hover:bg-muted p-2 rounded-md"
+            >
+              <UserImage
+                src={ipx(`http://${following.profileImage ?? ""}`, "w_512")}
+                className="size-32"
+              />
+              <div>
+                <div className="text-lg">{following.fullName}</div>
+                <div className="text-muted-foreground">
+                  @{following.username}
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
