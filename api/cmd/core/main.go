6c1668183fee0ef167d55a71ca8fa39e3221524c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 03:43:30 +0300

    register diary module

--- api/cmd/core/main.go
+++ api/cmd/core/main.go
@@ -14,6 +14,7 @@ import (
 	"wanderlust/internal/app/categories"
 	"wanderlust/internal/app/cities"
 	"wanderlust/internal/app/collections"
+	"wanderlust/internal/app/diary"
 	"wanderlust/internal/app/favorites"
 	"wanderlust/internal/app/health"
 	"wanderlust/internal/app/lists"
@@ -104,6 +105,7 @@ func New() *core.Application {
 		categories.New(&application.SharedModules),
 		cities.New(&application.SharedModules),
 		collections.New(&application.SharedModules),
+		diary.New(&application.SharedModules),
 		favorites.New(&application.SharedModules),
 		health.New(),
 		lists.New(&application.SharedModules),
