385ba5f04a16ca076d18dc87f7da404587c82c40
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-15 06:51:39 +0300

    update schema

--- web/app/routes/diary/new/schema.tsx
+++ web/app/routes/diary/new/schema.tsx
@@ -1,9 +1,21 @@
 import { z } from "zod";
 
 export const schema = z.object({
-  title: z.string().min(1).max(128),
-  description: z.string().min(1).max(4096),
+  title: z.string().min(1).max(128).default(""),
+  description: z.string().min(1).max(4096).default(""),
   shareWithFriends: z.boolean().default(false),
+  locations: z
+    .array(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        image: z.string(),
+        categoryName: z.string(),
+        city: z.string(),
+        state: z.string(),
+      })
+    )
+    .default([]),
 });
 
 export type FormInput = z.infer<typeof schema>;
