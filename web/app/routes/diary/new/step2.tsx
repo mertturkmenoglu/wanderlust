381c252e3fb8f4dca5cd4e09823481bc8fa5a96d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-16 10:45:31 +0300

    add ability to change order and remove list items

--- web/app/routes/diary/new/step2.tsx
+++ web/app/routes/diary/new/step2.tsx
@@ -1,3 +1,4 @@
+import { ArrowDownIcon, ArrowUpIcon, XIcon } from "lucide-react";
 import { InstantSearch } from "react-instantsearch";
 import { ClientOnly } from "remix-utils/client-only";
 import { Autocomplete } from "~/components/blocks/autocomplete";
@@ -12,6 +13,46 @@ type Props = {
 
 export default function Step2({ form }: Props) {
   const searchClient = useSearchClient();
+  const locations = form.watch("locations") ?? [];
+
+  const moveItemUp = (id: string) => {
+    const index = locations.findIndex((l) => l.id === id);
+
+    if (index <= 0) {
+      return;
+    }
+    const arr = [...locations];
+    const tmp = arr[index - 1];
+    arr[index - 1] = arr[index];
+    arr[index] = tmp;
+    form.setValue("locations", arr);
+  };
+
+  const moveItemDown = (id: string) => {
+    const index = locations.findIndex((l) => l.id === id);
+
+    if (index >= locations.length - 1 || index === -1) {
+      return;
+    }
+
+    const arr = [...locations];
+    const tmp = arr[index + 1];
+    arr[index + 1] = arr[index];
+    arr[index] = tmp;
+    form.setValue("locations", arr);
+  };
+
+  const removeItem = (id: string) => {
+    const index = locations.findIndex((l) => l.id === id);
+
+    if (index === -1) {
+      return;
+    }
+
+    const arr = [...locations];
+    arr.splice(index, 1);
+    form.setValue("locations", arr);
+  };
 
   return (
     <div className="w-full mt-16">
@@ -53,32 +94,60 @@ export default function Step2({ form }: Props) {
               <h3 className="mt-8 text-lg font-bold tracking-tight">
                 Selected Locations
               </h3>
-              {form.watch("locations").length === 0 && (
+              {locations.length === 0 && (
                 <div className="text-center text-sm text-muted-foreground my-8">
                   No locations selected
                 </div>
               )}
-              {form.watch("locations").map((l) => (
-                <Card key={l.id} className="mt-4 flex gap-8 p-4">
-                  <img
-                    src={l.image}
-                    alt=""
-                    className="aspect-video w-48 rounded-lg object-cover"
-                  />
+              {locations.map((l, i) => (
+                <div key={l.id} className="flex flex-col">
+                  <Card className="mt-4 flex gap-8 p-4 flex-1">
+                    <img
+                      src={l.image}
+                      alt=""
+                      className="aspect-video w-48 rounded-lg object-cover"
+                    />
 
-                  <div>
-                    <div className="line-clamp-1 text-lg font-semibold capitalize leading-none tracking-tight">
-                      {l.name}
-                    </div>
-                    <div className="my-1 line-clamp-1 text-sm text-muted-foreground">
-                      {l.city} / {l.state}
-                    </div>
+                    <div>
+                      <div className="line-clamp-1 text-lg font-semibold capitalize leading-none tracking-tight">
+                        {l.name}
+                      </div>
+                      <div className="my-1 line-clamp-1 text-sm text-muted-foreground">
+                        {l.city} / {l.state}
+                      </div>
 
-                    <div className="text-sm font-semibold leading-none tracking-tight text-primary">
-                      {l.categoryName}
+                      <div className="text-sm font-semibold leading-none tracking-tight text-primary">
+                        {l.categoryName}
+                      </div>
                     </div>
+                  </Card>
+
+                  <div className="ml-auto mt-1">
+                    <button
+                      className="p-1.5 hover:bg-muted rounded-full disabled:hover:bg-transparent"
+                      disabled={i === 0}
+                      onClick={() => moveItemUp(l.id)}
+                    >
+                      <ArrowUpIcon className="size-3" />
+                      <span className="sr-only">Move {l.name} up</span>
+                    </button>
+                    <button
+                      className="p-1.5 hover:bg-muted rounded-full disabled:hover:bg-transparent"
+                      disabled={i === locations.length - 1}
+                      onClick={() => moveItemDown(l.id)}
+                    >
+                      <ArrowDownIcon className="size-3" />
+                      <span className="sr-only">Move {l.name} down</span>
+                    </button>
+                    <button
+                      className="p-1.5 hover:bg-muted rounded-full"
+                      onClick={() => removeItem(l.id)}
+                    >
+                      <XIcon className="size-3" />
+                      <span className="sr-only">Remove {l.name}</span>
+                    </button>
                   </div>
-                </Card>
+                </div>
               ))}
             </div>
           )}
