4211e956798cf5f1f59277156cb24732033f40ea
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-17 10:29:18 +0300

    update queries

--- api/internal/pkg/db/follows.sql.go
+++ api/internal/pkg/db/follows.sql.go
@@ -173,6 +173,73 @@ func (q *Queries) IsUserFollowing(ctx context.Context, arg IsUserFollowingParams
 	return exists, err
 }
 
+const searchUserFollowing = `-- name: SearchUserFollowing :many
+SELECT
+  users.id, users.email, users.username, users.full_name, users.password_hash, users.google_id, users.fb_id, users.is_email_verified, users.is_onboarding_completed, users.is_active, users.is_business_account, users.is_verified, users.role, users.password_reset_token, users.password_reset_expires, users.login_attempts, users.lockout_until, users.bio, users.pronouns, users.website, users.phone, users.profile_image, users.banner_image, users.followers_count, users.following_count, users.last_login, users.created_at, users.updated_at
+FROM follows
+  LEFT JOIN users ON users.id = follows.following_id
+WHERE follows.follower_id = $1 AND users.username ILIKE $2
+ORDER BY follows.created_at DESC
+`
+
+type SearchUserFollowingParams struct {
+	FollowerID string
+	Username   string
+}
+
+type SearchUserFollowingRow struct {
+	User User
+}
+
+func (q *Queries) SearchUserFollowing(ctx context.Context, arg SearchUserFollowingParams) ([]SearchUserFollowingRow, error) {
+	rows, err := q.db.Query(ctx, searchUserFollowing, arg.FollowerID, arg.Username)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []SearchUserFollowingRow
+	for rows.Next() {
+		var i SearchUserFollowingRow
+		if err := rows.Scan(
+			&i.User.ID,
+			&i.User.Email,
+			&i.User.Username,
+			&i.User.FullName,
+			&i.User.PasswordHash,
+			&i.User.GoogleID,
+			&i.User.FbID,
+			&i.User.IsEmailVerified,
+			&i.User.IsOnboardingCompleted,
+			&i.User.IsActive,
+			&i.User.IsBusinessAccount,
+			&i.User.IsVerified,
+			&i.User.Role,
+			&i.User.PasswordResetToken,
+			&i.User.PasswordResetExpires,
+			&i.User.LoginAttempts,
+			&i.User.LockoutUntil,
+			&i.User.Bio,
+			&i.User.Pronouns,
+			&i.User.Website,
+			&i.User.Phone,
+			&i.User.ProfileImage,
+			&i.User.BannerImage,
+			&i.User.FollowersCount,
+			&i.User.FollowingCount,
+			&i.User.LastLogin,
+			&i.User.CreatedAt,
+			&i.User.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
 const unfollow = `-- name: Unfollow :exec
 DELETE FROM follows
 WHERE follower_id = $1 AND following_id = $2
