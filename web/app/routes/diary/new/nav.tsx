b0480ebf294c7d22d371b06b8c3d393c7051ac2e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-20 10:41:00 +0300

    add onNavigationChange function

--- web/app/routes/diary/new/nav.tsx
+++ web/app/routes/diary/new/nav.tsx
@@ -4,16 +4,24 @@ import { Button } from "~/components/ui/button";
 type Props = {
   currentStep: number;
   setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+  onNavigationChange: () => void;
 };
 
-export default function Nav({ currentStep, setCurrentStep }: Props) {
+export default function Nav({
+  currentStep,
+  setCurrentStep,
+  onNavigationChange,
+}: Props) {
   return (
     <div className="max-w-xl mx-auto flex items-center gap-4 justify-between mt-16 mb-48">
       <Button
         variant="ghost"
         className="flex items-center gap-2"
         disabled={currentStep === 1}
-        onClick={() => setCurrentStep(currentStep - 1)}
+        onClick={() => {
+          setCurrentStep(currentStep - 1);
+          onNavigationChange();
+        }}
       >
         <ArrowLeftIcon className="size-4" />
         <span>Previous</span>
@@ -22,7 +30,10 @@ export default function Nav({ currentStep, setCurrentStep }: Props) {
         variant="ghost"
         className="flex items-center gap-2"
         disabled={currentStep === 6}
-        onClick={() => setCurrentStep(currentStep + 1)}
+        onClick={() => {
+          setCurrentStep(currentStep + 1);
+          onNavigationChange();
+        }}
       >
         <span>Next</span>
         <ArrowRightIcon className="size-4" />
