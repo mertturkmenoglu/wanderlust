f4a025770321ea3c9f1bb5337e02afa388a757cc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-21 18:54:22 +0300

    update users errors

--- api/internal/app/users/errors.go
+++ api/internal/app/users/errors.go
@@ -9,4 +9,5 @@ import (
 var (
 	ErrUserNotFound        = errs.NewApiError(http.StatusNotFound, "0100", errors.New("user not found"))
 	ErrUsernameNotProvided = errs.NewApiError(http.StatusBadRequest, "0101", errors.New("username not provided"))
+	ErrInvalidUsername     = errs.NewApiError(http.StatusUnprocessableEntity, "0102", errors.New("invalid username"))
 )
