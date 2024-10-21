8e32ca958542d992b3e1c2e128ce5350e8e703ae
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-22 00:01:13 +0300

    update error handling on login

--- web/app/routes/_auth/sign-in/hooks.ts
+++ web/app/routes/_auth/sign-in/hooks.ts
@@ -2,7 +2,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
-import api from "~/lib/api";
+import api, { isApiError } from "~/lib/api";
+import { ErrorResponse } from "~/lib/dto";
 import { FormInput, FormSchema } from "./schema";
 
 export function useSignInForm() {
@@ -26,8 +27,15 @@ export function useLoginMutation() {
     onSuccess: () => {
       window.location.href = "/";
     },
-    onError: () => {
-      toast.error("Something went wrong");
+    onError: async (e) => {
+      if (isApiError(e)) {
+        const res = await e.response.json<ErrorResponse>();
+        const msg = res.errors[0].title;
+        const fmt = msg[0].toUpperCase() + msg.slice(1);
+        toast.error(fmt);
+      } else {
+        toast.error("Something went wrong");
+      }
     },
   });
 }
