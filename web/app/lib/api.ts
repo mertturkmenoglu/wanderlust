b56ae7db854e1ab86831e7113f3ed358a7fe02d3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-22 00:01:02 +0300

    add api error utility

--- web/app/lib/api.ts
+++ web/app/lib/api.ts
@@ -1,10 +1,14 @@
-import ky from "ky";
+import ky, { HTTPError } from "ky";
 
 const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_URL,
   credentials: "include",
   retry: 0,
 });
+
+export function isApiError<T>(e: Error): e is HTTPError<T> {
+  return e.name === "HTTPError";
+}
 
 export default api;
 
