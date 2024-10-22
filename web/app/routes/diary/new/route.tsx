2ca20f63751b6fc3ab4dd2b66f5d05bf739a283b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-22 04:39:54 +0300

    code cleanup

--- web/app/routes/diary/new/route.tsx
+++ web/app/routes/diary/new/route.tsx
@@ -1,19 +1,16 @@
-import { useEffect, useMemo, useState } from "react";
-
 import { json, redirect, useLoaderData } from "@remix-run/react";
-import { toast } from "sonner";
+import { useState } from "react";
 import BackLink from "~/components/blocks/back-link";
 import { Button } from "~/components/ui/button";
 import { getMe } from "~/lib/api";
-import { useNewDiaryEntryForm } from "./hooks";
-import Nav from "./nav";
-import { schema } from "./schema";
-import Step1 from "./step1";
-import Step2 from "./step2";
-import Step3 from "./step3";
-import Step4 from "./step4";
-import Step5 from "./step5";
-import Stepper from "./stepper";
+import Nav from "./__components/nav";
+import Step1 from "./__components/step1";
+import Step2 from "./__components/step2";
+import Step3 from "./__components/step3";
+import Step4 from "./__components/step4";
+import Step5 from "./__components/step5";
+import Stepper from "./__components/stepper";
+import { useNewDiaryEntryForm, useSaveToLocalStorage } from "./hooks";
 import { steps } from "./steps";
 
 export async function clientLoader() {
@@ -51,50 +48,14 @@ export function meta() {
 
 clientLoader.hydrate = true;
 
-type SaveStatus = "saving" | "saved" | "idle";
-
 export default function Page() {
   const { baseApiUrl } = useLoaderData<typeof clientLoader>();
   const form = useNewDiaryEntryForm();
   const [currentStep, setCurrentStep] = useState(1);
-  const [saveStatus, setSaveStatus] = useState<SaveStatus>("idle");
-
   const title = form.watch("title");
-  const displayTitle = useMemo(() => {
-    if (title && title !== "") {
-      return title;
-    }
-
-    return "New Diary Entry";
-  }, [title]);
-
-  const saveToLocalStorage = async () => {
-    setSaveStatus("saving");
-
-    await new Promise((res) => setTimeout(res, 1000));
-
-    localStorage.setItem("diary-entry", JSON.stringify(form.getValues()));
-    setSaveStatus("saved");
-
-    toast.success("Diary entry saved");
-
-    await new Promise((res) => setTimeout(res, 1000));
-
-    setSaveStatus("idle");
-  };
-
-  useEffect(() => {
-    const v = localStorage.getItem("diary-entry");
-
-    if (!v) {
-      return;
-    }
-
-    try {
-      const parsed = schema.parse(JSON.parse(v));
-      form.reset(parsed);
-    } catch (e) {}
-  }, []);
+  const displayTitle = title && title !== "" ? title : "New Diary Entry";
+  const { saveStatus, saveText, saveToLocalStorage } =
+    useSaveToLocalStorage(form);
 
   return (
     <div className="container mx-auto my-8">
@@ -107,11 +68,7 @@ export default function Page() {
           variant="link"
           disabled={saveStatus !== "idle"}
         >
-          {saveStatus === "saving"
-            ? "Saving..."
-            : saveStatus === "saved"
-            ? "Saved"
-            : "Save"}
+          {saveText}
         </Button>
       </div>
 
