0b5347d3854be8dedc6e594d5f0cd8f490789996
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-24 02:46:38 +0300

    use extracted components

--- web/app/routes/diary/new/__components/step2/index.tsx
+++ web/app/routes/diary/new/__components/step2/index.tsx
@@ -1,25 +1,14 @@
-import { ArrowDownIcon, ArrowUpIcon, PencilIcon, XIcon } from "lucide-react";
+import { ArrowDownIcon, ArrowUpIcon, XIcon } from "lucide-react";
 import { InstantSearch } from "react-instantsearch";
 import { ClientOnly } from "remix-utils/client-only";
 import { Autocomplete } from "~/components/blocks/autocomplete";
-import InputInfo from "~/components/kit/input-info";
-import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
-import { Label } from "~/components/ui/label";
 import { Separator } from "~/components/ui/separator";
-import { Textarea } from "~/components/ui/textarea";
 import { useSearchClient } from "~/hooks/use-search-client";
-import { lengthTracker } from "~/lib/form-utils";
-import { FormType, useSaveToLocalStorage } from "../../hooks";
+import { FormType } from "../../hooks";
+import ActionButton from "./action-button";
+import { moveItemDown, moveItemUp, removeItem } from "./actions";
+import EditDialog from "./edit-dialog";
 
 type Props = {
   form: FormType;
@@ -28,46 +17,6 @@ type Props = {
 export default function Step2({ form }: Props) {
   const searchClient = useSearchClient();
   const locations = form.watch("locations") ?? [];
-  const { saveToLocalStorage } = useSaveToLocalStorage(form);
-
-  const moveItemUp = (id: string) => {
-    const index = locations.findIndex((l) => l.id === id);
-
-    if (index <= 0) {
-      return;
-    }
-    const arr = [...locations];
-    const tmp = arr[index - 1];
-    arr[index - 1] = arr[index];
-    arr[index] = tmp;
-    form.setValue("locations", arr);
-  };
-
-  const moveItemDown = (id: string) => {
-    const index = locations.findIndex((l) => l.id === id);
-
-    if (index >= locations.length - 1 || index === -1) {
-      return;
-    }
-
-    const arr = [...locations];
-    const tmp = arr[index + 1];
-    arr[index + 1] = arr[index];
-    arr[index] = tmp;
-    form.setValue("locations", arr);
-  };
-
-  const removeItem = (id: string) => {
-    const index = locations.findIndex((l) => l.id === id);
-
-    if (index === -1) {
-      return;
-    }
-
-    const arr = [...locations];
-    arr.splice(index, 1);
-    form.setValue("locations", arr);
-  };
 
   return (
     <div className="w-full mt-16">
@@ -144,79 +93,26 @@ export default function Step2({ form }: Props) {
                   </Card>
 
                   <div className="ml-auto mt-1">
-                    <Dialog>
-                      <DialogTrigger asChild>
-                        <button className="p-1.5 hover:bg-muted rounded-full">
-                          <PencilIcon className="size-3" />
-                          <span className="sr-only">
-                            Edit {l.name} description
-                          </span>
-                        </button>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px]">
-                        <DialogHeader>
-                          <DialogTitle>Description</DialogTitle>
-                          <DialogDescription>
-                            <div className="text-sm">
-                              You can add a short description for this location.
-                            </div>
-                          </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-2">
-                          <div className="">
-                            <Label htmlFor="description" className="text-right">
-                              Description
-                            </Label>
-                            <Textarea
-                              id="description"
-                              className="col-span-3"
-                              placeholder="Add a short description"
-                              {...form.register(`locations.${i}.description`)}
-                            />
-                            <InputInfo
-                              text={lengthTracker(
-                                form.watch(`locations.${i}.description`),
-                                256
-                              )}
-                            />
-                          </div>
-                        </div>
-                        <DialogFooter>
-                          <Button
-                            type="button"
-                            onClick={async () => {
-                              await saveToLocalStorage();
-                            }}
-                          >
-                            Save changes
-                          </Button>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
+                    <EditDialog form={form} name={l.name} index={i} />
 
-                    <button
-                      className="p-1.5 hover:bg-muted rounded-full disabled:hover:bg-transparent"
+                    <ActionButton
                       disabled={i === 0}
-                      onClick={() => moveItemUp(l.id)}
+                      onClick={() => moveItemUp(form, l.id)}
                     >
                       <ArrowUpIcon className="size-3" />
                       <span className="sr-only">Move {l.name} up</span>
-                    </button>
-                    <button
-                      className="p-1.5 hover:bg-muted rounded-full disabled:hover:bg-transparent"
+                    </ActionButton>
+                    <ActionButton
                       disabled={i === locations.length - 1}
-                      onClick={() => moveItemDown(l.id)}
+                      onClick={() => moveItemDown(form, l.id)}
                     >
                       <ArrowDownIcon className="size-3" />
                       <span className="sr-only">Move {l.name} down</span>
-                    </button>
-                    <button
-                      className="p-1.5 hover:bg-muted rounded-full"
-                      onClick={() => removeItem(l.id)}
-                    >
+                    </ActionButton>
+                    <ActionButton onClick={() => removeItem(form, l.id)}>
                       <XIcon className="size-3" />
                       <span className="sr-only">Remove {l.name}</span>
-                    </button>
+                    </ActionButton>
                   </div>
                 </div>
               ))}
