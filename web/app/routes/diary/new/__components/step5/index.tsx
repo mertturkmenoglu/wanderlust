21824ff9ae00e458ac96b3f0d8d295cae29b196e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-23 07:02:58 +0300

    refactor Step5 component to use default values and improve file summaries and button state logic

--- web/app/routes/diary/new/__components/step5/index.tsx
+++ web/app/routes/diary/new/__components/step5/index.tsx
@@ -10,12 +10,24 @@ type Props = {
   uppy: Uppy<Meta, Record<string, never>>;
 };
 
+function strOrDefault(s: string, defaultValue: string): string {
+  return s === "" ? defaultValue : s;
+}
+
 export default function Step5({ form, uppy }: Props) {
   const files = useUppyState(uppy, (s) => s.files);
   const fileCount = Object.keys(files).length;
+
   const description = form.watch("description");
+  const title = form.watch("title");
+  const date = form.watch("date");
+  const locations = form.watch("locations");
+  const friends = form.watch("friends");
+
   const shortDescription =
     description.length > 256 ? description.slice(0, 256) + "..." : description;
+
+  const canCreateEntry = form.formState.isValid;
 
   return (
     <div className="flex flex-col mx-auto max-w-xl">
@@ -23,26 +35,28 @@ export default function Step5({ form, uppy }: Props) {
         <h3 className="text-muted-foreground text-lg">Summary</h3>
         <div className="grid grid-cols-2 gap-2 text-sm text-muted-foreground mt-2">
           <div>Title:</div>
-          <div>{form.watch("title")}</div>
+          <div>{strOrDefault(title, "No title")}</div>
           <div>Description:</div>
-          <div>{shortDescription}</div>
+          <div>{strOrDefault(shortDescription, "No description")}</div>
           <div>Date:</div>
-          <div>{form.watch("date").toLocaleDateString()}</div>
+          <div>{date.toLocaleDateString()}</div>
           <div>Locations:</div>
           <ul>
-            {form.watch("locations").map((l) => (
+            {locations.map((l) => (
               <li key={l.id} className="list-disc list-outside">
                 {l.name}
               </li>
             ))}
+            {locations.length === 0 && <div>No locations</div>}
           </ul>
           <div>Friends:</div>
           <ul>
-            {form.watch("friends").map((f) => (
+            {friends.map((f) => (
               <li key={f.id} className="list-disc list-outside">
                 {f.fullName}
               </li>
             ))}
+            {friends.length === 0 && <div>No friends</div>}
           </ul>
           <div>Media:</div>
           <div>
@@ -56,7 +70,9 @@ export default function Step5({ form, uppy }: Props) {
         <Label htmlFor="share-with-friends">Share with friends</Label>
       </div>
 
-      <Button className="mt-4">Create Diary Entry</Button>
+      <Button className="mt-4" disabled={!canCreateEntry}>
+        Create Diary Entry
+      </Button>
     </div>
   );
 }
