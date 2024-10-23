ff6c6dfdec28e3cc1288807a07ac3a6e62e7e039
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-23 06:46:00 +0300

    update media step

---
+++ web/app/routes/diary/new/__components/step4/index.tsx
@@ -0,0 +1,52 @@
+import Uppy, { Meta } from "@uppy/core";
+import { Dashboard } from "@uppy/react";
+import { ClientOnly } from "remix-utils/client-only";
+import { FormType } from "../../hooks";
+
+type Props = {
+  form: FormType;
+  baseApiUrl: string;
+  uppy: Uppy<Meta, Record<string, never>>;
+};
+
+export default function Step4({ uppy }: Props) {
+  return (
+    <div className="w-full mt-16 max-w-2xl mx-auto">
+      <div className="text-lg text-muted-foreground text-center">
+        Now let's add a couple of pictures
+      </div>
+
+      <ClientOnly fallback={<div>Loading...</div>}>
+        {() => (
+          <>
+            <Dashboard
+              uppy={uppy}
+              className="col-span-2 mx-auto mt-4"
+              hideUploadButton={true}
+              hideCancelButton={true}
+              note={"Images only, 1-10 files, up to 5 MB"}
+              metaFields={[
+                { id: "name", name: "Name", placeholder: "file name" },
+                {
+                  id: "caption",
+                  name: "Caption",
+                  placeholder: "Describe what the image is about",
+                },
+                {
+                  id: "alt",
+                  name: "Alt text",
+                  placeholder: "Describe the image for accessibility tools",
+                },
+                {
+                  id: "order",
+                  name: "Order",
+                  placeholder: "Order of the image",
+                },
+              ]}
+            />
+          </>
+        )}
+      </ClientOnly>
+    </div>
+  );
+}
