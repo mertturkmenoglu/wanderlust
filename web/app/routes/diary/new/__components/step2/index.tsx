f5b345d94e1bcd886ac8d0d6c587de0e9d61824a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-24 18:55:56 +0300

    wrap locations with scroll area component

--- web/app/routes/diary/new/__components/step2/index.tsx
+++ web/app/routes/diary/new/__components/step2/index.tsx
@@ -5,6 +5,7 @@ import { ClientOnly } from "remix-utils/client-only";
 import { toast } from "sonner";
 import { Autocomplete } from "~/components/blocks/autocomplete";
 import { Card } from "~/components/ui/card";
+import { ScrollArea } from "~/components/ui/scroll-area";
 import { Separator } from "~/components/ui/separator";
 import { useSearchClient } from "~/hooks/use-search-client";
 import { FormInput } from "../../schema";
@@ -76,58 +77,60 @@ export default function Step2() {
               <h3 className="mt-8 text-lg font-bold tracking-tight">
                 Selected Locations
               </h3>
-              {locations.length === 0 && (
-                <div className="text-center text-sm text-muted-foreground my-8">
-                  No locations selected
-                </div>
-              )}
-              {locations.map((l, i) => (
-                <div key={l.id} className="flex flex-col">
-                  <Card className="mt-4 flex gap-8 p-4 flex-1">
-                    <img
-                      src={l.image}
-                      alt=""
-                      className="aspect-video w-48 rounded-lg object-cover"
-                    />
-
-                    <div>
-                      <div className="line-clamp-1 text-lg font-semibold capitalize leading-none tracking-tight">
-                        {l.name}
-                      </div>
-                      <div className="my-1 line-clamp-1 text-sm text-muted-foreground">
-                        {l.city} / {l.state}
-                      </div>
-
-                      <div className="text-sm font-semibold leading-none tracking-tight text-primary">
-                        {l.categoryName}
-                      </div>
-                    </div>
-                  </Card>
-
-                  <div className="ml-auto mt-1">
-                    <EditDialog form={form} name={l.name} index={i} />
-
-                    <ActionButton
-                      disabled={i === 0}
-                      onClick={() => moveItemUp(form, l.id)}
-                    >
-                      <ArrowUpIcon className="size-3" />
-                      <span className="sr-only">Move {l.name} up</span>
-                    </ActionButton>
-                    <ActionButton
-                      disabled={i === locations.length - 1}
-                      onClick={() => moveItemDown(form, l.id)}
-                    >
-                      <ArrowDownIcon className="size-3" />
-                      <span className="sr-only">Move {l.name} down</span>
-                    </ActionButton>
-                    <ActionButton onClick={() => removeItem(form, l.id)}>
-                      <XIcon className="size-3" />
-                      <span className="sr-only">Remove {l.name}</span>
-                    </ActionButton>
+              <ScrollArea className="h-[640px]">
+                {locations.length === 0 && (
+                  <div className="text-center text-sm text-muted-foreground my-8">
+                    No locations selected
                   </div>
-                </div>
-              ))}
+                )}
+                {locations.map((l, i) => (
+                  <div key={l.id} className="flex flex-col">
+                    <Card className="mt-4 flex gap-8 p-4 flex-1">
+                      <img
+                        src={l.image}
+                        alt=""
+                        className="aspect-video w-48 rounded-lg object-cover"
+                      />
+
+                      <div>
+                        <div className="line-clamp-1 text-lg font-semibold capitalize leading-none tracking-tight">
+                          {l.name}
+                        </div>
+                        <div className="my-1 line-clamp-1 text-sm text-muted-foreground">
+                          {l.city} / {l.state}
+                        </div>
+
+                        <div className="text-sm font-semibold leading-none tracking-tight text-primary">
+                          {l.categoryName}
+                        </div>
+                      </div>
+                    </Card>
+
+                    <div className="ml-auto mt-1">
+                      <EditDialog form={form} name={l.name} index={i} />
+
+                      <ActionButton
+                        disabled={i === 0}
+                        onClick={() => moveItemUp(form, l.id)}
+                      >
+                        <ArrowUpIcon className="size-3" />
+                        <span className="sr-only">Move {l.name} up</span>
+                      </ActionButton>
+                      <ActionButton
+                        disabled={i === locations.length - 1}
+                        onClick={() => moveItemDown(form, l.id)}
+                      >
+                        <ArrowDownIcon className="size-3" />
+                        <span className="sr-only">Move {l.name} down</span>
+                      </ActionButton>
+                      <ActionButton onClick={() => removeItem(form, l.id)}>
+                        <XIcon className="size-3" />
+                        <span className="sr-only">Remove {l.name}</span>
+                      </ActionButton>
+                    </div>
+                  </div>
+                ))}
+              </ScrollArea>
             </div>
           )}
         </ClientOnly>
