6ce5a2eb56c518cdec089dee0c0224f74f9ed102
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-24 11:36:47 +0300

    add truncate with ellipsis utility function

--- web/app/lib/form-utils.ts
+++ web/app/lib/form-utils.ts
@@ -1,3 +1,7 @@
 export function lengthTracker(s: string | undefined, max: number): string {
   return `${s?.length ?? 0}/${max}`;
 }
+
+export function truncateWithEllipses(s: string, max: number): string {
+  return s.length > max ? s.slice(0, max) + "..." : s;
+}
