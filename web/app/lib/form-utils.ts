8276136f34d2d2551caee6e6da985cc47c4316e7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-24 11:36:23 +0300

    add explicit return type to function signature

--- web/app/lib/form-utils.ts
+++ web/app/lib/form-utils.ts
@@ -1,3 +1,3 @@
-export function lengthTracker(s: string | undefined, max: number) {
+export function lengthTracker(s: string | undefined, max: number): string {
   return `${s?.length ?? 0}/${max}`;
 }
