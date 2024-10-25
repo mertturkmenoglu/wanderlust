8c3949e286eaa9885bff2d2be343f67fc74fdb0d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-25 17:01:32 +0300

    extract city breadcrumb component

---
+++ web/app/routes/cities.$/components/city-breadcrumb.tsx
@@ -0,0 +1,32 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
+
+type Props = {
+  cityName: string;
+};
+
+export default function CityBreadcrumb({ cityName }: Props) {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Discover</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/cities/list">Cities</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{cityName}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
