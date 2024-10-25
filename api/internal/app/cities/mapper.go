5347a705df111a0dee4bf9a5711e212612474772
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-25 10:14:47 +0300

    add image license fields mappings

--- api/internal/app/cities/mapper.go
+++ api/internal/app/cities/mapper.go
@@ -2,20 +2,25 @@ package cities
 
 import (
 	"wanderlust/internal/pkg/db"
+	"wanderlust/internal/pkg/utils"
 )
 
 func mapGetCityByIdRowToDto(v db.City) GetCityByIdResponseDto {
 	return GetCityByIdResponseDto{
-		ID:          v.ID,
-		Name:        v.Name,
-		StateCode:   v.StateCode,
-		StateName:   v.StateName,
-		CountryCode: v.CountryCode,
-		CountryName: v.CountryName,
-		ImageUrl:    v.ImageUrl,
-		Latitude:    v.Latitude,
-		Longitude:   v.Longitude,
-		Description: v.Description,
+		ID:                   v.ID,
+		Name:                 v.Name,
+		StateCode:            v.StateCode,
+		StateName:            v.StateName,
+		CountryCode:          v.CountryCode,
+		CountryName:          v.CountryName,
+		ImageUrl:             v.ImageUrl,
+		Latitude:             v.Latitude,
+		Longitude:            v.Longitude,
+		Description:          v.Description,
+		ImageLicense:         utils.TextOrNil(v.ImgLicense),
+		ImageLicenseLink:     utils.TextOrNil(v.ImgLicenseLink),
+		ImageAttribution:     utils.TextOrNil(v.ImgAttr),
+		ImageAttributionLink: utils.TextOrNil(v.ImgAttrLink),
 	}
 }
 
@@ -45,30 +50,38 @@ func mapGetFeaturedCitiesToDto(v []db.City) GetFeaturedCitiesResponseDto {
 
 func mapCreateCityResponseToDto(v db.City) CreateCityResponseDto {
 	return CreateCityResponseDto{
-		ID:          int32(v.ID),
-		Name:        v.Name,
-		StateCode:   v.StateCode,
-		StateName:   v.StateName,
-		CountryCode: v.CountryCode,
-		CountryName: v.CountryName,
-		ImageUrl:    v.ImageUrl,
-		Latitude:    v.Latitude,
-		Longitude:   v.Longitude,
-		Description: v.Description,
+		ID:                   int32(v.ID),
+		Name:                 v.Name,
+		StateCode:            v.StateCode,
+		StateName:            v.StateName,
+		CountryCode:          v.CountryCode,
+		CountryName:          v.CountryName,
+		ImageUrl:             v.ImageUrl,
+		Latitude:             v.Latitude,
+		Longitude:            v.Longitude,
+		Description:          v.Description,
+		ImageLicense:         utils.TextOrNil(v.ImgLicense),
+		ImageLicenseLink:     utils.TextOrNil(v.ImgLicenseLink),
+		ImageAttribution:     utils.TextOrNil(v.ImgAttr),
+		ImageAttributionLink: utils.TextOrNil(v.ImgAttrLink),
 	}
 }
 
 func mapUpdateCityResponseToDto(v db.City) UpdateCityResponseDto {
 	return UpdateCityResponseDto{
-		ID:          int32(v.ID),
-		Name:        v.Name,
-		StateCode:   v.StateCode,
-		StateName:   v.StateName,
-		CountryCode: v.CountryCode,
-		CountryName: v.CountryName,
-		ImageUrl:    v.ImageUrl,
-		Latitude:    v.Latitude,
-		Longitude:   v.Longitude,
-		Description: v.Description,
+		ID:                   int32(v.ID),
+		Name:                 v.Name,
+		StateCode:            v.StateCode,
+		StateName:            v.StateName,
+		CountryCode:          v.CountryCode,
+		CountryName:          v.CountryName,
+		ImageUrl:             v.ImageUrl,
+		Latitude:             v.Latitude,
+		Longitude:            v.Longitude,
+		Description:          v.Description,
+		ImageLicense:         utils.TextOrNil(v.ImgLicense),
+		ImageLicenseLink:     utils.TextOrNil(v.ImgLicenseLink),
+		ImageAttribution:     utils.TextOrNil(v.ImgAttr),
+		ImageAttributionLink: utils.TextOrNil(v.ImgAttrLink),
 	}
 }
