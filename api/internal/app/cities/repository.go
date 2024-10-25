dfe9051d246c1e99afd82e1baea023b77c9e9735
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-25 10:14:55 +0300

    update database queries

--- api/internal/app/cities/repository.go
+++ api/internal/app/cities/repository.go
@@ -3,6 +3,7 @@ package cities
 import (
 	"context"
 	"wanderlust/internal/pkg/db"
+	"wanderlust/internal/pkg/utils"
 )
 
 func (r *repository) getCityById(id int32) (db.City, error) {
@@ -19,31 +20,39 @@ func (r *repository) getFeaturedCities(cityIds []int32) ([]db.City, error) {
 
 func (r *repository) createCity(dto CreateCityRequestDto) (db.City, error) {
 	return r.di.Db.Queries.CreateCity(context.Background(), db.CreateCityParams{
-		ID:          dto.ID,
-		Name:        dto.Name,
-		StateCode:   dto.StateCode,
-		StateName:   dto.StateName,
-		CountryCode: dto.CountryCode,
-		CountryName: dto.CountryName,
-		ImageUrl:    dto.ImageUrl,
-		Latitude:    dto.Latitude,
-		Longitude:   dto.Longitude,
-		Description: dto.Description,
+		ID:             dto.ID,
+		Name:           dto.Name,
+		StateCode:      dto.StateCode,
+		StateName:      dto.StateName,
+		CountryCode:    dto.CountryCode,
+		CountryName:    dto.CountryName,
+		ImageUrl:       dto.ImageUrl,
+		Latitude:       dto.Latitude,
+		Longitude:      dto.Longitude,
+		Description:    dto.Description,
+		ImgLicense:     utils.StrToText(dto.ImageLicense),
+		ImgLicenseLink: utils.StrToText(dto.ImageLicenseLink),
+		ImgAttr:        utils.StrToText(dto.ImageAttribution),
+		ImgAttrLink:    utils.StrToText(dto.ImageAttributionLink),
 	})
 }
 
 func (r *repository) updateCity(id int32, dto UpdateCityRequestDto) (db.City, error) {
 	return r.di.Db.Queries.UpdateCity(context.Background(), db.UpdateCityParams{
-		ID:          id,
-		Name:        dto.Name,
-		StateCode:   dto.StateCode,
-		StateName:   dto.StateName,
-		CountryCode: dto.CountryCode,
-		CountryName: dto.CountryName,
-		ImageUrl:    dto.ImageUrl,
-		Latitude:    dto.Latitude,
-		Longitude:   dto.Longitude,
-		Description: dto.Description,
+		ID:             id,
+		Name:           dto.Name,
+		StateCode:      dto.StateCode,
+		StateName:      dto.StateName,
+		CountryCode:    dto.CountryCode,
+		CountryName:    dto.CountryName,
+		ImageUrl:       dto.ImageUrl,
+		Latitude:       dto.Latitude,
+		Longitude:      dto.Longitude,
+		Description:    dto.Description,
+		ImgLicense:     utils.NilStrToText(dto.ImageLicense),
+		ImgLicenseLink: utils.NilStrToText(dto.ImageLicenseLink),
+		ImgAttr:        utils.NilStrToText(dto.ImageAttribution),
+		ImgAttrLink:    utils.NilStrToText(dto.ImageAttributionLink),
 	})
 }
 
