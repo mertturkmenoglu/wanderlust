c90a40f74dd574573f826392c21aba024e1fb54c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-25 10:15:53 +0300

    update styles

--- web/app/routes/cities.$/components/map-container.tsx
+++ web/app/routes/cities.$/components/map-container.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function MapContainer({ lat, lng }: Props) {
   return (
     <>
-      <h3 className="text-2xl font-bold mt-8">Location</h3>
+      <h3 className="text-2xl font-bold mt-8 lg:mt-0">Location</h3>
       <ClientOnly fallback={<div className="w-full h-[400px] bg-muted mt-4" />}>
         {() => <Map lat={lat} lng={lng} />}
       </ClientOnly>
