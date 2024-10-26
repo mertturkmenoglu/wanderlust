ac77a0ee30b9470bbb8242bca2a349104d3404d9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-26 18:42:53 +0300

    create new migration for diary feature

---
+++ api/internal/pkg/db/migrations/000007_create_diaries_table.down.sql
@@ -0,0 +1,11 @@
+DROP INDEX IF EXISTS idx_media_diary_entries;
+
+DROP TABLE IF EXISTS diary_media;
+
+DROP TABLE IF EXISTS diary_entries_users;
+
+DROP TABLE IF EXISTS diary_entries_pois;
+
+DROP INDEX IF EXISTS idx_diary_entries_date;
+
+DROP TABLE IF EXISTS diary_entries;
---
+++ api/internal/pkg/db/migrations/000007_create_diaries_table.up.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS diary_entries (
+  id TEXT PRIMARY KEY,
+  title VARCHAR(128) NOT NULL,
+  description VARCHAR(4096) NOT NULL,
+  share_with_friends BOOLEAN NOT NULL DEFAULT false,
+  date TIMESTAMPTZ NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_diary_entries_date ON diary_entries(date);
+
+CREATE TABLE IF NOT EXISTS diary_entries_pois (
+  diary_entry_id TEXT NOT NULL,
+  poi_id TEXT NOT NULL,
+  description VARCHAR(256),
+  list_index INT NOT NULL,
+  PRIMARY KEY (diary_entry_id, poi_id),
+  CONSTRAINT fk_diary_entries_entry FOREIGN KEY (diary_entry_id) REFERENCES diary_entries(id) ON DELETE CASCADE,
+  CONSTRAINT fk_diary_entries_poi FOREIGN KEY (poi_id) REFERENCES pois(id) ON DELETE CASCADE,
+  CONSTRAINT idx_unique_list_items_diary_pois UNIQUE (diary_entry_id, list_index)
+);
+
+CREATE TABLE IF NOT EXISTS diary_entries_users (
+  diary_entry_id TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  list_index INT NOT NULL,
+  PRIMARY KEY (diary_entry_id, user_id),
+  CONSTRAINT fk_diary_entries_entry FOREIGN KEY (diary_entry_id) REFERENCES diary_entries(id) ON DELETE CASCADE,
+  CONSTRAINT fk_diary_entries_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  CONSTRAINT idx_unique_list_items_diary_users UNIQUE (diary_entry_id, list_index)
+);
+
+CREATE TABLE IF NOT EXISTS diary_media (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  diary_entry_id TEXT NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  alt VARCHAR(255) NOT NULL,
+  caption VARCHAR(255),
+  media_order SMALLINT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  CONSTRAINT fk_diary_media_diary_entry FOREIGN KEY (diary_entry_id) REFERENCES diary_entries(id) ON DELETE SET DEFAULT
+);
+
+-- Create indexes
+CREATE INDEX IF NOT EXISTS idx_media_diary_entries ON diary_media(diary_entry_id);
