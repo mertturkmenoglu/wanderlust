29a8dc114394412b2df21bf28840c516c6cde663
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-27 14:43:11 +0300

    add create diary entry route

--- api/internal/app/diary/routes.go
+++ api/internal/app/diary/routes.go
@@ -1,10 +1,15 @@
 package diary
 
-import "github.com/labstack/echo/v4"
+import (
+	"wanderlust/internal/pkg/middlewares"
+
+	"github.com/labstack/echo/v4"
+)
 
 func (m *Module) RegisterRoutes(e *echo.Group) {
 	routes := e.Group("/diary")
 	{
 		routes.GET("/", m.handlers.getDiary)
+		routes.POST("/", m.handlers.createNewDiaryEntry, middlewares.IsAuth, middlewares.ParseBody[CreateDiaryEntryRequestDto])
 	}
 }
