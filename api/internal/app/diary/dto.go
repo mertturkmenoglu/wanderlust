a962875f6de2f21f48b075c8f3b0ecbd383381af
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-28 16:59:04 +0300

    add additional validations and datetime validation format

--- api/internal/app/diary/dto.go
+++ api/internal/app/diary/dto.go
@@ -4,9 +4,9 @@ type CreateDiaryEntryRequestDto struct {
 	ShareWithFriends bool                          `json:"shareWithFriends" validate:"boolean"`
 	Title            string                        `json:"title" validate:"required,min=1,max=128"`
 	Description      string                        `json:"description" validate:"required,min=1,max=128"`
-	Date             string                        `json:"date" validate:"required,datetime"`
-	Friends          []string                      `json:"friends" validate:"required,dive,required"`
-	Locations        []CreateDiaryEntryLocationDto `json:"locations" validate:"required,dive,required"`
+	Date             string                        `json:"date" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
+	Friends          []string                      `json:"friends" validate:"required,min=1,max=32,dive,required"`
+	Locations        []CreateDiaryEntryLocationDto `json:"locations" validate:"required,min=1,max=32,dive,required"`
 }
 
 type CreateDiaryEntryLocationDto struct {
