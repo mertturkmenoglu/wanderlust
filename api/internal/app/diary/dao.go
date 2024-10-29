f0d7979b28f865fa4003bd75a172ae0ae37b6d3b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-29 11:29:28 +0300

    create diary dao

---
+++ api/internal/app/diary/dao.go
@@ -0,0 +1,9 @@
+package diary
+
+import "wanderlust/internal/pkg/db"
+
+type GetDiaryEntryByIdDao struct {
+	DiaryEntry db.GetDiaryEntryByIdRow
+	Users      []db.GetDiaryEntryUsersRow
+	Pois       []db.GetDiaryEntryPoisRow
+}
