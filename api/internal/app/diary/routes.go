38bede4ab29ed9bb018646593112fbc7a64b8ba3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-29 10:37:55 +0300

    add authz check to upload media route

--- api/internal/app/diary/routes.go
+++ api/internal/app/diary/routes.go
@@ -1,6 +1,7 @@
 package diary
 
 import (
+	"wanderlust/internal/pkg/authz"
 	"wanderlust/internal/pkg/middlewares"
 
 	"github.com/labstack/echo/v4"
@@ -11,6 +12,6 @@ func (m *Module) RegisterRoutes(e *echo.Group) {
 	{
 		routes.GET("/", m.handlers.getDiary)
 		routes.POST("/", m.handlers.createNewDiaryEntry, middlewares.IsAuth, middlewares.ParseBody[CreateDiaryEntryRequestDto])
-		routes.POST("/media/:id", m.handlers.uploadDiaryMedia, middlewares.IsAuth)
+		routes.POST("/media/:id", m.handlers.uploadDiaryMedia, middlewares.IsAuth, middlewares.Authz(authz.ActDiaryUploadMedia))
 	}
 }
