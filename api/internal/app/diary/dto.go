ae3eb29e2a768274f65f069ede3f9959db7230f6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-10-30 15:43:19 +0300

    update create diary validation rules

--- api/internal/app/diary/dto.go
+++ api/internal/app/diary/dto.go
@@ -23,9 +23,9 @@ type DiaryEntryDto struct {
 type CreateDiaryEntryRequestDto struct {
 	ShareWithFriends bool                          `json:"shareWithFriends" validate:"boolean"`
 	Title            string                        `json:"title" validate:"required,min=1,max=128"`
-	Description      string                        `json:"description" validate:"required,min=1,max=128"`
+	Description      string                        `json:"description" validate:"required,min=1,max=4096"`
 	Date             string                        `json:"date" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
-	Friends          []string                      `json:"friends" validate:"required,min=1,max=32,dive,required"`
+	Friends          []string                      `json:"friends" validate:"required,min=0,max=32,dive,required"`
 	Locations        []CreateDiaryEntryLocationDto `json:"locations" validate:"required,min=1,max=32,dive,required"`
 }
 
