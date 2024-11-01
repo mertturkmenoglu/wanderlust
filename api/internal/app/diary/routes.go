f826fb0baecfd7d8c4e2278d8c712a26d22f9ae4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-01 17:36:21 +0300

    add authz check

--- api/internal/app/diary/routes.go
+++ api/internal/app/diary/routes.go
@@ -11,7 +11,7 @@ func (m *Module) RegisterRoutes(e *echo.Group) {
 	routes := e.Group("/diary")
 	{
 		routes.GET("/", m.handlers.listDiaryEntries, middlewares.IsAuth)
-		routes.GET("/:id", m.handlers.getDiaryEntryById, middlewares.IsAuth)
+		routes.GET("/:id", m.handlers.getDiaryEntryById, middlewares.IsAuth, middlewares.Authz(authz.ActDiaryRead))
 		routes.POST("/", m.handlers.createNewDiaryEntry, middlewares.IsAuth, middlewares.ParseBody[CreateDiaryEntryRequestDto])
 		routes.POST("/media/:id", m.handlers.uploadDiaryMedia, middlewares.IsAuth, middlewares.Authz(authz.ActDiaryUploadMedia))
 		routes.PATCH("/:id/sharing", m.handlers.changeSharing, middlewares.IsAuth, middlewares.Authz(authz.ActDiaryChangeSharing))
