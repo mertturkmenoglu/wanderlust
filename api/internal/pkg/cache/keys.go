65f04a52c515fb6eb210a4a3b0ae64e0842c33d3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-01 17:40:03 +0300

    update keys and add key builder helper

--- api/internal/pkg/cache/keys.go
+++ api/internal/pkg/cache/keys.go
@@ -2,4 +2,16 @@ package cache
 
 const (
 	KeyHomeAggregations = "home-aggregations"
+	KeyDiaryEntry       = "diary-entry"
 )
+
+func KeyBuilder(parts ...string) string {
+	key := ""
+	for i, part := range parts {
+		if i != 0 {
+			key += ":"
+		}
+		key += part
+	}
+	return key
+}
