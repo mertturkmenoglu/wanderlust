b3a21a216423d0cfd3dabbd1808865caa6d6484f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-09 20:24:55 +0300

    add create review req and res dtos

---
+++ api/internal/app/reviews/dto.go
@@ -0,0 +1,19 @@
+package reviews
+
+import "time"
+
+type CreateReviewRequestDto struct {
+	PoiID   string `json:"poiId" validate:"required,min=1,max=32"`
+	Content string `json:"content" validate:"required,min=5,max=2048"`
+	Rating  int16  `json:"rating" validate:"required,min=1,max=5"`
+}
+
+type CreateReviewResponseDto struct {
+	ID        string    `json:"id"`
+	PoiID     string    `json:"poiId"`
+	UserID    string    `json:"userId"`
+	Content   string    `json:"content"`
+	Rating    int16     `json:"rating"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
