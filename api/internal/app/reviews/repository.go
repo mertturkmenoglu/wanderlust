b094329804697963bde2e6b633679e754fdf63a4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-09 20:25:36 +0300

    add repository

---
+++ api/internal/app/reviews/repository.go
@@ -0,0 +1,84 @@
+package reviews
+
+import (
+	"context"
+	"errors"
+	"wanderlust/internal/pkg/db"
+	"wanderlust/internal/pkg/utils"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func (r *repository) createReview(userId string, dto CreateReviewRequestDto) (db.Review, error) {
+	ctx := context.Background()
+	tx, err := r.di.Db.Pool.Begin(ctx)
+
+	if err != nil {
+		return db.Review{}, err
+	}
+
+	defer tx.Rollback(ctx)
+
+	qtx := r.di.Db.Queries.WithTx(tx)
+
+	lastReview, err := qtx.LastReviewOfUserForPoi(ctx, db.LastReviewOfUserForPoiParams{
+		PoiID:  dto.PoiID,
+		UserID: userId,
+	})
+
+	isFirstReview := err != nil && errors.Is(err, pgx.ErrNoRows)
+
+	review, err := qtx.CreateReview(ctx, db.CreateReviewParams{
+		ID:      utils.GenerateId(r.di.Flake),
+		PoiID:   dto.PoiID,
+		UserID:  userId,
+		Content: dto.Content,
+		Rating:  dto.Rating,
+	})
+
+	if err != nil {
+		return db.Review{}, err
+	}
+
+	if isFirstReview {
+		err = qtx.IncrementTotalVotes(ctx, dto.PoiID)
+
+		if err != nil {
+			return db.Review{}, err
+		}
+	}
+
+	var diff int32 = 0
+	if !isFirstReview {
+		diff = int32(dto.Rating) - int32(lastReview.Rating)
+	} else {
+		diff = int32(dto.Rating)
+	}
+
+	err = qtx.IncrementTotalPoints(ctx, db.IncrementTotalPointsParams{
+		ID:          dto.PoiID,
+		TotalPoints: diff,
+	})
+
+	if err != nil {
+		return db.Review{}, err
+	}
+
+	err = qtx.SetPreviousReviewRatings(ctx, db.SetPreviousReviewRatingsParams{
+		UserID: userId,
+		Rating: dto.Rating,
+		PoiID:  dto.PoiID,
+	})
+
+	if err != nil {
+		return db.Review{}, err
+	}
+
+	err = tx.Commit(ctx)
+
+	if err != nil {
+		return db.Review{}, err
+	}
+
+	return review, nil
+}
