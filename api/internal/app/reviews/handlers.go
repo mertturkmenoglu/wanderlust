b9d92fb81980e36e185b42e1e935a45bfad7f759
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-09 20:25:03 +0300

    add reviews handlers

--- api/internal/app/reviews/handlers.go
+++ api/internal/app/reviews/handlers.go
@@ -1,7 +1,43 @@
 package reviews
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+	"wanderlust/internal/pkg/core"
+
+	"github.com/labstack/echo/v4"
+)
 
 func (h *handlers) getReviewById(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) createReview(c echo.Context) error {
+	userId := c.Get("user_id").(string)
+	dto := c.Get("body").(CreateReviewRequestDto)
+
+	res, err := h.service.createReview(userId, dto)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, core.Response{
+		Data: res,
+	})
+}
+
+func (h *handlers) deleteReview(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) getReviewsByUsername(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) getReviewsByPoiId(c echo.Context) error {
+	return echo.ErrNotImplemented
+}
+
+func (h *handlers) uploadMedia(c echo.Context) error {
 	return echo.ErrNotImplemented
 }
