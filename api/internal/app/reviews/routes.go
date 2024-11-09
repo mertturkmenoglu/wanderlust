8b5d6106b23f8b27cb3c7fd0dfaa0f0ce49d24ce
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-09 20:25:15 +0300

    define routes

--- api/internal/app/reviews/routes.go
+++ api/internal/app/reviews/routes.go
@@ -1,12 +1,20 @@
 package reviews
 
 import (
+	"wanderlust/internal/pkg/authz"
+	"wanderlust/internal/pkg/middlewares"
+
 	"github.com/labstack/echo/v4"
 )
 
 func (m *Module) RegisterRoutes(e *echo.Group) {
 	routes := e.Group("/reviews")
 	{
-		routes.GET("/", m.handlers.getReviewById)
+		routes.GET("/:id", m.handlers.getReviewById)
+		routes.POST("/", m.handlers.createReview, middlewares.ParseBody[CreateReviewRequestDto], middlewares.IsAuth, middlewares.Authz(authz.ActReviewCreate))
+		routes.DELETE("/", m.handlers.deleteReview, middlewares.IsAuth, middlewares.Authz(authz.ActReviewDelete))
+		routes.GET("/user/:username", m.handlers.getReviewsByUsername)
+		routes.GET("/poi/:id", m.handlers.getReviewsByPoiId)
+		routes.POST("/media", m.handlers.uploadMedia, middlewares.IsAuth, middlewares.Authz(authz.ActReviewUploadMedia))
 	}
 }
