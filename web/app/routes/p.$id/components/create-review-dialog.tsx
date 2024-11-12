1b18aeeed8f27ad4b6e05d88fb0e04d557880e99
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-12 12:59:29 +0300

    extract create review dialog component

---
+++ web/app/routes/p.$id/components/create-review-dialog.tsx
@@ -0,0 +1,144 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import Uppy from "@uppy/core";
+import ImageEditor from "@uppy/image-editor";
+import { Dashboard } from "@uppy/react";
+import XHRUpload from "@uppy/xhr-upload";
+import { PlusIcon, UploadIcon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import InputError from "~/components/kit/input-error";
+import { Rating } from "~/components/kit/rating";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
+import { Button } from "~/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "~/components/ui/collapsible";
+import { Label } from "~/components/ui/label";
+import { Textarea } from "~/components/ui/textarea";
+
+const schema = z.object({
+  content: z.string().min(5).max(2048),
+});
+
+type FormInput = z.infer<typeof schema>;
+
+export default function CreateReviewDialog() {
+  const [rating, setRating] = useState(0);
+  const form = useForm<FormInput>({
+    resolver: zodResolver(schema),
+  });
+
+  const [uppy] = useState(() =>
+    new Uppy({
+      restrictions: {
+        allowedFileTypes: [".jpg", ".jpeg", ".png"],
+        maxNumberOfFiles: 10,
+        maxFileSize: 5 * 1024 * 1024,
+      },
+    })
+      .use(ImageEditor)
+      .use(XHRUpload, {
+        endpoint: `pois/media`,
+        withCredentials: true,
+        shouldRetry: () => false,
+        fieldName: "files",
+        limit: 5,
+      })
+      .on("file-added", (file) => {
+        console.log("file added", file);
+      })
+
+      .on("complete", () => {
+        console.log("complete");
+      })
+  );
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="default">
+          <span>Add a review</span>
+          <PlusIcon className="size-4 ml-2" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="sm:max-w-4xl">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Add a review</AlertDialogTitle>
+          <AlertDialogDescription>
+            Add a review to Colosseum
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div className="grid gap-4 py-2">
+          <div className="flex flex-row gap-2 items-center justify-center">
+            <span className="sr-only">Rating</span>
+            <Rating
+              id="rating"
+              defaultValue={0}
+              onChange={(v) => setRating(v.value)}
+              disabled={false}
+              starsClassName="size-8"
+            />
+          </div>
+
+          <div>
+            <Label htmlFor="content">Review</Label>
+            <Textarea
+              id="content"
+              rows={5}
+              placeholder="Leave a review"
+              {...form.register("content")}
+            />
+            <InputError error={form.formState.errors.content} />
+          </div>
+
+          <Collapsible className="mx-auto sm:max-w-4xl">
+            <CollapsibleTrigger className="w-full">
+              <Button variant="ghost">
+                <UploadIcon className="size-4" />
+              </Button>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="max-w-md sm:max-w-xl md:max-w-3xl">
+                <Dashboard
+                  uppy={uppy}
+                  hideUploadButton={true}
+                  hideCancelButton={true}
+                  height={384}
+                  note={"Maximum 4 images. Limit 5 MB."}
+                />
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() =>
+              alert(
+                JSON.stringify({
+                  content: form.getValues("content"),
+                  rating,
+                })
+              )
+            }
+          >
+            Create
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- web/app/routes/p.$id/components/reviews.tsx
+++ web/app/routes/p.$id/components/reviews.tsx
@@ -1,94 +1,11 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { PlusIcon } from "lucide-react";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import InputError from "~/components/kit/input-error";
-import { Rating } from "~/components/kit/rating";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "~/components/ui/alert-dialog";
-import { Button } from "~/components/ui/button";
-import { Label } from "~/components/ui/label";
-import { Textarea } from "~/components/ui/textarea";
-
-const schema = z.object({
-  content: z.string().min(5).max(2048),
-});
-
-type FormInput = z.infer<typeof schema>;
+import CreateReviewDialog from "./create-review-dialog";
 
 export default function Reviews() {
-  const [rating, setRating] = useState(0);
-  const form = useForm<FormInput>({
-    resolver: zodResolver(schema),
-  });
-
   return (
     <div className="mt-8">
       <div className="flex items-center justify-between">
         <h3 className="text-2xl font-bold tracking-tight">Reviews</h3>
-
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button variant="default">
-              <span>Add a review</span>
-              <PlusIcon className="size-4 ml-2" />
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent className="sm:max-w-xl">
-            <AlertDialogHeader>
-              <AlertDialogTitle>Add a review</AlertDialogTitle>
-            </AlertDialogHeader>
-            <div className="grid gap-4 py-2">
-              <div>
-                <Label htmlFor="content">Review</Label>
-                <Textarea
-                  id="content"
-                  rows={10}
-                  placeholder="Leave a review"
-                  {...form.register("content")}
-                />
-                <InputError error={form.formState.errors.content} />
-              </div>
-
-              <div className="flex flex-row gap-2 items-center">
-                <span className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                  Rating
-                </span>
-                <Rating
-                  id="rating"
-                  defaultValue={0}
-                  onChange={(v) => setRating(v.value)}
-                  disabled={false}
-                  starsClassName="size-4"
-                />
-              </div>
-            </div>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction
-                onClick={() =>
-                  alert(
-                    JSON.stringify({
-                      content: form.getValues("content"),
-                      rating,
-                    })
-                  )
-                }
-              >
-                Create
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        <CreateReviewDialog />
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-4">
         reviews will be here
