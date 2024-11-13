d15b3ff82a0e18fd95fdf837b8d924158ec1e2ea
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-13 18:58:01 +0300

    define new api error

--- api/internal/app/reviews/errors.go
+++ api/internal/app/reviews/errors.go
@@ -9,4 +9,5 @@ import (
 var (
 	ErrIdRequired = errs.NewApiError(http.StatusBadRequest, "0800", errors.New("id is required"))
 	ErrNotFound   = errs.NewApiError(http.StatusNotFound, "0801", errors.New("review not found"))
+	ErrMediaOrder = errs.NewApiError(http.StatusInternalServerError, "0802", errors.New("media order cast error"))
 )
