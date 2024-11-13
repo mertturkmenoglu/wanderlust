0367797b678b8c0831d5c0136dc0c57cfc941b63
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-13 18:59:28 +0300

    implement review deletion and pagination support in reviews service

--- api/internal/app/reviews/repository.go
+++ api/internal/app/reviews/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"wanderlust/internal/pkg/db"
+	"wanderlust/internal/pkg/pagination"
 	"wanderlust/internal/pkg/utils"
 
 	"github.com/jackc/pgx/v5"
@@ -89,4 +90,48 @@ func (r *repository) getReviewById(id string) (db.GetReviewByIdRow, error) {
 
 func (r *repository) getReviewMedia(id string) ([]db.ReviewMedium, error) {
 	return r.di.Db.Queries.GetReviewMedia(context.Background(), id)
+}
+
+func (r *repository) deleteReview(id string) error {
+	return r.di.Db.Queries.DeleteReview(context.Background(), id)
+}
+
+func (r *repository) getReviewsByPoiId(id string, params pagination.Params) ([]db.GetReviewsByPoiIdRow, error) {
+	return r.di.Db.Queries.GetReviewsByPoiId(context.Background(), db.GetReviewsByPoiIdParams{
+		PoiID:  id,
+		Offset: int32(params.Offset),
+		Limit:  int32(params.PageSize),
+	})
+}
+
+func (r *repository) countReviewsByPoiId(id string) (int64, error) {
+	return r.di.Db.Queries.CountReviewsByPoiId(context.Background(), id)
+}
+
+func (r *repository) getReviewMediaByReviewIds(ids []string) ([]db.ReviewMedium, error) {
+	return r.di.Db.Queries.GetReviewMediaByReviewIds(context.Background(), ids)
+}
+
+func (r *repository) addMedia(id string, url string) error {
+	lastMediaOrder, err := r.di.Db.Queries.GetLastMediaOrderOfReview(context.Background(), id)
+
+	if err != nil {
+		return err
+	}
+
+	ord, ok := lastMediaOrder.(int32)
+
+	if !ok {
+		return ErrMediaOrder
+	}
+
+	order := int16(ord) + 1
+
+	_, err = r.di.Db.Queries.CreateReviewMedia(context.Background(), db.CreateReviewMediaParams{
+		ReviewID:   id,
+		Url:        url,
+		MediaOrder: order,
+	})
+
+	return err
 }
--- api/internal/app/reviews/service.go
+++ api/internal/app/reviews/service.go
@@ -1,9 +1,15 @@
 package reviews
 
 import (
+	"context"
 	"errors"
+	"mime/multipart"
+	"strings"
+	"wanderlust/internal/pkg/pagination"
+	"wanderlust/internal/pkg/upload"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/minio/minio-go/v7"
 )
 
 func (s *service) createReview(userId string, dto CreateReviewRequestDto) (CreateReviewResponseDto, error) {
@@ -38,4 +44,95 @@ func (s *service) getReviewById(id string) (GetReviewByIdResponseDto, error) {
 	v := mapToGetReviewByIdResponseDto(review, media)
 
 	return v, nil
+}
+
+func (s *service) deleteReview(id string) error {
+	media, err := s.repository.getReviewMedia(id)
+
+	if err != nil {
+		return err
+	}
+
+	err = s.repository.deleteReview(id)
+
+	if err != nil {
+		return err
+	}
+
+	bucketName := "reviews"
+
+	for _, m := range media {
+		_, after, found := strings.Cut(m.Url, "reviews/")
+
+		if !found {
+			continue
+		}
+
+		err = s.di.Upload.Client.RemoveObject(context.Background(), bucketName, after, minio.RemoveObjectOptions{})
+
+		if err != nil {
+			s.di.Logger.Error("error deleting review media", s.di.Logger.Args("error", err))
+		}
+	}
+
+	return nil
+}
+
+func (s *service) getReviewsByPoiId(id string, params pagination.Params) (GetReviewsByPoiIdResponseDto, int64, error) {
+	res, err := s.repository.getReviewsByPoiId(id, params)
+
+	if err != nil {
+		return GetReviewsByPoiIdResponseDto{}, 0, err
+	}
+
+	total, err := s.repository.countReviewsByPoiId(id)
+
+	if err != nil {
+		return GetReviewsByPoiIdResponseDto{}, 0, err
+	}
+
+	reviewIds := make([]string, 0)
+
+	for _, r := range res {
+		reviewIds = append(reviewIds, r.Review.ID)
+	}
+
+	media, err := s.repository.getReviewMediaByReviewIds(reviewIds)
+
+	if err != nil {
+		return GetReviewsByPoiIdResponseDto{}, 0, err
+	}
+
+	v := mapToGetReviewsByPoiIdResponseDto(res, media)
+
+	return v, total, nil
+}
+
+func (s *service) uploadMedia(id string, mpf *multipart.Form) (string, error) {
+	uploader := sImageUploader{
+		mpf:    mpf,
+		client: s.di.Upload,
+	}
+
+	f, err := uploader.GetSingleFile()
+
+	if err != nil {
+		return "", err
+	}
+
+	defer f.file.Close()
+
+	url, err := uploader.UploadFile(f)
+
+	if err != nil {
+		return "", upload.ErrInvalidFile
+	}
+
+	err = s.repository.addMedia(id, url)
+
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
 }
