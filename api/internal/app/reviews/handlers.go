c17f054f751d921699b128808eb49c87ace702c7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-13 18:58:43 +0300

    implement handlers

--- api/internal/app/reviews/handlers.go
+++ api/internal/app/reviews/handlers.go
@@ -3,6 +3,8 @@ package reviews
 import (
 	"net/http"
 	"wanderlust/internal/pkg/core"
+	"wanderlust/internal/pkg/pagination"
+	"wanderlust/internal/pkg/upload"
 
 	"github.com/labstack/echo/v4"
 )
@@ -41,7 +43,19 @@ func (h *handlers) createReview(c echo.Context) error {
 }
 
 func (h *handlers) deleteReview(c echo.Context) error {
-	return echo.ErrNotImplemented
+	id := c.Param("id")
+
+	if id == "" {
+		return ErrIdRequired
+	}
+
+	err := h.service.deleteReview(id)
+
+	if err != nil {
+		return err
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
 
 func (h *handlers) getReviewsByUsername(c echo.Context) error {
@@ -49,9 +63,58 @@ func (h *handlers) getReviewsByUsername(c echo.Context) error {
 }
 
 func (h *handlers) getReviewsByPoiId(c echo.Context) error {
-	return echo.ErrNotImplemented
+	id := c.Param("id")
+	params, err := pagination.GetParamsFromContext(c)
+
+	if id == "" {
+		return ErrIdRequired
+	}
+
+	if err != nil {
+		return err
+	}
+
+	res, total, err := h.service.getReviewsByPoiId(id, params)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, core.PaginatedResponse{
+		Data:       res,
+		Pagination: pagination.Compute(params, total),
+	})
 }
 
 func (h *handlers) uploadMedia(c echo.Context) error {
-	return echo.ErrNotImplemented
+	id := c.Param("id")
+
+	if id == "" {
+		return ErrIdRequired
+	}
+
+	err := c.Request().ParseMultipartForm(maxMemory)
+
+	if err != nil {
+		return upload.ErrParseMultipartForm
+	}
+
+	mpf := c.Request().MultipartForm
+	err = validateMpf(mpf)
+
+	if err != nil {
+		return err
+	}
+
+	res, err := h.service.uploadMedia(id, mpf)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, core.Response{
+		Data: echo.Map{
+			"url": res,
+		},
+	})
 }
