369f1c744dbd1e162a117afaad511f81fd6eef99
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-17 10:46:20 +0300

    update top margin

--- web/app/routes/p.$id/components/reviews/index.tsx
+++ web/app/routes/p.$id/components/reviews/index.tsx
@@ -3,7 +3,7 @@ import { Section } from "./section";
 
 export default function Reviews() {
   return (
-    <div className="mt-8">
+    <div className="mt-4">
       <Header />
       <div className="flex flex-col gap-4 mt-8">
         <Section />
