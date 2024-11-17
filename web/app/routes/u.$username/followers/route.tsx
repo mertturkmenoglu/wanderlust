1d6fbf84339b4b545295ccd4cfd51e21079a096f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-17 18:22:53 +0300

    use two column grid in followers and following pages

--- web/app/routes/u.$username/followers/route.tsx
+++ web/app/routes/u.$username/followers/route.tsx
@@ -24,7 +24,7 @@ export default function Page() {
       )}
 
       {followers.length > 0 && (
-        <div className="grid grid-cols-1 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {followers.map((follower) => (
             <Link
               to={`/u/${follower.username}`}
--- web/app/routes/u.$username/following/route.tsx
+++ web/app/routes/u.$username/following/route.tsx
@@ -25,7 +25,7 @@ export default function Page() {
       )}
 
       {following.length > 0 && (
-        <div className="grid grid-cols-1 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {following.map((following) => (
             <Link
               to={`/u/${following.username}`}
