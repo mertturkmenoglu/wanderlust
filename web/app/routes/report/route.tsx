9f04350e9c2f6e56656ec707b0b64a9a8cb6547e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-17 19:51:36 +0300

    add dummy report page

---
+++ web/app/routes/report/route.tsx
@@ -0,0 +1,10 @@
+import AppMessage from "~/components/blocks/app-message";
+
+export default function Page() {
+  return (
+    <AppMessage
+      emptyMessage="We are working on reporting feature. Check later."
+      className="my-32"
+    />
+  );
+}
