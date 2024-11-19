b283ee3094570b3dee186d8117958b557194c67d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-19 20:54:53 +0300

    add open in map link

--- web/app/routes/p.$id/components/map-container.tsx
+++ web/app/routes/p.$id/components/map-container.tsx
@@ -1,5 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
+import { ExternalLinkIcon } from "lucide-react";
 import { ClientOnly } from "remix-utils/client-only";
+import { Button } from "~/components/ui/button";
 import { loader } from "../route";
 import { Map } from "./map.client";
 
@@ -7,10 +9,19 @@ export default function MapContainer() {
   const { poi } = useLoaderData<typeof loader>();
   const lat = poi.address.lat;
   const lng = poi.address.lng;
+  const zoom = 17;
 
   return (
     <>
-      <h3 className="text-2xl font-bold">Location</h3>
+      <div className="flex items-end justify-between">
+        <h3 className="text-2xl font-bold">Location</h3>
+        <Button variant="link" className="px-0" size="sm" asChild>
+          <a href={`https://www.google.com/maps/@${lat},${lng},${zoom}z`}>
+            <ExternalLinkIcon className="size-3" />
+            <span className="ml-1">Open in Maps</span>
+          </a>
+        </Button>
+      </div>
       <ClientOnly fallback={<div className="w-full h-[400px] bg-muted mt-4" />}>
         {() => <Map lat={lat} lng={lng} />}
       </ClientOnly>
