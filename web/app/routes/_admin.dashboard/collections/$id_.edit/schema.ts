ac7c8979f49c467b0ea9f6d99e41bfcebe9f959b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-21 21:19:08 +0300

    add validation schemas

--- web/app/routes/_admin.dashboard/collections/$id_.edit/schema.ts
+++ web/app/routes/_admin.dashboard/collections/$id_.edit/schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const schema = z.object({
   name: z.string().min(1).max(128),
-  description: z.string().min(1).max(1024),
+  description: z.string().min(1).max(4096),
 });
 
 export type FormInput = z.infer<typeof schema>;
