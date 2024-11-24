7c4ce159ba07f0b2b10f315eefcb573bdfcb95da
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-24 22:02:48 +0300

    add dummy activities page

--- web/app/routes/u.$username/activities/route.tsx
+++ web/app/routes/u.$username/activities/route.tsx
@@ -1,7 +1,43 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { useQuery } from "@tanstack/react-query";
+import { LoaderIcon } from "lucide-react";
+import invariant from "tiny-invariant";
+import AppMessage from "~/components/blocks/app-message";
+import { getUserActivities } from "~/lib/api-requests";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  invariant(params.username, "username is required");
+  return json({ username: params.username });
+}
+
 export default function ActivitiesPage() {
+  const { username } = useLoaderData<typeof loader>();
+
+  const query = useQuery({
+    queryKey: ["user-activities", username],
+    queryFn: async () => getUserActivities(username),
+  });
+
+  if (query.error) {
+    return (
+      <AppMessage
+        errorMessage="Something went wrong"
+        className="my-16"
+        showBackButton={false}
+      />
+    );
+  }
+
+  if (query.data) {
+    return (
+      <div>
+        <pre>{JSON.stringify(query.data.data, null, 2)}</pre>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <div>This is the user activities page</div>
-    </div>
+    <LoaderIcon className="my-16 mx-auto size-8 text-primary animate-spin" />
   );
 }
