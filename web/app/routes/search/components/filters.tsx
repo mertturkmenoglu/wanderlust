03b46516a31dbc9a2e0e14bee904d61db88d0ef2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-11-29 20:14:49 +0300

    add clear filters button

--- web/app/routes/search/components/filters.tsx
+++ web/app/routes/search/components/filters.tsx
@@ -1,10 +1,32 @@
+import { XIcon } from "lucide-react";
+import { useClearRefinements } from "react-instantsearch";
+import { Button } from "~/components/ui/button";
 import RefinementList from "./refinement-list";
 
 export default function Filters() {
+  const { canRefine, refine } = useClearRefinements();
+
   return (
     <>
-      <div className="text-lg font-semibold tracking-tight underline">
-        Filters
+      <div className="flex items-center justify-between">
+        <div className="text-lg font-semibold tracking-tight underline">
+          Filters
+        </div>
+
+        <div>
+          {canRefine && (
+            <Button
+              disabled={!canRefine}
+              onClick={refine}
+              variant="ghost"
+              size="sm"
+              className=""
+            >
+              <XIcon className="size-3" />
+              <span className="ml-1">Clear</span>
+            </Button>
+          )}
+        </div>
       </div>
 
       <RefinementList attribute="poi.Category.Name" />
