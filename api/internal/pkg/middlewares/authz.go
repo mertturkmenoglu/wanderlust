e10c7409920705630f9796bf4a3b39d48570c625
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-07 16:50:59 +0300

    add new authorization middleware version

--- api/internal/pkg/middlewares/authz.go
+++ api/internal/pkg/middlewares/authz.go
@@ -53,3 +53,46 @@ func Authz(key authz.AuthzAct) echo.MiddlewareFunc {
 		}
 	}
 }
+
+func Authorize(fn authz.AuthzFn) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			az := authz.New(getDb())
+			isAuthorized, err := fn(az, c)
+
+			if err != nil {
+				var echoErr *echo.HTTPError
+
+				if errors.As(err, &echoErr) {
+					return c.JSON(echoErr.Code, api_errors.ErrorResponse{
+						Errors: []api_errors.ErrorDto{
+							{
+								Status: "0000",
+								Code:   fmt.Sprintf("%d", echoErr.Code),
+								Title:  echoErr.Message.(string),
+								Detail: echoErr.Error(),
+							},
+						},
+					})
+				}
+
+				return c.JSON(http.StatusInternalServerError, echo.Map{
+					"errors": []echo.Map{
+						{
+							"status": "0000",
+							"code":   "500",
+							"title":  fmt.Sprintf("An error happened: %v", err.Error()),
+							"detail": fmt.Sprintf("An error happened: %v", err.Error()),
+						},
+					},
+				})
+			}
+
+			if !isAuthorized {
+				return echo.NewHTTPError(http.StatusForbidden, "unauthorized to perform this action")
+			}
+
+			return next(c)
+		}
+	}
+}
