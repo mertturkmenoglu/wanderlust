97d9fe549afd420d0fd3a8e33cc9d44c672767a5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-22 15:47:23 +0300

    add dummy trips pages

---
+++ web/app/routes/trips/_index/route.tsx
@@ -0,0 +1,20 @@
+import SquirrelIcon from "~/components/icons/squirrel";
+
+export default function Page() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <img src="/trip.png" className="size-64" />
+      <h2 className="mt-8 text-4xl font-bold text-center">
+        Plan your next trip with
+        <br />
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-primary">
+          Wanderlust
+        </span>
+      </h2>
+      <SquirrelIcon className="size-32 text-primary my-8" />
+      <div>New Trip</div>
+      <div>Discover Trips</div>
+      <div>My Trips</div>
+    </div>
+  );
+}
---
+++ web/app/routes/trips/route.tsx
@@ -0,0 +1,36 @@
+import { Link, Outlet, redirect } from "react-router";
+import { getMe } from "~/lib/api";
+import { getCookiesFromRequest } from "~/lib/cookies";
+import type { Route } from "./+types/route";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  try {
+    const auth = await getMe({
+      headers: { Cookie: getCookiesFromRequest(request) },
+    });
+
+    if (!auth.data) {
+      throw redirect("/");
+    }
+
+    return { auth: auth.data };
+  } catch (e) {
+    throw redirect("/");
+  }
+}
+
+export function meta({}: Route.MetaArgs): Route.MetaDescriptors {
+  return [
+    {
+      title: "Trips | Wanderlust",
+    },
+  ];
+}
+
+export default function Page() {
+  return (
+    <div className="my-16 max-w-7xl mx-auto">
+      <Outlet />
+    </div>
+  );
+}
