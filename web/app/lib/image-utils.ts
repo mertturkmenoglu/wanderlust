0355d2427a79135f0cad319be0e3b3df6c58e43a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-12-25 15:55:09 +0300

    update image utils

--- web/app/lib/image-utils.ts
+++ web/app/lib/image-utils.ts
@@ -1,13 +1,26 @@
 import { CreateMediaDto } from "./dto";
 
 export function userImage(s: string | null): string {
+  if (globalThis.window !== undefined) {
+    if (s === null) {
+      return window.location.origin + "/profile.png";
+    }
+
+    if (s.startsWith("//")) {
+      return window.location.protocol + s;
+    }
+
+    return s;
+  }
+
+  console.error("WINDOW UNDEFINED");
   if (s == null) {
-    return "http://localhost:5173/profile.png";
+    return "/profile.png";
   }
 
   if (s.startsWith("//")) {
     // TODO: change protocol according to dev later
-    return "http:" + s;
+    return "https:" + s;
   }
 
   return s;
