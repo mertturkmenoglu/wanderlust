09f50a19e9a34d2857c070141037d39d0732691b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-04-29 17:39:40 +0300

    use conveniance methods

--- api/internal/app/health/module.go
+++ api/internal/app/health/module.go
@@ -2,26 +2,24 @@ package health
 
 import (
 	"context"
-	"net/http"
 	"wanderlust/internal/pkg/dto"
 
 	"github.com/danielgtaylor/huma/v2"
 )
 
-func Register(api *huma.API) {
-	huma.Register(*api, huma.Operation{
-		Method:        http.MethodGet,
-		Path:          "/health",
-		Summary:       "Get Health",
-		OperationID:   "get-health",
-		Description:   "A simple health check mechanism to verify that the API is operational",
-		Tags:          []string{"Health"},
-		DefaultStatus: http.StatusOK,
-	}, func(ctx context.Context, input *dto.HealthInput) (*dto.HealthOutput, error) {
+func Register(grp *huma.Group) {
+	grp.UseSimpleModifier(func(op *huma.Operation) {
+		op.Tags = []string{"Health"}
+	})
+
+	huma.Get(grp, "/health", func(ctx context.Context, input *dto.HealthInput) (*dto.HealthOutput, error) {
 		return &dto.HealthOutput{
 			Body: dto.HealthOutputBody{
 				Message: "OK",
 			},
 		}, nil
+	}, func(o *huma.Operation) {
+		o.Summary = "Get Health"
+		o.Description = "A simple health check mechanism to verify that the API is operational"
 	})
 }
