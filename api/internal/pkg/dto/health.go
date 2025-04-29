bf1f2d8e4c8969e61d0935b2210ff63224da4945
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-04-29 18:19:46 +0300

    reformat

--- api/internal/pkg/dto/health.go
+++ api/internal/pkg/dto/health.go
@@ -5,6 +5,7 @@ type HealthInput struct{}
 type HealthOutput struct {
 	Body HealthOutputBody
 }
+
 type HealthOutputBody struct {
 	Message string `json:"message" example:"OK" doc:"Health message of the API"`
 }
