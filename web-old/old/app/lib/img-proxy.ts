b82d27fb5c1c52dc362d11b6eb7e572261de86d0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-01 20:08:49 +0300

    remove old code

--- web-old/old/app/lib/img-proxy.ts
+++
@@ -1,5 +0,0 @@
-export const imgProxyBaseUrl = import.meta.env.VITE_IMG_PROXY_URL ?? "";
-
-export function ipx(ogUrl: string, ops: string): string {
-  return `${imgProxyBaseUrl}/${ops}/${ogUrl}`;
-}
