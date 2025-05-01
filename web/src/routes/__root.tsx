b9b03e121a5cddd0d99147108d24cb438a6b807d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-01 15:05:46 +0300

    update web app

---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,23 @@
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+
+import TanstackQueryLayout from '../integrations/tanstack-query/layout'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <body className="mx-4 md:mx-8 lg:mx-16 2xl:mx-32">
+
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanstackQueryLayout />
+    </body>
+  ),
+})
