23628ab3d7a68c14425427f85f07181ef3b4b26d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-06 03:41:29 +0300

    add poi draft page

---
+++ web/src/routes/_admin/dashboard/pois/drafts/$id/-delete-dialog.tsx
@@ -0,0 +1,72 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from '@tanstack/react-router';
+import { TrashIcon } from 'lucide-react';
+import { toast } from 'sonner';
+
+type Props = {
+  id: string;
+};
+
+export default function DeleteDialog({ id }: Props) {
+  const navigate = useNavigate();
+
+  // TODO: implement delete mutation
+  const deleteMutation = useMutation({
+    mutationKey: ['delete-poi-draft', id],
+    mutationFn: async () => {
+      toast.error('Not implemented');
+    },
+    onSuccess: () => {
+      toast.success('Draft deleted');
+      navigate({
+        to: '/dashboard/pois/drafts',
+      });
+    },
+    onError: () => {
+      toast.error('Something went wrong');
+    },
+  });
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive" size="icon">
+          <TrashIcon className="size-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Are you sure?</DialogTitle>
+        </DialogHeader>
+        <div className="flex items-center space-x-2 text-sm">
+          Are you sure you want to delete this draft? This action cannot be
+          undone and all data will be permanently deleted.
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <Button
+            type="button"
+            variant="destructive"
+            onClick={() => deleteMutation.mutate()}
+          >
+            Delete
+          </Button>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
