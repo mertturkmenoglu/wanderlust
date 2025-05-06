12f8ba9ae630a635edf21189f334e1a9b2119318
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-06 04:07:23 +0300

    use steps

--- web/src/routes/_admin/dashboard/pois/drafts/$id/index.tsx
+++ web/src/routes/_admin/dashboard/pois/drafts/$id/index.tsx
@@ -8,6 +8,8 @@ import StepsIndicator from './-steps-indicator';
 import Step1 from './-steps/step-1';
 import Step2 from './-steps/step-2';
 import Step3 from './-steps/step-3';
+import Step4 from './-steps/step-4';
+import Step6 from './-steps/step-6';
 
 const schema = z.object({
   step: z.number().min(1).max(6).catch(1),
@@ -64,9 +66,9 @@ function RouteComponent() {
       {step === 1 && <Step1 />}
       {step === 2 && <Step2 />}
       {step === 3 && <Step3 />}
-      {/* {step === 4 && <Step4 />} */}
+      {step === 4 && <Step4 />}
       {/* {step === 5 && <Step5 />} */}
-      {/* {step === 6 && <Step6 />} */}
+      {step === 6 && <Step6 />}
     </div>
   );
 }
