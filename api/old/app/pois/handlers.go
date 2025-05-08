492e05949508ef58cc1c6343b370175f2232e64d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-08 09:27:05 +0300

    remove old code

--- api/old/app/pois/handlers.go
+++ api/old/app/pois/handlers.go
@@ -8,63 +8,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func (h *handlers) DeleteDraft(c echo.Context) error {
-	id := c.Param("id")
-
-	if id == "" {
-		return ErrIdRequired
-	}
-
-	err := h.service.deleteDraft(id)
-
-	if err != nil {
-		return err
-	}
-
-	return c.NoContent(http.StatusNoContent)
-}
-
-func (h *handlers) UploadMedia(c echo.Context) error {
-	err := c.Request().ParseMultipartForm(maxMemory)
-
-	if err != nil {
-		return upload.ErrParseMultipartForm
-	}
-
-	mpf := c.Request().MultipartForm
-	err = h.service.validateMediaUpload(mpf)
-
-	if err != nil {
-		return err
-	}
-
-	res, err := h.service.uploadMedia(mpf)
-
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(http.StatusOK, core.Response{
-		Data: res,
-	})
-}
-
-func (h *handlers) DeleteMedia(c echo.Context) error {
-	name := c.QueryParam("name")
-	draftId := c.QueryParam("draftId")
-
-	if name == "" || draftId == "" {
-		return ErrIdRequired
-	}
-
-	err := h.service.deleteMedia(draftId, name)
-
-	if err != nil {
-		return err
-	}
-
-	return c.NoContent(http.StatusNoContent)
-}
 
 func (h *handlers) PublishDraft(c echo.Context) error {
 	id := c.Param("id")
