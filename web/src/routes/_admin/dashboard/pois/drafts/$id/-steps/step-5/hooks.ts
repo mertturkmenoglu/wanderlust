a8921e50c9bdc9d4b98b6d6c060b4f675ba6ef7b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-08 05:53:58 +0300

    update hook

--- web/src/routes/_admin/dashboard/pois/drafts/$id/-steps/step-5/hooks.ts
+++ web/src/routes/_admin/dashboard/pois/drafts/$id/-steps/step-5/hooks.ts
@@ -5,7 +5,7 @@ import { useId } from 'react';
 export function useUpload() {
   const service = useMachine(fileUpload.machine, {
     id: useId(),
-    accept: 'image/*',
+    accept: ['image/jpeg', 'image/png'],
     maxFiles: 1,
     maxFileSize: 1024 * 1024 * 10, // 10MB
   });
