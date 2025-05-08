87312d8d63ad171f72687615aa65a8799f07dcac
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-09 00:01:15 +0300

    add api error check

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,6 +1,6 @@
 import createFetchClient from 'openapi-fetch';
 import createClient from 'openapi-react-query';
-import type { paths } from './api-types';
+import type { components, paths } from './api-types';
 
 export const fetchClient = createFetchClient<paths>({
   baseUrl: 'http://localhost:5000',
@@ -10,3 +10,24 @@ export const fetchClient = createFetchClient<paths>({
 });
 
 export const api = createClient(fetchClient);
+
+export type ApiError = components['schemas']['ErrorModel'];
+
+export function isApiError(v: unknown): v is ApiError {
+  if (typeof v !== 'object' || v === null) {
+    return false;
+  }
+
+  if (
+    'title' in v &&
+    'status' in v &&
+    'detail' in v &&
+    typeof v.title === 'string' &&
+    typeof v.status === 'number' &&
+    typeof v.detail === 'string'
+  ) {
+    return true;
+  }
+
+  return false;
+}
