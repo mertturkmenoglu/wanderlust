4d280c6d29356c326b6b4150a52942d1c25362d2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-09 21:07:10 +0300

    change sign in button link to mask

--- web/src/components/blocks/header/sign-in-button.tsx
+++ web/src/components/blocks/header/sign-in-button.tsx
@@ -1,10 +1,20 @@
-import { Button } from "@/components/ui/button";
-import { Link } from "@tanstack/react-router";
-import { ArrowRight } from "lucide-react";
+import { Button } from '@/components/ui/button';
+import { Link, useLocation } from '@tanstack/react-router';
+import { ArrowRight } from 'lucide-react';
 
 export default function SignInButton() {
+  const location = useLocation();
+
   return (
-    <Link to="/sign-in">
+    <Link
+      to={location.pathname}
+      search={{
+        signInModal: true,
+      }}
+      mask={{
+        to: location.pathname,
+      }}
+    >
       <Button variant="default" className="" asChild>
         <div className="flex items-center gap-2">
           <div>Sign in</div>
