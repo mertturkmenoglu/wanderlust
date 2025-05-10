e8af7de3106b02b320a09d06694a6c673bb03ae7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-10 23:12:10 +0300

    add collections page

---
+++ web/src/routes/_admin/dashboard/collections/$id/items/-delete-item-dialog.tsx
@@ -0,0 +1,86 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { useInvalidator } from '@/hooks/use-invalidator';
+import { api } from '@/lib/api';
+import { TrashIcon } from 'lucide-react';
+import { toast } from 'sonner';
+
+type Props = {
+  collectionId: string;
+  poiName: string;
+  index: number;
+};
+
+export default function DeleteItemDialog({
+  collectionId,
+  poiName,
+  index,
+}: Props) {
+  const invalidator = useInvalidator();
+
+  const mutation = api.useMutation(
+    'delete',
+    '/api/v2/collections/{id}/items/{index}',
+    {
+      onSuccess: async () => {
+        toast.success('Item removed from collection');
+        await invalidator.invalidate();
+      },
+      onError: (e) => {
+        toast.error(e.title ?? 'Something went wrong');
+      },
+    },
+  );
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive" size="icon">
+          <TrashIcon className="size-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-xl">
+        <DialogHeader>
+          <DialogTitle>Remove Item from Collection</DialogTitle>
+        </DialogHeader>
+        <div className="text-sm">
+          <div>
+            Are you sure you want to remove this item from the collection?
+          </div>
+          <div className="mt-2 font-bold">{poiName}</div>
+        </div>
+        <DialogFooter className="sm:justify-end">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+          <Button
+            type="button"
+            variant="destructive"
+            onClick={() =>
+              mutation.mutate({
+                params: {
+                  path: {
+                    id: collectionId,
+                    index,
+                  },
+                },
+              })
+            }
+          >
+            Remove
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- web/src/routes/_admin/dashboard/collections/$id/items/index.tsx
+++ web/src/routes/_admin/dashboard/collections/$id/items/index.tsx
@@ -1,11 +1,222 @@
-import { createFileRoute } from '@tanstack/react-router';
+import AppMessage from '@/components/blocks/app-message';
+import BackLink from '@/components/blocks/back-link';
+import PoiCard from '@/components/blocks/poi-card';
+import { Button } from '@/components/ui/button';
+import { api } from '@/lib/api';
+import { cn } from '@/lib/utils';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { GripVerticalIcon, LinkIcon } from 'lucide-react';
+import { useRef, useState } from 'react';
+import { toast } from 'sonner';
+import { utils } from 'swapy';
+import AddItemDialog from './-add-item-dialog';
+import DeleteItemDialog from './-delete-item-dialog';
 
 export const Route = createFileRoute(
   '/_admin/dashboard/collections/$id/items/',
 )({
   component: RouteComponent,
+  loader: async ({ context, params }) => {
+    return context.queryClient.ensureQueryData(
+      api.queryOptions('get', '/api/v2/collections/{id}', {
+        params: {
+          path: {
+            id: params.id,
+          },
+        },
+      }),
+    );
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/_admin/dashboard/collections/$id/items/"!</div>;
+  const { collection } = Route.useLoaderData();
+  const { items, id, name } = collection;
+  const [addItemOpen, setAddItemOpen] = useState(false);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [slotItemMap, setSlotItemMap] = useState(
+    utils.initSlotItemMap(items, 'poiId'),
+  );
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const mutation = api.useMutation('patch', '/api/v2/collections/{id}/items', {
+    onSuccess: async () => {
+      toast.success('Collection items updated');
+      setIsEditMode(false);
+    },
+    onError: () => {
+      toast.error('Something went wrong');
+    },
+  });
+
+  // useEffect(() => {
+  //   if (!isEditMode) {
+  //     return;
+  //   }
+
+  //   if (!containerRef.current) {
+  //     return;
+  //   }
+
+  //   const swapy = createSwapy(containerRef.current, {
+  //     swapMode: 'drop',
+  //     autoScrollOnDrag: true,
+  //   });
+
+  //   swapy.onSwapEnd(({ slotItemMap }) => {
+  //     setArr(data.array);
+  //   });
+
+  //   return () => {
+  //     swapy.destroy();
+  //   };
+  // }, [isEditMode]);
+
+  return (
+    <>
+      <BackLink
+        href={`/dashboard/collections/${id}`}
+        text="Go back to collection details page"
+      />
+
+      <h2 className="text-4xl font-bold mt-4">{name} Items</h2>
+
+      <div className="flex flex-row gap-2 w-min items-start mt-4">
+        {!isEditMode && (
+          <>
+            <AddItemDialog
+              collectionId={id}
+              open={addItemOpen}
+              setOpen={setAddItemOpen}
+            />
+
+            <Button variant="outline" onClick={() => setIsEditMode(true)}>
+              Edit
+            </Button>
+          </>
+        )}
+
+        {isEditMode && (
+          <>
+            <Button
+              variant="outline"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Cancel
+            </Button>
+
+            <Button
+              variant="default"
+              onClick={() => {
+                mutation.mutate({
+                  params: {
+                    path: {
+                      id,
+                    },
+                  },
+                  body: {
+                    newOrder: slotItemMap.map((el) => ({
+                      listIndex: +el.slot + 1,
+                      poiId: el.item!,
+                    })),
+                  },
+                });
+              }}
+            >
+              Save
+            </Button>
+          </>
+        )}
+      </div>
+
+      <div
+        className={cn('grid grid-cols-1 gap-4 mt-4', {
+          'grid-cols-5': isEditMode,
+          'md:grid-cols-3': !isEditMode,
+        })}
+        ref={containerRef}
+      >
+        {items.map((item, i) => (
+          <div
+            key={item.poiId}
+            className="flex flex-col gap-2"
+            data-swapy-slot={`${i}`}
+          >
+            <div data-swapy-item={item.poiId}>
+              {!isEditMode && (
+                <Link
+                  to="/p/$id"
+                  params={{
+                    id: item.poiId,
+                  }}
+                >
+                  <PoiCard
+                    poi={{
+                      ...item.poi,
+                      image: {
+                        alt: item.poi.media[0]?.alt ?? '',
+                        url: item.poi.media[0]?.url ?? '',
+                      },
+                    }}
+                  />
+                </Link>
+              )}
+
+              {isEditMode && (
+                <div className="flex flex-row gap-4 items-center">
+                  <div className="cursor-grabbing">
+                    <GripVerticalIcon className="size-6" />
+                  </div>
+                  <PoiCard
+                    poi={{
+                      ...item.poi,
+                      image: {
+                        alt: item.poi.media[0]?.alt ?? '',
+                        url: item.poi.media[0]?.url ?? '',
+                      },
+                    }}
+                  />
+                </div>
+              )}
+
+              {!isEditMode && (
+                <div className="flex flex-row gap-2">
+                  <DeleteItemDialog
+                    collectionId={id}
+                    poiName={item.poi.name}
+                    index={i}
+                  />
+
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    onClick={async () => {
+                      await window.navigator.clipboard.writeText(
+                        new URL(
+                          `/p/${item.poiId}`,
+                          window.location.origin,
+                        ).toString(),
+                      );
+                      toast.success('Link copied to clipboard');
+                    }}
+                  >
+                    <LinkIcon className="size-4" />
+                  </Button>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+        {items.length === 0 && (
+          <AppMessage
+            emptyMessage="This collection is empty"
+            showBackButton={false}
+            className="col-span-full my-8"
+          />
+        )}
+      </div>
+    </>
+  );
 }
