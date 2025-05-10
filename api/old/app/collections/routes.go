5d10b4de7c2b8321659b131dbc802a0bc479b5d5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-10 10:43:33 +0300

    remove old code

--- api/old/app/collections/routes.go
+++ api/old/app/collections/routes.go
@@ -10,17 +10,6 @@ import (
 func (m *Module) RegisterRoutes(e *echo.Group) {
 	routes := e.Group("/collections")
 	{
-		routes.GET("/", m.handlers.GetCollections, middlewares.IsAuth, middlewares.Authz(authz.ActCollectionRead))
-		routes.GET("/:id", m.handlers.GetCollectionById)
-		routes.POST("/", m.handlers.CreateCollection, middlewares.ParseBody[CreateCollectionRequestDto], middlewares.IsAuth, middlewares.Authz(authz.ActCollectionCreate))
-		routes.DELETE("/:id", m.handlers.DeleteCollection, middlewares.IsAuth, middlewares.Authz(authz.ActCollectionDelete))
-		routes.PATCH("/:id", m.handlers.UpdateCollection, middlewares.ParseBody[UpdateCollectionRequestDto], middlewares.IsAuth, middlewares.Authz(authz.ActCollectionUpdate))
-
-		routes.GET("/:id/items", m.handlers.GetCollectionItems)
-		routes.POST("/:id/items", m.handlers.CreateCollectionItem, middlewares.ParseBody[CreateCollectionItemRequestDto], middlewares.IsAuth, middlewares.Authz(authz.ActCollectionItemCreate))
-		routes.DELETE("/:id/items/:poiId", m.handlers.DeleteCollectionItem, middlewares.IsAuth, middlewares.Authz(authz.ActCollectionItemDelete))
-		routes.PATCH("/:id/items", m.handlers.UpdateCollectionItems, middlewares.ParseBody[UpdateCollectionItemsRequestDto], middlewares.IsAuth, middlewares.Authz(authz.ActCollectionItemUpdate))
-
 		routes.POST("/:id/poi/:poiId", m.handlers.CreateCollectionPoiRelation, middlewares.IsAuth, middlewares.Authz(authz.ActCollectionPoiRelationCreate))
 		routes.POST("/:id/city/:cityId", m.handlers.CreateCollectionCityRelation, middlewares.IsAuth, middlewares.Authz(authz.ActCollectionCityRelationCreate))
 		routes.DELETE("/:id/poi/:poiId", m.handlers.RemoveCollectionPoiRelation, middlewares.IsAuth, middlewares.Authz(authz.ActCollectionPoiRelationDelete))
