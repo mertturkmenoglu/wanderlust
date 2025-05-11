06fda912310e75edf0ccc04c9fcfffef795cb65e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-11 06:24:56 +0300

    simplify

--- web/src/routes/_admin/dashboard.tsx
+++ web/src/routes/_admin/dashboard.tsx
@@ -1,9 +1,4 @@
-import {
-  createFileRoute,
-  Link,
-  Outlet,
-  redirect,
-} from '@tanstack/react-router';
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_admin/dashboard')({
   component: RouteComponent,
@@ -18,19 +13,8 @@ export const Route = createFileRoute('/_admin/dashboard')({
 
 function RouteComponent() {
   return (
-    <div className="my-16 max-w-7xl mx-auto">
-      <div>
-        <Link to="/dashboard">
-          <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-            Admin Dashboard
-          </h2>
-        </Link>
-      </div>
-      <div className="my-8 flex flex-col gap-8">
-        <div className="w-full">
-          <Outlet />
-        </div>
-      </div>
+    <div className="my-8 max-w-7xl mx-auto">
+      <Outlet />
     </div>
   );
 }
