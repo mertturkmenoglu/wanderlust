48c4940882f0ca43ff3b8b13a7559a780dcd23ae
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-12 04:46:24 +0300

    remove export

--- web/src/routes/bookmarks/index.tsx
+++ web/src/routes/bookmarks/index.tsx
@@ -15,7 +15,7 @@ import { LoaderCircleIcon } from 'lucide-react';
 import { z } from 'zod';
 import BookmarkCard from './-card';
 
-export const bookmarksSearchSchema = z.object({
+const bookmarksSearchSchema = z.object({
   page: z.number().min(1).max(100).default(1).catch(1),
   pageSize: z.number().min(1).max(100).multipleOf(10).default(10).catch(10),
 });
