d856336949e6847225e532c911350d865a0885b2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-12 10:52:29 +0300

    update global middlewares

--- api/cmd/core/bootstrap/bootstrap.go
+++ api/cmd/core/bootstrap/bootstrap.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"encoding/json"
 	"net/http"
 	"time"
 	"wanderlust/internal/pkg/activities"
@@ -13,12 +14,15 @@ import (
 	"wanderlust/internal/pkg/middlewares"
 	"wanderlust/internal/pkg/tasks"
 	"wanderlust/internal/pkg/upload"
+	"wanderlust/internal/pkg/utils"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sony/sonyflake"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 func LoadEnv() {
@@ -33,9 +37,36 @@ func InitGlobalMiddlewares(e *echo.Echo) {
 	e.Use(middleware.Recover())
 
 	if cfg.Get(cfg.ENV) == "dev" {
+		e.Use(otelecho.Middleware("wanderlust", otelecho.WithSkipper(func(c echo.Context) bool {
+			return c.Request().Method == http.MethodOptions
+		})))
+
 		e.Use(middleware.RequestID())
 		e.Use(middlewares.Cors())
 		e.Use(middlewares.PTermLogger)
+		e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+			sp := utils.NewSpan(c.Request().Context(), "Request and Response Dump")
+			defer sp.End()
+
+			paramNames := c.ParamNames()
+			params := make(map[string]any, 0)
+
+			for _, paramName := range paramNames {
+				params[paramName] = c.Param(paramName)
+			}
+
+			paramsBytes, err := json.Marshal(params)
+
+			if err != nil {
+				sp.RecordError(err)
+			}
+
+			sp.SetAttributes(attribute.String("Request Body", string(reqBody)))
+			sp.SetAttributes(attribute.String("Request Params", string(paramsBytes)))
+			sp.SetAttributes(attribute.String("Request Query", c.QueryString()))
+			sp.SetAttributes(attribute.Int("Response Status", c.Response().Status))
+			sp.SetAttributes(attribute.String("Response Body", string(resBody)))
+		}))
 	}
 
 	e.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
