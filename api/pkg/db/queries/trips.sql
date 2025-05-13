5da3c6db101cf37cb6e7b3bfa70c912309758c50
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-13 11:30:02 +0300

    update trips queries

--- api/pkg/db/queries/trips.sql
+++ api/pkg/db/queries/trips.sql
@@ -15,5 +15,99 @@ INSERT INTO trips (
   $6
 ) RETURNING *;
 
+-- name: BatchCreateTrips :copyfrom
+INSERT INTO trips (
+  id,
+  owner_id,
+  status,
+  visibility_level,
+  start_at,
+  end_at
+) VALUES (
+  $1,
+  $2,
+  $3,
+  $4,
+  $5,
+  $6
+);
+
 -- name: GetTripById :one
 SELECT * FROM trips WHERE id = $1;
+
+-- name: GetTripsByIdsPopulated :many
+SELECT
+  sqlc.embed(trips),
+  jsonb_build_object(
+    'id', u.id,
+    'fullName', u.full_name,
+    'username', u.username,
+    'profileImage', u.profile_image
+  ) AS owner,
+  (SELECT json_agg(DISTINCT jsonb_build_object(
+    'id', u.id,
+    'fullName', u.full_name,
+    'username', u.username,
+    'profileImage', u.profile_image
+  ))
+  FROM trips_participants tp
+  JOIN profile par ON par.id = tp.user_id
+  WHERE tp.trip_id = trips.id
+  ) as participants,
+  (SELECT json_agg(to_jsonb(am.*))
+  FROM trips_amenities ta
+  JOIN amenities am ON am.id = ta.amenity_id
+  WHERE ta.trip_id = trips.id
+  ) AS amenities,
+  (SELECT json_agg(DISTINCT jsonb_build_object(
+    'id', tc.id,
+    'from', jsonb_build_object(
+      'id', profile.id,
+      'fullName', profile.full_name,
+      'username', profile.username,
+      'profileImage', profile.profile_image
+    ),
+    'content', tc.content,
+    'createdAt', tc.created_at
+  ))
+  FROM trips_comments tc
+  JOIN profile ON profile.id = tc.from_id
+  WHERE tc.trip_id = trips.id
+  ) AS comments,
+  (SELECT json_agg(to_jsonb(td.*))
+  FROM trips_days td
+  WHERE td.trip_id = trips.id
+  ) AS days,
+  (SELECT json_agg(to_jsonb(tdl.*))
+  FROM trips_days_locations tdl
+  WHERE tdl.day_id in (SELECT id FROM trips_days WHERE trip_id = trips.id)
+  ) AS locations,
+  COALESCE(json_agg(DISTINCT jsonb_build_object(
+    'poi', to_jsonb(poi.*),
+    'poiCategory', to_jsonb(cat.*),
+    'poiAddress', to_jsonb(addr.*),
+    'poiCity', to_jsonb(cities.*),
+    'poiAmenities', COALESCE(poi_amenities.amenities, '[]'),
+    'poiMedia', COALESCE(poi_media.media, '[]')
+  )) FILTER (WHERE trips_days_locations.poi_id IS NOT NULL), '[]') AS ps
+FROM trips
+LEFT JOIN users u ON u.id = trips.owner_id
+LEFT JOIN trips_days ON trips_days.trip_id = trips.id
+LEFT JOIN trips_days_locations ON trips_days_locations.day_id = trips_days.id
+LEFT JOIN pois poi ON poi.id = trips_days_locations.poi_id
+LEFT JOIN categories cat ON cat.id = poi.category_id
+LEFT JOIN addresses addr ON addr.id = poi.address_id
+LEFT JOIN cities ON cities.id = addr.city_id
+LEFT JOIN LATERAL (
+  SELECT json_agg(to_jsonb(a.*)) AS amenities
+  FROM amenities_pois pa
+  JOIN amenities a ON a.id = pa.amenity_id
+  WHERE pa.poi_id = poi.id
+) AS poi_amenities ON TRUE
+LEFT JOIN LATERAL (
+  SELECT json_agg(to_jsonb(pm.*)) AS media
+  FROM media pm
+  WHERE pm.poi_id = poi.id
+) AS poi_media ON TRUE
+WHERE trips.id = ANY($1::TEXT[])
+GROUP BY trips.id, u.id;
