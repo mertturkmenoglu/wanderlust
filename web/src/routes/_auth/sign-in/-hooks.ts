b0dad2876d4e0baa17bc95476a6fa6a2883c4bfa
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-13 11:57:31 +0300

    remove unused import

--- web/src/routes/_auth/sign-in/-hooks.ts
+++ web/src/routes/_auth/sign-in/-hooks.ts
@@ -4,7 +4,6 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from '@tanstack/react-router';
 import { useForm } from 'react-hook-form';
-import { toast } from 'sonner';
 import { type FormInput, FormSchema } from './-schema';
 
 export function useSignInForm() {
