95802f555b4345e673ec663a8c746fd33a489d9f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-14 20:24:08 +0300

    add trips breadcrumb

---
+++ web/src/routes/trips/-breadcrumb.tsx
@@ -0,0 +1,47 @@
+import {
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  Breadcrumb as ShadcnBreadcrumb,
+} from '@/components/ui/breadcrumb';
+import { Link } from '@tanstack/react-router';
+
+type Props = {
+  items: Array<{
+    name: string;
+    href: string;
+  }>;
+};
+
+export function Breadcrumb({ items }: Readonly<Props>) {
+  return (
+    <ShadcnBreadcrumb>
+      <BreadcrumbList className="text-lg">
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link to="/trips">Trips</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        {items.slice(0, -1).map((item) => (
+          <div
+            key={item.href}
+            className="flex items-center flex-wrap gap-1.5 break-words sm:gap-2.5"
+          >
+            <BreadcrumbSeparator className="text-primary" />
+            <BreadcrumbItem key={item.name}>
+              <BreadcrumbLink asChild>
+                <Link to={item.href}>{item.name}</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </div>
+        ))}
+        <BreadcrumbSeparator className="text-primary" />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{items.at(-1)?.name ?? '-'}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </ShadcnBreadcrumb>
+  );
+}
