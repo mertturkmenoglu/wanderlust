881ff28c7527b84cf373da0afc3d3d4412c83409
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-14 16:35:35 +0300

    add loader to my trips page

--- web/src/routes/trips/my-trips/index.tsx
+++ web/src/routes/trips/my-trips/index.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { api } from '@/lib/api';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/trips/my-trips/')({
   component: RouteComponent,
-})
+  loader: ({ context }) => {
+    return context.queryClient.ensureQueryData(
+      api.queryOptions('get', '/api/v2/trips/'),
+    );
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/trips/my-trips/"!</div>
+  const { trips } = Route.useLoaderData();
+
+  return (
+    <div>
+      <pre>{JSON.stringify(trips, null, 2)}</pre>
+    </div>
+  );
 }
