71fe00da3eb97c44361997a20cbcfa35c821e63e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-14 16:30:34 +0300

    update error component

--- web/src/components/blocks/error-component/index.tsx
+++ web/src/components/blocks/error-component/index.tsx
@@ -1,31 +1,73 @@
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
 import { isApiError } from '@/lib/api';
-import type { ErrorComponentProps } from '@tanstack/react-router';
-import AppMessage from '../app-message';
+import { Link, type ErrorComponentProps } from '@tanstack/react-router';
+import { useMemo } from 'react';
 
 export function ErrorComponent({ error }: ErrorComponentProps) {
-  let msg = (() => {
+  const code = useMemo(() => {
     if (isApiError(error)) {
-      if (error.detail) {
-        return error.detail;
-      }
-
-      if (error.title) {
-        return error.title;
-      }
+      return error.status ?? 500;
     }
 
-    return 'Something went wrong';
-  })();
+    return 500;
+  }, [error]);
+
+  const message = useMemo(() => {
+    if (code === 400) {
+      return 'Bad request';
+    } else if (code === 401) {
+      return 'Unauthenticated';
+    } else if (code === 403) {
+      return 'Unauthorized';
+    } else if (code === 404) {
+      return 'Page not found';
+    } else if (code === 500) {
+      return 'Internal server error';
+    } else {
+      return 'Something went wrong';
+    }
+  }, [code]);
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <AppMessage
-        errorMessage={msg}
-        showBackButton={true}
-        backLink="/"
-        backLinkText="Go back to the home page"
-        className="my-32"
-      />
-    </div>
+    <>
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-center lg:mx-32 my-64 gap-24 lg:gap-48">
+        <div className="text-sky-600">
+          <div className="text-sky-600 text-2xl font-bold">Error {code}</div>
+          <div className="text-sky-600 text-6xl font-bold mt-4">{message}</div>
+          <div className="text-xl mt-8">
+            Something squirrely happened somewhere!
+          </div>
+          <div>You can try to refresh the page or go to the homepage.</div>
+          <Link
+            to="/"
+            className="rounded bg-sky-600 text-white px-8 py-2 mt-8 hover:bg-sky-700 flex font-bold text-lg"
+          >
+            Country roads take me home
+          </Link>
+        </div>
+        <div>
+          <img
+            src="/logo.png"
+            alt="Wanderlust"
+            className="size-48 min-w-48 min-h-48"
+          />
+        </div>
+      </div>
+
+      <Collapsible className="mx-auto text-center">
+        <CollapsibleTrigger className="text-sm hover:underline text-center">
+          If you are a wise (or curious) squirrel, click here to see the error.
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <code className="border border-sky-600 rounded p-4 mt-4 block text-xs text-left">
+            <pre>{JSON.stringify(error, null, 2)}</pre>
+          </code>
+        </CollapsibleContent>
+      </Collapsible>
+    </>
   );
 }
