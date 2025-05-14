72a2d86115f9a3970088199b0014e03b623a071a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-14 16:35:20 +0300

    remove child route auth check

--- web/src/routes/trips/$id/index.tsx
+++ web/src/routes/trips/$id/index.tsx
@@ -1,16 +1,9 @@
 import { ErrorComponent } from '@/components/blocks/error-component';
 import { api } from '@/lib/api';
-import { createFileRoute, redirect } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/trips/$id/')({
   component: RouteComponent,
-  beforeLoad: ({ context }) => {
-    if (!context.auth.user) {
-      throw redirect({
-        to: '/sign-in',
-      });
-    }
-  },
   loader: ({ context, params }) => {
     return context.queryClient.ensureQueryData(
       api.queryOptions('get', '/api/v2/trips/{id}', {
