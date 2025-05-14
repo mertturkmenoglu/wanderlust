04822304ccec67a7b8576427c435d75908d1e5ee
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-14 13:12:23 +0300

    update dtos

--- api/pkg/dto/aggregator.go
+++ api/pkg/dto/aggregator.go
@@ -5,18 +5,8 @@ type HomeAggregatorOutput struct {
 }
 
 type HomeAggregatorOutputBody struct {
-	New       []HomeAggregatorPoi `json:"new"`
-	Popular   []HomeAggregatorPoi `json:"popular"`
-	Featured  []HomeAggregatorPoi `json:"featured"`
-	Favorites []HomeAggregatorPoi `json:"favorites"`
-}
-
-type HomeAggregatorPoi struct {
-	ID         string   `json:"id"`
-	Name       string   `json:"name"`
-	AddressID  int32    `json:"addressId"`
-	Address    Address  `json:"address"`
-	CategoryID int16    `json:"categoryId"`
-	Category   Category `json:"category"`
-	FirstMedia Media    `json:"media"`
+	New       []Poi `json:"new"`
+	Popular   []Poi `json:"popular"`
+	Featured  []Poi `json:"featured"`
+	Favorites []Poi `json:"favorites"`
 }
