b23c2b6a1bd98e11e708a94e4595e48078fc5e0a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-14 16:31:07 +0300

    add dummy content to trip details page

--- web/src/routes/trips/$id/index.tsx
+++ web/src/routes/trips/$id/index.tsx
@@ -1,9 +1,36 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { ErrorComponent } from '@/components/blocks/error-component';
+import { api } from '@/lib/api';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/trips/$id/')({
   component: RouteComponent,
+  beforeLoad: ({ context }) => {
+    if (!context.auth.user) {
+      throw redirect({
+        to: '/sign-in',
+      });
+    }
+  },
+  loader: ({ context, params }) => {
+    return context.queryClient.ensureQueryData(
+      api.queryOptions('get', '/api/v2/trips/{id}', {
+        params: {
+          path: {
+            id: params.id,
+          },
+        },
+      }),
+    );
+  },
+  errorComponent: ErrorComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/trips/$id/"!</div>;
+  const { trip } = Route.useLoaderData();
+
+  return (
+    <div>
+      <pre>{JSON.stringify(trip, null, 2)}</pre>
+    </div>
+  );
 }
