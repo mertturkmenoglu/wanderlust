b9f4e94c03578b7f61af029acd026a38191c40ac
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-15 09:22:33 +0300

    get invites on trips invites page

--- web/src/routes/trips/invites/index.tsx
+++ web/src/routes/trips/invites/index.tsx
@@ -1,9 +1,23 @@
+import { api } from '@/lib/api';
 import { createFileRoute } from '@tanstack/react-router';
+import { Breadcrumb } from '../-breadcrumb';
 
 export const Route = createFileRoute('/trips/invites/')({
   component: RouteComponent,
+  loader: ({ context }) => {
+    return context.queryClient.ensureQueryData(
+      api.queryOptions('get', '/api/v2/trips/invites'),
+    );
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/trips/invites/"!</div>;
+  const { invites } = Route.useLoaderData();
+
+  return (
+    <div>
+      <Breadcrumb items={[{ name: 'My Invites', href: '/trips/invites' }]} />
+      <pre className="mt-8">{JSON.stringify(invites, null, 2)}</pre>
+    </div>
+  );
 }
