b4371ab4ce4a346250b852992c6d192a3b4757f6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-17 10:29:02 +0300

    add logger init

--- api/cmd/core/main.go
+++ api/cmd/core/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"wanderlust/cmd/core/bootstrap"
+	"wanderlust/pkg/logs"
+	"wanderlust/pkg/tracing"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humaecho"
@@ -11,21 +13,23 @@ import (
 func main() {
 	bootstrap.LoadEnv()
 
-	tracingShutdown := bootstrap.InitTracer()
+	logger := logs.NewZapLogger(tracing.NewOtlpWriter())
+	tracingShutdown := bootstrap.InitTracer(logger)
+
 	defer tracingShutdown()
 
 	e := echo.New()
 
 	humaConfig := huma.DefaultConfig(bootstrap.API_NAME, bootstrap.API_VERSION)
 
-	bootstrap.InitGlobalMiddlewares(e)
+	bootstrap.InitGlobalMiddlewares(e, logger)
 	bootstrap.SetupOpenApiSecurityConfig(&humaConfig)
 
 	huma.DefaultArrayNullable = false
 	api := humaecho.New(e, humaConfig)
 
 	bootstrap.SetupOpenApiDocs(&api)
-	bootstrap.RegisterRoutes(&api)
+	bootstrap.RegisterRoutes(&api, logger)
 	bootstrap.RunMigrations()
 	bootstrap.ScalarDocs(e)
 	bootstrap.StartServer(e)
