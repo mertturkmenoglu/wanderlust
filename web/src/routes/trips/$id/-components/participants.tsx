c4ba188073924ba39326c7488f6080914ff73382
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-18 20:13:34 +0300

    update participants dialog visibility section

--- web/src/routes/trips/$id/-components/participants.tsx
+++ web/src/routes/trips/$id/-components/participants.tsx
@@ -3,10 +3,7 @@ import UserImage from '@/components/blocks/user-image';
 import Spinner from '@/components/kit/spinner';
 import {
   AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
@@ -25,7 +22,7 @@ import { cn } from '@/lib/utils';
 import { useDebouncedValue } from '@tanstack/react-pacer';
 import { getRouteApi, Link } from '@tanstack/react-router';
 import { GlobeIcon, LockIcon, PlusIcon, UsersIcon, XIcon } from 'lucide-react';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export function ParticipantsDialog() {
   const [open, setOpen] = useState(false);
@@ -74,11 +71,6 @@ export function ParticipantsDialog() {
             <VisibilitySection />
           </TabsContent>
         </Tabs>
-
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Save</AlertDialogAction>
-        </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
   );
@@ -269,43 +261,76 @@ function ShowSearch() {
 function VisibilitySection() {
   const route = getRouteApi('/trips/$id/');
   const { trip } = route.useLoaderData();
+  const [value, setValue] = useState(trip.visibilityLevel);
+  const isChanged = value !== trip.visibilityLevel;
+  const infoText = useMemo(() => {
+    if (value === 'public') {
+      return 'Anyone can see this trip';
+    } else if (value === 'friends') {
+      return 'Only participants can see this trip';
+    } else {
+      return 'Only you can see this trip';
+    }
+  }, [value]);
 
   return (
-    <RadioGroup
-      defaultValue={trip.visibilityLevel}
-      className="grid grid-cols-3 gap-4 h-[200px] items-center"
-    >
-      <div>
-        <RadioGroupItem value="public" id="public" className="peer sr-only" />
-        <Label
-          htmlFor="public"
-          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-        >
-          <GlobeIcon className="size-4 text-muted-foreground" />
-          <span>Public</span>
-        </Label>
-      </div>
-      <div>
-        <RadioGroupItem value="friends" id="friends" className="peer sr-only" />
-        <Label
-          htmlFor="friends"
-          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-        >
-          <UsersIcon className="size-4 text-muted-foreground" />
-          <span>Participants</span>
-        </Label>
-      </div>
-      <div>
-        <RadioGroupItem value="private" id="private" className="peer sr-only" />
-        <Label
-          htmlFor="private"
-          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-        >
-          <LockIcon className="size-4 text-muted-foreground" />
-          <span>Private</span>
-        </Label>
-      </div>
-    </RadioGroup>
+    <div className="h-[200px] flex flex-col justify-center">
+      <RadioGroup
+        value={value}
+        onValueChange={setValue}
+        className="grid grid-cols-3 gap-4 items-center"
+      >
+        <div>
+          <RadioGroupItem value="public" id="public" className="peer sr-only" />
+          <Label
+            htmlFor="public"
+            className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+          >
+            <GlobeIcon className="size-4 text-muted-foreground" />
+            <span>Public</span>
+          </Label>
+        </div>
+        <div>
+          <RadioGroupItem
+            value="friends"
+            id="friends"
+            className="peer sr-only"
+          />
+          <Label
+            htmlFor="friends"
+            className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+          >
+            <UsersIcon className="size-4 text-muted-foreground" />
+            <span>Participants</span>
+          </Label>
+        </div>
+        <div>
+          <RadioGroupItem
+            value="private"
+            id="private"
+            className="peer sr-only"
+          />
+          <Label
+            htmlFor="private"
+            className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+          >
+            <LockIcon className="size-4 text-muted-foreground" />
+            <span>Private</span>
+          </Label>
+        </div>
+      </RadioGroup>
+      <div className="text-sm mt-4 text-muted-foreground">{infoText}</div>
+
+      <Button
+        variant="default"
+        size="sm"
+        className="mt-4 flex w-min mx-auto px-8"
+        onClick={() => alert('save')}
+        disabled={!isChanged}
+      >
+        Save
+      </Button>
+    </div>
   );
 }
 
