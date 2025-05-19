82013df1a81d85f99779d907aec7833412a767e2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-19 17:52:14 +0300

    update trip detail page

--- web/src/routes/trips/$id/index.tsx
+++ web/src/routes/trips/$id/index.tsx
@@ -1,4 +1,7 @@
+import CollapsibleText from '@/components/blocks/collapsible-text';
 import { createFileRoute, getRouteApi } from '@tanstack/react-router';
+import { differenceInDays } from 'date-fns';
+import { InfoCard } from './-info-card';
 
 export const Route = createFileRoute('/trips/$id/')({
   component: RouteComponent,
@@ -9,8 +12,27 @@ function RouteComponent() {
   const { trip } = route.useLoaderData();
 
   return (
-    <div className="">
-      <pre className="mt-8">{JSON.stringify(trip, null, 2)}</pre>
+    <div className="mt-4">
+      <div>
+        <div className="text-lg underline">Description</div>
+        <CollapsibleText
+          text={
+            trip.description.length > 0 ? trip.description : 'No description.'
+          }
+          className="mt-2"
+        />
+      </div>
+
+      <div className="grid sm:grid-cols-3 gap-4 mt-4">
+        <InfoCard type="participants" count={trip.participants.length} />
+
+        <InfoCard
+          type="days"
+          count={differenceInDays(trip.endAt, trip.startAt)}
+        />
+
+        <InfoCard type="locations" count={trip.locations.length} />
+      </div>
     </div>
   );
 }
