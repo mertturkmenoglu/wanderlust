b19aa50273262ada583aba802437a603948ac1ea
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-19 12:22:47 +0300

    update table definitions

--- api/pkg/db/migrations/000013_create_trips_table.down.sql
+++ api/pkg/db/migrations/000013_create_trips_table.down.sql
@@ -1,41 +1,35 @@
-DROP INDEX IF EXISTS idx_trips_participants_unique;
+DROP INDEX IF EXISTS idx_trip_participants_unique;
 
-DROP INDEX IF EXISTS idx_trips_participants_user;
+DROP INDEX IF EXISTS idx_trip_participants_user;
 
-DROP INDEX IF EXISTS idx_trips_participants_trip;
+DROP INDEX IF EXISTS idx_trip_participants_trip;
 
-DROP TABLE IF EXISTS trips_participants;
+DROP TABLE IF EXISTS trip_participants;
 
-DROP INDEX IF EXISTS idx_trips_amenities_unique;
+DROP INDEX IF EXISTS idx_trip_amenities_trip;
 
-DROP INDEX IF EXISTS idx_trips_amenities_amenity;
+DROP TABLE IF EXISTS trip_amenities;
 
-DROP INDEX IF EXISTS idx_trips_amenities_trip;
+DROP INDEX IF EXISTS idx_trip_locations_poi;
 
-DROP TABLE IF EXISTS trips_amenities;
+DROP TABLE IF EXISTS trip_locations;
 
-DROP INDEX IF EXISTS idx_trips_days_locations_poi;
+DROP INDEX IF EXISTS idx_trip_comments_from;
 
-DROP TABLE IF EXISTS trips_days_locations;
+DROP INDEX IF EXISTS idx_trip_comments_trip;
 
-DROP TABLE IF EXISTS trips_days;
+DROP TABLE IF EXISTS trip_comments;
 
-DROP INDEX IF EXISTS idx_trips_comments_from;
+DROP INDEX IF EXISTS idx_trip_invites_unique;
 
-DROP INDEX IF EXISTS idx_trips_comments_trip;
+DROP INDEX IF EXISTS idx_trip_invites_to;
 
-DROP TABLE IF EXISTS trips_comments;
+DROP INDEX IF EXISTS idx_trip_invites_from;
 
-DROP INDEX IF EXISTS idx_trips_invites_unique;
+DROP INDEX IF EXISTS idx_trip_invites_trip;
 
-DROP INDEX IF EXISTS idx_trips_invites_to;
+DROP TABLE IF EXISTS trip_invites;
 
-DROP INDEX IF EXISTS idx_trips_invites_from;
-
-DROP INDEX IF EXISTS idx_trips_invites_trip;
-
-DROP TABLE IF EXISTS trips_invites;
-
-DROP INDEX IF EXISTS idx_trips_owner;
+DROP INDEX IF EXISTS idx_trip_owner;
 
 DROP TABLE IF EXISTS trips;
--- api/pkg/db/migrations/000013_create_trips_table.up.sql
+++ api/pkg/db/migrations/000013_create_trips_table.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS trips (
   owner_id TEXT NOT NULL,
   status VARCHAR(16) NOT NULL,
   title VARCHAR(128) NOT NULL,
+  description VARCHAR(1024) NOT NULL DEFAULT '',
   visibility_level VARCHAR(16) NOT NULL,
   start_at TIMESTAMPTZ NOT NULL,
   end_at TIMESTAMPTZ NOT NULL,
@@ -18,96 +19,87 @@ CREATE OR REPLACE TRIGGER update_trips_timestamp BEFORE
 UPDATE
   ON trips FOR EACH ROW EXECUTE FUNCTION update_timestamp();
 
-CREATE TABLE IF NOT EXISTS trips_invites (
+CREATE TABLE IF NOT EXISTS trip_invites (
   id TEXT PRIMARY KEY,
   trip_id TEXT NOT NULL,
   from_id TEXT NOT NULL,
   to_id TEXT NOT NULL,
   sent_at TIMESTAMPTZ NOT NULL,
   expires_at TIMESTAMPTZ NOT NULL,
+  trip_title VARCHAR(128) NOT NULL,
+  trip_description VARCHAR(1024) NOT NULL,
   role VARCHAR(16) NOT NULL,
   CONSTRAINT
-    fk_trips_invites_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
+    fk_trip_invites_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_trips_invites_from FOREIGN KEY (from_id) REFERENCES users(id) ON DELETE CASCADE,
+    fk_trip_invites_from FOREIGN KEY (from_id) REFERENCES users(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_trips_invites_to FOREIGN KEY (to_id) REFERENCES users(id) ON DELETE CASCADE
+    fk_trip_invites_to FOREIGN KEY (to_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_trips_invites_trip ON trips_invites(trip_id);
+CREATE INDEX IF NOT EXISTS idx_trip_invites_trip ON trip_invites(trip_id);
 
-CREATE INDEX IF NOT EXISTS idx_trips_invites_from ON trips_invites(from_id);
+CREATE INDEX IF NOT EXISTS idx_trip_invites_from ON trip_invites(from_id);
 
-CREATE INDEX IF NOT EXISTS idx_trips_invites_to ON trips_invites(to_id);
+CREATE INDEX IF NOT EXISTS idx_trip_invites_to ON trip_invites(to_id);
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_trips_invites_unique ON trips_invites(trip_id, from_id, to_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_trip_invites_unique ON trip_invites(trip_id, to_id);
 
-CREATE TABLE IF NOT EXISTS trips_comments (
+CREATE TABLE IF NOT EXISTS trip_comments (
   id TEXT PRIMARY KEY,
   trip_id TEXT NOT NULL,
   from_id TEXT NOT NULL,
   content VARCHAR(255) NOT NULL,
   created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
   CONSTRAINT
-    fk_trips_comments_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
+    fk_trip_comments_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_trips_comments_from FOREIGN KEY (from_id) REFERENCES users(id) ON DELETE CASCADE
+    fk_trip_comments_from FOREIGN KEY (from_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_trips_comments_trip ON trips_comments(trip_id);
+CREATE INDEX IF NOT EXISTS idx_trip_comments_trip ON trip_comments(trip_id);
 
-CREATE INDEX IF NOT EXISTS idx_trips_comments_from ON trips_comments(from_id);
+CREATE INDEX IF NOT EXISTS idx_trip_comments_from ON trip_comments(from_id);
 
-CREATE TABLE IF NOT EXISTS trips_days (
+CREATE TABLE IF NOT EXISTS trip_locations (
   trip_id TEXT NOT NULL,
-  day_no INT NOT NULL,
-  description VARCHAR(255) NOT NULL,
-  PRIMARY KEY (trip_id, day_no),
-  CONSTRAINT
-    fk_trips_days_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS trips_days_locations (
-  trip_id TEXT NOT NULL,
-  day_no INT NOT NULL,
+  scheduled_time TIMESTAMPTZ NOT NULL,
   poi_id TEXT NOT NULL,
-  PRIMARY KEY (trip_id, day_no, poi_id),
+  description VARCHAR(1024) NOT NULL,
+  PRIMARY KEY (trip_id, poi_id),
   CONSTRAINT
-    fk_trips_days_locations_days FOREIGN KEY (trip_id, day_no) REFERENCES trips_days(trip_id, day_no) ON DELETE CASCADE,
+    fk_trip_locations_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_trips_days_locations_poi FOREIGN KEY (poi_id) REFERENCES pois(id) ON DELETE CASCADE
+    fk_trip_locations_poi FOREIGN KEY (poi_id) REFERENCES pois(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_trips_days_locations_poi ON trips_days_locations(poi_id);
+CREATE INDEX IF NOT EXISTS idx_trip_locations_poi ON trip_locations(poi_id);
 
-CREATE TABLE IF NOT EXISTS trips_amenities (
+CREATE TABLE IF NOT EXISTS trip_amenities (
   trip_id TEXT NOT NULL,
   amenity_id INT NOT NULL,
+  PRIMARY KEY (trip_id, amenity_id),
   CONSTRAINT
-    fk_trips_amenities_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
+    fk_trip_amenities_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_trips_amenities_amenity FOREIGN KEY (amenity_id) REFERENCES amenities(id) ON DELETE CASCADE
+    fk_trip_amenities_amenity FOREIGN KEY (amenity_id) REFERENCES amenities(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_trips_amenities_trip ON trips_amenities(trip_id);
+CREATE INDEX IF NOT EXISTS idx_trip_amenities_trip ON trip_amenities(trip_id);
 
-CREATE INDEX IF NOT EXISTS idx_trips_amenities_amenity ON trips_amenities(amenity_id);
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_trips_amenities_unique ON trips_amenities(trip_id, amenity_id);
-
-CREATE TABLE IF NOT EXISTS trips_participants (
+CREATE TABLE IF NOT EXISTS trip_participants (
   id TEXT PRIMARY KEY,
   trip_id TEXT NOT NULL,
   user_id TEXT NOT NULL,
   role VARCHAR(16) NOT NULL,
   CONSTRAINT
-    fk_trips_participants_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
+    fk_trip_participants_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_trips_participants_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    fk_trip_participants_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_trips_participants_trip ON trips_participants(trip_id);
+CREATE INDEX IF NOT EXISTS idx_trip_participants_trip ON trip_participants(trip_id);
 
-CREATE INDEX IF NOT EXISTS idx_trips_participants_user ON trips_participants(user_id);
+CREATE INDEX IF NOT EXISTS idx_trip_participants_user ON trip_participants(user_id);
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_trips_participants_unique ON trips_participants(trip_id, user_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_trip_participants_unique ON trip_participants(trip_id, user_id);
