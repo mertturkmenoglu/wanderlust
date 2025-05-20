1444061e121763fcf973c4ea6f27664d2f26e583
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-20 12:01:24 +0300

    format

--- web/src/routes/trips/$id/amenities/-view.tsx
+++ web/src/routes/trips/$id/amenities/-view.tsx
@@ -18,7 +18,10 @@ export function ViewAmenities() {
   return (
     <ul className="mt-4 list-disc grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 list-inside">
       {trip.requestedAmenities.map((amenity) => (
-        <li key={amenity.id} className="text-sm text-muted-foreground">
+        <li
+          key={amenity.id}
+          className="text-sm text-muted-foreground"
+        >
           {amenity.name}
         </li>
       ))}
