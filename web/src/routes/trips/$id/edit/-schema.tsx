d1f8c6aadb188582c30c74c74171669b8f45a74b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-20 13:14:47 +0300

    add edit trip schema

---
+++ web/src/routes/trips/$id/edit/-schema.tsx
@@ -0,0 +1,75 @@
+import { isBefore } from 'date-fns';
+import { z } from 'zod';
+
+export const visibility = ['public', 'friends', 'private'] as const;
+
+export type Visibility = (typeof visibility)[number];
+
+export const visibilityOptions: Array<{
+  label: string;
+  value: Visibility;
+  info: string;
+}> = [
+  {
+    label: 'Public',
+    value: 'public',
+    info: 'Anyone can see your trip.',
+  },
+  {
+    label: 'Friends',
+    value: 'friends',
+    info: 'Only your friends can see your trip.',
+  },
+  {
+    label: 'Private',
+    value: 'private',
+    info: 'Only you can see your trip. Any participants and comments will be removed.',
+  },
+];
+
+export const schema = z
+  .object({
+    title: z.string().min(1).max(128),
+    description: z.string().min(0).max(1024),
+    startAt: z.string(),
+    endAt: z.string(),
+    visibility: z.enum(visibility),
+  })
+  .superRefine((data, ctx) => {
+    if (!isBefore(data.startAt, data.endAt)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_date,
+        message: 'Start date must be before end date',
+        path: ['startAt'],
+        fatal: true,
+      });
+
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_date,
+        message: 'Start date must be before end date',
+        path: ['endAt'],
+        fatal: true,
+      });
+
+      return z.NEVER;
+    }
+
+    if (isBefore(data.startAt, new Date())) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Start date must be in the future',
+        path: ['startAt'],
+        fatal: true,
+      });
+
+      return z.NEVER;
+    }
+  });
+
+export function asVisibility(v: string): Visibility {
+  if (v === 'public' || v === 'friends' || v === 'private') {
+    return v;
+  }
+
+  throw new Error('Invalid visibility');
+}
