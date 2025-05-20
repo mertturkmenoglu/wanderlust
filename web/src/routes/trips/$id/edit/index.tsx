f3102013c0d8b38f9e91209c996d2a63f85ddb34
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-20 13:14:58 +0300

    add edit trip page

--- web/src/routes/trips/$id/edit/index.tsx
+++ web/src/routes/trips/$id/edit/index.tsx
@@ -1,52 +1,162 @@
+import InputError from '@/components/kit/input-error';
+import InputInfo from '@/components/kit/input-info';
 import { Button } from '@/components/ui/button';
-import { useInvalidator } from '@/hooks/use-invalidator';
-import { api } from '@/lib/api';
-import { createFileRoute, useNavigate } from '@tanstack/react-router';
-import { toast } from 'sonner';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Textarea } from '@/components/ui/textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createFileRoute, getRouteApi, Link } from '@tanstack/react-router';
+import { formatDate } from 'date-fns';
+import { AlertTriangleIcon } from 'lucide-react';
+import { Controller, useForm } from 'react-hook-form';
+import { DeleteButton } from './-delete-button';
+import { asVisibility, schema, visibilityOptions } from './-schema';
 
 export const Route = createFileRoute('/trips/$id/edit/')({
   component: RouteComponent,
 });
 
-function RouteComponent() {
-  const { id } = Route.useParams();
-  const invalidator = useInvalidator();
-  const navigate = useNavigate();
+const datetimeFormat = "yyyy-MM-dd'T'HH:mm";
 
-  const deleteTripMutation = api.useMutation('delete', '/api/v2/trips/{id}', {
-    onSuccess: async () => {
-      await invalidator.invalidate();
-      await navigate({
-        to: '/trips',
-      });
-      toast.success('Trip deleted');
+function RouteComponent() {
+  const rootRoute = getRouteApi('/trips/$id');
+  const { trip } = rootRoute.useLoaderData();
+
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      description: trip.description,
+      startAt: formatDate(trip.startAt, datetimeFormat),
+      endAt: formatDate(trip.endAt, datetimeFormat),
+      visibility: asVisibility(trip.visibilityLevel),
+      title: trip.title,
     },
   });
 
   return (
     <div>
-      <Button
-        variant="destructive"
-        size="sm"
-        onClick={(e) => {
-          e.preventDefault();
-          if (
-            confirm(
-              'Are you sure you want to delete this trip? This action is irreversible.',
-            )
-          ) {
-            deleteTripMutation.mutate({
-              params: {
-                path: {
-                  id,
-                },
-              },
-            });
-          }
-        }}
+      <form
+        onSubmit={form.handleSubmit((data) => {
+          console.log(data);
+        })}
+        className="w-full flex flex-col"
       >
-        Delete Trip
-      </Button>
+        <DeleteButton className="self-end mt-4" />
+
+        <div className="mt-4">
+          <Label htmlFor="title">Title</Label>
+          <Input
+            type="text"
+            id="title"
+            placeholder="My Awesome Trip"
+            autoComplete="off"
+            className="mt-1"
+            {...form.register('title')}
+          />
+          <InputError error={form.formState.errors.title} />
+        </div>
+
+        <div className="mt-4">
+          <Label htmlFor="desc">Description</Label>
+          <Textarea
+            id="desc"
+            placeholder="You can add a description for you trip."
+            autoComplete="off"
+            className="mt-1"
+            {...form.register('description')}
+          />
+          <InputError error={form.formState.errors.description} />
+        </div>
+
+        <div className="mt-4">
+          <Controller
+            name="visibility"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <div>
+                  <Label htmlFor="visibility">Visibility</Label>
+
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value ?? undefined}
+                  >
+                    <SelectTrigger id="visibility" className="mt-1 w-full">
+                      <SelectValue placeholder="Select a visibility" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {visibilityOptions.map((op) => (
+                        <SelectItem key={op.value} value={op.value}>
+                          {op.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <InputInfo
+                    text={
+                      visibilityOptions.find((op) => op.value === field.value)
+                        ?.info ?? ''
+                    }
+                    className="mt-1"
+                  />
+                </div>
+              );
+            }}
+          />
+          <InputError error={form.formState.errors.visibility} />
+        </div>
+
+        <div className="mt-4">
+          <Label htmlFor="startAt">Start Date</Label>
+          <Input
+            id="startAt"
+            type="datetime-local"
+            className="mt-1"
+            {...form.register('startAt')}
+          />
+          <InputError error={form.formState.errors.startAt} />
+        </div>
+
+        <div className="mt-4">
+          <Label htmlFor="endAt">End Date</Label>
+          <Input
+            id="endAt"
+            type="datetime-local"
+            placeholder='Format: "YYYY-MM-DD"'
+            className="mt-1"
+            {...form.register('endAt')}
+          />
+          <InputError error={form.formState.errors.endAt} />
+        </div>
+
+        <div className="mt-4 flex items-center">
+          <div className="text-muted-foreground text-xs max-w-sm flex gap-2">
+            <AlertTriangleIcon className="size-6 text-yellow-400" />
+            If you change start or end date, we will automatically move the
+            locations to the new date range.
+          </div>
+          <Button size="sm" variant="ghost" className="ml-auto" asChild>
+            <Link
+              to="/trips/$id"
+              params={{
+                id: trip.id,
+              }}
+            >
+              Cancel
+            </Link>
+          </Button>
+          <Button size="sm" variant="default" className="ml-2">
+            Save
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
