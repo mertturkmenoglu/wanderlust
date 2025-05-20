f36d777af73919bc66b97f7a30966af72691277a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-20 12:12:16 +0300

    add dummy delete trip functionality

--- web/src/routes/trips/$id/edit/index.tsx
+++ web/src/routes/trips/$id/edit/index.tsx
@@ -1,9 +1,52 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
+import { useInvalidator } from '@/hooks/use-invalidator';
+import { api } from '@/lib/api';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { toast } from 'sonner';
 
 export const Route = createFileRoute('/trips/$id/edit/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/trips/$id/edit/"!</div>;
+  const { id } = Route.useParams();
+  const invalidator = useInvalidator();
+  const navigate = useNavigate();
+
+  const deleteTripMutation = api.useMutation('delete', '/api/v2/trips/{id}', {
+    onSuccess: async () => {
+      await invalidator.invalidate();
+      await navigate({
+        to: '/trips',
+      });
+      toast.success('Trip deleted');
+    },
+  });
+
+  return (
+    <div>
+      <Button
+        variant="destructive"
+        size="sm"
+        onClick={(e) => {
+          e.preventDefault();
+          if (
+            confirm(
+              'Are you sure you want to delete this trip? This action is irreversible.',
+            )
+          ) {
+            deleteTripMutation.mutate({
+              params: {
+                path: {
+                  id,
+                },
+              },
+            });
+          }
+        }}
+      >
+        Delete Trip
+      </Button>
+    </div>
+  );
 }
