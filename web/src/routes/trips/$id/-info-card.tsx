f42dd2dccc61c62529c392e36c0cf901957fa90f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-20 20:16:57 +0300

    update info card styles

--- web/src/routes/trips/$id/-info-card.tsx
+++ web/src/routes/trips/$id/-info-card.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils';
 import { CalendarIcon, MapPinnedIcon, UsersIcon } from 'lucide-react';
 
 type Props = {
@@ -15,10 +16,38 @@ export function InfoCard({ type, count }: Props) {
   const Icon = icons[type];
 
   return (
-    <div className="border border-border p-4 rounded-md flex flex-col gap-2 aspect-[5/2]">
-      <Icon className="size-8 text-primary" />
-      <div className="text-lg mt-4">
-        {count} {type}
+    <div
+      className={cn('flex flex-col gap-2 aspect-[9/2] rounded-md', {
+        'bg-pink-900': type === 'days',
+        'bg-emerald-900': type === 'locations',
+        'bg-sky-900': type === 'participants',
+      })}
+    >
+      <div
+        className={cn('w-min p-2 rounded-md m-4', {
+          'bg-pink-50': type === 'days',
+          'bg-emerald-50': type === 'locations',
+          'bg-sky-50': type === 'participants',
+        })}
+      >
+        <Icon
+          className={cn('size-6', {
+            'text-pink-400': type === 'days',
+            'text-emerald-400': type === 'locations',
+            'text-sky-400': type === 'participants',
+          })}
+        />
+      </div>
+      <div
+        className={cn('mt-auto rounded-b-md px-2 py-1.5', {
+          'bg-pink-600': type === 'days',
+          'bg-emerald-600': type === 'locations',
+          'bg-sky-600': type === 'participants',
+        })}
+      >
+        <div className="text-sm text-white font-bold">
+          {count} {type}
+        </div>
       </div>
     </div>
   );
