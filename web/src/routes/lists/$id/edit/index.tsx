44b03e95a166f53d93e818b0cf7f3efbc5a1a9f4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-22 13:40:44 +0300

    update list edit page

--- web/src/routes/lists/$id/edit/index.tsx
+++ web/src/routes/lists/$id/edit/index.tsx
@@ -1,12 +1,17 @@
+import AppMessage from '@/components/blocks/app-message';
 import BackLink from '@/components/blocks/back-link';
 import InputInfo from '@/components/kit/input-info';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
+import { useInvalidator } from '@/hooks/use-invalidator';
 import { api } from '@/lib/api';
+import { ipx } from '@/lib/ipx';
 import { cn } from '@/lib/utils';
-import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
+import { ArrowDownIcon, ArrowUpIcon, SaveIcon, Trash2Icon } from 'lucide-react';
 import { useState } from 'react';
 import { toast } from 'sonner';
 
@@ -32,24 +37,35 @@ export const Route = createFileRoute('/lists/$id/edit/')({
 
 function RouteComponent() {
   const { list } = Route.useLoaderData();
-  const navigate = useNavigate();
+  const invalidator = useInvalidator();
+
   const [name, setName] = useState(list.name);
   const [isPublic, setIsPublic] = useState(list.isPublic);
-  const [isDirty, setIsDirty] = useState(false);
+  const [items, setItems] = useState(list.items);
+  const [isListDirty, setIsListDirty] = useState(false);
+  const [isItemsDirty, setIsItemsDirty] = useState(false);
   const isErr = name.length > 128 || name.length < 1;
-  const showErr = isDirty && isErr;
+  const showErr = isListDirty && isErr;
 
-  const mutation = api.useMutation('patch', '/api/v2/lists/{id}', {
-    onSuccess: () => {
-      navigate({
-        to: '/lists/$id',
-        params: {
-          id: list.id,
-        },
-      });
+  const updateListMutation = api.useMutation('patch', '/api/v2/lists/{id}', {
+    onSuccess: async () => {
+      await invalidator.invalidate();
+      setIsListDirty(false);
       toast.success('List updated');
     },
   });
+
+  const updateListItemsMutation = api.useMutation(
+    'patch',
+    '/api/v2/lists/{id}/items',
+    {
+      onSuccess: async () => {
+        await invalidator.invalidate();
+        setIsItemsDirty(false);
+        toast.success('List items updated');
+      },
+    },
+  );
 
   return (
     <div className="max-w-7xl mx-auto my-8">
@@ -66,16 +82,7 @@ function RouteComponent() {
           </div>
         </div>
       </div>
-      <div>
-        <Link
-          to="/lists/$id/items"
-          params={{
-            id: list.id,
-          }}
-          className={cn('my-4', buttonVariants({ variant: 'outline' }))}
-        >
-          Edit List Items
-        </Link>
+      <div className="mt-4">
         <div className="space-y-4 max-w-xl">
           <div className="w-full">
             <Label htmlFor="name">Name</Label>
@@ -88,7 +95,7 @@ function RouteComponent() {
               value={name}
               onChange={(e) => {
                 setName(e.target.value);
-                setIsDirty(true);
+                setIsListDirty(true);
               }}
             />
             {showErr && (
@@ -103,7 +110,7 @@ function RouteComponent() {
               id="is-public"
               checked={isPublic}
               onCheckedChange={(c) => {
-                setIsDirty(true);
+                setIsListDirty(true);
                 setIsPublic(c === true);
               }}
             />
@@ -114,9 +121,10 @@ function RouteComponent() {
           </div>
 
           <Button
-            disabled={!isDirty}
+            size="sm"
+            disabled={!isListDirty}
             onClick={() =>
-              mutation.mutate({
+              updateListMutation.mutate({
                 params: {
                   path: {
                     id: list.id,
@@ -132,6 +140,114 @@ function RouteComponent() {
             Update
           </Button>
         </div>
+      </div>
+
+      <Separator className="my-4" />
+
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <div className="font-bold">List Items</div>
+          {items.length > 0 && isItemsDirty && (
+            <div className="flex">
+              <Button
+                variant="default"
+                size="sm"
+                className="ml-auto"
+                disabled={!isItemsDirty}
+                onClick={() => {
+                  updateListItemsMutation.mutate({
+                    params: {
+                      path: {
+                        id: list.id,
+                      },
+                    },
+                    body: {
+                      poiIds: items.map((item) => item.poiId),
+                    },
+                  });
+                }}
+              >
+                <SaveIcon className="size-4" />
+                <span>Save Changes</span>
+              </Button>
+            </div>
+          )}
+        </div>
+        {items.length === 0 && (
+          <AppMessage
+            emptyMessage="This list is empty"
+            className="my-16"
+            showBackButton={false}
+          />
+        )}
+        {items.map((item, i) => (
+          <div
+            key={item.poi.id}
+            className="flex items-center gap-2"
+          >
+            <div className={cn('group', 'flex items-center gap-4')}>
+              <img
+                src={ipx(item.poi.firstMedia.url, 'w_512')}
+                alt={item.poi.firstMedia.alt}
+                className="aspect-video w-full rounded-md object-cover max-w-36"
+              />
+
+              <div className="my-2">
+                <div className="mt-2 text-lg font-semibold capitalize">
+                  {item.poi.name}
+                </div>
+              </div>
+            </div>
+            <Button
+              variant="destructive"
+              size="icon"
+              className="ml-auto"
+              onClick={() => {
+                setIsItemsDirty(true);
+                setItems(items.filter((i) => i.poi.id !== item.poi.id));
+              }}
+            >
+              <span className="sr-only">Remove</span>
+              <Trash2Icon className="size-4" />
+            </Button>
+            <Button
+              variant="ghost"
+              size="icon"
+              disabled={i === 0}
+              onClick={() => {
+                setIsItemsDirty(true);
+                const newItems = [...items];
+                [newItems[i - 1], newItems[i]] = [
+                  newItems[i]!,
+                  newItems[i - 1]!,
+                ];
+
+                setItems([...newItems]);
+              }}
+            >
+              <span className="sr-only">Move Up</span>
+              <ArrowUpIcon className="size-4" />
+            </Button>
+
+            <Button
+              variant="ghost"
+              size="icon"
+              disabled={i === items.length - 1}
+              onClick={() => {
+                setIsItemsDirty(true);
+                const newItems = [...items];
+                const temp = items[i]!;
+                newItems[i] = newItems[i + 1]!;
+                newItems[i + 1] = temp;
+
+                setItems([...newItems]);
+              }}
+            >
+              <span className="sr-only">Move Down</span>
+              <ArrowDownIcon className="size-4" />
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   );
