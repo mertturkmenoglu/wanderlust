9a7f73e4f17e66664b005f96c9430848e529e886
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-23 16:43:10 +0300

    add update friends mutation

--- web/src/routes/diary/$id/edit/friends/index.tsx
+++ web/src/routes/diary/$id/edit/friends/index.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@/components/ui/button';
+import { useInvalidator } from '@/hooks/use-invalidator';
+import { api } from '@/lib/api';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createFileRoute, getRouteApi } from '@tanstack/react-router';
 import { FormProvider, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
 import { List } from './-list';
 import SearchResults from './-results';
 import { schema } from './-schema';
@@ -13,6 +17,7 @@ export const Route = createFileRoute('/diary/$id/edit/friends/')({
 function RouteComponent() {
   const route = getRouteApi('/diary/$id/edit');
   const { entry } = route.useLoaderData();
+  const invalidator = useInvalidator();
 
   const form = useForm({
     resolver: zodResolver(schema),
@@ -27,18 +32,53 @@ function RouteComponent() {
     },
   });
 
+  const updateFriendsMutation = api.useMutation(
+    'patch',
+    '/api/v2/diary/{id}/friends',
+    {
+      onSuccess: async () => {
+        await invalidator.invalidate();
+        toast.success('Friends updated successfully.');
+      },
+    },
+  );
+
   return (
     <div className="my-8 max-w-2xl">
       <FormProvider {...form}>
-        <h3 className="text-lg">Edit Friends</h3>
+        <form
+          onSubmit={form.handleSubmit((data) => {
+            updateFriendsMutation.mutate({
+              params: {
+                path: {
+                  id: entry.id,
+                },
+              },
+              body: {
+                friends: data.friends.map((f) => f.id),
+              },
+            });
+          })}
+        >
+          <div className="flex justify-between items-center">
+            <h3 className="text-lg">Edit Friends</h3>
+            <Button
+              variant="default"
+              size="sm"
+              disabled={!form.formState.isDirty}
+            >
+              Update
+            </Button>
+          </div>
 
-        <div className="group mt-2">
-          <SearchInput />
+          <div className="group mt-2">
+            <SearchInput />
 
-          <SearchResults className="hidden group-focus-within:block" />
-        </div>
+            <SearchResults className="hidden group-focus-within:block" />
+          </div>
 
-        <List />
+          <List />
+        </form>
       </FormProvider>
     </div>
   );
