6f2c9e2b142ceed7d22abeb5776861c1c8b7e565
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-23 21:57:48 +0300

    implement update diary entry locations feature

--- web/src/routes/diary/$id/edit/locations/index.tsx
+++ web/src/routes/diary/$id/edit/locations/index.tsx
@@ -1,4 +1,6 @@
 import { Autocomplete } from '@/components/blocks/autocomplete';
+import { Button } from '@/components/ui/button';
+import { useInvalidator } from '@/hooks/use-invalidator';
 import { useSearchClient } from '@/hooks/use-search-client';
 import { api } from '@/lib/api';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -27,6 +29,7 @@ export const Route = createFileRoute('/diary/$id/edit/locations/')({
 function RouteComponent() {
   const { entry } = Route.useLoaderData();
   const searchClient = useSearchClient();
+  const invalidator = useInvalidator();
 
   const form = useForm({
     resolver: zodResolver(schema),
@@ -48,65 +51,109 @@ function RouteComponent() {
     name: 'locations',
   });
 
+  const updateDiaryLocationsMutation = api.useMutation(
+    'patch',
+    '/api/v2/diary/{id}/locations',
+    {
+      onSuccess: async () => {
+        await invalidator.invalidate();
+        toast.success('Locations updated successfully.');
+      },
+    },
+  );
+
+  const locations = form.watch('locations');
+
   return (
-    <div className="my-8 max-w-2xl">
-      <h3
-        className="text-lg"
-        id="diary-locations"
-      >
-        Edit Locations
-      </h3>
-
-      <InstantSearch
-        indexName="pois"
-        searchClient={searchClient}
-        routing={false}
-        future={{
-          preserveSharedStateOnUnmount: true,
-        }}
-      >
-        <Autocomplete
-          showAdvancedSearch={false}
-          showAllResultsButton={false}
-          isCardClickable={true}
-          onCardClick={(v) => {
-            const maxAllowedCount = 32;
-            const alreadyInList =
-              array.fields.findIndex((lo) => lo.id === v.id) !== -1;
-
-            if (alreadyInList) {
-              toast.error('Location is already added.');
-              return;
-            }
-
-            if (array.fields.length >= maxAllowedCount) {
-              toast.error(`Maximum ${maxAllowedCount} locations can be added.`);
-              return;
-            }
-
-            array.append({
-              ...v,
-              description: '',
-            });
-          }}
-        />
-      </InstantSearch>
-
-      <FormProvider {...form}>
-        <div className="max-w-4xl mx-auto">
-          {array.fields.length === 0 && (
-            <div className="text-sm text-muted-foreground my-8">
-              No locations selected
-            </div>
-          )}
-          {array.fields.map((l, i) => (
-            <Item
-              index={i}
-              key={l.id}
-            />
-          ))}
+    <FormProvider {...form}>
+      <div className="my-8 max-w-2xl">
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="text-lg">Edit Locations</h3>
         </div>
-      </FormProvider>
-    </div>
+
+        <InstantSearch
+          indexName="pois"
+          searchClient={searchClient}
+          routing={false}
+          future={{
+            preserveSharedStateOnUnmount: true,
+          }}
+        >
+          <Autocomplete
+            showAdvancedSearch={false}
+            showAllResultsButton={false}
+            isCardClickable={true}
+            onCardClick={(v) => {
+              const maxAllowedCount = 32;
+              const alreadyInList =
+                array.fields.findIndex((lo) => lo.id === v.id) !== -1;
+
+              if (alreadyInList) {
+                toast.error('Location is already added.');
+                return;
+              }
+
+              if (array.fields.length >= maxAllowedCount) {
+                toast.error(
+                  `Maximum ${maxAllowedCount} locations can be added.`,
+                );
+                return;
+              }
+
+              array.append({
+                ...v,
+                description: '',
+              });
+            }}
+          />
+        </InstantSearch>
+        <form
+          onSubmit={form.handleSubmit(
+            (data) => {
+              updateDiaryLocationsMutation.mutate({
+                params: {
+                  path: {
+                    id: entry.id,
+                  },
+                },
+                body: {
+                  locations: data.locations.map((l) => ({
+                    poiId: l.id,
+                    description: l.description,
+                  })),
+                },
+              });
+            },
+            () => {
+              toast.error('Failed to update locations');
+            },
+          )}
+        >
+          <div className="max-w-4xl mx-auto flex flex-col">
+            {locations.length > 0 && (
+              <Button
+                size="sm"
+                disabled={!form.formState.isDirty}
+                type="submit"
+                className="mt-4 ml-auto"
+              >
+                Update
+              </Button>
+            )}
+            {locations.length === 0 && (
+              <div className="text-sm text-muted-foreground my-8">
+                No locations selected
+              </div>
+            )}
+            {locations.map((l, i) => (
+              <Item
+                index={i}
+                key={l.id}
+              />
+            ))}
+          </div>
+        </form>
+      </div>
+    </FormProvider>
   );
 }
