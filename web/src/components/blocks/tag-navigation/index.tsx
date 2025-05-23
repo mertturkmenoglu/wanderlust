62e780da0f4c16f1e3f7a4d1d14d96bfeae1b9b1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-23 22:21:08 +0300

    add optional urlSuffix prop

--- web/src/components/blocks/tag-navigation/index.tsx
+++ web/src/components/blocks/tag-navigation/index.tsx
@@ -2,13 +2,18 @@ import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import data from './data';
 import NavItem from './item';
 
-export default function TagNavigation(): React.ReactElement {
+type Props = {
+  urlSuffix?: string;
+};
+
+export default function TagNavigation({ urlSuffix = '' }: Props) {
   return (
     <ScrollArea>
       <ul className="flex items-center justify-center space-x-4">
         {data.map((item) => (
           <NavItem
             {...item}
+            href={item.href + urlSuffix}
             key={item.href}
           />
         ))}
