e4f629ed916d668e33df322dce6cf3ebe7628064
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-23 05:03:12 +0300

    update import

--- web/src/components/blocks/back-link/index.tsx
+++ web/src/components/blocks/back-link/index.tsx
@@ -1,7 +1,7 @@
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { Link } from '@tanstack/react-router';
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeftIcon } from 'lucide-react';
 
 type Props = {
   className?: string;
@@ -16,7 +16,7 @@ export default function BackLink({ href, className, text = 'Go back' }: Props) {
       className={cn(buttonVariants({ variant: 'link' }), '!px-0', className)}
     >
       <div className="flex items-center gap-2 px-0">
-        <ArrowLeft className="size-4" />
+        <ArrowLeftIcon className="size-4" />
         <div>{text}</div>
       </div>
     </Link>
