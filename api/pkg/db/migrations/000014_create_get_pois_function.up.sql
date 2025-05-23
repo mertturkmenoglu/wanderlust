2d02f1b96f21e5d47370a38e44770df7bc696d66
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-23 04:06:12 +0300

    add get pois database function

---
+++ api/pkg/db/migrations/000014_create_get_pois_function.up.sql
@@ -0,0 +1,88 @@
+CREATE OR REPLACE FUNCTION get_pois(ids TEXT[])
+RETURNS JSONB
+AS $$
+BEGIN
+	RETURN (
+    SELECT jsonb_agg(jsonb_build_object(
+      'id', pois.id,
+      'name', pois.name,
+      'description', pois.description,
+      'phone', pois.phone,
+      'addressId', pois.address_id,
+      'website', pois.website,
+      'priceLevel', pois.price_level,
+      'accessibilityLevel', pois.accessibility_level,
+      'totalVotes', pois.total_votes,
+      'totalPoints', pois.total_points,
+      'totalFavorites', pois.total_favorites,
+      'categoryId', pois.category_id,
+      'openTimes', pois.open_times,
+      'createdAt', pois.created_at,
+      'updatedAt', pois.updated_at,
+      'address', jsonb_build_object(
+        'id', addr.id,
+        'cityId', addr.city_id,
+        'line1', addr.line1,
+        'line2', addr.line2,
+        'postalCode', addr.postal_code,
+        'lat', addr.lat,
+        'lng', addr.lng
+      ),
+      'city', jsonb_build_object(
+        'id', c.id,
+        'name', c.name,
+        'description', c.description,
+        'stateCode', c.state_code,
+        'stateName', c.state_name,
+        'countryCode', c.country_code,
+        'countryName', c.country_name,
+        'imageUrl', c.image_url,
+        'imageLicense', c.img_license,
+        'imageLicenseLink', c.img_license_link,
+        'imageAttribution', c.img_attr,
+        'imageAttributionLink', c.img_attr_link,
+        'latitude', c.latitude,
+        'longitude', c.longitude
+      ),
+      'category', jsonb_build_object(
+        'id', cat.id,
+        'name', cat.name,
+        'image', cat.image
+      ),
+      'media', media_agg.media,
+      'amenities', amenities_agg.amenities
+    ))
+    FROM public.pois
+    JOIN public.addresses addr ON pois.address_id = addr.id
+    JOIN public.cities c ON addr.city_id = c.id
+    JOIN public.categories cat ON pois.category_id = cat.id
+
+    -- LATERAL join for media
+    LEFT JOIN LATERAL (
+      SELECT json_agg(jsonb_build_object(
+        'id', m.id,
+        'url', m.url,
+        'alt', m.alt,
+        'caption', m.caption,
+        'mediaOrder', m.media_order
+      ) ORDER BY m.media_order) AS media
+      FROM public.media m
+      WHERE m.poi_id = pois.id
+    ) media_agg ON true
+
+    -- LATERAL join for amenities
+    LEFT JOIN LATERAL (
+      SELECT json_agg(jsonb_build_object(
+        'id', a.id,
+        'name', a.name
+      )) AS amenities
+      FROM public.amenities_pois ap
+      JOIN public.amenities a ON ap.amenity_id = a.id
+      WHERE ap.poi_id = pois.id
+    ) amenities_agg ON true
+
+    WHERE pois.id = ANY(ids)
+  );
+END;
+$$ LANGUAGE plpgsql;
+
