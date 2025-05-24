423599fd0f0bb98e81cdae7ff4c26175dc516f01
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-24 16:56:32 +0300

    update follow fake data generation

--- api/cmd/fake/handle_follows.go
+++ api/cmd/fake/handle_follows.go
@@ -12,10 +12,35 @@ import (
 
 func handleFollows() error {
 	path, _ := pterm.DefaultInteractiveTextInput.Show("Enter path for the file containin user ids")
-	f, err := os.Open(path)
+	userIds, err := readUserIdsFromFile(path)
 
 	if err != nil {
 		return err
+	}
+
+	logger.Trace("Read user ids", logger.Args("count", len(userIds)))
+	logger.Trace("Trying to follow users")
+
+	tryFollowing(userIds)
+
+	ans, _ := pterm.DefaultInteractiveTextInput.Show("Inserted to follows table. Do you want to update users table? [Y/n]")
+
+	if ans == "n" || ans == "N" {
+		return nil
+	}
+
+	logger.Trace("Updating users table")
+
+	tryUpdatingUsers(userIds)
+
+	return nil
+}
+
+func readUserIdsFromFile(path string) ([]string, error) {
+	f, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
 	}
 
 	defer f.Close()
@@ -29,24 +54,25 @@ func handleFollows() error {
 	}
 
 	if err := scanner.Err(); err != nil {
-		return err
+		return nil, err
 	}
 
-	logger.Trace("Read user ids", logger.Args("count", len(userIds)))
-	logger.Trace("Trying to follow users")
+	return userIds, nil
+}
 
+func tryFollowing(userIds []string) {
 	d := GetDb()
 	ctx := context.Background()
-
 	successCounter := 0
 	failCounter := 0
+	batch := make([]db.BatchFollowParams, 0, 100)
 
 	for i, userId := range userIds {
-		if i%100 == 0 {
+		if i%1000 == 0 {
 			logger.Trace("Following users", logger.Args("i", i, "success", successCounter, "fail", failCounter))
 		}
 
-		for range 20 {
+		for range 10 {
 			idx := gofakeit.IntRange(0, len(userIds)-1)
 			targetUserId := userIds[idx]
 
@@ -54,20 +80,85 @@ func handleFollows() error {
 				continue
 			}
 
-			err := d.Queries.Follow(ctx, db.FollowParams{
+			batch = append(batch, db.BatchFollowParams{
 				FollowerID:  userId,
 				FollowingID: targetUserId,
 			})
+		}
+
+		if len(batch) >= 100 {
+			_, err := d.Queries.BatchFollow(ctx, batch)
 
 			if err != nil {
 				failCounter++
 			} else {
 				successCounter++
 			}
+
+			batch = make([]db.BatchFollowParams, 0, 100)
 		}
 	}
 
-	logger.Trace("Followed users", logger.Args("success", successCounter, "key collision fails", failCounter))
+	if len(batch) >= 100 {
+		_, err := d.Queries.BatchFollow(ctx, batch)
 
-	return nil
+		if err != nil {
+			failCounter++
+		} else {
+			successCounter++
+		}
+	}
+
+	logger.Trace("Followed users", logger.Args("success", successCounter, "fails", failCounter))
+}
+
+func tryUpdatingUsers(userIds []string) {
+	d := GetDb()
+	ctx := context.Background()
+	successCounter := 0
+	failCounter := 0
+
+	for i, userId := range userIds {
+		if i%1000 == 0 {
+			logger.Trace("Updating users", logger.Args("i", i, "success", successCounter, "fail", failCounter))
+		}
+
+		followingCount, err := d.Queries.GetFollowingCount(ctx, userId)
+
+		if err != nil {
+			failCounter++
+			continue
+		}
+
+		followersCount, err := d.Queries.GetFollowersCount(ctx, userId)
+
+		if err != nil {
+			failCounter++
+			continue
+		}
+
+		err = d.Queries.SetFollowersCount(ctx, db.SetFollowersCountParams{
+			ID:             userId,
+			FollowersCount: int32(followersCount),
+		})
+
+		if err != nil {
+			failCounter++
+			continue
+		}
+
+		err = d.Queries.SetFollowingCount(ctx, db.SetFollowingCountParams{
+			ID:             userId,
+			FollowingCount: int32(followingCount),
+		})
+
+		if err != nil {
+			failCounter++
+			continue
+		}
+
+		successCounter++
+	}
+
+	logger.Trace("Updated users", logger.Args("success", successCounter, "fails", failCounter))
 }
