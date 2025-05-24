889d0c706adbd4b4bfc38928dbfeef8bbfaa7169
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-24 16:55:42 +0300

    add follows queries

--- api/pkg/db/queries/follows.sql
+++ api/pkg/db/queries/follows.sql
@@ -14,6 +14,23 @@ INSERT INTO follows (
   $2
 );
 
+-- name: BatchFollow :copyfrom
+INSERT INTO follows (
+  follower_id,
+  following_id
+) VALUES (
+  $1,
+  $2
+);
+
+-- name: GetFollowersCount :one
+SELECT COUNT(*) FROM follows
+WHERE following_id = $1;
+
+-- name: GetFollowingCount :one
+SELECT COUNT(*) FROM follows
+WHERE follower_id = $1;
+
 -- name: Unfollow :exec
 DELETE FROM follows
 WHERE follower_id = $1 AND following_id = $2;
