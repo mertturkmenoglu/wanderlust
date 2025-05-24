4f977a53ada160cf437cad890a066e5216c3b5eb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-24 19:10:36 +0300

    update body dump middleware

--- api/pkg/middlewares/body_dump.go
+++ api/pkg/middlewares/body_dump.go
@@ -1,15 +1,16 @@
 package middlewares
 
 import (
+	"fmt"
+	"log/slog"
 	"net/http"
+	"wanderlust/pkg/tracing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-func CustomBodyDump(logger *zap.Logger) echo.MiddlewareFunc {
+func CustomBodyDump() echo.MiddlewareFunc {
 	return middleware.BodyDumpWithConfig(middleware.BodyDumpConfig{
 		Skipper: func(c echo.Context) bool {
 			return c.Request().Method == http.MethodOptions
@@ -22,19 +23,25 @@ func CustomBodyDump(logger *zap.Logger) echo.MiddlewareFunc {
 			status := res.Status
 			qp := c.QueryParams().Encode()
 
-			logger.Info("Request",
-				zap.String("http.method", method),
-				zap.String("http.url", uri),
-				zap.Int("http.status_code", status),
-				zap.String("http.request_body", string(reqBody)),
-				zap.String("http.response_body", string(resBody)),
-				zap.String("http.query_params", qp),
-				zap.Object("http.path_params", zapcore.ObjectMarshalerFunc(func(oe zapcore.ObjectEncoder) error {
-					for _, p := range c.ParamNames() {
-						oe.AddString(p, c.Param(p))
-					}
-					return nil
-				})),
+			pp := make([]string, 0)
+
+			for _, p := range c.ParamNames() {
+				pp = append(pp, fmt.Sprintf("%s=%s", p, c.Param(p)))
+			}
+
+			msg := method + " " + uri
+
+			tracing.Slog.LogAttrs(
+				c.Request().Context(),
+				slog.LevelDebug,
+				msg,
+				slog.String("http.method", method),
+				slog.String("http.url", uri),
+				slog.Int("http.status_code", status),
+				slog.String("http.request_body", string(reqBody)),
+				slog.String("http.response_body", string(resBody)),
+				slog.String("http.query_params", qp),
+				slog.Any("http.path_params", pp),
 			)
 		},
 	})
