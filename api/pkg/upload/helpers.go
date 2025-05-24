dc18f558e43e1b12b57cbed90b0671185c072aef
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-24 14:19:23 +0300

    add upload service helpers

--- api/pkg/upload/helpers.go
+++ api/pkg/upload/helpers.go
@@ -1,6 +1,11 @@
 package upload
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/minio/minio-go/v7"
+)
 
 func GetFileExtensionFromMimeType(mimeType string) (string, error) {
 	switch mimeType {
@@ -21,4 +26,25 @@ func GetFileExtensionFromMimeType(mimeType string) (string, error) {
 
 func ConstructFilename(key string, fileExtension string) string {
 	return fmt.Sprintf("%s.%s", key, fileExtension)
+}
+
+// Get the object name from the URL
+func (svc *UploadService) GetObjectNameFromUrl(url string, bucket BucketName) string {
+	endpoint := svc.Client.EndpointURL().String()
+	removePrefix := endpoint + "/" + string(bucket) + "/"
+	after, _ := strings.CutPrefix(url, removePrefix)
+	return after
+}
+
+// Given the URL and the bucket name, remove the object from the bucket.
+// URL is used to get the object name.
+func (svc *UploadService) RemoveFileFromUrl(url string, bucket BucketName) error {
+	objectName := svc.GetObjectNameFromUrl(url, bucket)
+
+	return svc.Client.RemoveObject(
+		svc.Context,
+		string(bucket),
+		objectName,
+		minio.RemoveObjectOptions{},
+	)
 }
