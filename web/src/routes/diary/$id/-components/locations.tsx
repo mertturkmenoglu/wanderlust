de1a0b8b098b8bb99e1c801bb49283464b3c6ec7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-24 14:27:21 +0300

    change grid cols

--- web/src/routes/diary/$id/-components/locations.tsx
+++ web/src/routes/diary/$id/-components/locations.tsx
@@ -43,7 +43,7 @@ export default function Locations() {
       </div>
 
       {isGridMode ? (
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {entry.locations.map((location) => (
             <Link
               to="/p/$id"
