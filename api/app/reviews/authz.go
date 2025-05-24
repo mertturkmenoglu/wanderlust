b23141951d02dd3b43aa56c3346cbfa47bcc90d7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-25 00:15:59 +0300

    update authz rules

--- api/app/reviews/authz.go
+++ api/app/reviews/authz.go
@@ -5,3 +5,7 @@ import "wanderlust/pkg/dto"
 func canRemoveReview(review *dto.Review, userId string) bool {
 	return review.UserID == userId
 }
+
+func canUploadMedia(review *dto.Review, userId string) bool {
+	return review.UserID == userId
+}
