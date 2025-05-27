b59fed9206ccca128b56abb3173df920e9ce084e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-28 02:56:39 +0300

    remove steps

--- web/src/routes/_admin/dashboard/pois/drafts/$id/index.tsx
+++ web/src/routes/_admin/dashboard/pois/drafts/$id/index.tsx
@@ -8,9 +8,6 @@ import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { toast } from 'sonner';
 import { z } from 'zod';
 import StepsIndicator from './-steps-indicator';
-import Step1 from './-steps/step-1';
-import Step2 from './-steps/step-2';
-import Step3 from './-steps/step-3';
 import Step4 from './-steps/step-4';
 import Step5 from './-steps/step-5';
 import Step6 from './-steps/step-6';
@@ -103,9 +100,6 @@ function RouteComponent() {
         <StepsIndicator draftId={draft.id as string} />
       </div>
 
-      {step === 1 && <Step1 />}
-      {step === 2 && <Step2 />}
-      {step === 3 && <Step3 />}
       {step === 4 && <Step4 />}
       {step === 5 && <Step5 />}
       {step === 6 && <Step6 />}
