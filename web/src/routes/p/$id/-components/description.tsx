603a6e3218b57ffc229d9d884565734ad2e2994c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-29 01:19:54 +0300

    add description component

--- web/src/routes/p/$id/-components/description.tsx
+++ web/src/routes/p/$id/-components/description.tsx
@@ -1,7 +1,16 @@
 import CollapsibleText from '@/components/blocks/collapsible-text';
+import FormattedRating from '@/components/kit/formatted-rating';
+import { Button } from '@/components/ui/button';
+import { computeRating } from '@/lib/rating';
 import { cn } from '@/lib/utils';
 import { getRouteApi } from '@tanstack/react-router';
-import { MapIcon } from 'lucide-react';
+import {
+  DollarSignIcon,
+  HeartIcon,
+  MapIcon,
+  PersonStandingIcon,
+} from 'lucide-react';
+import { InfoCard } from './info-card';
 
 type Props = {
   className?: string;
@@ -10,24 +19,109 @@ type Props = {
 export function Description({ className }: Props) {
   const route = getRouteApi('/p/$id/');
   const { poi } = route.useLoaderData();
+  const rating = computeRating(poi.totalPoints, poi.totalVotes);
+  const fmt = Intl.NumberFormat('en-US', {
+    style: 'decimal',
+    compactDisplay: 'short',
+    notation: 'compact',
+  });
 
   return (
-    <div className={cn(className)}>
-      <h3 className="text-xl font-semibold">Description</h3>
-      <CollapsibleText
-        text={poi.description}
-        charLimit={1000}
-      />
-      <button
-        className={cn(
-          'bg-primary text-white rounded-md w-full lg:w-1/3 lg:mx-auto',
-          'px-8 py-2.5 mt-4 flex items-center gap-2 justify-center hover:opacity-90',
-          'transition-all duration-200 ease-in-out',
-        )}
-      >
-        <MapIcon className="size-5" />
-        <span className="text-base">Plan a trip</span>
-      </button>
+    <div className={cn('grid grid-cols-1 md:grid-cols-2 gap-4', className)}>
+      <div className="flex flex-col">
+        <h3 className="text-xl font-semibold">Description</h3>
+        <CollapsibleText
+          text={poi.description}
+          charLimit={1000}
+        />
+        <Button
+          size="lg"
+          className="mt-4 w-2/3 mx-auto"
+        >
+          <MapIcon className="size-5" />
+          <span className="text-base">Plan a trip</span>
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
+        <InfoCard.Root>
+          <InfoCard.Content>
+            <InfoCard.NumberColumn>
+              {computeRating(poi.totalPoints, poi.totalVotes)}
+            </InfoCard.NumberColumn>
+            <InfoCard.DescriptionColumn>
+              <FormattedRating
+                rating={parseFloat(rating)}
+                votes={poi.totalVotes}
+                showNumbers={false}
+              />
+              <span className="text-xs text-muted-foreground tracking-tight">
+                {fmt.format(poi.totalVotes)} reviews
+              </span>
+            </InfoCard.DescriptionColumn>
+          </InfoCard.Content>
+        </InfoCard.Root>
+
+        <InfoCard.Root>
+          <InfoCard.Content>
+            <InfoCard.NumberColumn>
+              {fmt.format(poi.totalFavorites)}
+            </InfoCard.NumberColumn>
+            <InfoCard.DescriptionColumn>
+              <div className="flex items-center gap-1">
+                <HeartIcon className="fill-primary text-primary size-4" />
+                <HeartIcon className="fill-primary text-primary size-4" />
+                <HeartIcon className="fill-primary text-primary size-4" />
+              </div>
+              <span className="text-xs text-muted-foreground tracking-tight">
+                Favorites
+              </span>
+            </InfoCard.DescriptionColumn>
+          </InfoCard.Content>
+        </InfoCard.Root>
+
+        <InfoCard.Root>
+          <InfoCard.Content>
+            <InfoCard.NumberColumn>{poi.priceLevel}/5</InfoCard.NumberColumn>
+            <InfoCard.DescriptionColumn>
+              <div className="flex items-center">
+                {Array.from({ length: poi.priceLevel }).map((_, i) => (
+                  <DollarSignIcon
+                    className="text-primary size-4"
+                    key={i}
+                  />
+                ))}
+              </div>
+
+              <span className="text-xs text-muted-foreground tracking-tight">
+                Price Level
+              </span>
+            </InfoCard.DescriptionColumn>
+          </InfoCard.Content>
+        </InfoCard.Root>
+
+        <InfoCard.Root>
+          <InfoCard.Content>
+            <InfoCard.NumberColumn>
+              {poi.accessibilityLevel}/5
+            </InfoCard.NumberColumn>
+            <InfoCard.DescriptionColumn>
+              <div className="flex items-center">
+                {Array.from({ length: poi.accessibilityLevel }).map((_, i) => (
+                  <PersonStandingIcon
+                    className="text-primary size-4"
+                    key={i}
+                  />
+                ))}
+              </div>
+
+              <span className="text-xs text-muted-foreground tracking-tight">
+                Accessibility Level
+              </span>
+            </InfoCard.DescriptionColumn>
+          </InfoCard.Content>
+        </InfoCard.Root>
+      </div>
     </div>
   );
 }
