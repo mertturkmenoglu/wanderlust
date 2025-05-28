3e7201af990df2313c53bde5a0f99b250f68ba55
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-29 00:34:01 +0300

    add image grid component

---
+++ web/src/routes/p/$id/-components/image-grid.tsx
@@ -0,0 +1,103 @@
+import { cn } from '@/lib/utils';
+import { getRouteApi } from '@tanstack/react-router';
+import { useMemo, useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+
+type Props = {
+  className?: string;
+};
+
+export function ImageGrid({ className }: Props) {
+  const route = getRouteApi('/p/$id/');
+  const { poi } = route.useLoaderData();
+  const media = poi.media;
+  const first = media[0];
+
+  const rest = useMemo(() => {
+    const slice = media.slice(1, 5) as Array<{
+      id: number;
+      url: string;
+      alt: string;
+    }>;
+    if (slice.length < 4) {
+      let pad = 4 - slice.length;
+
+      for (let i = 0; i < pad; i++) {
+        slice.push({
+          id: Math.random(),
+          url: '',
+          alt: '',
+        });
+      }
+    }
+    return slice;
+  }, [media]);
+
+  const [open, setOpen] = useState(false);
+
+  if (!first) {
+    return null;
+  }
+
+  return (
+    <div
+      className={cn(
+        'grid grid-cols-4 grid-rows-2 gap-2 rounded-xl h-[256px] md:h-[384px] lg:h-[512px] relative',
+        className,
+      )}
+    >
+      <div className="col-span-2 row-span-2">
+        <img
+          src={first.url}
+          alt={first.alt}
+          className="w-full h-full object-cover rounded-l-xl"
+        />
+      </div>
+      {rest.map((m, i) => (
+        <div
+          className={cn('col-span-1 row-span-1')}
+          key={m.id}
+        >
+          {m.url !== '' ? (
+            <img
+              src={m.url}
+              alt={m.alt}
+              className={cn('w-full h-full object-cover', {
+                'rounded-tr-xl': i === 1,
+                'rounded-br-xl': i === 3,
+              })}
+            />
+          ) : (
+            <div
+              className={cn('w-full h-full object-cover bg-muted', {
+                'rounded-tr-xl': i === 1,
+                'rounded-br-xl': i === 3,
+              })}
+            />
+          )}
+        </div>
+      ))}
+      <button
+        className="absolute bottom-4 right-4 bg-primary text-white rounded-md px-4 py-2"
+        onClick={() => setOpen(true)}
+      >
+        See more
+      </button>
+
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={media.map((m) => ({
+          src: m.url,
+        }))}
+        animation={{ fade: 0 }}
+        controller={{ closeOnPullDown: true, closeOnBackdropClick: true }}
+        styles={{
+          container: {
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          },
+        }}
+      />
+    </div>
+  );
+}
