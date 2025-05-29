4b143c669c6b3d48d7268d4fd65ed26e46373c5b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-30 02:30:09 +0300

    move map pin icon

--- web/src/components/icons/leaflet.tsx
+++ web/src/components/icons/leaflet.tsx
@@ -1,8 +1,8 @@
+import mapPinIcon from '@/map-pin.svg';
 import L from 'leaflet';
 import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import iconMarker from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import mapPinIcon from '../../../public/map.svg';
 
 export const icon = L.icon({
   iconUrl: mapPinIcon,
