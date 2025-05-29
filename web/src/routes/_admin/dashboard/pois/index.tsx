624b29f11a67da5c5cd5d9f5cac17ea31dc4721a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-29 03:45:41 +0300

    remove invalid link

--- web/src/routes/_admin/dashboard/pois/index.tsx
+++ web/src/routes/_admin/dashboard/pois/index.tsx
@@ -1,12 +1,9 @@
-import DashboardActions from '@/components/blocks/dashboard/actions';
 import DashboardBreadcrumb from '@/components/blocks/dashboard/breadcrumb';
 import { poisCols } from '@/components/blocks/dashboard/columns';
 import { DataTable } from '@/components/blocks/dashboard/data-table';
-import { buttonVariants } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { api } from '@/lib/api';
-import { cn } from '@/lib/utils';
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_admin/dashboard/pois/')({
   component: RouteComponent,
@@ -21,15 +18,6 @@ function RouteComponent() {
         items={[{ name: 'Point of Interests', href: '/dashboard/pois' }]}
       />
       <Separator className="my-2" />
-
-      <DashboardActions>
-        <Link
-          to="/dashboard/pois/drafts"
-          className={cn(buttonVariants({ variant: 'link' }), 'px-0')}
-        >
-          Drafts
-        </Link>
-      </DashboardActions>
 
       {query.data?.pois && (
         <DataTable
