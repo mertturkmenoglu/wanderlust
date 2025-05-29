e61fec47cec21e88e5e8ae604ff8c4dbab88aedb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-30 00:38:24 +0300

    update leaflet icons

--- web/src/components/icons/leaflet.tsx
+++ web/src/components/icons/leaflet.tsx
@@ -2,8 +2,14 @@ import L from 'leaflet';
 import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
 import iconMarker from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import mapPinIcon from '../../../public/map.svg';
 
 export const icon = L.icon({
+  iconUrl: mapPinIcon,
+  iconRetinaUrl: mapPinIcon,
+});
+
+export const defaultIcon = L.icon({
   iconRetinaUrl: iconRetina,
   iconUrl: iconMarker,
   shadowUrl: iconShadow,
