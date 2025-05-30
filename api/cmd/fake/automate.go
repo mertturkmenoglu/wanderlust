6c172e0ea04a0f9688aa305674a2a56cda4ca6ca
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-31 00:07:12 +0300

    clean up automation

--- api/cmd/fake/automate.go
+++ api/cmd/fake/automate.go
@@ -1,71 +1,74 @@
 package main
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+)
+
+const (
+	poisPath    = "tmp/pois.txt"
+	usersPath   = "tmp/users.txt"
+	reviewsPath = "tmp/reviews.txt"
+)
+
+var steps = [...]string{
+	"amenities",
+	"categories",
+	"cities",
+	"addresses",
+	"users",
+	"pois",
+	"fake-id",
+	"amenities-pois",
+	"media-for-many-pois",
+	"follows",
+	"reviews",
+	"fake-id", // run fake id again to get review ids
+	"review-media",
+}
 
 func automate() error {
-	var poisPath = "tmp/pois.txt"
-	var usersPath = "tmp/users.txt"
-
 	logger.Info("Starting automated generation")
 
-	err := handleAmenities()
-
-	if err != nil {
-		return err
+	for _, step := range steps {
+		err := mux(step)
+		if err != nil {
+			return err
+		}
 	}
-
-	err = handleCategories()
-
-	if err != nil {
-		return err
-	}
-
-	err = handleCities()
-
-	if err != nil {
-		return err
-	}
-
-	err = handleAddresses(10_000)
-
-	if err != nil {
-		return err
-	}
-
-	err = handleUsers(10_000)
-
-	if err != nil {
-		return err
-	}
-
-	err = handlePois(10_000)
-
-	if err != nil {
-		return err
-	}
-
-	cmd := exec.Command("just", "fake-id")
-	err = cmd.Run()
-
-	if err != nil {
-		return err
-	}
-
-	err = handleAmenitiesPois(poisPath)
-
-	if err != nil {
-		return err
-	}
-
-	err = handleMediaForManyPois(poisPath)
-
-	if err != nil {
-		return err
-	}
-
-	err = handleFollows(usersPath)
 
 	logger.Info("Automated generation completed.")
 
-	return err
+	return nil
+}
+
+func mux(t string) error {
+	switch t {
+	case "amenities":
+		return handleAmenities()
+	case "categories":
+		return handleCategories()
+	case "cities":
+		return handleCities()
+	case "addresses":
+		return handleAddresses(10_000)
+	case "users":
+		return handleUsers(10_000)
+	case "pois":
+		return handlePois(10_000)
+	case "fake-id":
+		return exec.Command("just", "fake-id").Run()
+	case "amenities-pois":
+		return handleAmenitiesPois(poisPath)
+	case "media-for-many-pois":
+		return handleMediaForManyPois(poisPath)
+	case "follows":
+		return handleFollows(usersPath)
+	case "reviews":
+		return handleReviews(poisPath, usersPath)
+	case "review-media":
+		return handleReviewMedia(reviewsPath)
+	default:
+		return fmt.Errorf("invalid task type: %s", t)
+	}
 }
