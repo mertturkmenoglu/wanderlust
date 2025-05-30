f52a85d58e95354044d9dc5c9f3d3c7c9ca1eb9f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-31 00:05:48 +0300

    add fake data generation utils

--- api/cmd/fake/utils.go
+++ api/cmd/fake/utils.go
@@ -1,6 +1,12 @@
 package main
 
-import "math/rand"
+import (
+	"bufio"
+	"math/rand"
+	"os"
+
+	"github.com/pterm/pterm"
+)
 
 func randElem[T any](arr []T) T {
 	return arr[rand.Intn(len(arr))]
@@ -15,4 +21,33 @@ func randElems[T any](arr []T, n int) []T {
 	})
 
 	return newArr[0:n]
+}
+
+// A shorter alias for pterm.DefaultInteractiveTextInput.Show
+func input(text ...string) (string, error) {
+	return pterm.DefaultInteractiveTextInput.Show(text...)
+}
+
+func readFile(path string) ([]string, error) {
+	f, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	lines := make([]string, 0)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
 }
