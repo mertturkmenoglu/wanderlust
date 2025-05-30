70b0a2cee339c671b8a0ed5bce0dd4ed53d7f30f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-31 00:09:53 +0300

    use search params to control upsert location dialog

--- web/src/routes/trips/$id/-upsert-location-dialog.tsx
+++ web/src/routes/trips/$id/-upsert-location-dialog.tsx
@@ -1,5 +1,5 @@
 import { Autocomplete } from '@/components/blocks/autocomplete';
-import type { AutocompleteItemInfo } from '@/components/blocks/autocomplete/card';
+import Spinner from '@/components/kit/spinner';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -18,6 +18,7 @@ import { Textarea } from '@/components/ui/textarea';
 import { useInvalidator } from '@/hooks/use-invalidator';
 import { useSearchClient } from '@/hooks/use-search-client';
 import { api } from '@/lib/api';
+import { getRouteApi, useNavigate } from '@tanstack/react-router';
 import { formatDate } from 'date-fns';
 import {
   ArrowLeftIcon,
@@ -25,33 +26,78 @@ import {
   Settings2Icon,
   Trash2Icon,
 } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { InstantSearch } from 'react-instantsearch';
-
-type Props = {
-  tripId: string;
-  initial?: {
-    locationId: string;
-    item: AutocompleteItemInfo | null;
-    desc: string;
-    time: string;
-  };
-};
 
 const fmtString = "yyyy-MM-dd'T'HH:mm";
 
-export function UpsertLocationDialog({ tripId, initial }: Props) {
+type Props = {
+  onOpen?: () => void;
+};
+
+export function UpsertLocationDialog({ onOpen }: Props) {
+  const route = getRouteApi('/trips/$id');
+  const {
+    showLocationDialog,
+    isUpdate,
+    poiId,
+    description,
+    scheduledTime,
+    locId,
+  } = route.useSearch();
+  const { id: tripId } = route.useParams();
   const searchClient = useSearchClient();
-  const isUpdate = initial !== undefined;
-  const [item, setItem] = useState<AutocompleteItemInfo | null>(
-    initial?.item ?? null,
-  );
-  const [desc, setDesc] = useState(initial?.desc ?? '');
-  const [time, setTime] = useState(
-    formatDate(initial?.time ?? new Date(), fmtString),
-  );
   const invalidator = useInvalidator();
-  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const query = api.useQuery(
+    'get',
+    '/api/v2/pois/{id}',
+    {
+      params: {
+        path: {
+          id: poiId ?? '',
+        },
+      },
+    },
+    {
+      enabled: poiId !== undefined,
+      retry: false,
+    },
+  );
+
+  const [desc, setDesc] = useState('');
+  const [time, setTime] = useState(formatDate(new Date(), fmtString));
+  const open = showLocationDialog === true;
+
+  useEffect(() => {
+    setDesc((prev) => (description !== undefined ? description : prev));
+    setTime((prev) =>
+      scheduledTime !== undefined ? formatDate(scheduledTime, fmtString) : prev,
+    );
+  }, [description, scheduledTime]);
+
+  const closeDialog = () => {
+    setDesc('');
+    setTime(formatDate(new Date(), fmtString));
+
+    navigate({
+      to: '.',
+      search: () => ({}),
+    });
+  };
+
+  const openDialog = () => {
+    if (onOpen) {
+      onOpen();
+      return;
+    }
+
+    navigate({
+      to: '.',
+      search: () => ({ showLocationDialog: true }),
+    });
+  };
 
   const addLocationMutation = api.useMutation(
     'post',
@@ -59,10 +105,9 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
     {
       onSuccess: async () => {
         await invalidator.invalidate();
-        setItem(null);
         setDesc('');
         setTime(formatDate(new Date(), fmtString));
-        setOpen(false);
+        closeDialog();
       },
     },
   );
@@ -73,7 +118,7 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
     {
       onSuccess: async () => {
         await invalidator.invalidate();
-        setOpen(false);
+        closeDialog();
       },
     },
   );
@@ -84,16 +129,28 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
     {
       onSuccess: async () => {
         await invalidator.invalidate();
-        setOpen(false);
+        closeDialog();
       },
     },
   );
 
   return (
-    <AlertDialog open={open} onOpenChange={setOpen}>
+    <AlertDialog
+      open={open}
+      onOpenChange={(open) => {
+        if (open) {
+          openDialog();
+          return;
+        }
+        closeDialog();
+      }}
+    >
       <AlertDialogTrigger asChild>
-        {isUpdate ? (
-          <Button variant="ghost" size="icon">
+        {isUpdate || onOpen ? (
+          <Button
+            variant="ghost"
+            size="icon"
+          >
             <Settings2Icon className="size-4" />
           </Button>
         ) : (
@@ -101,7 +158,9 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
             variant="secondary"
             size="sm"
             className="ml-auto"
-            onClick={() => setOpen(true)}
+            onClick={() => {
+              openDialog();
+            }}
           >
             <MapPinPlusIcon className="size-4" />
             <span>Add Location</span>
@@ -114,36 +173,53 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
             {isUpdate ? 'Update Location' : 'Add Location to Trip'}
           </AlertDialogTitle>
           <AlertDialogDescription>
-            {item !== null ? (
+            {poiId !== undefined ? (
               <div>
                 {!isUpdate && (
                   <Button
                     variant="link"
                     size="sm"
                     className="!px-0"
-                    onClick={() => setItem(null)}
+                    onClick={() => {
+                      navigate({
+                        to: '.',
+                        search: (prev) => ({ ...prev, poiId: undefined }),
+                      });
+                    }}
                   >
                     <ArrowLeftIcon className="size-4" />
                     <span>Go back</span>
                   </Button>
                 )}
-                <img
-                  src={item.image}
-                  alt=""
-                  className="aspect-[5/2] w-full rounded-md mt-4 object-cover"
-                />
-                <div className="mt-4">
-                  <div className="text-lg font-semibold leading-none tracking-tight text-black">
-                    {item.name}
-                  </div>
-                  <div className="my-1 line-clamp-1 text-sm text-muted-foreground">
-                    {item.city} / {item.state}
-                  </div>
 
-                  <div className="text-sm font-semibold leading-none tracking-tight text-primary">
-                    {item.categoryName}
+                {query.isLoading && (
+                  <div className="flex items-center justify-center">
+                    <Spinner className="mx-auto my-16 size-12" />
                   </div>
-                </div>
+                )}
+
+                {query.data && (
+                  <>
+                    <img
+                      src={query.data.poi.media[0]?.url ?? ''}
+                      alt=""
+                      className="aspect-[5/2] w-full rounded-md mt-4 object-cover"
+                    />
+                    <div className="mt-4">
+                      <div className="text-lg font-semibold leading-none tracking-tight text-black">
+                        {query.data.poi.name}
+                      </div>
+                      <div className="my-1 line-clamp-1 text-sm text-muted-foreground">
+                        {query.data.poi.address.city.name} /{' '}
+                        {query.data.poi.address.city.country.name}
+                      </div>
+
+                      <div className="text-sm font-semibold leading-none tracking-tight text-primary">
+                        {query.data.poi.category.name}
+                      </div>
+                    </div>
+                  </>
+                )}
 
                 <div className="mt-4">
                   <div>
@@ -188,7 +264,10 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
                   showAllResultsButton={false}
                   isCardClickable={true}
                   onCardClick={(v) => {
-                    setItem(v);
+                    navigate({
+                      to: '.',
+                      search: (prev) => ({ ...prev, poiId: v.id }),
+                    });
                   }}
                 />
               </InstantSearch>
@@ -202,11 +281,14 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
               onClick={(e) => {
                 e.preventDefault();
                 if (confirm('Are you sure you want to delete this location?')) {
+                  if (!locId) {
+                    return;
+                  }
                   deleteLocationMutation.mutate({
                     params: {
                       path: {
                         tripId,
-                        locationId: initial.locationId,
+                        locationId: locId,
                       },
                     },
                   });
@@ -219,15 +301,15 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
           )}
           <AlertDialogCancel className="ml-auto">Cancel</AlertDialogCancel>
           <AlertDialogAction
-            disabled={item === null || addLocationMutation.isPending}
+            disabled={poiId === undefined || addLocationMutation.isPending}
             onClick={(e) => {
               e.preventDefault();
-              if (isUpdate) {
+              if (isUpdate && locId !== undefined) {
                 updateLocationMutation.mutate({
                   params: {
                     path: {
                       tripId,
-                      locationId: initial.locationId,
+                      locationId: locId,
                     },
                   },
                   body: {
@@ -243,7 +325,7 @@ export function UpsertLocationDialog({ tripId, initial }: Props) {
                     },
                   },
                   body: {
-                    poiId: item?.id ?? '',
+                    poiId: poiId ?? '',
                     scheduledTime: new Date(time).toISOString(),
                     description: desc,
                   },
