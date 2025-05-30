20fa4fa01d68ab7ece4a07c0b4c7435012fabcc1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-05-31 00:10:41 +0300

    change query to infinite query

--- web/src/routes/trips/my-trips/index.tsx
+++ web/src/routes/trips/my-trips/index.tsx
@@ -1,5 +1,6 @@
 import AppMessage from '@/components/blocks/app-message';
 import { Breadcrumb } from '@/components/blocks/trips/breadcrumb';
+import Spinner from '@/components/kit/spinner';
 import { Separator } from '@/components/ui/separator';
 import { api } from '@/lib/api';
 import { createFileRoute } from '@tanstack/react-router';
@@ -7,35 +8,76 @@ import { TripCard } from './-components/card';
 
 export const Route = createFileRoute('/trips/my-trips/')({
   component: RouteComponent,
-  loader: ({ context }) => {
-    return context.queryClient.ensureQueryData(
-      api.queryOptions('get', '/api/v2/trips/'),
-    );
-  },
 });
 
 function RouteComponent() {
-  const { trips } = Route.useLoaderData();
-
   return (
     <div>
       <Breadcrumb items={[{ name: 'My Trips', href: '/trips/my-trips' }]} />
-      <div className="mt-8">
-        {trips.map((trip, i) => (
-          <div key={trip.id}>
-            <TripCard trip={trip} />
-            {i !== trips.length - 1 && <Separator className="my-1" />}
-          </div>
-        ))}
-        {trips.length === 0 && (
-          <AppMessage
-            emptyMessage="You haven't created or joined any trips yet"
-            backLink="/trips"
-            backLinkText="Go to Trips page"
-            className="my-16"
-          />
-        )}
+      <Content />
+    </div>
+  );
+}
+
+function Content() {
+  const query = api.useInfiniteQuery(
+    'get',
+    '/api/v2/trips/',
+    {
+      params: {
+        query: {
+          pageSize: 10,
+        },
+      },
+    },
+    {
+      initialPageParam: 1,
+      pageParamName: 'page',
+      getNextPageParam: (lastPage) =>
+        lastPage.pagination.hasNext ? lastPage.pagination.page + 1 : undefined,
+    },
+  );
+
+  if (query.isPending) {
+    return (
+      <div>
+        <Spinner className="mx-auto my-32 size-12" />
       </div>
+    );
+  }
+
+  if (query.error) {
+    return (
+      <AppMessage
+        errorMessage={query.error.title ?? 'Something went wrong'}
+        backLink="/trips"
+        backLinkText="Go to Trips page"
+        className="my-16"
+      />
+    );
+  }
+
+  const trips = query.data.pages.flatMap((page) => page.trips);
+
+  if (trips.length === 0) {
+    return (
+      <AppMessage
+        emptyMessage="You haven't created or joined any trips yet"
+        backLink="/trips"
+        backLinkText="Go to Trips page"
+        className="my-16"
+      />
+    );
+  }
+
+  return (
+    <div className="mt-8">
+      {trips.map((trip, i) => (
+        <div key={trip.id}>
+          <TripCard trip={trip} />
+          {i !== trips.length - 1 && <Separator className="my-1" />}
+        </div>
+      ))}
     </div>
   );
 }
