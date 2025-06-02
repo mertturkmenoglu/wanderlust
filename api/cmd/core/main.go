c0027bdfa24bed91ed1a68fef2a16c8bba3ac90a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-02 22:18:33 +0300

    move routes

--- api/cmd/core/main.go
+++ api/cmd/core/main.go
@@ -1,6 +1,23 @@
 package main
 
 import (
+	"wanderlust/app/aggregator"
+	"wanderlust/app/amenities"
+	"wanderlust/app/auth"
+	"wanderlust/app/bookmarks"
+	"wanderlust/app/categories"
+	"wanderlust/app/cities"
+	"wanderlust/app/collections"
+	"wanderlust/app/diaries"
+	"wanderlust/app/favorites"
+	"wanderlust/app/health"
+	"wanderlust/app/images"
+	"wanderlust/app/lists"
+	"wanderlust/app/pois"
+	"wanderlust/app/reports"
+	"wanderlust/app/reviews"
+	"wanderlust/app/trips"
+	"wanderlust/app/users"
 	"wanderlust/pkg/core"
 
 	"github.com/danielgtaylor/huma/v2/humacli"
@@ -8,6 +25,26 @@ import (
 
 type Options struct {
 	Port int `help:"Port to run the server on" default:"5000"`
+}
+
+var routes = []core.RegisterFunc{
+	aggregator.Register,
+	amenities.Register,
+	auth.Register,
+	bookmarks.Register,
+	categories.Register,
+	cities.Register,
+	collections.Register,
+	diaries.Register,
+	favorites.Register,
+	health.Register,
+	images.Register,
+	lists.Register,
+	pois.Register,
+	reports.Register,
+	reviews.Register,
+	trips.Register,
+	users.Register,
 }
 
 func main() {
