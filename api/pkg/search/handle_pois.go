0aaf4b986a752668d1c60752123e8be700bc2096
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-03 00:02:32 +0300

    add poi sync

---
+++ api/pkg/search/handle_pois.go
@@ -0,0 +1,90 @@
+package search
+
+import (
+	"context"
+	"wanderlust/app/pois"
+	"wanderlust/pkg/core"
+	"wanderlust/pkg/db"
+
+	"github.com/pterm/pterm"
+	"github.com/sony/sonyflake"
+	tsapi "github.com/typesense/typesense-go/v2/typesense/api"
+)
+
+func handlePoiSync() error {
+	logger := pterm.DefaultLogger.WithLevel(pterm.LogLevelTrace)
+	d := db.NewDb()
+	ctx := context.Background()
+
+	totalCount, err := d.Queries.CountPois(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	var i int64 = 0
+	flake, err := sonyflake.New(sonyflake.Settings{})
+
+	if err != nil {
+		return err
+	}
+
+	searchService := New()
+	s := pois.NewService(&core.Application{
+		Db:    d,
+		Flake: flake,
+	})
+
+	const step int64 = 1000
+
+	_, err = searchService.Client.Collection(string(CollectionPois)).Delete(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	searchService.CreateSchemas()
+
+	for i = 0; i <= totalCount; i += step {
+		logger.Trace("syncing", logger.Args("i", i))
+		ids, err := d.Queries.GetPaginatedPoiIds(ctx, db.GetPaginatedPoiIdsParams{
+			Offset: int32(i),
+			Limit:  int32(step),
+		})
+
+		if err != nil {
+			return err
+		}
+
+		pois, err := s.FindMany(ctx, ids)
+
+		if err != nil {
+			return err
+		}
+
+		var docs = make([]any, len(pois))
+
+		for i, poi := range pois {
+			docs[i] = map[string]any{
+				"name":     poi.Name,
+				"poi":      poi,
+				"location": []float64{poi.Address.Lat, poi.Address.Lng},
+			}
+		}
+
+		if len(docs) == 0 {
+			continue
+		}
+
+		_, err = searchService.Client.
+			Collection(string(CollectionPois)).
+			Documents().
+			Import(ctx, docs, &tsapi.ImportDocumentsParams{})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ api/pkg/search/sync.go
@@ -0,0 +1,28 @@
+package search
+
+import (
+	"log"
+	"time"
+
+	"github.com/pterm/pterm"
+)
+
+func Sync() error {
+	spinner, _ := pterm.DefaultSpinner.Start("Syncing search index")
+	start := time.Now()
+
+	err := handlePoiSync()
+
+	elapsed := time.Since(start)
+
+	if err != nil {
+		spinner.Fail("Failed to sync search index")
+		return err
+	} else {
+		spinner.Success()
+	}
+
+	log.Println("Synced search index in", elapsed.String())
+
+	return nil
+}
