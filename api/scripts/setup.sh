137deb007f623656de184817304b824e946981a8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-03 02:09:56 +0300

    change the order of setup steps

--- api/scripts/setup.sh
+++ api/scripts/setup.sh
@@ -1,7 +1,7 @@
 	echo "=> Setting up the project..."
 	go mod download
 	just env-pull
+	just docker
 	goose up
 	just sqlc
-	just docker
 	echo "=> Setup completed. Run 'just watch' to start the server."
