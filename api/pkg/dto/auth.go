0b6a00064411f93e2bb63d9def0b3b397fe6bca8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-03 12:03:58 +0300

    add token refresh dtos

--- api/pkg/dto/auth.go
+++ api/pkg/dto/auth.go
@@ -24,6 +24,14 @@ type LoginOutputBody struct {
 	Token string `json:"token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" doc:"JWT token for the user"`
 }
 
+type RefreshInput struct {
+	CookieRefreshToken string `cookie:"refresh_token" doc:"Refresh token of the user"`
+}
+
+type RefreshOutput struct {
+	SetCookie []http.Cookie `header:"Set-Cookie"`
+}
+
 type RegisterInput struct {
 	Body RegisterInputBody
 }
@@ -53,22 +61,22 @@ type GetMeOutput struct {
 }
 
 type GetMeOutputBody struct {
-	ID                    string    `json:"id" example:"564457817990234127" doc:"ID of the user"`
-	Email                 string    `json:"email" example:"john@example.com" doc:"Email of the user"`
-	Username              string    `json:"username" example:"johndoe" doc:"Username of the user"`
-	FullName              string    `json:"fullName" example:"John Doe" doc:"Full name of the user"`
-	GoogleID              *string   `json:"googleId" example:"1234567890" doc:"Google ID of the user" required:"true"`
-	FacebookID            *string   `json:"facebookId" example:"1234567890" doc:"Facebook ID of the user" required:"true"`
-	IsVerified            bool      `json:"isVerified" example:"false" doc:"Is the user verified"`
-	Bio                   *string   `json:"bio" example:"Lorem ipsum" doc:"Bio of the user" required:"true"`
-	Pronouns              *string   `json:"pronouns" example:"he/him" doc:"Pronouns of the user" required:"true"`
-	Website               *string   `json:"website" example:"https://example.com" doc:"Website of the user" required:"true"`
-	ProfileImage          *string   `json:"profileImage" example:"https://example.com/profile.jpg" doc:"Profile image of the user" required:"true"`
-	BannerImage           *string   `json:"bannerImage" example:"https://example.com/banner.jpg" doc:"Banner image of the user" required:"true"`
-	FollowersCount        int32     `json:"followersCount" example:"100" doc:"Number of followers"`
-	FollowingCount        int32     `json:"followingCount" example:"50" doc:"Number of following"`
-	CreatedAt             time.Time `json:"createdAt" example:"2023-01-01T00:00:00Z" doc:"Creation date of the user"`
-	UpdatedAt             time.Time `json:"updatedAt" example:"2023-01-01T00:00:00Z" doc:"Last update date of the user"`
+	ID             string    `json:"id" example:"564457817990234127" doc:"ID of the user"`
+	Email          string    `json:"email" example:"john@example.com" doc:"Email of the user"`
+	Username       string    `json:"username" example:"johndoe" doc:"Username of the user"`
+	FullName       string    `json:"fullName" example:"John Doe" doc:"Full name of the user"`
+	GoogleID       *string   `json:"googleId" example:"1234567890" doc:"Google ID of the user" required:"true"`
+	FacebookID     *string   `json:"facebookId" example:"1234567890" doc:"Facebook ID of the user" required:"true"`
+	IsVerified     bool      `json:"isVerified" example:"false" doc:"Is the user verified"`
+	Bio            *string   `json:"bio" example:"Lorem ipsum" doc:"Bio of the user" required:"true"`
+	Pronouns       *string   `json:"pronouns" example:"he/him" doc:"Pronouns of the user" required:"true"`
+	Website        *string   `json:"website" example:"https://example.com" doc:"Website of the user" required:"true"`
+	ProfileImage   *string   `json:"profileImage" example:"https://example.com/profile.jpg" doc:"Profile image of the user" required:"true"`
+	BannerImage    *string   `json:"bannerImage" example:"https://example.com/banner.jpg" doc:"Banner image of the user" required:"true"`
+	FollowersCount int32     `json:"followersCount" example:"100" doc:"Number of followers"`
+	FollowingCount int32     `json:"followingCount" example:"50" doc:"Number of following"`
+	CreatedAt      time.Time `json:"createdAt" example:"2023-01-01T00:00:00Z" doc:"Creation date of the user"`
+	UpdatedAt      time.Time `json:"updatedAt" example:"2023-01-01T00:00:00Z" doc:"Last update date of the user"`
 }
 
 type OAuthInput struct {
