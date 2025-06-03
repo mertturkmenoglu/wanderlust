c14e30d79d048550528ea5b7796d41cff6d1f882
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-03 04:51:44 +0300

    update infisical script

--- api/scripts/infisical.sh
+++ api/scripts/infisical.sh
@@ -1,18 +1,19 @@
 op=$1
 
-if [ $op = "env-example" ]; then
+if [ $op = "example" ]; then
   echo "=> Creating example .env file..."
 	infisical secrets generate-example-env > .env.example
 	echo "=> Created example .env file"
-elif [ $op = "env-pull" ]; then
+elif [ $op = "pull" ]; then
   echo "=> Exporting secrets to .env file..."
 	infisical export > .env
 	echo "=> Exported secrets to .env file"
-elif [ $op = "env-push" ]; then
+elif [ $op = "push" ]; then
   echo "=> Pushing secrets to Infisical..."
 	infisical secrets set --file="./.env"
 	echo "=> Pushed secrets to Infisical"
 else
   echo "Invalid operation: $op"
+  echo "Valid operations: example, pull, push"
   exit 1
 fi
