7583f40ad62d623ddf7aa160f91268f7c6d6a55a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-03 05:44:56 +0300

    add trips tasks processor

---
+++ api/pkg/tasks/process_trips.go
@@ -0,0 +1,51 @@
+package tasks
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+)
+
+const (
+	TypeFindExpiredTripInvites  = "trip:find-expired-invites"
+	TypeDeleteExpiredTripInvite = "trip:delete-expired-invite"
+)
+
+type DeleteExpiredTripInvitePayload struct {
+	ID string
+}
+
+func (svc *TasksService) FindExpiredTripInvitesTask(ctx context.Context, t *asynq.Task) error {
+	ids, err := svc.db.Queries.FindExpiredTripInvites(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	svc.logger.Info("FindExpiredTripInvitesTask", svc.logger.Args("count", len(ids)))
+
+	for _, id := range ids {
+		_, err = svc.CreateAndEnqueue(Job{
+			Type: TypeDeleteExpiredTripInvite,
+			Data: DeleteExpiredTripInvitePayload{
+				ID: id,
+			},
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (svc *TasksService) DeleteExpiredTripInviteTask(ctx context.Context, t *asynq.Task) error {
+	p, err := parse[DeleteExpiredTripInvitePayload](t.Payload())
+
+	if err != nil {
+		return err
+	}
+
+	return svc.db.Queries.DeleteInvite(ctx, p.ID)
+}
