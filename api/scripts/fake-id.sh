eff9ab517c181e7149e0dbbd88102874ef22bf2a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-04 09:14:06 +0300

    use loop

--- api/scripts/fake-id.sh
+++ api/scripts/fake-id.sh
@@ -1,23 +1,20 @@
+#!/bin/env bash
+
 # Run SQL queries and dump their results to a CSV file.
 # Then copy the CSV file from the Docker container to the host machine.
 # Make sure you are running this script from the api project root directory.
 
-docker exec -i wl-postgres psql -d wanderlust -U postgres -c "SELECT id FROM pois" --csv -o /home/pois.csv
-docker cp wl-postgres:/home/pois.csv tmp/pois.csv
-tail -n +2 tmp/pois.csv > tmp/pois.txt
-rm tmp/pois.csv
+domains=(
+  "pois"
+  "users"
+  "reviews"
+  "collections"
+)
 
-docker exec -i wl-postgres psql -d wanderlust -U postgres -c "SELECT id FROM users" --csv -o /home/users.csv
-docker cp wl-postgres:/home/users.csv tmp/users.csv
-tail -n +2 tmp/users.csv > tmp/users.txt
-rm tmp/users.csv
-
-docker exec -i wl-postgres psql -d wanderlust -U postgres -c "SELECT id FROM reviews" --csv -o /home/reviews.csv
-docker cp wl-postgres:/home/reviews.csv tmp/reviews.csv
-tail -n +2 tmp/reviews.csv > tmp/reviews.txt
-rm tmp/reviews.csv
-
-docker exec -i wl-postgres psql -d wanderlust -U postgres -c "SELECT id FROM collections" --csv -o /home/collections.csv
-docker cp wl-postgres:/home/collections.csv tmp/collections.csv
-tail -n +2 tmp/collections.csv > tmp/collections.txt
-rm tmp/collections.csv
+for i in "${!domains[@]}"; do
+  domain="${domains[$i]}"
+  docker exec -i wl-postgres psql -d wanderlust -U postgres -c "SELECT id FROM $domain" --csv -o /home/$domain.csv
+  docker cp wl-postgres:/home/$domain.csv tmp/$domain.csv
+  tail -n +2 tmp/$domain.csv > tmp/$domain.txt
+  rm tmp/$domain.csv
+done
