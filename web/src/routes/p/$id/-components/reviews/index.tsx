0c645085e2d9e2918a778f14eb9b86bd30760abf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-04 10:41:05 +0300

    move component

--- web/src/routes/p/$id/-components/reviews/index.tsx
+++ web/src/routes/p/$id/-components/reviews/index.tsx
@@ -1,4 +1,3 @@
-import { ReviewImages } from './images';
 import { RatingsSection } from './ratings-section';
 import { Section } from './section';
 
@@ -7,7 +6,6 @@ export default function Reviews() {
     <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-4">
       <RatingsSection className="md:col-span-1" />
       <div className="flex flex-col gap-4 md:col-span-2">
-        <ReviewImages />
         <Section />
       </div>
     </div>
