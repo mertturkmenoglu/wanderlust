a426ef433005895933c1f33c7f929765d41d64ee
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-04 10:04:17 +0300

    change layout

--- web/src/routes/p/$id/-components/reviews/index.tsx
+++ web/src/routes/p/$id/-components/reviews/index.tsx
@@ -1,11 +1,13 @@
-import Header from './header';
+import { ReviewImages } from './images';
+import { RatingsSection } from './ratings-section';
 import { Section } from './section';
 
 export default function Reviews() {
   return (
-    <div className="mt-4">
-      <Header />
-      <div className="flex flex-col gap-4 mt-8">
+    <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <RatingsSection className="md:col-span-1" />
+      <div className="flex flex-col gap-4 md:col-span-2">
+        <ReviewImages />
         <Section />
       </div>
     </div>
