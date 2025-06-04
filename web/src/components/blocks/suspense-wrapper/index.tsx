a3e8458b4844eee4e737fa6c30e359c6b23cada9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-04 10:03:10 +0300

    add suspense wrapper component

---
+++ web/src/components/blocks/suspense-wrapper/index.tsx
@@ -0,0 +1,31 @@
+import Spinner from '@/components/kit/spinner';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import AppMessage from '../app-message';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function SuspenseWrapper({ children }: Props) {
+  return (
+    <ErrorBoundary
+      fallback={
+        <AppMessage
+          errorMessage="Something went wrong"
+          showBackButton={false}
+        />
+      }
+    >
+      <Suspense
+        fallback={
+          <div className="flex justify-center">
+            <Spinner className="size-12 mx-auto my-4" />
+          </div>
+        }
+      >
+        {children}
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
