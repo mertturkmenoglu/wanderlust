8dcd2a0afae8b0a7916dff2a7326452babd3f4fb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-04 10:04:10 +0300

    rename

--- web/src/routes/p/$id/-components/reviews/ratings-section.tsx
+++ web/src/routes/p/$id/-components/reviews/ratings-section.tsx
@@ -3,19 +3,25 @@ import { Progress } from '@/components/ui/progress';
 import { api } from '@/lib/api';
 import type { components } from '@/lib/api-types';
 import { computeRating } from '@/lib/rating';
+import { cn } from '@/lib/utils';
 import { getRouteApi } from '@tanstack/react-router';
 import { LoaderCircleIcon, StarIcon } from 'lucide-react';
 import CreateReviewDialog from './create-dialog';
 
-export default function Header() {
+type Props = {
+  className?: string;
+};
+
+const fmt = Intl.NumberFormat('en-US', {
+  style: 'decimal',
+  compactDisplay: 'short',
+  notation: 'compact',
+});
+
+export function RatingsSection({ className }: Props) {
   const route = getRouteApi('/p/$id/');
   const { poi } = route.useLoaderData();
   const rating = computeRating(poi.totalPoints, poi.totalVotes);
-  const fmt = Intl.NumberFormat('en-US', {
-    style: 'decimal',
-    compactDisplay: 'short',
-    notation: 'compact',
-  });
 
   const query = api.useQuery('get', '/api/v2/reviews/poi/{id}/ratings', {
     params: {
@@ -26,7 +32,12 @@ export default function Header() {
   });
 
   return (
-    <div className="bg-gradient-to-r from-accent/50 to-primary/10 p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+    <div
+      className={cn(
+        'bg-gradient-to-r from-accent/50 to-primary/10 p-8 flex flex-col gap-8',
+        className,
+      )}
+    >
       <div>
         <h3 className="font-bold text-xl text-primary">Reviews</h3>
         <div className="my-2 flex items-center gap-4">
@@ -54,23 +65,18 @@ export default function Header() {
   );
 }
 
-type Props = {
+type RatingsProps = {
   ratings: components['schemas']['GetRatingsByPoiIdOutputBody'];
 };
 
-function Ratings({ ratings }: Props) {
+function Ratings({ ratings }: RatingsProps) {
   const entries = Object.entries(ratings.ratings).map(
     (v) => [+v[0], v[1]] as const,
   );
   const sorted = entries.sort((a, b) => b[0] - a[0]);
-  const fmt = Intl.NumberFormat('en-US', {
-    compactDisplay: 'short',
-    style: 'decimal',
-    notation: 'compact',
-  });
 
   return (
-    <div className="lg:col-span-2 space-y-2">
+    <div className="space-y-2">
       {sorted.map(([rating, count]) => (
         <div
           key={rating}
