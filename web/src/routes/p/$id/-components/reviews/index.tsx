bbd7017e95c5b00eace48feec37c53a369d42dc9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-04 11:46:38 +0300

    make ratings sticky

--- web/src/routes/p/$id/-components/reviews/index.tsx
+++ web/src/routes/p/$id/-components/reviews/index.tsx
@@ -4,7 +4,9 @@ import { Section } from './section';
 export default function Reviews() {
   return (
     <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-      <RatingsSection className="md:col-span-1" />
+      <div className="md:col-span-1">
+        <RatingsSection className="sticky top-8" />
+      </div>
       <div className="flex flex-col gap-4 md:col-span-2">
         <Section />
       </div>
