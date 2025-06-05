2b76ce946b0259c5cdd4d2e80333faf8866f28a6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-05 11:04:05 +0300

    remove id column

--- api/pkg/db/migrations/20250531105845_favorites.sql
+++ api/pkg/db/migrations/20250531105845_favorites.sql
@@ -1,16 +1,14 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS favorites (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   poi_id TEXT NOT NULL,
   user_id TEXT NOT NULL,
   created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  PRIMARY KEY (poi_id, user_id),
   CONSTRAINT
     fk_favorites_poi FOREIGN KEY (poi_id) REFERENCES pois(id) ON DELETE CASCADE,
   CONSTRAINT
-    fk_favorites_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-  CONSTRAINT 
-    idx_favorites_unique UNIQUE (poi_id, user_id)
+    fk_favorites_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- Create indexes
