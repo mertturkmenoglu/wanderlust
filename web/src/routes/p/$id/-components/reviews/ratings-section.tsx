0a88f8306efbbdd9ba0bc0793cf39cc5b12f7630
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-05 06:21:04 +0300

    add filtering

--- web/src/routes/p/$id/-components/reviews/ratings-section.tsx
+++ web/src/routes/p/$id/-components/reviews/ratings-section.tsx
@@ -1,11 +1,14 @@
 import FormattedRating from '@/components/kit/formatted-rating';
+import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
+import { Slider } from '@/components/ui/slider';
 import { api } from '@/lib/api';
 import type { components } from '@/lib/api-types';
 import { computeRating } from '@/lib/rating';
 import { cn } from '@/lib/utils';
-import { getRouteApi } from '@tanstack/react-router';
+import { getRouteApi, useNavigate } from '@tanstack/react-router';
 import { LoaderCircleIcon, StarIcon } from 'lucide-react';
+import { useState } from 'react';
 import { CreateReviewDialog } from './create/create-dialog';
 import { ReviewImages } from './images';
 
@@ -64,6 +67,7 @@ export function RatingsSection({ className }: Props) {
         )}
 
         {query.data && <Ratings ratings={query.data} />}
+        {query.data && <Filters />}
       </div>
 
       <div className="mt-4">
@@ -102,6 +106,106 @@ function Ratings({ ratings }: RatingsProps) {
           </span>
         </div>
       ))}
+    </div>
+  );
+}
+
+function Filters() {
+  const route = getRouteApi('/p/$id/');
+  const search = route.useSearch();
+  const [minRating, setMinRating] = useState(search.minRating ?? 0);
+  const [maxRating, setMaxRating] = useState(search.maxRating ?? 5);
+  const [sortBy, setSortBy] = useState(search.sortBy ?? 'created_at');
+  const [sortOrd, setSortOrd] = useState(search.sortOrd ?? 'desc');
+  const navigate = useNavigate();
+
+  const isAllRatings = minRating === 0 && maxRating === 5;
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between gap-2">
+          <span className="font-semibold">Filters</span>
+          <Button
+            size="sm"
+            onClick={() => {
+              navigate({
+                to: '.',
+                search: (prev) => ({
+                  ...prev,
+                  minRating: minRating,
+                  maxRating: maxRating,
+                  sortBy: sortBy,
+                  sortOrd: sortOrd,
+                }),
+              });
+            }}
+          >
+            Apply
+          </Button>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <div className="text-sm font-semibold text-muted-foreground">
+            Rating {isAllRatings ? '' : `(${minRating} - ${maxRating})`}
+          </div>
+          <Slider
+            defaultValue={[0, 5]}
+            value={[minRating, maxRating]}
+            onValueChange={([min, max]) => {
+              setMinRating(min ?? 0);
+              setMaxRating(max ?? 5);
+            }}
+            minStepsBetweenThumbs={0}
+            max={5}
+            step={1}
+          />
+        </div>
+
+        <div className="flex flex-col gap-2 mt-4">
+          <div className="text-sm font-semibold text-muted-foreground">
+            Sort By
+          </div>
+          <div className="flex items-center gap-2">
+            <Button
+              variant={sortBy === 'created_at' ? 'default' : 'ghost'}
+              size="sm"
+              onClick={() => setSortBy('created_at')}
+            >
+              Date
+            </Button>
+            <Button
+              variant={sortBy === 'rating' ? 'default' : 'ghost'}
+              size="sm"
+              onClick={() => setSortBy('rating')}
+            >
+              Rating
+            </Button>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 mt-4">
+          <div className="text-sm font-semibold text-muted-foreground">
+            Sort Order
+          </div>
+          <div className="flex items-center gap-2">
+            <Button
+              variant={sortOrd === 'desc' ? 'default' : 'ghost'}
+              size="sm"
+              onClick={() => setSortOrd('desc')}
+            >
+              {sortBy === 'created_at' ? 'Newest First' : 'Highest First'}
+            </Button>
+            <Button
+              variant={sortOrd === 'asc' ? 'default' : 'ghost'}
+              size="sm"
+              onClick={() => setSortOrd('asc')}
+            >
+              {sortBy === 'created_at' ? 'Oldest First' : 'Lowest First'}
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
