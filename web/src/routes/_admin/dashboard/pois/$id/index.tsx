ab61fb9120fa6e5c3cc19ac562d6f0759c4f2901
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-06 07:31:37 +0300

    update poi detail display

--- web/src/routes/_admin/dashboard/pois/$id/index.tsx
+++ web/src/routes/_admin/dashboard/pois/$id/index.tsx
@@ -1,11 +1,14 @@
 import DashboardActions from '@/components/blocks/dashboard/actions';
 import DashboardBreadcrumb from '@/components/blocks/dashboard/breadcrumb';
+import { keyValueCols } from '@/components/blocks/dashboard/columns';
+import { DataTable } from '@/components/blocks/dashboard/data-table';
 import { buttonVariants } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { api } from '@/lib/api';
 import { ipx } from '@/lib/ipx';
 import { cn } from '@/lib/utils';
 import { createFileRoute, Link } from '@tanstack/react-router';
+import { formatDistanceToNow } from 'date-fns';
 
 export const Route = createFileRoute('/_admin/dashboard/pois/$id/')({
   component: RouteComponent,
@@ -69,10 +72,84 @@ function RouteComponent() {
       />
 
       <div className="flex flex-col gap-2 mt-2">
-        <div className="font-semibold">All Details:</div>
-        <pre className="max-w-3xl break-words flex-wrap text-wrap whitespace-pre-wrap">
-          {JSON.stringify(poi, null, 2)}
-        </pre>
+        <DataTable
+          columns={keyValueCols}
+          filterColumnId=""
+          data={[
+            {
+              k: 'ID',
+              v: poi.id,
+            },
+            {
+              k: 'Name',
+              v: poi.name,
+            },
+            {
+              k: 'Phone',
+              v: poi.phone ?? '-',
+            },
+            {
+              k: 'Description',
+              v: poi.description,
+            },
+            {
+              k: 'Address ID',
+              v: `${poi.addressId}`,
+            },
+            {
+              k: 'Website',
+              v: poi.website ?? '-',
+            },
+            {
+              k: 'Price Level',
+              v: `${poi.priceLevel}`,
+            },
+            {
+              k: 'Accessibility Level',
+              v: `${poi.accessibilityLevel}`,
+            },
+            {
+              k: 'Total Votes',
+              v: `${poi.totalVotes}`,
+            },
+            {
+              k: 'Total Points',
+              v: `${poi.totalPoints}`,
+            },
+            {
+              k: 'Total Favorites',
+              v: `${poi.totalFavorites}`,
+            },
+            {
+              k: 'Category ID',
+              v: `${poi.categoryId}`,
+            },
+            {
+              k: 'Hours',
+              v: Object.keys(poi.hours).join(', ').toUpperCase(),
+            },
+            {
+              k: 'Created At',
+              v: `${formatDistanceToNow(new Date(poi.createdAt))} ago`,
+            },
+            {
+              k: 'Updated At',
+              v: `${formatDistanceToNow(new Date(poi.updatedAt))} ago`,
+            },
+            {
+              k: 'Total Amenities',
+              v: `${poi.amenities.length}`,
+            },
+            {
+              k: 'Total Media',
+              v: `${poi.media.length}`,
+            },
+            {
+              k: 'Address',
+              v: JSON.stringify(poi.address, null, 2),
+            },
+          ]}
+        />
       </div>
     </div>
   );
