1e2f44fab0f6c0bfbd166a8845f0f2936eb4f6b4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-07 11:33:43 +0300

    use env variable to enable/disable ipx

--- web/src/lib/ipx.ts
+++ web/src/lib/ipx.ts
@@ -1,5 +1,11 @@
 export const imgProxyBaseUrl = import.meta.env.VITE_IMG_PROXY_URL ?? '';
 
+const enableIpx = import.meta.env.VITE_ENABLE_IPX ?? false;
+
 export function ipx(ogUrl: string, ops: string): string {
-  return `${imgProxyBaseUrl}/${ops}/${ogUrl}`;
+  if (!enableIpx) {
+    return ogUrl;
+  } else {
+    return `${imgProxyBaseUrl}/${ops}/${ogUrl}`;
+  }
 }
