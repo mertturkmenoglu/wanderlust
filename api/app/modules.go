18bfe2ef100a3d3969d0523736b1ff9e10d8c443
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-07 16:52:54 +0300

    register modules in app package

---
+++ api/app/modules.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"wanderlust/app/aggregator"
+	"wanderlust/app/amenities"
+	"wanderlust/app/auth"
+	"wanderlust/app/bookmarks"
+	"wanderlust/app/categories"
+	"wanderlust/app/cities"
+	"wanderlust/app/collections"
+	"wanderlust/app/diaries"
+	"wanderlust/app/favorites"
+	"wanderlust/app/health"
+	"wanderlust/app/images"
+	"wanderlust/app/lists"
+	"wanderlust/app/pois"
+	"wanderlust/app/reports"
+	"wanderlust/app/reviews"
+	"wanderlust/app/trips"
+	"wanderlust/app/users"
+	"wanderlust/pkg/core"
+)
+
+var Modules = []core.RegisterFunc{
+	aggregator.Register,
+	amenities.Register,
+	auth.Register,
+	bookmarks.Register,
+	categories.Register,
+	cities.Register,
+	collections.Register,
+	diaries.Register,
+	favorites.Register,
+	health.Register,
+	images.Register,
+	lists.Register,
+	pois.Register,
+	reports.Register,
+	reviews.Register,
+	trips.Register,
+	users.Register,
+}
--- api/main.go
+++ api/main.go
@@ -1,23 +1,7 @@
 package main
 
 import (
-	"wanderlust/app/aggregator"
-	"wanderlust/app/amenities"
-	"wanderlust/app/auth"
-	"wanderlust/app/bookmarks"
-	"wanderlust/app/categories"
-	"wanderlust/app/cities"
-	"wanderlust/app/collections"
-	"wanderlust/app/diaries"
-	"wanderlust/app/favorites"
-	"wanderlust/app/health"
-	"wanderlust/app/images"
-	"wanderlust/app/lists"
-	"wanderlust/app/pois"
-	"wanderlust/app/reports"
-	"wanderlust/app/reviews"
-	"wanderlust/app/trips"
-	"wanderlust/app/users"
+	"wanderlust/app"
 	"wanderlust/pkg/commands"
 	"wanderlust/pkg/core"
 
@@ -29,33 +13,13 @@ type Options struct {
 	EnvFile string `help:"Path to the .env file" default:".env"`
 }
 
-var routes = []core.RegisterFunc{
-	aggregator.Register,
-	amenities.Register,
-	auth.Register,
-	bookmarks.Register,
-	categories.Register,
-	cities.Register,
-	collections.Register,
-	diaries.Register,
-	favorites.Register,
-	health.Register,
-	images.Register,
-	lists.Register,
-	pois.Register,
-	reports.Register,
-	reviews.Register,
-	trips.Register,
-	users.Register,
-}
-
 func main() {
 	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
 		core.LoadEnv(options.EnvFile)
 
 		w := core.New()
 		w.SetupEcho()
-		w.Routing(routes...)
+		w.Routing(app.Modules...)
 
 		hooks.OnStart(func() {
 			w.StartServer()
