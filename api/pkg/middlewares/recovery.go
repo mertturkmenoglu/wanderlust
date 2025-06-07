4b122af1214508c9c001243e2334002f465a786d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-07 09:27:43 +0300

    handle recovery middleware inner errors

--- api/pkg/middlewares/recovery.go
+++ api/pkg/middlewares/recovery.go
@@ -12,11 +12,26 @@ import (
 func CustomRecovery() echo.MiddlewareFunc {
 	return middleware.RecoverWithConfig(middleware.RecoverConfig{
 		LogErrorFunc: func(c echo.Context, err error, stack []byte) error {
-			f, _ := os.Create("tmp/" + random.FromLetters(16) + ".log")
+			f, ferr := os.Create("tmp/" + random.FromLetters(16) + ".log")
+
+			if ferr != nil {
+				return ferr
+			}
+
 			defer f.Close()
 
-			f.Write([]byte(err.Error()))
-			f.Write(stack)
+			_, ferr = f.Write([]byte(err.Error()))
+
+			if ferr != nil {
+				return ferr
+			}
+
+			_, ferr = f.Write(stack)
+
+			if ferr != nil {
+				return ferr
+			}
+
 			log.Println(err)
 
 			return err
