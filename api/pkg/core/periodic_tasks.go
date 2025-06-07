1dcbf7c46d2ca9e73af8af4e1b22d8ef76b0b186
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-07 17:16:55 +0300

    update periodic task cron values

--- api/pkg/core/periodic_tasks.go
+++ api/pkg/core/periodic_tasks.go
@@ -1,32 +1,14 @@
 package core
 
 import (
-	"wanderlust/pkg/cfg"
 	"wanderlust/pkg/tasks"
 
 	"github.com/hibiken/asynq"
 )
 
-func GetCronSpecForTask(taskType string) string {
-	isDev := cfg.Env.Env == "dev"
-
-	switch taskType {
-	case tasks.TypeFindExpiredTripInvites:
-		{
-			if isDev {
-				return "@every 1m"
-			} else {
-				return "@every 5m"
-			}
-		}
-	default:
-		panic("unknown task type")
-	}
-}
-
 func (w *Wanderlust) RegisterPeriodicTasks() {
 	_, err := w.app.Tasks.Schedule(
-		GetCronSpecForTask(tasks.TypeFindExpiredTripInvites),
+		cronFor(tasks.TypeFindExpiredTripInvites),
 		asynq.NewTask(tasks.TypeFindExpiredTripInvites, []byte{}, asynq.MaxRetry(1)),
 	)
 
