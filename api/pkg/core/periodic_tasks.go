93c970cf5ea5987e44087f0eed2a4734d1a9b297
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-07 12:19:17 +0300

    change cron spec strings according to env

--- api/pkg/core/periodic_tasks.go
+++ api/pkg/core/periodic_tasks.go
@@ -1,13 +1,34 @@
 package core
 
 import (
+	"wanderlust/pkg/cfg"
 	"wanderlust/pkg/tasks"
 
 	"github.com/hibiken/asynq"
 )
 
+func GetCronSpecForTask(taskType string) string {
+	isDev := cfg.Env.Env == "dev"
+
+	switch taskType {
+	case tasks.TypeFindExpiredTripInvites:
+		{
+			if isDev {
+				return "@every 1m"
+			} else {
+				return "@every 5m"
+			}
+		}
+	default:
+		panic("unknown task type")
+	}
+}
+
 func (w *Wanderlust) RegisterPeriodicTasks() {
-	_, err := w.app.Tasks.Schedule("@every 1m", asynq.NewTask(tasks.TypeFindExpiredTripInvites, []byte{}, asynq.MaxRetry(1)))
+	_, err := w.app.Tasks.Schedule(
+		GetCronSpecForTask(tasks.TypeFindExpiredTripInvites),
+		asynq.NewTask(tasks.TypeFindExpiredTripInvites, []byte{}, asynq.MaxRetry(1)),
+	)
 
 	if err != nil {
 		panic(err)
