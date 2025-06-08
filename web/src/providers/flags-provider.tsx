b933ac9a91d7babb7f6298cedc8cbf4147f9d27c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-08 19:50:48 +0300

    add feature flags provider

---
+++ web/src/providers/flags-provider.tsx
@@ -0,0 +1,55 @@
+import { env } from '@/lib/env';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+
+export type FlagsResponse = {
+  version: string;
+  flags: Record<string, any>;
+};
+
+export type FlagsContextState = {
+  flags: FlagsResponse;
+};
+
+export const FlagsContext = React.createContext<FlagsContextState>({
+  flags: {
+    flags: {},
+    version: '',
+  },
+});
+
+export function FlagsContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const query = useSuspenseQuery({
+    queryKey: ['flags'],
+    queryFn: async () => {
+      const baseUrl = env.VITE_FLAGS_SERVICE_URL;
+      const res = await fetch(`${baseUrl}/flags`);
+      const data = (await res.json()) as FlagsResponse;
+      return data;
+    },
+  });
+
+  return (
+    <FlagsContext.Provider
+      value={{
+        flags: query.data,
+      }}
+    >
+      {children}
+    </FlagsContext.Provider>
+  );
+}
+
+export function useFeatureFlags() {
+  const ctx = useContext(FlagsContext);
+
+  if (!ctx) {
+    throw new Error('useFeatureFlags must be used within a FlagsProvider');
+  }
+
+  return ctx.flags.flags;
+}
