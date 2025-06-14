a119c1892f237a43a45718ec09a95e6c1a372f39
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-14 12:11:08 +0300

    read and write osm data

---
+++ api/app/ingest/osm_rw.go
@@ -0,0 +1,54 @@
+package ingest
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+)
+
+func readOsmData(path string) (*OsmData, error) {
+	var osmData OsmData
+
+	f, err := os.ReadFile(path)
+
+	if err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	err = xml.Unmarshal([]byte(f), &osmData)
+
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling xml: %w", err)
+	}
+
+	return &osmData, nil
+}
+
+func writeFilteredOsmData(data any) error {
+	asXml, err := xml.MarshalIndent(data, "", "  ")
+
+	if err != nil {
+		return fmt.Errorf("xml marshal error: %w", err)
+	}
+
+	err = os.WriteFile("tmp/ingest_test.xml", asXml, 0644)
+
+	if err != nil {
+		return fmt.Errorf("xml write error: %w", err)
+	}
+
+	asJson, err := json.Marshal(data)
+
+	if err != nil {
+		return fmt.Errorf("json marshal error: %w", err)
+	}
+
+	err = os.WriteFile("tmp/ingest_test.json", asJson, 0644)
+
+	if err != nil {
+		return fmt.Errorf("json write error: %w", err)
+	}
+
+	return nil
+}
