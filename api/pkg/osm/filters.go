64dfc7910c8df1af0f15380522f1d3bfd1c5f726
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-06-15 00:02:47 +0300

    add osm node and relation filtering

---
+++ api/pkg/osm/filters.go
@@ -0,0 +1,56 @@
+package osm
+
+func (osmData *Data) FilterNodes() {
+	outNodes := make([]Node, 0)
+
+	for _, node := range osmData.Nodes {
+		if isNodeValid(node) {
+			outNodes = append(outNodes, node)
+		}
+	}
+
+	osmData.Nodes = outNodes
+}
+
+func (osmData *Data) FilterRelations() {
+	outRelations := make([]Relation, 0)
+
+	for _, relation := range osmData.Relations {
+		if isRelationValid(relation) {
+			outRelations = append(outRelations, relation)
+		}
+	}
+
+	osmData.Relations = outRelations
+}
+
+func getTagWeight(tag Tag) int {
+	switch tag.K {
+	case "name":
+		return 1
+	case "wikidata":
+		return 1
+	case "public_transport":
+		if tag.V == "stop_position" {
+			return -1
+		}
+		fallthrough
+	case "place":
+		if tag.V == "neighbourhood" {
+			return -1
+		}
+		fallthrough
+	case "railway":
+		return -1
+	default:
+		return 0
+	}
+}
+
+func isNodeValid(node Node) bool {
+	return len(node.Tags) != 0 && getTagWeight(node.Tags[0]) == 2
+}
+
+func isRelationValid(relation Relation) bool {
+	return len(relation.Tags) != 0 && getTagWeight(relation.Tags[0]) == 2
+}
