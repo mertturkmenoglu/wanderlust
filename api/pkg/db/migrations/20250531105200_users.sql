9cf75b043b7bcd9393e00068b4b86e04f5c0ee81
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-06 03:48:19 +0300

    update migration

--- api/pkg/db/migrations/20250531105200_users.sql
+++ api/pkg/db/migrations/20250531105200_users.sql
@@ -34,19 +34,14 @@ UPDATE
   ON users FOR EACH ROW EXECUTE FUNCTION update_timestamp();
 
 CREATE TABLE IF NOT EXISTS admins (
-  user_id TEXT PRIMARY KEY,
-  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+  user_id TEXT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS follows (
-  follower_id TEXT NOT NULL,
-  following_id TEXT NOT NULL,
+  follower_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  following_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-  PRIMARY KEY (follower_id, following_id),
-  CONSTRAINT 
-    fk_follows_follower FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
-  CONSTRAINT 
-    fk_follows_following FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
+  PRIMARY KEY (follower_id, following_id)
 );
 
 CREATE INDEX IF NOT EXISTS idx_follows_follower ON follows(follower_id);
