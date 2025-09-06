7abcc774bbd3f80688b5c70373285c968e99ce94
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-06 03:48:30 +0300

    commit previous work

--- api/app/ingest/ingest.go
+++ api/app/ingest/ingest.go
@@ -1,13 +1,9 @@
 package ingest
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
-	"os"
 	"wanderlust/pkg/osm"
-	"wanderlust/pkg/wiki"
-	"wanderlust/pkg/wiki/wikidata"
-	"wanderlust/pkg/wiki/wikimedia"
 )
 
 func Ingest() {
@@ -26,53 +22,6 @@ func Ingest() {
 		log.Fatal(err)
 	}
 
-	wdc := wikidata.NewWikidataClient(wikidata.WithAlternativeLanguage("es"))
-	wid, err := wiki.NewWikiID("Q48435") // Sagrada Familia
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	wdcRes, err := wdc.Fetch(wid)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	lang, title, err := wdc.GetTitle(wdcRes)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println(title)
-
-	ser, _ := json.Marshal(wdcRes)
-	_ = os.WriteFile("tmp/Q48435.json", ser, 0600)
-
-	wmc := wikimedia.NewWikimediaClient()
-	wmcRes, err := wmc.Fetch(lang, title)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ser, _ = json.Marshal(wmcRes)
-	_ = os.WriteFile("tmp/Q48435.wikimedia.json", ser, 0600)
-
-	sec, err := wiki.GetWikiTextFirstSection(wmcRes.Source)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out, err := wiki.ConvertWikiTextToPlainText(sec)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	out = wiki.RemoveFootnotes(out)
-
-	_ = os.WriteFile("tmp/Q48435.plaintext.txt", []byte(out), 0600)
+	fmt.Println("osm nodes", len(osmData.Nodes))
+	fmt.Println("osm relations", len(osmData.Relations))
 }
