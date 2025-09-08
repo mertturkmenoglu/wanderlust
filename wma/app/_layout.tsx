7bdc34f0f0e82415a1e7ddb6c72d644f53e59662
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-08 14:33:10 +0300

    add query client and provider

--- wma/app/_layout.tsx
+++ wma/app/_layout.tsx
@@ -3,16 +3,46 @@ import "react-native-reanimated";
 
 import "../global.css";
 
+import { isApiError } from "@/api/api";
 import { AuthContextProvider, useSession } from "@/providers/auth-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastAndroid } from "react-native";
 import SplashScreenController from "./splash";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: Infinity,
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+    mutations: {
+      onError: (err) => {
+        if (isApiError(err)) {
+          console.log({ err });
+          ToastAndroid.show(
+            err.detail ?? "Something went wrong",
+            ToastAndroid.SHORT
+          );
+        } else {
+          console.log(JSON.stringify(err));
+          ToastAndroid.show("Something went wrong", ToastAndroid.SHORT);
+        }
+      },
+    },
+  },
+});
 
 export default function Root() {
   // Set up the auth context and render our layout inside of it.
   return (
-    <AuthContextProvider>
-      <SplashScreenController />
-      <RootNavigator />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <SplashScreenController />
+        <RootNavigator />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
@@ -23,7 +53,7 @@ function RootNavigator() {
   return (
     <Stack>
       <Stack.Protected guard={!!session.user}>
-        <Stack.Screen name="(app)" />
+        <Stack.Screen name="(app)" options={{ headerShown: false }} />
       </Stack.Protected>
 
       <Stack.Protected guard={!session.user}>
