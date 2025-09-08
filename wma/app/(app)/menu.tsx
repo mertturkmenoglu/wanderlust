6a07aa1dab812ac4a56922f1197c5aa7d4ff70bd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-08 14:33:53 +0300

    add dummy sign out button

--- wma/app/(app)/menu.tsx
+++ wma/app/(app)/menu.tsx
@@ -1,11 +1,23 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { Pressable, ScrollView, StyleSheet, ToastAndroid } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { api } from "@/api/api";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "expo-router";
 
 export default function TabTwoScreen() {
+  const router = useRouter();
+  const qc = useQueryClient();
+  const signOutMutation = api.useMutation("post", "/api/v2/auth/logout", {
+    onSuccess: async () => {
+      await qc.invalidateQueries();
+      ToastAndroid.show("Signed out", ToastAndroid.SHORT);
+      router.navigate("/sign-in");
+    },
+  });
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView>
@@ -20,6 +32,13 @@ export default function TabTwoScreen() {
           </ThemedText>
         </ThemedView>
         <ThemedText>Menu page</ThemedText>
+        <Pressable
+          onPress={() => {
+            signOutMutation.mutate({});
+          }}
+        >
+          <ThemedText>Sign Out</ThemedText>
+        </Pressable>
       </ScrollView>
     </SafeAreaView>
   );
