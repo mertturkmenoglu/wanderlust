865bb6f0f7310fc3e6bf5acc3f45a91a71f2bf12
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-09 08:33:16 +0300

    update sign in layout and add password show hide feature

--- wma/app/sign-in.tsx
+++ wma/app/sign-in.tsx
@@ -1,6 +1,5 @@
 import { api } from "@/api/api";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { Colors } from "@/constants/Colors";
 import { useQueryClient } from "@tanstack/react-query";
 import { Link, useRouter } from "expo-router";
@@ -8,15 +7,22 @@ import { useState } from "react";
 import {
   Alert,
   Image,
+  Keyboard,
+  KeyboardAvoidingView,
   Pressable,
   TextInput,
   ToastAndroid,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
+
+import { EyeIcon, EyeOffIcon } from "lucide-react-native";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   const router = useRouter();
   const qc = useQueryClient();
 
@@ -29,95 +35,123 @@ export default function SignIn() {
   });
 
   return (
-    <ThemedView className="flex-1 items-center px-16">
-      <Image
-        style={{ width: 100, height: 100 }}
-        source={require("@/assets/images/logo.png")}
-        className="mt-64"
-      />
+    <KeyboardAvoidingView className="flex-1 bg-white px-16" behavior={"height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="flex-1 flex justify-center flex-col">
+          <View>
+            <Image
+              style={{ width: 100, height: 100 }}
+              source={require("@/assets/images/logo.png")}
+            />
 
-      <ThemedText type="subtitle" className="mt-16">
-        Sign in to Wanderlust
-      </ThemedText>
+            <ThemedText type="subtitle" className="mt-16">
+              Sign in to Wanderlust
+            </ThemedText>
 
-      <TextInput
-        className="bg-zinc-100 rounded-md w-full mt-12 py-4 px-4"
-        onChangeText={setEmail}
-        value={email}
-        textContentType="emailAddress"
-        placeholder="Email"
-      />
+            <TextInput
+              className="bg-zinc-100 rounded-md w-full mt-12 py-4 px-4"
+              onChangeText={setEmail}
+              value={email}
+              textContentType="emailAddress"
+              placeholder="Email"
+            />
 
-      <TextInput
-        className="bg-zinc-100 rounded-md w-full mt-4 py-4 px-4"
-        onChangeText={setPassword}
-        value={password}
-        placeholder="Password"
-        textContentType="password"
-      />
+            <View className="bg-zinc-100 mt-4 w-full rounded-md flex-row items-center pr-2">
+              <TextInput
+                className="py-4 px-4 w-[90%]"
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Password"
+                textContentType="password"
+                secureTextEntry={!showPassword}
+                enablesReturnKeyAutomatically
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
 
-      <Pressable
-        onPress={() => Alert.alert("Forgot password clicked")}
-        className="text-primary my-4 self-end"
-      >
-        <ThemedText
-          type="small"
-          style={{
-            color: Colors.light.primary,
-          }}
-        >
-          Forgot password?
-        </ThemedText>
-      </Pressable>
+              <Pressable onPress={() => setShowPassword((prev) => !prev)}>
+                {showPassword ? (
+                  <EyeIcon
+                    className="pr-4"
+                    size={24}
+                    color={Colors.light.primary}
+                  />
+                ) : (
+                  <EyeOffIcon
+                    className="pr-4"
+                    size={24}
+                    color={Colors.light.primary}
+                  />
+                )}
+              </Pressable>
+            </View>
 
-      <Pressable
-        onPress={() => {
-          mutation.mutate({
-            body: {
-              email,
-              password,
-            },
-          });
-        }}
-        className="bg-Colors-light-primary rounded-md w-full py-2"
-      >
-        <ThemedText
-          className="text-center"
-          style={{
-            color: "white",
-          }}
-        >
-          Sign in
-        </ThemedText>
-      </Pressable>
+            <Pressable
+              onPress={() => Alert.alert("Forgot password clicked")}
+              className="text-primary my-4 self-end"
+            >
+              <ThemedText
+                type="small"
+                style={{
+                  color: Colors.light.primary,
+                }}
+              >
+                Forgot password?
+              </ThemedText>
+            </Pressable>
 
-      <View className="flex flex-row justify-center items-end gap-2">
-        <ThemedText type="default" className="mt-4">
-          Don&apos;t have an account?
-        </ThemedText>
+            <Pressable
+              onPress={() => {
+                mutation.mutate({
+                  body: {
+                    email,
+                    password,
+                  },
+                });
+              }}
+              className="bg-Colors-light-primary rounded-md w-full py-2"
+            >
+              <ThemedText
+                className="text-center"
+                style={{
+                  color: "white",
+                }}
+              >
+                Sign in
+              </ThemedText>
+            </Pressable>
 
-        <Link href="/sign-up">
+            <View className="flex flex-row justify-center items-end gap-2">
+              <ThemedText type="default" className="mt-4">
+                Don&apos;t have an account?
+              </ThemedText>
+
+              <Link href="/sign-up">
+                <ThemedText
+                  type="default"
+                  className=""
+                  style={{
+                    color: Colors.light.primary,
+                  }}
+                >
+                  Sign up
+                </ThemedText>
+              </Link>
+            </View>
+          </View>
+
           <ThemedText
-            type="default"
-            className=""
+            type="small"
+            className="mt-16 mb-16 text-center"
             style={{
-              color: Colors.light.primary,
+              color: "#71717a",
             }}
           >
-            Sign up
+            By using this app, you agree to our Terms of Service and Privacy
+            Policy.
           </ThemedText>
-        </Link>
-      </View>
-
-      <ThemedText
-        type="small"
-        className="mt-auto mb-16"
-        style={{
-          color: "#71717a",
-        }}
-      >
-        By using this app, you agree to our Terms of Service and Privacy Policy.
-      </ThemedText>
-    </ThemedView>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
