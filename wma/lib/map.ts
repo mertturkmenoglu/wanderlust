966d3a32c8dc7a02aea16e90e7ba7117ee010b7f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-11 09:39:37 +0300

    update env var

--- wma/lib/map.ts
+++ wma/lib/map.ts
@@ -1,1 +1,1 @@
-export const tileUrl = `https://api.maptiler.com/maps/streets-v2-light/256/{z}/{x}/{y}.png?key=${process.env.MAPTILER_KEY}`;
+export const tileUrl = `https://api.maptiler.com/maps/streets-v2-light/256/{z}/{x}/{y}.png?key=${process.env.EXPO_PUBLIC_MAPTILER_KEY}`;
