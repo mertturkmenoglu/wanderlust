54ebbeac51f7beec2282fd64b7e44c88c007f661
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-09-14 11:26:59 +0300

    update user profile page

--- wma/app/(app)/u/[username]/index.tsx
+++ wma/app/(app)/u/[username]/index.tsx
@@ -1,14 +1,9 @@
 import { api } from "@/api/api";
 import { PoiCard } from "@/components/blocks/poi-card";
 import { InfoCard } from "@/components/blocks/poi/info-card";
-import ParallaxScrollView from "@/components/ui/parallax-scroll-view";
 import { Colors } from "@/constants/Colors";
-import { useUser } from "@/hooks/use-user";
-import { blurhash } from "@/lib/image";
 import { cn } from "@/lib/utils";
-import { useQueryClient } from "@tanstack/react-query";
-import { Image } from "expo-image";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 import { openBrowserAsync } from "expo-web-browser";
 import { LeafIcon, LinkIcon, SpeechIcon } from "lucide-react-native";
 import { Suspense } from "react";
@@ -17,9 +12,7 @@ import {
   FlatList,
   Platform,
   Pressable,
-  StyleSheet,
   Text,
-  ToastAndroid,
   View,
 } from "react-native";
 
@@ -33,6 +26,7 @@ export default function Page() {
 
 function Content() {
   const { username } = useLocalSearchParams<{ username: string }>();
+
   const query = api.useSuspenseQuery("get", "/api/v2/users/{username}", {
     params: {
       path: {
@@ -40,6 +34,7 @@ function Content() {
       },
     },
   });
+
   const favoritesQuery = api.useSuspenseQuery(
     "get",
     "/api/v2/users/{username}/top",
@@ -54,112 +49,9 @@ function Content() {
 
   const user = query.data.profile;
   const favoriteLocations = favoritesQuery.data.pois;
-  const meta = query.data.meta;
-  const router = useRouter();
-  const thisUser = useUser();
-  const isThisUser = thisUser.username === user.username;
-  const qc = useQueryClient();
-
-  const changeFollowMutation = api.useMutation(
-    "post",
-    "/api/v2/users/follow/{username}",
-    {
-      onSuccess: async () => {
-        await qc.invalidateQueries();
-        ToastAndroid.show(
-          meta.isFollowing ? "Unfollowed" : "Followed",
-          ToastAndroid.SHORT
-        );
-      },
-    }
-  );
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <Image
-          source={{
-            uri: user.bannerImage ?? "https://i.imgur.com/EwvUEmR.jpg",
-          }}
-          style={{
-            width: "100%",
-            height: 256,
-          }}
-          placeholder={{
-            blurhash,
-          }}
-        />
-      }
-    >
-      <View className="flex flex-row gap-4 items-center">
-        <Image
-          source={
-            user.profileImage !== null
-              ? { uri: user.profileImage }
-              : require("@/assets/images/profile.png")
-          }
-          style={{
-            width: 96,
-            height: 96,
-            borderRadius: 999,
-          }}
-          cachePolicy="memory-disk"
-          placeholder={{ blurhash }}
-          contentFit="cover"
-        />
-        <View>
-          <Text className="font-bold text-4xl mt-2">{user.fullName}</Text>
-          <Text className="text-primary text-lg">@{user.username}</Text>
-        </View>
-      </View>
-
-      {user.bio !== null ? (
-        <Text className="text-zinc-500 mt-4 text-sm">{user.bio}</Text>
-      ) : null}
-
-      {isThisUser ? (
-        <Pressable
-          className="border border-zinc-300 rounded-md p-2 mt-4 flex flex-row justify-center items-center text-zinc-500"
-          onPress={() => {
-            // TODO: Change later
-            router.navigate({
-              pathname: "/(app)",
-            });
-          }}
-        >
-          <Text>Settings</Text>
-        </Pressable>
-      ) : (
-        <Pressable
-          className="border border-zinc-300 rounded-md p-2 mt-4 flex flex-row justify-center items-center text-zinc-500"
-          disabled={changeFollowMutation.isPending}
-          onPress={() => {
-            changeFollowMutation.mutate({
-              params: {
-                path: {
-                  username: user.username,
-                },
-              },
-            });
-          }}
-        >
-          {changeFollowMutation.isPending ? (
-            <ActivityIndicator />
-          ) : (
-            <Text>{meta.isFollowing ? "Following" : "Follow"}</Text>
-          )}
-        </Pressable>
-      )}
-
-      <View
-        style={{
-          height: StyleSheet.hairlineWidth,
-          backgroundColor: Colors.light.text,
-          marginTop: 16,
-        }}
-      />
-
+    <View>
       <View
         className={cn("flex flex-row flex-wrap gap-2 w-full mt-4 max-h-56")}
       >
@@ -304,6 +196,6 @@ function Content() {
           ListEmptyComponent={<Text>No favorite locations</Text>}
         />
       </View>
-    </ParallaxScrollView>
+    </View>
   );
 }
