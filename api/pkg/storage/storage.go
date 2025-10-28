40726bf820727c98b50b96ef39b11df715c56c70
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-28 03:36:16 +0300

    add open bucket functionality

---
+++ api/pkg/storage/storage.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"wanderlust/pkg/cfg"
+
+	"gocloud.dev/blob"
+	"gocloud.dev/blob/fileblob"
+)
+
+var driver = "file"
+
+func OpenBucket(ctx context.Context, bucket BucketName) (*blob.Bucket, error) {
+	if driver == "file" {
+		path := "./tmp/storage/" + string(bucket)
+		return fileblob.OpenBucket(path, &fileblob.Options{
+			NoTempDir: true,
+			CreateDir: true,
+			URLSigner: fileblob.NewURLSignerHMAC(&url.URL{
+				Scheme: "http",
+				Host:   cfg.Env.URL,
+				Path:   "/uploads",
+			}, []byte("secret")),
+		})
+	}
+
+	// TODO: add s3 driver
+	return nil, fmt.Errorf("driver not supported")
+}
