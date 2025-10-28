b320fc21a6a9223e7dd0be140e45869dd9f6f2b9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-28 03:34:54 +0300

    add bucket names

---
+++ api/pkg/storage/buckets.go
@@ -0,0 +1,23 @@
+package storage
+
+var Buckets = []string{
+	"default",
+	"profile-images",
+	"banner-images",
+	"pois",
+	"reviews",
+	"diaries",
+	"exports",
+}
+
+type BucketName string
+
+const (
+	BUCKET_DEFAULT        BucketName = "default"
+	BUCKET_PROFILE_IMAGES BucketName = "profile-images"
+	BUCKET_BANNER_IMAGES  BucketName = "banner-images"
+	BUCKET_POIS           BucketName = "pois"
+	BUCKET_REVIEWS        BucketName = "reviews"
+	BUCKET_DIARIES        BucketName = "diaries"
+	BUCKET_EXPORTS        BucketName = "exports"
+)
