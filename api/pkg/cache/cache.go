4d49207860f478348da231b094a6c949d496392a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-28 16:10:27 +0300

    update cache module

--- api/pkg/cache/cache.go
+++ api/pkg/cache/cache.go
@@ -29,6 +29,29 @@ func New() *Cache {
 	}
 }
 
+func (c *Cache) Read(ctx context.Context, key string) (interface{}, error) {
+	ctx, sp := tracing.NewSpan(ctx)
+	defer sp.End()
+
+	res, err := c.Get(ctx, key).Result()
+
+	if err != nil {
+		sp.RecordError(err)
+		return nil, fmt.Errorf("getting key %s: %w", key, err)
+	}
+
+	var v any
+
+	err = json.Unmarshal([]byte(res), &v)
+
+	if err != nil {
+		sp.RecordError(err)
+		return nil, fmt.Errorf("unmarshaling key %s: %w", key, err)
+	}
+
+	return v, nil
+}
+
 func (c *Cache) ReadObj(ctx context.Context, key string, v any) error {
 	ctx, sp := tracing.NewSpan(ctx)
 	defer sp.End()
@@ -48,6 +71,10 @@ func (c *Cache) ReadObj(ctx context.Context, key string, v any) error {
 	}
 
 	return nil
+}
+
+func (c *Cache) Write(ctx context.Context, key string, data any, exp time.Duration) error {
+	return c.SetObj(ctx, key, data, exp)
 }
 
 func (c *Cache) SetObj(ctx context.Context, key string, data any, exp time.Duration) error {
