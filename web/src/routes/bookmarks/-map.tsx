0b0dc57d075f9db8c55fb16b2c599e846c428fd4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-30 00:17:02 +0300

    add bookmark item map component

---
+++ web/src/routes/bookmarks/-map.tsx
@@ -0,0 +1,35 @@
+import type { components } from '@/lib/api-types';
+import { createStyle } from '@/lib/map';
+import MapContainer, { Marker } from 'react-map-gl/maplibre';
+
+type Props = {
+  bookmark: components['schemas']['GetUserBookmarksOutputBody']['bookmarks'][number];
+};
+
+export function BookmarkItemMap({ bookmark: { poi } }: Props) {
+  return (
+    <MapContainer
+      initialViewState={{
+        latitude: poi.address.lat,
+        longitude: poi.address.lng,
+        zoom: 15,
+      }}
+      dragPan={false}
+      style={{
+        width: '100%',
+        height: '200px',
+        zIndex: 0,
+        marginTop: '16px',
+      }}
+      latitude={poi.address.lat}
+      longitude={poi.address.lng}
+      minZoom={12}
+      mapStyle={createStyle('streets-v2-light')}
+    >
+      <Marker
+        latitude={poi.address.lat}
+        longitude={poi.address.lng}
+      />
+    </MapContainer>
+  );
+}
