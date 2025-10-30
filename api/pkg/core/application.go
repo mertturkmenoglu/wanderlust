87b319ed2f67c08bcbcf6d2a1ee8fff7f80a2a63
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-30 14:52:05 +0300

    update service registery

--- api/pkg/core/application.go
+++ api/pkg/core/application.go
@@ -4,23 +4,19 @@ import (
 	"wanderlust/pkg/activities"
 	"wanderlust/pkg/cache"
 	"wanderlust/pkg/db"
-	"wanderlust/pkg/id"
 	"wanderlust/pkg/logs"
 	"wanderlust/pkg/mail"
 	"wanderlust/pkg/tasks"
 	"wanderlust/pkg/tracing"
 
 	"github.com/pterm/pterm"
-	"github.com/sony/sonyflake"
 	"go.uber.org/zap"
 )
 
 type Application struct {
 	Activities *activities.ActivityService
 	Db         *db.Db
-	Flake      *sonyflake.Sonyflake
 	Cache      *cache.Cache
-	ID         *id.Generator
 	Mail       *mail.MailService
 	Log        *zap.Logger
 	PLog       *pterm.Logger
@@ -31,15 +27,12 @@ func NewApp() *Application {
 	mailSvc := mail.New()
 	cacheSvc := cache.New()
 	logger := logs.NewZapLogger(tracing.NewOtlpWriter())
-	flake := sonyflake.NewSonyflake(sonyflake.Settings{})
 	database := db.NewDb()
 
 	return &Application{
 		Activities: activities.New(cacheSvc),
 		Db:         database,
-		Flake:      flake,
 		Cache:      cacheSvc,
-		ID:         id.NewGenerator(flake),
 		Mail:       mailSvc,
 		Log:        logger,
 		PLog:       logs.NewPTermLogger(),
