caff607c1e0b029e6414d3136e0eaf33cd421eef
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-30 14:28:50 +0300

    extract uploads handler

---
+++ api/app/uploads/handler.go
@@ -0,0 +1,56 @@
+package uploads
+
+import (
+	"net/http"
+	"wanderlust/pkg/storage"
+
+	"github.com/labstack/echo/v4"
+	"gocloud.dev/blob"
+)
+
+func Handler(c echo.Context) error {
+	bucketName, err := storage.ToBucketName(c.QueryParam("bucket"))
+
+	if err != nil {
+		return err
+	}
+
+	ctx := c.Request().Context()
+
+	b, err := storage.OpenBucket(ctx, bucketName)
+
+	if err != nil {
+		return err
+	}
+
+	defer b.Close()
+
+	fileName := c.QueryParam("obj")
+
+	if fileName == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "obj query param is required")
+	}
+
+	err = b.Upload(
+		ctx,
+		fileName,
+		c.Request().Body,
+		&blob.WriterOptions{
+			ContentType: c.QueryParam("contentType"),
+		},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	url, err := storage.GetUrl(ctx, bucketName, fileName)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusCreated, map[string]any{
+		"url": url,
+	})
+}
