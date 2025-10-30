753569641e1048cc28b29689bb272ff3941df458
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-10-30 03:27:22 +0300

    update diary details page

--- web/src/routes/diary/$id/index.tsx
+++ web/src/routes/diary/$id/index.tsx
@@ -1,11 +1,13 @@
-import { CollapsibleText } from '@/components/blocks/collapsible-text';
-import { Separator } from '@/components/ui/separator';
 import { api } from '@/lib/api';
 import { createFileRoute } from '@tanstack/react-router';
-import { Friends } from './-components/friends';
 import { Header } from './-components/header';
+import { format } from 'date-fns';
+import { CollapsibleText } from '@/components/blocks/collapsible-text';
+import { Separator } from '@/components/ui/separator';
 import { Locations } from './-components/locations';
+import { Friends } from './-components/friends';
 import { Media } from './-components/media';
+import { InfoColumn } from './-components/info-column';
 
 export const Route = createFileRoute('/diary/$id/')({
   component: RouteComponent,
@@ -26,37 +28,51 @@ function RouteComponent() {
   const { diary } = Route.useLoaderData();
 
   return (
-    <div className="max-w-7xl mx-auto my-8">
+    <div className="max-w-7xl mx-auto my-6">
       <Header />
 
-      <Separator className="my-2" />
+      <div className="grid grid-cols-4 mt-4 gap-8">
+        <InfoColumn className="col-span-1 border-r border-border pr-4" />
 
-      <div className="mx-auto">
-        <div className="text-lg my-8 ml-auto text-end">
-          {new Date(diary.date).toLocaleDateString('en-US', {
-            dateStyle: 'full',
-          })}
+        <div className="col-span-3 flex flex-col">
+          <div className="ml-auto italic text-sm">
+            {format(diary.date, 'PPP')}
+          </div>
+
+          <CollapsibleText
+            text={
+              diary.description.length > 0
+                ? diary.description
+                : 'No description'
+            }
+            charLimit={1000}
+            className="text-justify mt-8"
+          />
+
+          {diary.locations.length > 0 && (
+            <>
+              <Separator className="my-2" />
+
+              <Locations />
+            </>
+          )}
+
+          {diary.friends.length > 0 && (
+            <>
+              <Separator className="my-2" />
+
+              <Friends />
+            </>
+          )}
+
+          {diary.images.length > 0 && (
+            <>
+              <Separator className="my-2" />
+
+              <Media />
+            </>
+          )}
         </div>
-
-        <CollapsibleText
-          text={
-            diary.description.length > 0 ? diary.description : 'No description'
-          }
-          charLimit={1000}
-          className="mt-4 text-justify"
-        />
-
-        <Separator className="my-8" />
-
-        <Locations />
-
-        <Separator className="my-8" />
-
-        <Friends />
-
-        <Separator className="my-8" />
-
-        <Media />
       </div>
     </div>
   );
