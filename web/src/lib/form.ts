2c808ba4b70260cd0b57b16d2ff527bacef4727e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-01 01:27:34 +0300

    add normalizeMultipleErrors form utility

--- web/src/lib/form.ts
+++ web/src/lib/form.ts
@@ -1,3 +1,5 @@
+import type { MultipleFieldErrors } from 'react-hook-form';
+
 function lengthTracker(s: string | undefined, max: number): string {
   return `${s?.length ?? 0}/${max}`;
 }
@@ -6,4 +8,22 @@ function truncateWithEllipses(s: string, max: number): string {
   return s.length > max ? s.slice(0, max) + '...' : s;
 }
 
-export { lengthTracker, truncateWithEllipses };
+function normalizeMultipleErrors(
+  errors: MultipleFieldErrors | undefined,
+): Array<{ message: string }> {
+  if (!errors) {
+    return [];
+  }
+
+  return Object.values(errors).flatMap((err) => {
+    if (typeof err === 'string') {
+      return [{ message: err }];
+    } else if (Array.isArray(err)) {
+      return err.map((message) => ({ message }));
+    } else {
+      return [];
+    }
+  });
+}
+
+export { lengthTracker, truncateWithEllipses, normalizeMultipleErrors };
