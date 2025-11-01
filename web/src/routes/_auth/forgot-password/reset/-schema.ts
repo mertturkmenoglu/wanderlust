528be8f93280c18eaa217fb3249912d2944bd4a9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-01 12:40:13 +0300

    update schema

--- web/src/routes/_auth/forgot-password/reset/-schema.ts
+++ web/src/routes/_auth/forgot-password/reset/-schema.ts
@@ -8,10 +8,56 @@ export const FormSchema = z.object({
     .max(16, { message: 'Invalid code' }),
   newPassword: z
     .string()
-    .min(6, {
-      message: 'Password should be longer than 6 characters',
-    })
-    .max(64, { message: 'Too long' }),
+    .min(8, { message: 'At least 8 characters' })
+    .max(128, { message: 'Password is too long' })
+    .superRefine((data, ctx) => {
+      let flag = false;
+      if (data.includes(' ')) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'Password cannot contain spaces',
+        });
+        flag = true;
+      }
+
+      if (!/[A-Z]/.test(data)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least one uppercase letter',
+        });
+        flag = true;
+      }
+
+      if (!/[a-z]/.test(data)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least one lowercase letter',
+        });
+        flag = true;
+      }
+
+      if (!/[0-9]/.test(data)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least one number',
+        });
+        flag = true;
+      }
+
+      if (!/[^A-Za-z0-9]/.test(data)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'At least one special character',
+        });
+        flag = true;
+      }
+
+      if (flag) {
+        return z.NEVER;
+      }
+
+      return true;
+    }),
 });
 
 export type FormInput = z.infer<typeof FormSchema>;
