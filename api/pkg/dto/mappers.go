11634952ce7d3933b47a302ee2fd00d22a4e36bd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-03 01:28:52 +0300

    refactor: add profile and following mapping functions for improved user data handling

--- api/pkg/dto/mappers.go
+++ api/pkg/dto/mappers.go
@@ -231,3 +231,96 @@ func ToReport(r db.Report) Report {
 		UpdatedAt:    r.UpdatedAt.Time,
 	}
 }
+
+func ToProfile(dbProfile db.Profile) Profile {
+	return Profile{
+		ID:             dbProfile.ID,
+		Username:       dbProfile.Username,
+		FullName:       dbProfile.FullName,
+		IsVerified:     dbProfile.IsVerified,
+		Bio:            utils.TextToStr(dbProfile.Bio),
+		ProfileImage:   utils.TextToStr(dbProfile.ProfileImage),
+		BannerImage:    utils.TextToStr(dbProfile.BannerImage),
+		FollowersCount: dbProfile.FollowersCount,
+		FollowingCount: dbProfile.FollowingCount,
+		CreatedAt:      dbProfile.CreatedAt.Time,
+	}
+}
+
+func ToFollowers(dbFollowers []db.FindManyFollowersRow) []Profile {
+	followers := make([]Profile, len(dbFollowers))
+
+	for i, v := range dbFollowers {
+		followers[i] = ToProfile(db.Profile{
+			ID:             v.Profile.ID,
+			Username:       v.Profile.Username,
+			FullName:       v.Profile.FullName,
+			IsVerified:     v.Profile.IsVerified,
+			Bio:            v.Profile.Bio,
+			ProfileImage:   v.Profile.ProfileImage,
+			BannerImage:    v.Profile.BannerImage,
+			FollowersCount: v.Profile.FollowersCount,
+			FollowingCount: v.Profile.FollowingCount,
+			CreatedAt:      v.Profile.CreatedAt,
+		})
+	}
+
+	return followers
+}
+
+func ToFollowings(dbFollowings []db.FindManyFollowingRow) []Profile {
+	followings := make([]Profile, len(dbFollowings))
+
+	for i, v := range dbFollowings {
+		followings[i] = ToProfile(db.Profile{
+			ID:             v.Profile.ID,
+			Username:       v.Profile.Username,
+			FullName:       v.Profile.FullName,
+			IsVerified:     v.Profile.IsVerified,
+			Bio:            v.Profile.Bio,
+			ProfileImage:   v.Profile.ProfileImage,
+			BannerImage:    v.Profile.BannerImage,
+			FollowersCount: v.Profile.FollowersCount,
+			FollowingCount: v.Profile.FollowingCount,
+			CreatedAt:      v.Profile.CreatedAt,
+		})
+	}
+
+	return followings
+}
+
+func ToFollowingsFromSearch(dbSearches []db.SearchFollowingRow) []Profile {
+	followings := make([]Profile, len(dbSearches))
+
+	for i, v := range dbSearches {
+		followings[i] = ToProfile(db.Profile{
+			ID:             v.Profile.ID,
+			Username:       v.Profile.Username,
+			FullName:       v.Profile.FullName,
+			IsVerified:     v.Profile.IsVerified,
+			Bio:            v.Profile.Bio,
+			ProfileImage:   v.Profile.ProfileImage,
+			BannerImage:    v.Profile.BannerImage,
+			FollowersCount: v.Profile.FollowersCount,
+			FollowingCount: v.Profile.FollowingCount,
+			CreatedAt:      v.Profile.CreatedAt,
+		})
+	}
+
+	return followings
+}
+
+func ToProfileFromUser(dbUser db.User) Profile {
+	return Profile{
+		ID:             dbUser.ID,
+		Username:       dbUser.Username,
+		FullName:       dbUser.FullName,
+		IsVerified:     dbUser.IsVerified,
+		Bio:            utils.TextToStr(dbUser.Bio),
+		ProfileImage:   utils.TextToStr(dbUser.ProfileImage),
+		BannerImage:    utils.TextToStr(dbUser.BannerImage),
+		FollowersCount: dbUser.FollowersCount,
+		FollowingCount: dbUser.FollowingCount,
+		CreatedAt:      dbUser.CreatedAt.Time,
+	}
+}
