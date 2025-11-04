4c9bc810e0b704254ebb120140f058be440c07a0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-04 20:43:04 +0300

    feat: implement trip repository with CRUD operations and invite handling

---
+++ api/app/trips/repository.go
@@ -0,0 +1,119 @@
+package trips
+
+import (
+	"context"
+	"wanderlust/pkg/db"
+	"wanderlust/pkg/dto"
+	"wanderlust/pkg/pagination"
+	"wanderlust/pkg/tracing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pkg/errors"
+)
+
+type Repository struct {
+	db   *db.Queries
+	pool *pgxpool.Pool
+}
+
+func (r *Repository) list(ctx context.Context, ids []string) ([]dto.Trip, error) {
+	ctx, sp := tracing.NewSpan(ctx)
+	defer sp.End()
+
+	res, err := r.db.FindManyTripsByIdsPopulated(ctx, ids)
+
+	if err != nil {
+		return nil, errors.Wrap(ErrFailedToList, err.Error())
+	}
+
+	trips := make([]dto.Trip, len(res))
+
+	for i, item := range res {
+		mapped, err := dto.ToTrip(item)
+
+		if err != nil {
+			return nil, errors.Wrap(ErrFailedToList, err.Error())
+		}
+
+		trips[i] = mapped
+	}
+
+	return trips, nil
+}
+
+func (r *Repository) get(ctx context.Context, id string) (*dto.Trip, error) {
+	ctx, sp := tracing.NewSpan(ctx)
+	defer sp.End()
+
+	res, err := r.list(ctx, []string{id})
+
+	if err != nil {
+		return nil, errors.Wrap(ErrFailedToGet, err.Error())
+	}
+
+	if len(res) != 1 {
+		return nil, ErrNotFound
+	}
+
+	return &res[0], nil
+}
+
+func (r *Repository) listMyTrips(ctx context.Context, userId string, params dto.PaginationQueryParams) ([]dto.Trip, int64, error) {
+	ctx, sp := tracing.NewSpan(ctx)
+	defer sp.End()
+
+	rows, err := r.db.FindManyTripsByOwnerIdOrParticipantId(ctx, db.FindManyTripsByOwnerIdOrParticipantIdParams{
+		OwnerID: userId,
+		Offset:  int32(pagination.GetOffset(params)),
+		Limit:   int32(params.PageSize),
+	})
+
+	if err != nil {
+		return nil, 0, errors.Wrap(ErrFailedToList, err.Error())
+	}
+
+	ids := make([]string, len(rows))
+
+	for i, row := range rows {
+		ids[i] = row.ID
+	}
+
+	trips, err := r.list(ctx, ids)
+
+	if err != nil {
+		return nil, 0, errors.Wrap(ErrFailedToList, err.Error())
+	}
+
+	count, err := r.db.CountTripsByOwnerIdOrParticipantId(ctx, userId)
+
+	if err != nil {
+		return nil, 0, errors.Wrap(ErrFailedToList, err.Error())
+	}
+
+	return trips, count, nil
+}
+
+func (r *Repository) listMyInvites(ctx context.Context, userId string) ([]dto.TripInvite, error) {
+	ctx, sp := tracing.NewSpan(ctx)
+	defer sp.End()
+
+	dbInvites, err := r.db.FindManyTripInvitesByToUserId(ctx, userId)
+
+	if err != nil {
+		return nil, errors.Wrap(ErrFailedToListInvites, err.Error())
+	}
+
+	invites := make([]dto.TripInvite, len(dbInvites))
+
+	for i, dbInvite := range dbInvites {
+		res, err := dto.ToTripInviteFromInvitesByUserIdRow(dbInvite)
+
+		if err != nil {
+			return nil, errors.Wrap(ErrFailedToListInvites, err.Error())
+		}
+
+		invites[i] = res
+	}
+
+	return invites, nil
+}
