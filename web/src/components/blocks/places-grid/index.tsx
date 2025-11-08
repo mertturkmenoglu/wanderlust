35332239e1667826ba45774d323af3a1ef3e58c5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-08 08:25:38 +0300

    feat: add PlacesGrid component for displaying a grid of places

--- web/src/components/blocks/places-grid/index.tsx
+++ web/src/components/blocks/places-grid/index.tsx
@@ -1,28 +1,28 @@
-import { PoiCard } from '@/components/blocks/poi-card';
+import { PlaceCard } from '@/components/blocks/place-card';
 import type { components } from '@/lib/api-types';
 import { Link } from '@tanstack/react-router';
 
 type Props = {
   dataKey: 'new' | 'popular' | 'featured' | 'favorite';
-  data: components['schemas']['Poi'][];
+  data: components['schemas']['Place'][];
 };
 
 function getTitle(type: Props['dataKey']) {
   switch (type) {
     case 'new':
-      return 'New Locations';
+      return 'New Places';
     case 'popular':
-      return 'Popular Locations';
+      return 'Popular Places';
     case 'featured':
-      return 'Featured Locations';
+      return 'Featured Places';
     case 'favorite':
-      return 'Favorite Locations';
+      return 'Favorite Places';
     default:
       return '';
   }
 }
 
-export function PoiGrid({ dataKey: key, data }: Props) {
+export function PlacesGrid({ dataKey: key, data }: Props) {
   const title = getTitle(key);
   const sliced = data.slice(0, 6);
   const isEmpty = sliced.length === 0;
@@ -36,13 +36,13 @@ export function PoiGrid({ dataKey: key, data }: Props) {
       <div className="my-4 grid grid-cols-2 gap-4 lg:gap-8 lg:grid-cols-3">
         {isEmpty && <div>No data available.</div>}
         {!isEmpty &&
-          data.slice(0, 6).map((poi) => (
+          data.slice(0, 6).map((place) => (
             <Link
-              key={poi.id}
+              key={place.id}
               to="/p/$id"
-              params={{ id: poi.id }}
+              params={{ id: place.id }}
             >
-              <PoiCard poi={poi} />
+              <PlaceCard place={place} />
             </Link>
           ))}
       </div>
