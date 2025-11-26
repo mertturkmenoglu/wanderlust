92581e3b72a59a7b8c2e08a96ae51b44a52bbdf5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-26 18:02:11 +0300

    refactor: restructure server initialization and handler logic for improved clarity and performance

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,33 +1,38 @@
-import * as handler from "@/routes/handler";
-import { Elysia } from "elysia";
-import { bootstrapServices } from "./services";
+import { getApiHandler, getRpcHandler } from "@/routes/handler";
 import { createContext } from "./lib/context";
-import { ConfigServiceProvider } from "./lib/config";
+import { bootstrapServices, ioc } from "./ioc";
+import { ConfigProvider } from "./lib/config";
 
 async function main() {
-  const ioc = await bootstrapServices();
-  const config = ioc.resolve(ConfigServiceProvider.getIdentifier());
+  await bootstrapServices();
 
-  new Elysia()
-    .all("/rpc*", async (ctx) => {
-      const res = await handler.rpc.handle(ctx.request, {
-        prefix: "/rpc",
-        context: await createContext({ context: ctx, ioc }),
+  const config = ioc.resolve(ConfigProvider.id);
+
+  const api = getApiHandler();
+  const rpc = getRpcHandler();
+
+  const server = Bun.serve({
+    port: config.api.port,
+    async fetch(request) {
+      const url = new URL(request.url);
+      const isRpcRequest = url.pathname.startsWith("/rpc");
+      const handler = isRpcRequest ? rpc : api;
+      const prefix = isRpcRequest ? "/rpc" : "/api";
+
+      const res = await handler.handle(request, {
+        prefix,
+        context: await createContext({ request, ioc }),
       });
 
-      return res ?? new Response("Not Found", { status: 404 });
-    })
-    .all("/api*", async (ctx) => {
-      const res = await handler.api.handle(ctx.request, {
-        prefix: "/api",
-        context: await createContext({ context: ctx, ioc }),
-      });
+      if (res.matched) {
+        return res.response;
+      }
 
-      return res ?? new Response("Not Found", { status: 404 });
-    })
-    .listen(config.api.port, () => {
-      console.log(`Server is running on http://localhost:${config.api.port}`);
-    });
+      return new Response("Not Found", { status: 404 });
+    },
+  });
+
+  console.log(`Server running on ${server.hostname}:${server.port}`);
 }
 
 await main();
