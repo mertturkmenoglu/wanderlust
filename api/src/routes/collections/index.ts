7bcf6f43888e48be91edf63fcf465e96e30cbe50
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-28 02:37:53 +0300

    add collections module

---
+++ api/src/routes/collections/index.ts
@@ -0,0 +1,138 @@
+import { DbProvider } from "@/db";
+import { ioc } from "@/ioc";
+import { CollectionsRepository } from "./repository";
+import { CollectionsService } from "./service";
+import { implement } from "@orpc/server";
+import { contract } from "./contract";
+import type { Context } from "@/lib/context";
+import { requireAuth } from "@/middlewares/authn";
+import { isAdmin } from "@/middlewares/is-admin";
+
+export function getRouter() {
+  const os = implement(contract).$context<Context>();
+  const db = ioc.resolve(DbProvider.id);
+  const repo = new CollectionsRepository(db);
+  const service = new CollectionsService(repo);
+
+  return os.router({
+    list: os.list
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.list(input);
+
+        return result;
+      }),
+    get: os.get.handler(async ({ input }) => {
+      const result = await service.get(input);
+
+      return result;
+    }),
+    create: os.create
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.create(input);
+
+        return result;
+      }),
+    delete: os.delete
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        await service._delete(input);
+
+        return {};
+      }),
+    update: os.update
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.update(input);
+
+        return result;
+      }),
+    appendItem: os.appendItem
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.appendItem(input);
+
+        return result;
+      }),
+    removeItem: os.removeItem
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.removeItem(input);
+
+        return result;
+      }),
+    reorderItems: os.reorderItems
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.reorderItems(input);
+
+        return result;
+      }),
+    createPlaceRelation: os.createPlaceRelation
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.createPlaceRelation(input);
+
+        return result;
+      }),
+    deletePlaceRelation: os.deletePlaceRelation
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.deletePlaceRelation(input);
+
+        return result;
+      }),
+    createCityRelation: os.createCityRelation
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.createCityRelation(input);
+
+        return result;
+      }),
+    deleteCityRelation: os.deleteCityRelation
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.deleteCityRelation(input);
+
+        return result;
+      }),
+    listByPlaceId: os.listByPlaceId.handler(async ({ input }) => {
+      const result = await service.listByPlaceId(input);
+
+      return result;
+    }),
+    listByCityId: os.listByCityId.handler(async ({ input }) => {
+      const result = await service.listByCityId(input);
+
+      return result;
+    }),
+    listAllPlaceCollections: os.listAllPlaceCollections
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.listAllPlaceCollections(input);
+
+        return result;
+      }),
+    listAllCityCollections: os.listAllCityCollections
+      .use(requireAuth)
+      .use(isAdmin)
+      .handler(async ({ input }) => {
+        const result = await service.listAllCityCollections(input);
+
+        return result;
+      }),
+  });
+}
---
+++ api/src/routes/collections/service.ts
@@ -0,0 +1,141 @@
+import type { CollectionsRepository } from "./repository";
+import * as dto from "./dto";
+
+export class CollectionsService {
+  constructor(private readonly repo: CollectionsRepository) {}
+
+  async list(data: dto.ListInput): Promise<dto.ListOutput> {
+    const result = await this.repo.list(data);
+
+    return {
+      collections: result.collections,
+      pagination: result.pagination,
+    };
+  }
+
+  async get(data: dto.GetInput): Promise<dto.GetOutput> {
+    const collection = await this.repo.getById(data);
+
+    return {
+      collection,
+    };
+  }
+
+  async create(data: dto.CreateInput): Promise<dto.CreateOutput> {
+    const collection = await this.repo.create(data);
+
+    return {
+      collection,
+    };
+  }
+
+  async _delete(data: dto.DeleteInput): Promise<void> {
+    await this.repo._delete(data);
+  }
+
+  async update(data: dto.UpdateInput): Promise<dto.UpdateOutput> {
+    const collection = await this.repo.update(data);
+
+    return {
+      collection,
+    };
+  }
+
+  async appendItem(data: dto.AppendItemInput): Promise<dto.AppendItemOutput> {
+    const item = await this.repo.appendItem(data);
+
+    return {
+      collectionItem: item,
+    };
+  }
+
+  async removeItem(data: dto.RemoveItemInput): Promise<dto.RemoveItemOutput> {
+    await this.repo.removeItem(data);
+
+    return {};
+  }
+
+  async reorderItems(
+    data: dto.ReorderItemsInput
+  ): Promise<dto.ReorderItemsOutput> {
+    const collection = await this.repo.reorderItems(data);
+
+    return {
+      collection,
+    };
+  }
+
+  async createPlaceRelation(
+    data: dto.CreatePlaceRelationInput
+  ): Promise<dto.CreatePlaceRelationOutput> {
+    await this.repo.createPlaceRelation(data);
+
+    return {};
+  }
+
+  async deletePlaceRelation(
+    data: dto.DeletePlaceRelationInput
+  ): Promise<dto.DeletePlaceRelationOutput> {
+    await this.repo.deletePlaceRelation(data);
+
+    return {};
+  }
+
+  async createCityRelation(
+    data: dto.CreateCityRelationInput
+  ): Promise<dto.CreateCityRelationOutput> {
+    await this.repo.createCityRelation(data);
+
+    return {};
+  }
+
+  async deleteCityRelation(
+    data: dto.DeleteCityRelationInput
+  ): Promise<dto.DeleteCityRelationOutput> {
+    await this.repo.deleteCityRelation(data);
+
+    return {};
+  }
+
+  async listByPlaceId(
+    data: dto.ListByPlaceIdInput
+  ): Promise<dto.ListByPlaceIdOutput> {
+    const result = await this.repo.listByPlaceId(data);
+
+    return {
+      collections: result.collections,
+    };
+  }
+
+  async listByCityId(
+    data: dto.ListByCityIdInput
+  ): Promise<dto.ListByCityIdOutput> {
+    const result = await this.repo.listByCityId(data);
+
+    return {
+      collections: result.collections,
+    };
+  }
+
+  async listAllPlaceCollections(
+    data: dto.ListAllPlaceCollectionsInput
+  ): Promise<dto.ListAllPlaceCollectionsOutput> {
+    const result = await this.repo.listAllPlaceCollections(data);
+
+    return {
+      collections: result.collections,
+      pagination: result.pagination,
+    };
+  }
+
+  async listAllCityCollections(
+    data: dto.ListAllCityCollectionsInput
+  ): Promise<dto.ListAllCityCollectionsOutput> {
+    const result = await this.repo.listAllCityCollections(data);
+
+    return {
+      collections: result.collections,
+      pagination: result.pagination,
+    };
+  }
+}
