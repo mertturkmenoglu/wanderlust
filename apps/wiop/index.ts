f02f57887039cae5813b9e01b98e0d55e72d871c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-28 22:42:53 +0300

    update wiop

---
+++ apps/wiop/index.ts
@@ -0,0 +1,27 @@
+import {
+  createIPX,
+  createIPXWebServer,
+  ipxFSStorage,
+  ipxHttpStorage,
+} from "ipx";
+
+const ipx = createIPX({
+  storage: ipxFSStorage({ dir: "./public" }),
+  maxAge: 60 * 60 * 24,
+  httpStorage: ipxHttpStorage({
+    allowAllDomains: true,
+  }),
+});
+
+const handler = createIPXWebServer(ipx);
+
+const server = Bun.serve({
+  port: 3002,
+  fetch: (request) => {
+    return handler(request);
+  },
+});
+
+console.log(
+  `IPX server is running on http://${server.hostname}:${server.port}/`
+);
