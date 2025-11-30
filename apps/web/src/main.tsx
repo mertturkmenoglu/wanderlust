353852637d40a935071d29d43795ef73a37e4add
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-30 17:34:22 +0300

    update main

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,9 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: We are sure these values will be defined */
 import { createRouter, RouterProvider } from '@tanstack/react-router';
-import { StrictMode, useContext } from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-
-import * as TanstackQuery from './integrations/tanstack-query/root-provider.tsx';
-
-// Import the generated route tree
+import { FlagsContextProvider } from './providers/flags-provider.tsx';
+import reportWebVitals from './reportWebVitals.ts';
 import { routeTree } from './routeTree.gen.ts';
 
 import 'instantsearch.css/themes/reset.css';
@@ -13,76 +11,46 @@ import 'react-datepicker/dist/react-datepicker.css';
 import 'yet-another-react-lightbox/plugins/captions.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
 import 'yet-another-react-lightbox/styles.css';
-import { Spinner } from './components/kit/spinner.tsx';
-import {
-	AuthContext,
-	AuthContextProvider,
-} from './providers/auth-provider.tsx';
-import {
-	FlagsContext,
-	FlagsContextProvider,
-} from './providers/flags-provider.tsx';
-import reportWebVitals from './reportWebVitals.ts';
 import './styles.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Spinner } from './components/ui/spinner.tsx';
+import { orpc, queryClient } from './lib/orpc';
 
-// Create a new router instance
 const router = createRouter({
 	routeTree,
 	context: {
-		...TanstackQuery.getContext(),
-		auth: undefined!,
 		flags: undefined!,
+		orpc,
+		queryClient,
 	},
 	defaultPreload: false,
 	scrollRestoration: true,
 	defaultStructuralSharing: true,
 	defaultPreloadStaleTime: 0,
+	defaultPendingComponent: () => <Spinner />,
+	Wrap: function WrapComponent({ children }: { children: React.ReactNode }) {
+		return (
+			<QueryClientProvider client={queryClient}>
+				<FlagsContextProvider>{children}</FlagsContextProvider>
+			</QueryClientProvider>
+		);
+	},
 });
 
-// Register the router instance for type safety
 declare module '@tanstack/react-router' {
 	interface Register {
 		router: typeof router;
 	}
 }
 
-function App() {
-	return (
-		<FlagsContextProvider>
-			<AuthContextProvider>
-				<InnerApp />
-			</AuthContextProvider>
-		</FlagsContextProvider>
-	);
-}
-
-function InnerApp() {
-	const auth = useContext(AuthContext);
-	const flags = useContext(FlagsContext);
-
-	if (auth.isLoading && !auth.user) {
-		return (
-			<div className="flex h-screen items-center justify-center">
-				<Spinner className="size-12 animate-spin fill-primary text-gray-200" />
-			</div>
-		);
-	}
-
-	return (
-		<RouterProvider router={router} context={{ auth, flags: flags.flags }} />
-	);
-}
-
-// Render the app
 const rootElement = globalThis.document.querySelector('#app');
+
 if (rootElement && !rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
 	root.render(
 		<StrictMode>
-			<TanstackQuery.Provider>
-				<App />
-			</TanstackQuery.Provider>
+			<RouterProvider router={router} />
 		</StrictMode>,
 	);
 }
