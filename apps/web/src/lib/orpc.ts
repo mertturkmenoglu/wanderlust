064d404788de22b0f291f15506a74785added9b7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-11-30 17:32:48 +0300

    add orpc client

---
+++ apps/web/src/lib/orpc.ts
@@ -0,0 +1,49 @@
+import { createORPCClient, ORPCError } from '@orpc/client';
+import { RPCLink } from '@orpc/client/fetch';
+import { createTanstackQueryUtils } from '@orpc/tanstack-query';
+import { QueryCache, QueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import type { AppRouterClient } from '../../../api/src/routes/index';
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		mutations: {
+			onError: (e) => {
+				if (e instanceof ORPCError) {
+					toast.error(e.message);
+				} else {
+					toast.error('Something happened');
+				}
+			},
+		},
+	},
+	queryCache: new QueryCache({
+		onError: (error) => {
+			toast.error(`Error: ${error.message}`, {
+				action: {
+					label: 'retry',
+					onClick: () => {
+						queryClient.invalidateQueries();
+					},
+				},
+			});
+		},
+	}),
+});
+
+export const link = new RPCLink({
+	url: `${import.meta.env.VITE_API_URL}/rpc`,
+	fetch(url, options) {
+		return fetch(url, {
+			...options,
+			credentials: 'include',
+		});
+	},
+});
+
+export const client: AppRouterClient = createORPCClient(link);
+
+export const orpc = createTanstackQueryUtils(client);
+
+// Re-export types for convenience
+export type { AppRouterClient };
