b823747d78fe2128aa6c397baed1c54f81710aa1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-01 10:32:59 +0300

    add jobs provider

--- apps/api/src/ioc.ts
+++ apps/api/src/ioc.ts
@@ -3,8 +3,8 @@ import { AuthProvider } from './lib/auth';
 import { CacheProvider } from './lib/cache/service';
 import { ConfigProvider } from './lib/config';
 import { Container } from './lib/di';
-import { DurableProvider } from './lib/durable/service';
 import { EmailProvider } from './lib/email/service';
+import { JobsProvider } from './lib/jobs/service';
 import { StorageProvider } from './lib/storage/service';
 
 export const ioc = new Container();
@@ -12,7 +12,7 @@ export const ioc = new Container();
 export async function bootstrapServices() {
 	await ioc.provideAsync(ConfigProvider.id, ConfigProvider.createInstance(ioc));
 	ioc.provide(DatabaseProvider.id, new DatabaseProvider(ioc).get());
-	ioc.provide(DurableProvider.id, new DurableProvider(ioc).get());
+	ioc.provide(JobsProvider.id, new JobsProvider(ioc).get());
 	ioc.provide(StorageProvider.id, new StorageProvider(ioc).get());
 	ioc.provide(EmailProvider.id, new EmailProvider(ioc).get());
 	ioc.provide(CacheProvider.id, new CacheProvider(ioc).get());
