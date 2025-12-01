4317bd2c8abc2d16aa59f8605171c0e13c426f8f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-01 10:30:34 +0300

    remove durable module

--- apps/api/src/lib/durable/schemas.ts
+++
@@ -1,9 +0,0 @@
-import { EventSchemas } from 'inngest';
-import z from 'zod';
-
-export const schemas = new EventSchemas().fromSchema({
-	'emails/send-reset-password': z.object({
-		email: z.email(),
-		url: z.url(),
-	}),
-});
