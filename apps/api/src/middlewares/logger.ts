ce46b745c6420886ffff6c73c8e93e68ade26cdc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-02 05:25:38 +0300

    update logger

--- apps/api/src/middlewares/logger.ts
+++ apps/api/src/middlewares/logger.ts
@@ -7,9 +7,42 @@ export function logger(request: Request, response: Response, duration: number) {
 		return;
 	}
 
-	const method = styleText('bgGreen', `[${request.method}]`.padEnd(8));
-	const pathname = styleText('cyan', url.pathname.padEnd(20));
-	const status = styleText('red', `${response.status}`);
+	const method = getStyledMethod(request.method);
+	const pathname = styleText('white', url.pathname.padEnd(40));
+	const status = getStyledStatus(response.status);
 
-	console.log(`${method} ${pathname} - ${status} (${duration}ms)`);
+	console.log(`(${status}) ${method} ${pathname} (${duration}ms)`);
+}
+
+function getStyledMethod(method: string): string {
+	switch (method) {
+		case 'GET':
+			return styleText('green', `[${method}]`.padEnd(9));
+		case 'POST':
+			return styleText('blue', `[${method}]`.padEnd(9));
+		case 'PUT':
+			return styleText('cyan', `[${method}]`.padEnd(9));
+		case 'PATCH':
+			return styleText('cyan', `[${method}]`.padEnd(9));
+		case 'DELETE':
+			return styleText('red', `[${method}]`.padEnd(9));
+		default:
+			return styleText('gray', `[${method}]`.padEnd(9));
+	}
+}
+
+function getStyledStatus(status: number): string {
+	if (status >= 200 && status < 300) {
+		return styleText('green', `${status}`);
+	}
+
+	if (status >= 300 && status < 400) {
+		return styleText('blue', `${status}`);
+	}
+
+	if (status >= 400 && status < 500) {
+		return styleText('yellow', `${status}`);
+	}
+
+	return styleText('red', `${status}`);
 }
