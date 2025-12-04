f999c04989dc1d9b732f38431da2533c0e40ac18
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-05 00:22:39 +0300

    update city info component

--- apps/web/src/routes/p/$id/-components/city-info.tsx
+++ apps/web/src/routes/p/$id/-components/city-info.tsx
@@ -1,4 +1,5 @@
 import { getRouteApi } from '@tanstack/react-router';
+import { CollapsibleText } from '@/components/blocks/collapsible-text';
 import { ipx } from '@/lib/ipx';
 import { cn } from '@/lib/utils';
 
@@ -12,8 +13,8 @@ export function CityInfo({ className }: Props) {
 
 	return (
 		<div className={cn(className)}>
-			<div className="grid grid-cols-5 gap-8">
-				<div className="col-span-5 md:col-span-2">
+			<div className="grid grid-cols-3 gap-8 md:gap-16">
+				<div className="col-span-3 md:col-span-1">
 					<img
 						src={ipx(place.address.city.image, 'f_webp,w_1024')}
 						alt=""
@@ -21,14 +22,15 @@ export function CityInfo({ className }: Props) {
 					/>
 				</div>
 
-				<div className="col-span-5 md:col-span-3">
-					<h2 className="font-bold text-6xl">{place.address.city.name}</h2>
+				<div className="col-span-3 md:col-span-2">
+					<h2 className="font-bold text-4xl">{place.address.city.name}</h2>
 					<div className="mt-2 text-muted-foreground text-sm">
 						{place.address.city.stateName}/{place.address.city.countryName}
 					</div>
-					<div className="mt-4 text-lg text-muted-foreground">
-						{place.address.city.description}
-					</div>
+					<CollapsibleText
+						text={place.address.city.description}
+						charLimit={600}
+					/>
 				</div>
 			</div>
 		</div>
