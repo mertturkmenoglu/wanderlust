add97e09f734c61938b2c66d1112a5cac2ad47bc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2025-12-08 07:01:13 +0300

    update top places component to include search functionality and improve UI

--- apps/web/src/routes/u/$username/-components/top-places.tsx
+++ apps/web/src/routes/u/$username/-components/top-places.tsx
@@ -13,7 +13,7 @@ import {
 } from '@wanderlust/ui/components/dialog';
 import { cn } from '@wanderlust/ui/lib/utils';
 import { Settings2Icon } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { Activity, useEffect, useState } from 'react';
 import { InstantSearch } from 'react-instantsearch';
 import { toast } from 'sonner';
 import { Autocomplete } from '@/components/autocomplete';
@@ -32,6 +32,7 @@ export function TopPlaces({ className }: Props) {
 	const { profile, meta } = rootRoute.useLoaderData();
 	const invalidate = useInvalidator();
 	const [open, setOpen] = useState(false);
+	const [mode, setMode] = useState<'items' | 'search'>('items');
 
 	const {
 		data: { places },
@@ -74,92 +75,100 @@ export function TopPlaces({ className }: Props) {
 								<span className="sr-only">Edit top places</span>
 							</Button>
 						</DialogTrigger>
-						<DialogContent className="min-w-5xl">
+						<DialogContent className="">
 							<DialogHeader>
 								<DialogTitle>Update Top Places</DialogTitle>
 							</DialogHeader>
-							<div className="grid min-h-[500px] grid-cols-1 gap-8 md:grid-cols-2">
-								<div className="space-y-2">
-									<DragDropProvider
-										onDragEnd={(e) => {
-											// @ts-expect-error sortable type should exists but it's missing.
-											const src = e.operation.source?.sortable;
-											const initial = src.initialIndex;
-											const current = src.index;
-											const newArr = arrayMove(items, initial, current);
-
-											mutation.mutate({
-												placesIds: newArr.map((x) => x.id),
-											});
-										}}
-									>
-										{items.map((item, i) => (
-											<SortableItem
-												key={item.id}
-												item={item}
-												index={i}
-												onRemoveClick={(placeId) => {
-													const newItems = items.filter(
-														(i) => i.id !== placeId,
-													);
-
-													mutation.mutate({
-														placesIds: newItems.map((p) => p.id),
-													});
-												}}
-											/>
-										))}
-									</DragDropProvider>
-								</div>
-
-								<div>
-									<InstantSearch
-										indexName="places"
-										searchClient={searchClient}
-										routing={false}
-										future={{
-											preserveSharedStateOnUnmount: true,
-										}}
-									>
-										<Autocomplete
-											showAdvancedSearch={false}
-											showAllResultsButton={false}
-											isCardClickable
-											onCardClick={(v) => {
-												const maxAllowedCount = 4;
-												const alreadyInList = items.some(
-													(lo) => lo.id === v.id,
-												);
-
-												if (alreadyInList) {
-													toast.error('Place is already added.');
-													return;
-												}
-
-												if (items.length >= maxAllowedCount) {
-													toast.error(
-														`Maximum ${maxAllowedCount} places can be added.`,
-													);
-													return;
-												}
+							<div className="grid min-h-[300px] grid-cols-1 gap-8">
+								<Activity mode={mode === 'items' ? 'visible' : 'hidden'}>
+									<div className="space-y-2">
+										<DragDropProvider
+											onDragEnd={(e) => {
+												// @ts-expect-error sortable type should exists but it's missing.
+												const src = e.operation.source?.sortable;
+												const initial = src.initialIndex;
+												const current = src.index;
+												const newArr = arrayMove(items, initial, current);
 
 												mutation.mutate({
-													placesIds: [...items.map((place) => place.id), v.id],
+													placesIds: newArr.map((x) => x.id),
 												});
 											}}
-										/>
-									</InstantSearch>
-								</div>
+										>
+											{items.map((item, i) => (
+												<SortableItem
+													key={item.id}
+													item={item}
+													index={i}
+													onRemoveClick={(placeId) => {
+														const newItems = items.filter(
+															(i) => i.id !== placeId,
+														);
+
+														mutation.mutate({
+															placesIds: newItems.map((p) => p.id),
+														});
+													}}
+												/>
+											))}
+										</DragDropProvider>
+									</div>
+								</Activity>
+
+								{mode === 'search' && (
+									<div>
+										<InstantSearch
+											indexName="places"
+											searchClient={searchClient}
+											routing={false}
+											future={{
+												preserveSharedStateOnUnmount: true,
+											}}
+										>
+											<Autocomplete
+												showAdvancedSearch={false}
+												showAllResultsButton={false}
+												isCardClickable
+												onCardClick={(v) => {
+													const maxAllowedCount = 4;
+													const alreadyInList = items.some(
+														(lo) => lo.id === v.id,
+													);
+
+													if (alreadyInList) {
+														toast.error('Place is already added.');
+														return;
+													}
+
+													if (items.length >= maxAllowedCount) {
+														toast.error(
+															`Maximum ${maxAllowedCount} places can be added.`,
+														);
+														return;
+													}
+
+													mutation.mutate({
+														placesIds: [
+															...items.map((place) => place.id),
+															v.id,
+														],
+													});
+													setMode('items');
+												}}
+											/>
+										</InstantSearch>
+									</div>
+								)}
 							</div>
 							<DialogFooter className="sm:justify-end">
 								<Button
-									type="button"
-									variant="default"
-									onClick={() => {
-										setOpen(false);
+									variant="outline"
+									onClick={(e) => {
+										e.preventDefault();
+										setMode((prev) => (prev === 'items' ? 'search' : 'items'));
 									}}
 								>
-									Save
+									{mode === 'items' ? 'Add Place' : 'Switch to Items'}
 								</Button>
 							</DialogFooter>
 						</DialogContent>
