10bc9078b6b7a8bb89a1933fa52ae62ff86a3d94
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-12 15:52:18 +0300

    add auth error checking utility

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { redirect } from '@tanstack/react-router';
 import type { TAuthService } from '@wanderlust/api/lib/auth/index';
 import { inferAdditionalFields } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
+import z from 'zod';
 
 export const authClient = createAuthClient({
 	baseURL: import.meta.env.VITE_API_URL,
@@ -18,4 +19,18 @@ export async function authGuard() {
 	}
 
 	return { auth: session.data };
+}
+
+const authErrSchema = z.object({
+	error: z.object({
+		code: z.string(),
+	}),
+	status: z.number(),
+	message: z.string(),
+})
+
+type AuthError = z.infer<typeof authErrSchema>;
+
+export function isAuthError(err: unknown): err is AuthError {
+	return authErrSchema.safeParse(err).success;
 }
