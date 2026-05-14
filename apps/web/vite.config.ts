4d5e6c3b725c1070afc03faa24509de512a08467
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-14 10:21:31 +0300

    update vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,29 +1,16 @@
-import { resolve } from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		tanstackRouter({ autoCodeSplitting: true }),
+		tanstackRouter({ target: 'react', autoCodeSplitting: true }),
 		viteReact(),
 		tailwindcss(),
 	],
-	// https://github.com/vitejs/vite/discussions/17738
-	optimizeDeps: {
-		exclude: ['node_modules/.cache'],
-	},
 	resolve: {
-		alias: {
-			'@': resolve(__dirname, './src'),
-		},
-	},
-	build: {
-		rollupOptions: {
-			external: ['../../packages/ui'],
-		},
+		tsconfigPaths: true,
 	},
 });
