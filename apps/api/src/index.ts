91cdedfc734d78b52e774928d97387732dc0728c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-14 13:10:49 +0300

    add compress middleware

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,6 +6,7 @@ import { initLogger } from 'evlog';
 import { type EvlogVariables, evlog } from 'evlog/hono';
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/bun';
+import { compress } from 'hono/compress';
 import { getApiHandler, getRpcHandler } from '@/routes/handler';
 import { bootstrapServices, container } from './ioc';
 import { createContext } from './lib/context';
@@ -27,8 +28,9 @@ const auth = container.get(AuthService).get();
 app.use(evlog());
 app.use('*', evlogAuth(auth));
 app.use('/*', getCorsConfig(cfg));
-app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw));
 app.use('/uploads/*', serveStatic({ root: './' }));
+app.use(compress());
+app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw));
 
 const api = getApiHandler();
 const rpc = getRpcHandler();
@@ -53,4 +55,4 @@ app.use('/*', async (c, next) => {
 export default {
 	port: cfg.api.port,
 	fetch: app.fetch,
-}
+};
