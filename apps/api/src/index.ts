1cbbd06dfe3b8f04b0aa734965d47e2c8fc288fd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-14 10:22:34 +0300

    update routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 
 import { AuthService } from '@wanderlust/auth';
 import { ConfigService } from '@wanderlust/config';
+import consola from 'consola';
 import { getApiHandler, getRpcHandler } from '@/routes/handler';
 import { bootstrapServices, container } from './ioc';
 import { createContext } from './lib/context';
+import { timeFnAsync } from './lib/timer';
 import { withCors } from './middlewares/cors';
 import { logger } from './middlewares/logger';
 
@@ -19,26 +21,23 @@ async function main() {
 
 	const server = Bun.serve({
 		port: cfg.api.port,
+		routes: {
+			'/uploads/*': (req) => {
+				const url = new URL(req.url);
+				return new Response(Bun.file(`./${url.pathname}`));
+			},
+			'/api/auth/*': async (req) => {
+				const res = await auth.handler(req);
+				return withCors(res);
+			},
+		},
 		async fetch(request) {
-			const start = performance.now();
-
-			const res = await (async () => {
+			const [res, duration] = await timeFnAsync(async () => {
 				if (request.method === 'OPTIONS') {
 					return withCors(new Response(null, { status: 204 }));
 				}
 
-				const url = new URL(request.url);
-
-				if (url.pathname.startsWith('/uploads')) {
-					return new Response(Bun.file(`./${url.pathname}`));
-				}
-
-				if (url.pathname.startsWith('/api/auth/')) {
-					const res = await auth.handler(request);
-					return withCors(res);
-				}
-
-				const isRpcRequest = url.pathname.startsWith('/rpc');
+				const isRpcRequest = new URL(request.url).pathname.startsWith('/rpc');
 				const handler = isRpcRequest ? rpc : api;
 
 				const res = await handler.handle(request, {
@@ -52,16 +51,14 @@ async function main() {
 					: new Response('Not Found', { status: 404 });
 
 				return withCors(response);
-			})();
-
-			const duration = Math.round(performance.now() - start);
+			});
 
 			logger(request, res, duration);
 			return res;
 		},
 	});
 
-	console.log(
+	consola.info(
 		`API server is running on http://${server.hostname}:${server.port}/`,
 	);
 }
