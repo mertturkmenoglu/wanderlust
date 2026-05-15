8bc24f23461aec98f6b019650f664ece1386eaa2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-15 11:51:48 +0300

    add tanstack devtools

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,10 +1,12 @@
+import { TanStackDevtools } from '@tanstack/react-devtools';
 import type { QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
 import {
 	createRootRouteWithContext,
 	HeadContent,
 	Outlet,
 } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { Toaster } from 'sonner';
 import { ErrorComponent } from '@/components/error-component';
 import { Footer } from '@/components/footer';
@@ -58,7 +60,18 @@ function Component() {
 					<Outlet />
 				</main>
 				<Footer />
-				<TanStackRouterDevtools position="bottom-left" />
+				<TanStackDevtools
+					plugins={[
+						{
+							name: 'TanStack Query',
+							render: <ReactQueryDevtoolsPanel />,
+						},
+						{
+							name: 'TanStack Router',
+							render: <TanStackRouterDevtoolsPanel />,
+						},
+					]}
+				/>
 				<Toaster position="bottom-center" richColors />
 			</div>
 		</>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import { devtools } from '@tanstack/devtools-vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 /// <reference types="vitest/config" />
@@ -6,6 +7,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
+		devtools(),
 		tanstackRouter({ target: 'react', autoCodeSplitting: true }),
 		viteReact(),
 		tailwindcss(),
