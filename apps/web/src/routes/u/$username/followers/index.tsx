fe634553169d0525648449003c70d1edd7de10d8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-16 13:09:13 +0300

    update followers page

--- apps/web/src/routes/u/$username/followers/index.tsx
+++ apps/web/src/routes/u/$username/followers/index.tsx
@@ -1,16 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { Button } from '@wanderlust/ui/components/button';
+import { ButtonGroup } from '@wanderlust/ui/components/button-group';
 import { ItemGroup } from '@wanderlust/ui/components/item';
+import { ArrowLeftIcon, ArrowRightIcon } from 'lucide-react';
+import { z } from 'zod';
 import { EmptyState } from './-components/empty';
 import { FollowersItem } from './-components/item';
 
+const schema = z.object({
+	page: z.transform(Number).pipe(z.number()).optional(),
+});
+
 export const Route = createFileRoute('/u/$username/followers/')({
 	component: RouteComponent,
-	loader: ({ context, params }) => {
+	validateSearch: schema,
+	loaderDeps: ({ search }) => ({
+		page: search.page ?? 1,
+	}),
+	loader: ({ context, params, deps }) => {
 		return context.queryClient.ensureQueryData(
 			context.orpc.users.listFollowers.queryOptions({
 				input: {
 					username: params.username,
-					page: 1,
+					page: deps.page,
 					pageSize: 100,
 				},
 			}),
@@ -19,7 +31,8 @@ export const Route = createFileRoute('/u/$username/followers/')({
 });
 
 function RouteComponent() {
-	const { followers } = Route.useLoaderData();
+	const { followers, pagination } = Route.useLoaderData();
+	const navigate = useNavigate();
 
 	return (
 		<div className="my-8">
@@ -32,6 +45,44 @@ function RouteComponent() {
 					))}
 				</ItemGroup>
 			)}
+
+			<ButtonGroup className="mx-auto my-4">
+				<Button
+					variant="outline"
+					className="w-32"
+					disabled={!pagination.hasPrevious}
+					onClick={() =>
+						navigate({
+							to: '.',
+							search: {
+								page: pagination.page - 1,
+							},
+						})
+					}
+				>
+					<ArrowLeftIcon />
+					Previous
+				</Button>
+				<Button variant="secondary" className="w-16">
+					{pagination.page} / {pagination.totalPages}
+				</Button>
+				<Button
+					variant="outline"
+					className="w-32"
+					disabled={!pagination.hasNext}
+					onClick={() =>
+						navigate({
+							to: '.',
+							search: {
+								page: pagination.page + 1,
+							},
+						})
+					}
+				>
+					Next
+					<ArrowRightIcon />
+				</Button>
+			</ButtonGroup>
 		</div>
 	);
 }
