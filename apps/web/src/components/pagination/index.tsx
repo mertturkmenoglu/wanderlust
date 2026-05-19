80c58e46072b1d0b45758b552b97fca1a63dcd3e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-19 19:18:11 +0300

    create new pagination component

---
+++ apps/web/src/components/pagination/index.tsx
@@ -0,0 +1,50 @@
+import { Button } from '@wanderlust/ui/components/button';
+import { ButtonGroup } from '@wanderlust/ui/components/button-group';
+import { cn } from '@wanderlust/ui/lib/utils';
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+
+type Props = {
+	hasNextPage: boolean;
+	hasPreviousPage: boolean;
+	page: number;
+	totalPages: number;
+	className?: string;
+	onPrevClick: () => void;
+	onNextClick: () => void;
+};
+
+export function Pagination({
+	hasNextPage,
+	hasPreviousPage,
+	page,
+	totalPages,
+	className,
+	onPrevClick,
+	onNextClick,
+}: Props) {
+	return (
+		<ButtonGroup className={cn(className)}>
+			<Button
+				disabled={!hasPreviousPage}
+				variant="outline"
+				className="w-32"
+				onClick={onPrevClick}
+			>
+				<ChevronLeftIcon />
+				Previous
+			</Button>
+			<Button variant="outline">
+				{page} / {totalPages}
+			</Button>
+			<Button
+				disabled={!hasNextPage}
+				variant="outline"
+				className="w-32"
+				onClick={onNextClick}
+			>
+				Next
+				<ChevronRightIcon />
+			</Button>
+		</ButtonGroup>
+	);
+}
