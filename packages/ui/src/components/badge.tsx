ba2c113e102a6b06df7cf7aa374b8fe411cee4f2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-22 14:20:39 +0300

    add new badge variant

--- packages/ui/src/components/badge.tsx
+++ packages/ui/src/components/badge.tsx
@@ -16,6 +16,8 @@ const badgeVariants = cva(
 					'border-transparent bg-destructive text-white focus-visible:ring-destructive/20 dark:bg-destructive/60 dark:focus-visible:ring-destructive/40 [a&]:hover:bg-destructive/90',
 				outline:
 					'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+				midnight:
+					'border-midnight bg-midnight text-midnight-foreground hover:bg-midnight/90',
 			},
 		},
 		defaultVariants: {
