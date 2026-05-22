87eafebaa2a0512f4b81c53c0fa34e4a7f1fcb59
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-22 11:16:58 +0300

    Create -header.tsx

---
+++ apps/web/src/routes/trips/-header.tsx
@@ -0,0 +1,46 @@
+import { Link, useLoaderData } from '@tanstack/react-router';
+import { buttonVariants } from '@wanderlust/ui/components/button';
+import {
+	Item,
+	ItemContent,
+	ItemDescription,
+	ItemTitle,
+} from '@wanderlust/ui/components/item';
+import { format, formatDistanceToNow } from 'date-fns';
+import { BackLink } from '@/components/back-link';
+
+export function Header() {
+	const { trip } = useLoaderData({ from: '/trips/$id' });
+
+	return (
+		<>
+			<BackLink href="/trips/my-trips" text="Go back to my trips" />
+
+			<Item className="mt-4 p-0!">
+				<ItemContent>
+					<ItemTitle className="text-2xl">{trip.title}</ItemTitle>
+					<ItemDescription>
+						{format(trip.startAt, 'LLL do p')} -{' '}
+						{format(trip.endAt, 'LLL do p')}
+					</ItemDescription>
+					<ItemDescription>
+						Last updated:{' '}
+						{formatDistanceToNow(trip.updatedAt, { addSuffix: true })} by{' '}
+						<Link
+							to="/u/$username"
+							params={{
+								username: trip.owner.username,
+							}}
+							className={buttonVariants({
+								variant: 'link',
+								className: 'px-0!',
+							})}
+						>
+							{trip.owner.name}
+						</Link>
+					</ItemDescription>
+				</ItemContent>
+			</Item>
+		</>
+	);
+}
