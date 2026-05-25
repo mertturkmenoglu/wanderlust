179ee7af77fc90cd0e8eeb3272e817ee9ea3fa6a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-25 17:26:07 +0300

    update list details page

---
+++ apps/web/src/routes/lists/$id/-change-view.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@wanderlust/ui/components/button";
+import { ButtonGroup } from "@wanderlust/ui/components/button-group";
+import { Grid2X2Icon, TextAlignJustifyIcon } from "lucide-react";
+import { useListContext } from "./-context";
+
+export function ChangeView() {
+	const ctx = useListContext();
+	
+	return (
+		<ButtonGroup className="ml-auto">
+			<Button
+				onClick={() => ctx.setView('list')}
+				variant={ctx.view === 'list' ? 'default' : 'outline'}
+			>
+				<TextAlignJustifyIcon />
+				<span>List</span>
+			</Button>
+			<Button
+				onClick={() => ctx.setView('grid')}
+				variant={ctx.view === 'grid' ? 'default' : 'outline'}
+			>
+				<Grid2X2Icon />
+				<span>Grid</span>
+			</Button>
+		</ButtonGroup>
+	);
+}
--- apps/web/src/routes/lists/$id/-items.tsx
+++ apps/web/src/routes/lists/$id/-items.tsx
@@ -1,8 +1,7 @@
-import { Link, useLoaderData } from '@tanstack/react-router';
-import { Button } from '@wanderlust/ui/components/button';
-import { ButtonGroup } from '@wanderlust/ui/components/button-group';
-import { Grid2X2Icon, TextAlignJustifyIcon } from 'lucide-react';
+import { useLoaderData } from '@tanstack/react-router';
+import { cn } from '@wanderlust/ui/lib/utils';
 import { PlaceCard } from '@/components/place-card';
+import { ChangeView } from './-change-view';
 import { useListContext } from './-context';
 
 export function Items() {
@@ -11,52 +10,21 @@ export function Items() {
 
 	return (
 		<div>
-			<ButtonGroup className="ml-auto">
-				<Button
-					onClick={() => ctx.setView('list')}
-					variant={ctx.view === 'list' ? 'default' : 'outline'}
-				>
-					<TextAlignJustifyIcon />
-					<span>List</span>
-				</Button>
-				<Button
-					onClick={() => ctx.setView('grid')}
-					variant={ctx.view === 'grid' ? 'default' : 'outline'}
-				>
-					<Grid2X2Icon />
-					<span>Grid</span>
-				</Button>
-			</ButtonGroup>
-			{ctx.view === 'grid' && (
-				<div className="mt-2 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-					{list.items.map((listItem) => (
-						<Link
-							to="/p/$id"
-							params={{
-								id: listItem.placeId,
-							}}
-							key={listItem.placeId}
-						>
-							<PlaceCard place={listItem.place} />
-						</Link>
-					))}
-				</div>
-			)}
-			{ctx.view === 'list' && (
-				<div className="mt-2 grid grid-cols-1 gap-4">
-					{list.items.map((listItem) => (
-						<Link
-							to="/p/$id"
-							params={{
-								id: listItem.placeId,
-							}}
-							key={listItem.placeId}
-						>
-							<PlaceCard place={listItem.place} variant="item" />
-						</Link>
-					))}
-				</div>
-			)}
+			<ChangeView />
+			<div
+				className={cn('mt-2 grid grid-cols-1 gap-4', {
+					'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4': ctx.view === 'grid',
+				})}
+			>
+				{list.items.map((listItem) => (
+					<PlaceCard
+						key={listItem.placeId}
+						place={listItem.place}
+						as="link"
+						variant={ctx.view === 'grid' ? 'default' : 'item'}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
