684a314d04339543278a4597f3fb67ee0a8b2825
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-05-30 10:31:45 +0300

    refactor quick actions component

--- apps/web/src/components/quick-actions/card.tsx
+++ apps/web/src/components/quick-actions/card.tsx
@@ -1,18 +1,18 @@
-import { Link } from '@tanstack/react-router';
+import { Link, type LinkOptions } from '@tanstack/react-router';
 import { cn } from '@wanderlust/ui/lib/utils';
 import type { MapIcon } from 'lucide-react';
 
-type Props = {
-	to: string;
-	Icon: typeof MapIcon;
+export type QuickActionCardProps = {
+	icon: typeof MapIcon;
 	text: string;
+	link: LinkOptions;
 };
 
-export function Card({ to, Icon, text }: Props) {
+export function Card({ icon: Icon, text, link }: QuickActionCardProps) {
 	return (
 		<div className="group rounded-md bg-yellow-400">
 			<Link
-				to={to}
+				{...link}
 				className={cn(
 					'flex aspect-[3] flex-col items-center justify-center gap-4 rounded-md bg-slate-50 p-4',
 					'transition duration-200 group-hover:translate-x-2 group-hover:-translate-y-2',
--- apps/web/src/components/quick-actions/index.tsx
+++ apps/web/src/components/quick-actions/index.tsx
@@ -1,8 +1,46 @@
+import { linkOptions } from '@tanstack/react-router';
+import { cn } from '@wanderlust/ui/lib/utils';
 import { BookmarkIcon, ListIcon, MapIcon, MapPinHouseIcon } from 'lucide-react';
 import { authClient } from '@/lib/auth';
-import { Card } from './card';
+import { GradientText } from '../gradient-text';
+import { Card, type QuickActionCardProps } from './card';
 
-export function QuickActions() {
+type Props = {
+	className?: string;
+};
+
+const items: QuickActionCardProps[] = [
+	{
+		icon: MapIcon,
+		text: 'Trips',
+		link: linkOptions({
+			to: '/trips',
+		}),
+	},
+	{
+		icon: MapPinHouseIcon,
+		text: 'Discover Nearby',
+		link: linkOptions({
+			to: '/nearby',
+		}),
+	},
+	{
+		icon: BookmarkIcon,
+		text: 'Bookmarks',
+		link: linkOptions({
+			to: '/bookmarks',
+		}),
+	},
+	{
+		icon: ListIcon,
+		text: 'My Lists',
+		link: linkOptions({
+			to: '/lists',
+		}),
+	},
+];
+
+export function QuickActions({ className }: Props) {
 	const session = authClient.useSession();
 
 	if (!session.data?.user) {
@@ -12,22 +50,18 @@ export function QuickActions() {
 	const user = session.data.user;
 
 	return (
-		<div className="mt-4 md:mt-8">
+		<div className={cn(className)}>
 			<div className="text-2xl md:text-4xl">
-				Hello{' '}
-				<span className="bg-linear-to-r from-primary to-sky-600 bg-clip-text font-bold text-transparent">
-					{user.name ?? ''}
-				</span>
+				Hello <GradientText className="font-bold" text={user.name} />
 			</div>
 			<div className="text-sm md:mt-2 md:text-base">
 				How can we help you today?
 			</div>
 
 			<div className="mt-4 grid grid-cols-2 gap-2 md:mt-8 md:grid-cols-4 md:gap-4">
-				<Card to="/trips" Icon={MapIcon} text="Trips" />
-				<Card to="/nearby" Icon={MapPinHouseIcon} text="Discover Nearby" />
-				<Card to="/bookmarks" Icon={BookmarkIcon} text="Bookmarks" />
-				<Card to="/lists" Icon={ListIcon} text="My Lists" />
+				{items.map((item) => (
+					<Card {...item} key={item.text} />
+				))}
 			</div>
 		</div>
 	);
