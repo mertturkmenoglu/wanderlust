30c748c3f0be0335a359e21d2aa0ede5187cab9f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-07 12:01:45 +0300

    update collection markdown rendering

--- apps/web/src/routes/c/$id/-header.tsx
+++ apps/web/src/routes/c/$id/-header.tsx
@@ -1,4 +1,6 @@
+import { cn } from '@wanderlust/ui/lib/utils';
 import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { useCollectionQuery } from './-hooks';
 
 export function Header() {
@@ -6,11 +8,20 @@ export function Header() {
 	const { collection } = query.data;
 
 	return (
-		<>
-			<h2 className="text-2xl">{collection.name}</h2>
-			<div className="prose mt-8">
-				<Markdown>{collection.description}</Markdown>
-			</div>
-		</>
+		<div
+			className={cn(
+				'prose prose-lg prose-hr:my-8 max-w-none',
+				'prose-h2:mt-10 prose-h1:mb-2 prose-h2:mb-3',
+				'prose-img:rounded-md prose-blockquote:border-slate-300 prose-h2:border-slate-200 prose-hr:border-slate-200 prose-h2:border-b',
+				'prose-blockquote:border-l-4 prose-th:bg-slate-100 prose-h2:pb-2 prose-headings:font-bold',
+				'prose-th:font-semibold prose-a:text-primary',
+				'prose-blockquote:text-slate-500 prose-h1:text-4xl prose-h2:text-2xl',
+				'prose-li:text-slate-600 prose-p:text-slate-600 prose-strong:text-slate-700',
+				'prose-th:bg-transparent prose-table:text-sm',
+				'prose-blockquote:italic prose-p:leading-relaxed prose-a:no-underline hover:prose-a:underline',
+			)}
+		>
+			<Markdown remarkPlugins={[remarkGfm]}>{collection.description}</Markdown>
+		</div>
 	);
 }
