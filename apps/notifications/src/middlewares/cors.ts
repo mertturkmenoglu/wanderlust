f4a25be7e433ad3a61100926ef0c6d714294f8d4
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-09 14:39:52 +0300

    add cors middleware

---
+++ apps/notifications/src/middlewares/cors.ts
@@ -0,0 +1,11 @@
+import type { TConfigService } from '@wanderlust/config';
+import { cors } from 'hono/cors';
+
+export function getCorsConfig(cfg: TConfigService) {
+	return cors({
+		origin: cfg.notifications.cors.allowedOrigins,
+		allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+		allowHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+	});
+}
