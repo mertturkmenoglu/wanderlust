84c39286a2ac4e2204aa7e5229bbd549f7c3156c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-09 14:38:53 +0300

    update config service initialization

--- apps/api/src/ioc.ts
+++ apps/api/src/ioc.ts
@@ -1,6 +1,6 @@
 import { AuthService } from '@wanderlust/auth';
-import { CacheService } from '@wanderlust/cache';
-import { ConfigService, type TConfigService } from '@wanderlust/config';
+import { CacheService, RedisService } from '@wanderlust/cache';
+import { ConfigService } from '@wanderlust/config';
 import { DatabaseService } from '@wanderlust/db';
 import { EmailService } from '@wanderlust/email/email';
 import { JobsService } from '@wanderlust/jobs';
@@ -14,16 +14,12 @@ export const container = new Container({
 });
 
 export async function bootstrapServices() {
-	const configData = await ConfigService.init();
-
-	container.bind<TConfigService>('TConfigService').toConstantValue(configData);
-	container.bind<ConfigService>(ConfigService).toSelf().inSingletonScope();
-	container.get(ConfigService).set(configData);
-
+	container.bind(ConfigService).toSelf().inSingletonScope();
 	container.bind(DatabaseService).toSelf().inSingletonScope();
 	container.bind(StorageService).toSelf();
 	container.bind(EmailService).toSelf();
 	container.bind(CacheService).toSelf();
+	container.bind(RedisService).toSelf().inSingletonScope();
 	container.bind(JobsService).toSelf();
 	container.bind(AuthService).toSelf();
 	container.bind(SearchService).toSelf();
