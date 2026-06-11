44d3bf6224f0cb391c53032e1b1ab77539e9629e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-11 12:04:21 +0300

    update cors middleware

--- apps/notifications/src/middlewares/cors.ts
+++ apps/notifications/src/middlewares/cors.ts
@@ -1,11 +1,18 @@
-import type { TConfigService } from '@wanderlust/config';
+import { ConfigService } from '@wanderlust/config';
 import { cors } from 'hono/cors';
+import { createMiddleware } from 'hono/factory';
+import { container } from '@/ioc';
+import type { THonoContext } from '@/lib/context';
 
-export function getCorsConfig(cfg: TConfigService) {
-	return cors({
+export const useCors = createMiddleware<THonoContext>((c, next) => {
+	const cfg = container.get(ConfigService).get();
+
+	const mwHandler = cors({
 		origin: cfg.notifications.cors.allowedOrigins,
 		allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 		allowHeaders: ['Content-Type', 'Authorization'],
 		credentials: true,
 	});
-}
+
+	return mwHandler(c, next);
+});
