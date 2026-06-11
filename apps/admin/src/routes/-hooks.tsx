3b23c9a40be5d316367530dff53e62b26f75883c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-12 00:02:17 +0300

    add sign in hooks

---
+++ apps/admin/src/routes/-hooks.tsx
@@ -0,0 +1,54 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import z from 'zod';
+import { authClient, isAuthError } from '@/lib/auth';
+
+const schema = z.object({
+	email: z.email().min(1, { message: 'Email is required' }),
+	password: z.string().min(1, { message: 'Password is required' }),
+});
+
+export function useSignInForm() {
+	return useForm({
+		resolver: zodResolver(schema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	});
+}
+
+export function useSignInMutation() {
+	const navigate = useNavigate();
+
+	return useMutation({
+		mutationKey: ['sign-in'],
+		mutationFn: async (data: { email: string; password: string }) => {
+			return await authClient.signIn.email(
+				{
+					email: data.email,
+					password: data.password,
+				},
+				{
+					throw: true,
+				},
+			);
+		},
+		onSuccess: async () => {
+			await navigate({ to: '/' });
+			window.location.reload();
+		},
+		retry: false,
+		onError: (err) => {
+			if (isAuthError(err) && err.error.code === 'INVALID_EMAIL_OR_PASSWORD') {
+				toast.error('Invalid email or password');
+				return;
+			}
+
+			toast.error(err.message || 'Something went wrong');
+		},
+	});
+}
