748b662642461abacee203695ac6a5c83683144f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-11 12:05:49 +0300

    create ioc.ts

---
+++ apps/notifications/src/ioc.ts
@@ -0,0 +1,28 @@
+import { AuthService } from '@wanderlust/auth';
+import { CacheService, RedisService } from '@wanderlust/cache';
+import { ConfigService } from '@wanderlust/config';
+import { DatabaseService } from '@wanderlust/db';
+import { JobsService } from '@wanderlust/jobs';
+import { initLogger } from 'evlog';
+import { Container } from 'inversify';
+
+export const container = new Container({
+	autobind: true,
+});
+
+export async function bootstrapServices() {
+	container.bind(ConfigService).toSelf().inSingletonScope();
+	container.bind(DatabaseService).toSelf().inSingletonScope();
+	container.bind(CacheService).toSelf();
+	container.bind(RedisService).toSelf().inSingletonScope();
+	container.bind(JobsService).toSelf();
+	container.bind(AuthService).toSelf();
+
+	const cfg = container.get(ConfigService).get();
+
+	initLogger({
+		env: {
+			service: cfg.notifications.logger.serviceName,
+		},
+	});
+}
