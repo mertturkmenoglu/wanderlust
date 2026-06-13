a10bda432812d4ce916709255835de5659882bd3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-13 04:46:37 +0300

    update back link component

--- apps/web/src/components/back-link/index.tsx
+++ apps/web/src/components/back-link/index.tsx
@@ -1,18 +1,18 @@
-import { Link } from '@tanstack/react-router';
+import { Link, type LinkOptions } from '@tanstack/react-router';
 import { buttonVariants } from '@wanderlust/ui/components/button';
 import { cn } from '@wanderlust/ui/lib/utils';
 import { ArrowLeftIcon } from 'lucide-react';
 
 type Props = {
 	className?: string;
-	href: string;
+	link: LinkOptions;
 	text?: string;
 };
 
-export function BackLink({ href, className, text = 'Go back' }: Props) {
+export function BackLink({ link, className, text = 'Go back' }: Props) {
 	return (
 		<Link
-			to={href}
+			{...link}
 			className={cn(buttonVariants({ variant: 'link' }), 'px-0!', className)}
 		>
 			<div className="flex items-center gap-2 px-0">
