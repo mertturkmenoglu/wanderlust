797b25d45de1d36121d80a4c3f66bd7e60b44b12
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-16 06:52:12 +0300

    update search hit component

--- apps/web/src/routes/search/-components/hit.tsx
+++ apps/web/src/routes/search/-components/hit.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@tanstack/react-router';
 import { PlaceCard } from '@/components/place-card';
 import type { TSearchHit } from '@/lib/search';
 
@@ -7,29 +6,5 @@ export type Props = {
 };
 
 export function Hit({ hit }: Readonly<Props>) {
-	return (
-		<Link
-			to="/p/$id"
-			params={{
-				id: hit.place.id,
-			}}
-		>
-			<PlaceCard
-				variant="item"
-				place={{
-					id: hit.place.id,
-					name: hit.name,
-					category: hit.place.category,
-					assets: hit.place.assets.map((a) => ({
-						...a,
-						createdAt: new Date(a.createdAt),
-						updatedAt: new Date(a.updatedAt),
-					})),
-					address: hit.place.address,
-					totalPoints: 0,
-					totalVotes: 0,
-				}}
-			/>
-		</Link>
-	);
+	return <PlaceCard variant="item" as="link" place={hit.place} />;
 }
