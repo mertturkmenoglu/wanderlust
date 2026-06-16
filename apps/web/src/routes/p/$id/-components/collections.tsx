38ed8028e543468f994dc4d83b25b8f31aac88a8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-16 22:51:42 +0300

    refactor Collections component to replace Link with UnderlineLink for navigation

--- apps/web/src/routes/p/$id/-components/collections.tsx
+++ apps/web/src/routes/p/$id/-components/collections.tsx
@@ -1,9 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { getRouteApi, Link } from '@tanstack/react-router';
+import { getRouteApi } from '@tanstack/react-router';
 import { ScrollArea, ScrollBar } from '@wanderlust/ui/components/scroll-area';
 import { cn } from '@wanderlust/ui/lib/utils';
 import { PlaceCard } from '@/components/place-card';
 import { SuspenseWrapper } from '@/components/suspense-wrapper';
+import { UnderlineLink } from '@/components/underline-link';
 import { orpc } from '@/lib/orpc';
 
 type Props = {
@@ -40,29 +41,20 @@ function Content({ className }: ContentProps) {
 					<div key={collection.id}>
 						<div key={collection.id} className="mb-4 flex items-baseline gap-4">
 							<h3 className="font-bold text-2xl">{collection.name}</h3>
-							<Link
-								to="/c/$id"
-								params={{
-									id: collection.id,
-								}}
-								className="text-base text-primary decoration-2 decoration-primary underline-offset-4 hover:underline"
-							>
+							<UnderlineLink to="/c/$id" params={{ id: collection.id }}>
 								See more
-							</Link>
+							</UnderlineLink>
 						</div>
 
 						<ScrollArea>
 							<div className="my-4 flex gap-8">
 								{collection.items.map((item) => (
-									<Link
+									<PlaceCard
 										key={item.placeId}
-										to="/p/$id"
-										params={{
-											id: item.placeId,
-										}}
-									>
-										<PlaceCard place={item.place} className="w-[256px]" />
-									</Link>
+										as="link"
+										place={item.place}
+										className="w-[256px]"
+									/>
 								))}
 							</div>
 							<ScrollBar orientation="horizontal" className="mt-8" />
