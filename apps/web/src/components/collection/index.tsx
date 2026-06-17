b275135cfd90bad6df91f88688edf9982987e1d1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-18 02:55:40 +0300

    create collection component

---
+++ apps/web/src/components/collection/index.tsx
@@ -0,0 +1,46 @@
+import { ScrollArea, ScrollBar } from '@wanderlust/ui/components/scroll-area';
+import { cn } from '@wanderlust/ui/lib/utils';
+import type { Outputs } from '@/lib/orpc';
+import { PlaceCard } from '../place-card';
+import { UnderlineLink } from '../underline-link';
+
+type TCollection = Outputs['collections']['get']['collection'];
+
+export type CollectionProps = {
+	collection: TCollection;
+	className?: string;
+};
+
+export function Collection({ collection, className }: CollectionProps) {
+	return (
+		<div className={cn(className)}>
+			<div className="mb-4 flex items-baseline justify-between gap-4 md:justify-start">
+				<div className="line-clamp-1 text-lg md:text-2xl">
+					{collection.name}
+				</div>
+				<UnderlineLink
+					to="/c/$id"
+					params={{
+						id: collection.id,
+					}}
+				>
+					See more
+				</UnderlineLink>
+			</div>
+
+			<ScrollArea>
+				<div className="my-4 flex gap-8">
+					{collection.items.map((item) => (
+						<PlaceCard
+							key={item.placeId}
+							place={item.place}
+							className="w-[256px]"
+							as="link"
+						/>
+					))}
+				</div>
+				<ScrollBar orientation="horizontal" className="mt-8" />
+			</ScrollArea>
+		</div>
+	);
+}
