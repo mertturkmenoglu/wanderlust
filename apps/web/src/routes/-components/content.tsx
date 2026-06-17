26328c7c1be3feb290f848941c85cb8bfc4c92a2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-18 02:56:46 +0300

    use interleave renderer

--- apps/web/src/routes/-components/content.tsx
+++ apps/web/src/routes/-components/content.tsx
@@ -5,31 +5,44 @@ import {
 	TripPlannerBanner,
 	TripPlannerCta,
 } from '@/components/banner/common';
+import { useInterleaveRenderer } from '@/components/interleave-renderer';
 import { PlaceCatalog } from '@/routes/-components/place-catalog';
 import { FeaturedCitiesCatalog } from './featured-cities-catalog';
 
+const listA: React.ReactNode[] = [
+	<FeaturedCitiesCatalog />,
+
+	<CategoriesBanner />,
+
+	<TripPlannerCta />,
+
+	<NearbyLocationsBanner />,
+
+	<TripPlannerBanner />,
+
+	<EventBanner />,
+];
+
+const listB: React.ReactNode[] = [
+	null,
+
+	<PlaceCatalog accessor="featured" />,
+
+	<PlaceCatalog accessor="popular" />,
+
+	<PlaceCatalog accessor="favorites" />,
+
+	<PlaceCatalog accessor="new" />,
+];
+
 export function Content() {
+	const ilr = useInterleaveRenderer();
+
 	return (
-		<div className="flex flex-col gap-4 md:gap-8">
-			<FeaturedCitiesCatalog />
-
-			<CategoriesBanner />
-
-			<PlaceCatalog accessor="featured" />
-
-			<TripPlannerCta />
-
-			<PlaceCatalog accessor="popular" />
-
-			<NearbyLocationsBanner />
-
-			<PlaceCatalog accessor="favorites" />
-
-			<TripPlannerBanner />
-
-			<PlaceCatalog accessor="new" />
-
-			<EventBanner />
-		</div>
+		<ilr.Renderer
+			listA={listA}
+			listB={listB}
+			className="flex flex-col gap-4 md:gap-8"
+		/>
 	);
 }
