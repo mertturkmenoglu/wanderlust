3dbcda5ec0545cd75d15d113b141bf54dc47aab2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-18 20:39:33 +0300

    update place card default variant

--- apps/web/src/components/place-card/default-variant.tsx
+++ apps/web/src/components/place-card/default-variant.tsx
@@ -1,15 +1,20 @@
+import { Link } from '@tanstack/react-router';
 import { Image } from '@unpic/react';
 import { Badge } from '@wanderlust/ui/components/badge';
 import {
 	Card,
 	CardAction,
 	CardDescription,
-	CardFooter,
 	CardHeader,
 	CardTitle,
 } from '@wanderlust/ui/components/card';
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from '@wanderlust/ui/components/hover-card';
 import { cn } from '@wanderlust/ui/lib/utils';
-import { HeartIcon, StarIcon } from 'lucide-react';
+import { AwardIcon, HeartIcon, StarIcon } from 'lucide-react';
 import { useNumberFormatter } from '@/hooks/use-number-formatter';
 import { ipx } from '@/lib/ipx';
 import { usePlaceCardContext } from './context';
@@ -31,7 +36,11 @@ export function DefaultVariant({
 	const removeMutation = useRemoveFromFavoritesMutation();
 
 	return (
-		<Card size="sm" className={cn('group @container', className)} {...props}>
+		<Card
+			size="sm"
+			className={cn('group @container relative', className)}
+			{...props}
+		>
 			<Image
 				src={ipx(ctx.asset.url, 'w_512')}
 				alt={ctx.asset.description ?? ''}
@@ -40,29 +49,66 @@ export function DefaultVariant({
 				height={128}
 				className="aspect-video w-full object-cover"
 			/>
-			<CardHeader className="">
-				{ctx.meta && (
-					<CardAction>
-						<button
-							type="button"
-							onClick={(e) => {
-								e.preventDefault();
-								e.stopPropagation();
+			{ctx.place.accolades.length > 0 && (
+				<HoverCard>
+					<HoverCardTrigger asChild>
+						<div className="absolute top-2 left-2 z-10 flex items-center rounded-full bg-white p-2">
+							<AwardIcon className="fill-white text-primary" />
+						</div>
+					</HoverCardTrigger>
+					<HoverCardContent
+						align="center"
+						side="right"
+						className="flex flex-col gap-2 p-2"
+					>
+						{ctx.place.accolades.map((acc) => (
+							<Link
+								key={acc.id}
+								to="/a/$id"
+								params={{ id: acc.accolade.id }}
+								className="flex"
+							>
+								<Badge variant="warning">
+									<AwardIcon />
+									{acc.accolade.title}
+								</Badge>
+							</Link>
+						))}
+					</HoverCardContent>
+				</HoverCard>
+			)}
+			{ctx.meta && (
+				<button
+					type="button"
+					className="absolute top-2 right-2 z-10 rounded-full bg-background/80 p-2 transition-colors hover:bg-background/90"
+					onClick={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
 
-								if (ctx.meta?.isFavorite) {
-									removeMutation.mutate({ placeId: ctx.place.id });
-								} else {
-									addMutation.mutate({ placeId: ctx.place.id });
-								}
-							}}
-						>
-							<HeartIcon
-								className={cn({
-									'fill-primary text-primary': ctx.meta?.isFavorite,
-									'fill-muted text-muted-foreground': !ctx.meta?.isFavorite,
-								})}
-							/>
-						</button>
+						if (ctx.meta?.isFavorite) {
+							removeMutation.mutate({ placeId: ctx.place.id });
+						} else {
+							addMutation.mutate({ placeId: ctx.place.id });
+						}
+					}}
+				>
+					<HeartIcon
+						className={cn({
+							'fill-primary text-primary': ctx.meta?.isFavorite,
+							'fill-muted text-muted-foreground': !ctx.meta?.isFavorite,
+						})}
+					/>
+				</button>
+			)}
+			<CardHeader>
+				{ctx.rating !== '0.0' && (
+					<CardAction>
+						<Badge variant="default" size="default" className="ml-auto w-fit">
+							{ctx.rating} <StarIcon className="fill-white text-white" />
+							<span className="text-white/70 text-xs leading-px tracking-tighter">
+								({numFmt.format(ctx.place.totalVotes)})
+							</span>
+						</Badge>
 					</CardAction>
 				)}
 				<CardTitle className="line-clamp-1">{ctx.place.name}</CardTitle>
@@ -73,14 +119,6 @@ export function DefaultVariant({
 					{ctx.place.category.name}
 				</CardDescription>
 			</CardHeader>
-			<CardFooter>
-				<Badge variant="default" size="lg" className="ml-auto w-fit">
-					{ctx.rating} <StarIcon className="fill-white text-white" />
-					<span className="text-white/70 text-xs leading-px tracking-tighter">
-						({numFmt.format(ctx.place.totalVotes)})
-					</span>
-				</Badge>
-			</CardFooter>
 		</Card>
 	);
 }
