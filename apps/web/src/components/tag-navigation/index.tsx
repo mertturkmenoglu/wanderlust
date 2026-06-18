277054769128234e5948a52e78e55d7073b50ffb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-18 13:46:00 +0300

    add className prop

--- apps/web/src/components/tag-navigation/index.tsx
+++ apps/web/src/components/tag-navigation/index.tsx
@@ -1,14 +1,16 @@
 import { ScrollArea, ScrollBar } from '@wanderlust/ui/components/scroll-area';
+import { cn } from '@wanderlust/ui/lib/utils';
 import { data } from './data';
 import { NavItem } from './item';
 
 type Props = {
 	cityName?: string;
+	className?: string;
 };
 
-export function TagNavigation({ cityName }: Props) {
+export function TagNavigation({ cityName, className }: Props) {
 	return (
-		<ScrollArea className="mt-4 md:mt-8">
+		<ScrollArea className={cn(className)}>
 			<ul className="flex items-center justify-center space-x-4">
 				{data.map((item) => (
 					<NavItem key={item.text} {...item} cityName={cityName} />
