839d2801363e25e493e7aff5ba449a84d5cac249
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-18 16:10:51 +0300

    refactor search lib
    - add type definitions
    - create TypesenseQueryBuilder
    - create searchTypesense function

--- apps/web/src/lib/search.ts
+++ apps/web/src/lib/search.ts
@@ -1,4 +1,5 @@
 import type { IndexUiState } from 'instantsearch.js';
+import { env } from './env';
 import type { Outputs } from './orpc';
 
 export function serializeParams(params?: string[]): string | undefined {
@@ -23,9 +24,82 @@ export function deserializeParams(ser?: IndexUiState) {
 	return ser;
 }
 
+export type SearchResponse<T = TSearchHit> = {
+	found: number;
+	hits: {
+		document: T;
+	}[];
+	out_of: number;
+	page: number;
+};
+
 export type TSearchHit = {
 	id: string;
 	location: [number, number];
 	name: string;
 	place: Outputs['places']['get']['place'];
 };
+
+export type TSearchCityHit = {
+	id: string;
+	location: [number, number];
+	name: string;
+	city: Outputs['cities']['get']['city'];
+};
+
+type TypeSenseCollection = 'places' | 'cities';
+
+export class TypesenseQueryBuilder {
+	private readonly sp: URLSearchParams;
+
+	constructor() {
+		this.sp = new URLSearchParams();
+	}
+
+	append(name: string, value: string) {
+		this.sp.append(name, value);
+		return this;
+	}
+
+	delete(name: string, value?: string) {
+		this.sp.delete(name, value);
+		return this;
+	}
+
+	getSearchParams() {
+		return this.sp;
+	}
+
+	set(name: string, value: string) {
+		this.sp.set(name, value);
+		return this;
+	}
+
+	sort() {
+		this.sp.sort();
+		return this;
+	}
+
+	build() {
+		return this.sp.toString();
+	}
+}
+
+export async function searchTypesense<T = TSearchHit>(
+	collection: TypeSenseCollection,
+	query: string,
+) {
+	const searchApiKey = env.VITE_SEARCH_CLIENT_API_KEY;
+	const searchApiUrl = env.VITE_SEARCH_CLIENT_URL;
+
+	const url = new URL(`/collections/${collection}/documents/search?${query}`, searchApiUrl);
+
+	const res = await fetch(url.toString(), {
+		headers: {
+			'X-TYPESENSE-API-KEY': searchApiKey,
+		},
+	});
+
+	const data = (await res.json()) as SearchResponse<T>;
+	return data;
+}
