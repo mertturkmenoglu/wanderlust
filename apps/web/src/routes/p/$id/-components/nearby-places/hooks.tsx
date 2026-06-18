69e7c567c562ebadebeefcd62af04cf15fccfdea
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-18 16:11:11 +0300

    refactor nearby places hooks to use TypesenseQueryBuilder and remove unused types

--- apps/web/src/routes/p/$id/-components/nearby-places/hooks.tsx
+++ apps/web/src/routes/p/$id/-components/nearby-places/hooks.tsx
@@ -1,13 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useLoaderData } from '@tanstack/react-router';
-import { env } from '@/lib/env';
-import type { SearchResponse } from './types';
-
-const searchApiKey = env.VITE_SEARCH_CLIENT_API_KEY;
-const searchApiUrl = env.VITE_SEARCH_CLIENT_URL;
-const headers = {
-	'X-TYPESENSE-API-KEY': searchApiKey,
-};
+import {
+	searchTypesense,
+	type TSearchHit,
+	TypesenseQueryBuilder,
+} from '@/lib/search';
 
 export function useNearbyPlaces() {
 	const { place } = useLoaderData({ from: '/p/$id/' });
@@ -15,19 +12,16 @@ export function useNearbyPlaces() {
 	return useSuspenseQuery({
 		queryKey: ['place-nearby', place.id],
 		queryFn: async () => {
-			const sp = new URLSearchParams();
-			sp.append('q', '*');
-			sp.append('query_by', 'name');
-			sp.append(
-				'filter_by',
-				`location:(${place.address.lat},${place.address.lng},50 km)`,
-			);
-			const url = `${searchApiUrl}/collections/places/documents/search?${sp.toString()}`;
-			const res = await fetch(url, {
-				headers,
-			});
-			const data = (await res.json()) as SearchResponse;
-			return data;
+			const query = new TypesenseQueryBuilder()
+				.append('q', '*')
+				.append('query_by', 'name')
+				.append(
+					'filter_by',
+					`location:(${place.address.lat},${place.address.lng},50 km)`,
+				)
+				.build();
+
+			return searchTypesense<TSearchHit>('places', query);
 		},
 	});
 }
--- apps/web/src/routes/p/$id/-components/nearby-places/types.ts
+++ apps/web/src/routes/p/$id/-components/nearby-places/types.ts
@@ -1,14 +1,3 @@
-import type { TSearchHit } from '@/lib/search';
-
-export type SearchResponse = {
-	found: number;
-	hits: {
-		document: TSearchHit;
-	}[];
-	out_of: number;
-	page: number;
-};
-
 export type Props = {
 	className?: string;
 };
