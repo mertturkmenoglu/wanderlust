fc1f67a0b1b629e6f11d61ff705aa12b003a9fa7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-21 17:14:23 +0300

    update activities page data loading

--- apps/web/src/routes/u/$username/activities/index.tsx
+++ apps/web/src/routes/u/$username/activities/index.tsx
@@ -1,22 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { ItemGroup } from '@wanderlust/ui/components/item';
 import { AppMessage } from '@/components/app-message';
+import { orpc } from '@/lib/orpc';
+import { useListActivitiesQuery } from './-hooks';
 import { ActivityItem } from './-item';
 
 export const Route = createFileRoute('/u/$username/activities/')({
 	component: RouteComponent,
-	loader: ({ context: { orpc, queryClient }, params }) =>
-		queryClient.ensureQueryData(
+	loader: async ({ context: { queryClient }, params }) => {
+		await queryClient.ensureQueryData(
 			orpc.users.listActivities.queryOptions({
 				input: {
 					username: params.username,
 				},
 			}),
-		),
+		);
+	},
 });
 
 function RouteComponent() {
-	const { activities } = Route.useLoaderData();
+	const query = useListActivitiesQuery();
+	const activities = query.data.activities;
 
 	if (activities.length === 0) {
 		return <AppMessage empty="No activities" classNames={{ root: 'my-16' }} />;
