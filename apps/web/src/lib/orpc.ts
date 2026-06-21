84bbb92d334217c365a6245671ef3a3f1fd888e3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-21 11:39:55 +0300

    update orpc client

--- apps/web/src/lib/orpc.ts
+++ apps/web/src/lib/orpc.ts
@@ -1,40 +1,8 @@
-import {
-	createORPCClient,
-	type InferClientInputs,
-	type InferClientOutputs,
-	ORPCError,
-} from '@orpc/client';
+import { createORPCClient, type InferClientInputs, type InferClientOutputs } from '@orpc/client';
 import { RPCLink } from '@orpc/client/fetch';
+import type { ContractRouterClient } from '@orpc/contract';
 import { createTanstackQueryUtils } from '@orpc/tanstack-query';
-import { QueryCache, QueryClient } from '@tanstack/react-query';
-import type { AppRouterClient } from '@wanderlust/api/routes/client-types';
-import { toast } from 'sonner';
-
-export const queryClient = new QueryClient({
-	defaultOptions: {
-		mutations: {
-			onError: (e) => {
-				if (e instanceof ORPCError) {
-					toast.error(e.message);
-				} else {
-					toast.error('Something happened');
-				}
-			},
-		},
-	},
-	queryCache: new QueryCache({
-		onError: (error) => {
-			toast.error(`Error: ${error.message}`, {
-				action: {
-					label: 'retry',
-					onClick: () => {
-						queryClient.invalidateQueries();
-					},
-				},
-			});
-		},
-	}),
-});
+import type { AppRouter } from '@wanderlust/contract';
 
 export const link = new RPCLink({
 	url: `${import.meta.env.VITE_API_URL}/rpc`,
@@ -46,10 +14,10 @@ export const link = new RPCLink({
 	},
 });
 
-export const client: AppRouterClient = createORPCClient(link);
+export const client: ContractRouterClient<AppRouter> = createORPCClient(link);
 
 export const orpc = createTanstackQueryUtils(client);
 
-export type Inputs = InferClientInputs<AppRouterClient>;
+export type Inputs = InferClientInputs<typeof client>;
 
-export type Outputs = InferClientOutputs<AppRouterClient>;
+export type Outputs = InferClientOutputs<typeof client>;
