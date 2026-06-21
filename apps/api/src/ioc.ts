f8f6fdd7951cb148a46af6748cf947cef2cb8d21
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-21 11:37:26 +0300

    cleanup ioc file

--- apps/api/src/ioc.ts
+++ apps/api/src/ioc.ts
@@ -1,30 +1,5 @@
-import { AuthService } from '@wanderlust/auth';
-import { CacheService, RedisService } from '@wanderlust/cache';
-import { ConfigService } from '@wanderlust/config';
-import { DatabaseService } from '@wanderlust/db';
-import { EmailService } from '@wanderlust/email/email';
-import { JobsService } from '@wanderlust/jobs';
-import { StorageService } from '@wanderlust/storage';
 import { Container } from 'inversify';
-import { ActivitiesService } from './lib/activities';
-import { SearchService } from './lib/search';
-import { modules } from './routes';
 
 export const container = new Container({
 	autobind: true,
 });
-
-export async function bootstrapServices() {
-	container.bind(ConfigService).toSelf().inSingletonScope();
-	container.bind(DatabaseService).toSelf().inSingletonScope();
-	container.bind(StorageService).toSelf();
-	container.bind(EmailService).toSelf();
-	container.bind(CacheService).toSelf();
-	container.bind(RedisService).toSelf().inSingletonScope();
-	container.bind(JobsService).toSelf();
-	container.bind(AuthService).toSelf();
-	container.bind(SearchService).toSelf();
-	container.bind(ActivitiesService).toSelf();
-
-	container.load(...modules);
-}
