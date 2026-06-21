e0c3954e16787590660f0bf7f725f95dfebdcdbc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-21 11:40:06 +0300

    temporarily remove chat client codes

--- apps/web/src/lib/chat.ts
+++
@@ -1,8 +0,0 @@
-import type { ChatAppType } from '@wanderlust/chat';
-import { hc } from 'hono/client';
-
-export const chatClient = hc<ChatAppType>('http://localhost:5005', {
-	init: {
-		credentials: 'include',
-	},
-});
