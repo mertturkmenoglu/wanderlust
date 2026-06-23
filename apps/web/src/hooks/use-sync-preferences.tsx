e065413057b15237377cafbf4493dbc39e035a68
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-23 03:08:29 +0300

    add sync preferences hook

---
+++ apps/web/src/hooks/use-sync-preferences.tsx
@@ -0,0 +1,23 @@
+import { skipToken, useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { orpc } from '@/lib/orpc';
+import { usePreferencesStore } from '@/stores/preferences-context';
+import { useIsAuthenticated } from './use-is-authenticated';
+
+export function useSyncPreferences() {
+	const setPreferences = usePreferencesStore((s) => s.setPreferences);
+	const isAuth = useIsAuthenticated();
+
+	const query = useQuery(
+		orpc.preferences.get.queryOptions({
+			input: isAuth ? {} : skipToken,
+			staleTime: 0,
+		}),
+	);
+
+	useEffect(() => {
+		if (query.data) {
+			setPreferences(query.data.preferences);
+		}
+	}, [query.data]);
+}
