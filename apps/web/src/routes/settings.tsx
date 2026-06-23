831254e42e2d41ee1fb865f3468ab00ae9447e50
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-23 03:10:04 +0300

    use sheet to display settings sidebar on smaller viewports

--- apps/web/src/routes/settings.tsx
+++ apps/web/src/routes/settings.tsx
@@ -1,4 +1,12 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { Button } from '@wanderlust/ui/components/button';
+import { ScrollArea } from '@wanderlust/ui/components/scroll-area';
+import {
+	Sheet,
+	SheetContent,
+	SheetTrigger,
+} from '@wanderlust/ui/components/sheet';
+import { Settings2Icon } from 'lucide-react';
 import { authGuard } from '@/lib/auth';
 import { Sidebar } from './settings/-components/sidebar';
 
@@ -11,8 +19,21 @@ function RouteComponent() {
 	return (
 		<div className="mx-auto mt-8 w-full max-w-7xl">
 			<h2 className="text-2xl">Settings</h2>
+			<Sheet>
+				<SheetTrigger className="md:hidden" asChild>
+					<Button variant="secondary" className="mt-4 w-full" size="sm">
+						<Settings2Icon />
+						<span>Settings</span>
+					</Button>
+				</SheetTrigger>
+				<SheetContent side="left">
+					<ScrollArea className="mt-16 h-[80vh] pr-4">
+						<Sidebar />
+					</ScrollArea>
+				</SheetContent>
+			</Sheet>
 			<div className="mt-4 flex flex-col gap-4 md:mt-8 md:flex-row">
-				<div className="min-w-xs md:pr-8">
+				<div className="hidden min-w-xs md:block md:pr-8">
 					<Sidebar />
 				</div>
 				<div className="flex-1 md:-mt-4">
