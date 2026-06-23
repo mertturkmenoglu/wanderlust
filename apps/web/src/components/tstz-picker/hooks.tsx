df4daa3c38f193d217a6ebe4d321bfe3596e1aa5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-23 10:36:56 +0300

    refactor useFieldUpdateEffect to simplify date handling and remove unused code

--- apps/web/src/components/tstz-picker/hooks.tsx
+++ apps/web/src/components/tstz-picker/hooks.tsx
@@ -1,5 +1,4 @@
-import { TZDate, tz } from '@date-fns/tz';
-import { getHours, getMinutes, transpose } from 'date-fns';
+import { TZDate } from '@date-fns/tz';
 import { useEffect } from 'react';
 import { useTSTZPickerContext } from './context';
 
@@ -7,7 +6,7 @@ export function useFieldUpdateEffect() {
 	const ctx = useTSTZPickerContext();
 
 	useEffect(() => {
-		const newDate = new TZDate(ctx.value ?? new Date());
+		const newDate = new TZDate(ctx.value ?? new Date(), ctx.tzOffset);
 
 		if (Number.isNaN(ctx.hours24)) {
 			newDate.setHours(0);
@@ -21,28 +20,6 @@ export function useFieldUpdateEffect() {
 			newDate.setMinutes(ctx.minutes);
 		}
 
-		const transposed = transpose(newDate, tz(ctx.tzOffset));
-		const hours = getHours(transposed, {
-			in: tz(ctx.tzOffset),
-		});
-		const minutes = getMinutes(transposed, {
-			in: tz(ctx.tzOffset),
-		});
-
-		console.log({
-			value: ctx.value,
-			newDate,
-			transposed,
-			hours,
-			minutes,
-			tzOffset: ctx.tzOffset,
-			hoursVia: transposed.getUTCHours(),
-			minutesVia: transposed.getUTCMinutes(),
-		});
-
-		ctx.sSetHours12(hours.toString().padStart(2, '0'));
-		ctx.sSetMinutes(minutes.toString().padStart(2, '0'));
-
-		ctx.onChange(transposed);
+		ctx.onChange(newDate);
 	}, [ctx.hours24, ctx.minutes, ctx.tzOffset]);
 }
