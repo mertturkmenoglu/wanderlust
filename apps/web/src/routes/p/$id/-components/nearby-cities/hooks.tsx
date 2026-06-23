f5d92a9c4b63a88bfef09732b51012a73fb1b3e5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-23 14:42:32 +0300

    use searchRadius preference in typesense queries

--- apps/web/src/routes/p/$id/-components/nearby-cities/hooks.tsx
+++ apps/web/src/routes/p/$id/-components/nearby-cities/hooks.tsx
@@ -1,13 +1,33 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useLoaderData } from '@tanstack/react-router';
+import { useMemo } from 'react';
 import {
 	searchTypesense,
 	type TSearchCityHit,
 	TypesenseQueryBuilder,
 } from '@/lib/search';
+import { usePreferencesStore } from '@/stores/preferences-context';
 
 export function useNearbyCities() {
+	const searchRadiusPreference = usePreferencesStore(
+		(s) => s.preferences.searchRadius,
+	);
+
+	const radius = useMemo(() => {
+		switch (searchRadiusPreference) {
+			case 'close':
+				return '250 km';
+			case 'medium':
+				return '400 km';
+			case 'far':
+				return '500 km';
+			default:
+				return '250 km';
+		}
+	}, [searchRadiusPreference]);
+
 	const { place } = useLoaderData({ from: '/p/$id/' });
+	const { lat, lng } = place.address.city;
 
 	return useSuspenseQuery({
 		queryKey: ['nearby-cities', place.address.cityId],
@@ -15,10 +35,7 @@ export function useNearbyCities() {
 			const query = new TypesenseQueryBuilder()
 				.append('q', '*')
 				.append('query_by', 'name')
-				.append(
-					'filter_by',
-					`location:(${place.address.city.lat},${place.address.city.lng},250 km)`,
-				)
+				.append('filter_by', `location:(${lat},${lng},${radius})`)
 				.build();
 
 			return searchTypesense<TSearchCityHit>('cities', query);
--- apps/web/src/routes/p/$id/-components/nearby-places/hooks.tsx
+++ apps/web/src/routes/p/$id/-components/nearby-places/hooks.tsx
@@ -1,24 +1,41 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { useLoaderData } from '@tanstack/react-router';
+import { useMemo } from 'react';
 import {
 	searchTypesense,
 	type TSearchHit,
 	TypesenseQueryBuilder,
 } from '@/lib/search';
+import { usePreferencesStore } from '@/stores/preferences-context';
 
 export function useNearbyPlaces() {
+	const searchRadiusPreference = usePreferencesStore(
+		(s) => s.preferences.searchRadius,
+	);
+
+	const radius = useMemo(() => {
+		switch (searchRadiusPreference) {
+			case 'close':
+				return '50 km';
+			case 'medium':
+				return '100 km';
+			case 'far':
+				return '200 km';
+			default:
+				return '50 km';
+		}
+	}, [searchRadiusPreference]);
+
 	const { place } = useLoaderData({ from: '/p/$id/' });
+	const { lat, lng } = place.address;
 
 	return useSuspenseQuery({
-		queryKey: ['place-nearby', place.id],
+		queryKey: ['nearby-places', place.id],
 		queryFn: async () => {
 			const query = new TypesenseQueryBuilder()
 				.append('q', '*')
 				.append('query_by', 'name')
-				.append(
-					'filter_by',
-					`location:(${place.address.lat},${place.address.lng},50 km)`,
-				)
+				.append('filter_by', `location:(${lat},${lng},${radius})`)
 				.build();
 
 			return searchTypesense<TSearchHit>('places', query);
