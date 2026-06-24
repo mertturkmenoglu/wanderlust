d1c352887abc6e7d85a1bd8474630d6890816003
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-24 08:55:31 +0300

    remove autocomplete and custom search box components

--- apps/web/src/components/autocomplete/empty.tsx
+++
@@ -1,9 +0,0 @@
-import { UnderlineLink } from '../underline-link';
-
-export function EmptyState() {
-	return (
-		<UnderlineLink to="/search">
-			No results found. Try our advanced search.
-		</UnderlineLink>
-	);
-}
