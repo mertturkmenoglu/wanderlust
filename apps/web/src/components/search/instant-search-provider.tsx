7fba14a8d7e46bb3e3a2a9d3e072d75997bebbcd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-24 11:27:30 +0300

    update instant search provider

--- apps/web/src/components/search/instant-search-provider.tsx
+++ apps/web/src/components/search/instant-search-provider.tsx
@@ -1,6 +1,11 @@
-import type { PropsWithChildren } from 'react';
+import { type PropsWithChildren, useMemo } from 'react';
 import { InstantSearch } from 'react-instantsearch';
-import { useSearchClient } from '@/hooks/use-search-client';
+import {
+	useCitiesSearchClient,
+	usePlacesSearchClient,
+	useSearchClient,
+	useUsersSearchClient,
+} from '@/hooks/use-search-client';
 import { useSearchType } from '@/hooks/use-search-type';
 
 type Props = PropsWithChildren<{
@@ -9,12 +14,30 @@ type Props = PropsWithChildren<{
 
 export function InstantSearchProvider({ children, variant }: Props) {
 	const searchClient = useSearchClient();
+	const placesClient = usePlacesSearchClient();
+	const usersClient = useUsersSearchClient();
+	const citiesClient = useCitiesSearchClient();
 	const [searchType] = useSearchType();
+
+	const index = variant === 'global' ? searchType : 'places';
+
+	const client = useMemo(() => {
+		switch (index) {
+			case 'places':
+				return placesClient;
+			case 'users':
+				return usersClient;
+			case 'cities':
+				return citiesClient;
+			default:
+				return searchClient;
+		}
+	}, [index, searchClient, placesClient, usersClient, citiesClient]);
 
 	return (
 		<InstantSearch
 			indexName={variant === 'global' ? searchType : 'places'}
-			searchClient={searchClient}
+			searchClient={client}
 			routing={false}
 			future={{
 				preserveSharedStateOnUnmount: true,
