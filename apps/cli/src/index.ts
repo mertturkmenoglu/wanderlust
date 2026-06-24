507170085e1fc525e3d220a28c977393e0368e89
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-24 18:03:38 +0300

    add mapStyles script

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,8 +2,9 @@ import { run } from '@drizzle-team/brocli';
 import { clean } from './commands/clean';
 import { echo } from './commands/echo';
 import { fake } from './commands/fake';
+import { mapStyles } from './commands/map-styles';
 import { sync } from './commands/sync';
 
-run([echo, clean, fake, sync], {
+run([echo, clean, fake, sync, mapStyles], {
 	version: '1.0.0',
 });
