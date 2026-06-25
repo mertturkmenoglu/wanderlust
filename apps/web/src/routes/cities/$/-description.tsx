f960ef575867741c41f80deb63005b5f3178d358
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-25 10:52:18 +0300

    show local time of city

--- apps/web/src/routes/cities/$/-description.tsx
+++ apps/web/src/routes/cities/$/-description.tsx
@@ -1,6 +1,15 @@
-import { ipx } from '@/lib/ipx';
+import { tz } from '@date-fns/tz';
 import { useLoaderData } from '@tanstack/react-router';
 import { Image } from '@unpic/react';
+import { buttonVariants } from '@wanderlust/ui/components/button';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from '@wanderlust/ui/components/tooltip';
+import { format } from 'date-fns';
+import { ClockIcon } from 'lucide-react';
+import { ipx } from '@/lib/ipx';
 
 export function Description() {
 	const { city } = useLoaderData({ from: '/cities/$/' });
@@ -20,10 +29,32 @@ export function Description() {
 			</div>
 
 			<div className="col-span-5 lg:col-span-3">
-				<h2 className="font-bold text-3xl md:text-6xl">{city.name}</h2>
-				<div className="mt-2 text-muted-foreground text-sm md:font-semibold md:text-base">
-					{city.stateName}/{city.countryName}
+				<div className="flex items-center justify-between">
+					<div>
+						<h2 className="font-bold text-3xl md:text-6xl">{city.name}</h2>
+						<div className="mt-2 text-muted-foreground text-sm md:font-semibold md:text-base">
+							{city.stateName}/{city.countryName}
+						</div>
+					</div>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<div
+								className={buttonVariants({ variant: 'outline', size: 'sm' })}
+							>
+								<ClockIcon />
+								<div>
+									{format(new Date(), 'HH:mm', {
+										in: tz(city.timezone),
+									})}
+								</div>
+							</div>
+						</TooltipTrigger>
+						<TooltipContent side="left">
+							{city.name}'s current time
+						</TooltipContent>
+					</Tooltip>
 				</div>
+
 				<div className="mt-4 text-base text-muted-foreground md:text-lg">
 					{city.description}
 				</div>
