16ff3385e807a7e3a000e0df57207adc5586a1c2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-26 13:58:09 +0300

    refactor useTripIsPrivileged hook

--- apps/web/src/hooks/use-trip-is-privileged.tsx
+++ apps/web/src/hooks/use-trip-is-privileged.tsx
@@ -1,9 +1,26 @@
+import { useLoaderData, useRouteContext } from '@tanstack/react-router';
 import type { Outputs } from '@/lib/orpc';
 
-export function useTripIsPrivileged(
-	trip: Outputs['trips']['get']['trip'],
-	userId: string,
-) {
+type Trip = Outputs['trips']['get']['trip'];
+
+type Options =
+	| {
+			trip: Trip;
+			userId: string;
+	  }
+	| undefined;
+
+/**
+ * This hook checks if the current user is privileged (owner or editor) for the given trip.
+ * If no options are provided, it will use the trip and userId from the route context.
+ */
+export function useTripIsPrivileged(options?: Options) {
+	const { trip: tripFromRouter } = useLoaderData({ from: '/trips/$id' });
+	const { auth: authFromRouter } = useRouteContext({ from: '/trips/$id' });
+
+	const trip = options?.trip ?? tripFromRouter;
+	const userId = options?.userId ?? authFromRouter.user?.id ?? '';
+
 	if (trip.ownerId === userId) {
 		return true;
 	}
