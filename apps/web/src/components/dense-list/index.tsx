5e6003d57bbdb271ddb03dcace6e6dc6676288c5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-26 13:57:52 +0300

    create dense list component

---
+++ apps/web/src/components/dense-list/index.tsx
@@ -0,0 +1,33 @@
+import { cn } from '@wanderlust/ui/lib/utils';
+import React from 'react';
+
+type Props<T> = {
+	className?: string;
+	data: T[];
+	keyExtractor: (item: T) => string;
+	renderItem: (item: T, className: string, index: number) => React.ReactNode;
+	itemClassName?: string;
+};
+
+export function DenseList<T>({
+	className,
+	data,
+	keyExtractor,
+	renderItem,
+	itemClassName = 'p-2 first:rounded-t-lg last:rounded-b-lg hover:bg-muted',
+}: Props<T>) {
+	return (
+		<div
+			className={cn(
+				'flex flex-col divide-y divide-border rounded-lg border',
+				className,
+			)}
+		>
+			{data.map((item, index) => (
+				<React.Fragment key={keyExtractor(item)}>
+					{renderItem(item, itemClassName, index)}
+				</React.Fragment>
+			))}
+		</div>
+	);
+}
