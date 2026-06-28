ee987e86d786bcb6735fea451ca7fe10321846aa
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-28 21:00:50 +0300

    update partial includes definitions to v2

--- packages/db/src/includes.ts
+++ packages/db/src/includes.ts
@@ -1,98 +1,110 @@
-import type { TDatabaseService } from "./service";
+import type { TDatabaseService } from './service';
 
 type Query = TDatabaseService['query'];
 
 type Domain = keyof Query;
 
-type With<TDomain extends Domain> = NonNullable<Parameters<TDatabaseService['query'][TDomain]['findFirst']>[0]>['with'];
+type With<TDomain extends Domain> = NonNullable<
+	Parameters<TDatabaseService['query'][TDomain]['findFirst']>[0]
+>;
 
 const withAddress = {
-	city: true
+	with: {
+		city: true,
+	},
 } satisfies With<'addresses'>;
 
-const withAccolade = {
-	accolade: true,
-} satisfies With<'accoladeAssignments'>;
-
 const withPlace = {
-	address: {
-		with: withAddress,
+	with: {
+		address: withAddress,
+		accolades: {
+			with: {
+				accolade: true,
+			},
+		},
+		category: true,
+		assets: true,
 	},
-	accolades: {
-		with: withAccolade,
-	},
-	category: true,
-	assets: true,
 } satisfies With<'places'>;
 
 const withTripParticipant = {
-	user: {
-		columns: {
-			id: true,
-			name: true,
-			username: true,
-			image: true,
-		}
-	}
+	with: {
+		user: {
+			columns: {
+				id: true,
+				name: true,
+				username: true,
+				image: true,
+			},
+		},
+	},
 } satisfies With<'tripParticipants'>;
 
 const withTrip = {
-	participants: {
-		orderBy: (t, { desc }) => [desc(t.id)],
-		with: withTripParticipant,
-	},
-	owner: {
-		columns: {
-			id: true,
-			name: true,
-			username: true,
-			image: true,
+	with: {
+		participants: {
+			orderBy: {
+				id: 'desc',
+			},
+			with: withTripParticipant.with,
 		},
-	},
-	locations: {
-		orderBy: (t, { asc }) => [asc(t.scheduledTime)],
-		with: {
-			place: {
-				with: withPlace,
+		owner: {
+			columns: {
+				id: true,
+				name: true,
+				username: true,
+				image: true,
+			},
+		},
+		locations: {
+			orderBy: {
+				scheduledTime: 'asc',
+			},
+			with: {
+				place: withPlace,
 			},
 		},
 	},
 } satisfies With<'trips'>;
 
 const withTripInvite = {
-	fromUser: {
-		columns: {
-			id: true,
-			name: true,
-			username: true,
-			image: true,
+	with: {
+		fromUser: {
+			columns: {
+				id: true,
+				name: true,
+				username: true,
+				image: true,
+			},
 		},
-	},
-	toUser: {
-		columns: {
-			id: true,
-			name: true,
-			username: true,
-			image: true,
+		toUser: {
+			columns: {
+				id: true,
+				name: true,
+				username: true,
+				image: true,
+			},
 		},
 	},
 } satisfies With<'tripInvites'>;
 
 const withTripInviteDetails = {
-	...withTripInvite,
-	trip: {
-		columns: {
-			requestedAmenities: false,
-			visibilityLevel: false,
-			description: false,
-		},
-		with: {
-			owner: {
-				columns: {
-					id: true,
-					name: true,
-					username: true,
-					image: true,
+	with: {
+		...withTripInvite.with,
+		trip: {
+			columns: {
+				requestedAmenities: false,
+				visibilityLevel: false,
+				description: false,
+			},
+			with: {
+				owner: {
+					columns: {
+						id: true,
+						name: true,
+						username: true,
+						image: true,
+					},
 				},
 			},
 		},
@@ -100,18 +112,19 @@ const withTripInviteDetails = {
 } satisfies With<'tripInvites'>;
 
 const withTripComment = {
-	user: {
-		columns: {
-			id: true,
-			name: true,
-			username: true,
-			image: true,
+	with: {
+		user: {
+			columns: {
+				id: true,
+				name: true,
+				username: true,
+				image: true,
+			},
 		},
-	}
+	},
 } satisfies With<'tripComments'>;
 
 export const $includes = {
-	accolade: withAccolade,
 	address: withAddress,
 	place: withPlace,
 	tripParticipant: withTripParticipant,
