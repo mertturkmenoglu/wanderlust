43c14c20bd4ef101a23abab4413898044c57382a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-28 21:03:09 +0300

    add slugifyWithRandom utility

---
+++ apps/api/src/lib/slug.ts
@@ -0,0 +1,14 @@
+import shSlugify from '@sindresorhus/slugify';
+import { nanoid } from '@wanderlust/uid';
+
+export function slugifyWithRandom(input: string): string {
+	const slug = shSlugify(input, {
+		separator: '-',
+		lowercase: true,
+		decamelize: true,
+	});
+
+	const rand = nanoid(4);
+
+	return `${slug}-${rand}`;
+}
