2b2311c339e32ca336013c43f8a63973c9d3475a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-29 11:44:26 +0300

    remove separate notifications app

--- apps/notifications/src/lib/context.ts
+++
@@ -1,9 +0,0 @@
-import type { TAuthService } from '@wanderlust/auth';
-import type { EvlogVariables } from 'evlog/hono';
-
-export type THonoContext = EvlogVariables & {
-	Variables: {
-		user: TAuthService['$Infer']['Session']['user'];
-		session: TAuthService['$Infer']['Session']['session'];
-	};
-};
