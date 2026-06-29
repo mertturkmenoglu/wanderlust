1ea6bbb001f18c3886a828730c5719fe00db2933
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-29 18:34:14 +0300

    update home page

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Search } from '@/components/search';
 import { SuspenseWrapper } from '@/components/suspense-wrapper';
-import { Banner } from './-components/banner';
 import { Content } from './-components/content';
 
 export const Route = createFileRoute('/')({
@@ -10,10 +9,8 @@ export const Route = createFileRoute('/')({
 
 function App() {
 	return (
-		<div className="mx-auto flex w-full max-w-7xl flex-col gap-4 md:gap-8">
-			<Search className="mt-8 md:mt-16" variant="global" />
-
-			<Banner />
+		<div className="mx-auto w-full max-w-7xl">
+			<Search className="md:md-24 mt-8" variant="global" />
 
 			<SuspenseWrapper>
 				<Content />
