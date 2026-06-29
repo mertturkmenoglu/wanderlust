4c5cff9bb798fb327d2000aa0098693482758b38
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-06-29 16:16:36 +0300

    add scaffold command

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -4,8 +4,9 @@ import { doctor } from './commands/doctor';
 import { echo } from './commands/echo';
 import { fake } from './commands/fake';
 import { mapStyles } from './commands/map-styles';
+import { scaffold } from './commands/scaffold';
 import { sync } from './commands/sync';
 
-run([echo, clean, fake, sync, mapStyles, doctor], {
+run([echo, clean, fake, sync, mapStyles, doctor, scaffold], {
 	version: '1.0.0',
 });
