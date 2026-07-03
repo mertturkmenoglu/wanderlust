d6c0411ea5b067e039528baab38b0764b0929e4f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-04 00:21:28 +0300

    update container

--- apps/admin/src/components/container/index.tsx
+++ apps/admin/src/components/container/index.tsx
@@ -1,26 +1,42 @@
+import { Button } from '@wanderlust/ui/components/button';
 import { Separator } from '@wanderlust/ui/components/separator';
 import { cn } from '@wanderlust/ui/lib/utils';
+import { UserIcon } from 'lucide-react';
 import { useBreadcrumbs } from '@/hooks/use-breadcrumbs';
 import { Breadcrumbs } from '../breadcrumbs';
 
-type Props = React.ComponentPropsWithoutRef<'div'>;
+type Props = React.ComponentPropsWithoutRef<'div'> & {
+	title: string;
+	actions?: React.ReactNode;
+};
 
-export function Container({ className, children, ...props }: Props) {
+export function Container({
+	title,
+	className,
+	children,
+	actions,
+	...props
+}: Props) {
 	const crumbs = useBreadcrumbs();
 
 	return (
-		<div
-			className={cn(
-				'mx-auto my-16 w-full max-w-7xl px-4 sm:px-6 lg:px-8',
-				className,
-			)}
-			{...props}
-		>
-			<Breadcrumbs crumbs={crumbs} />
+		<div className={cn('w-full p-8', className)} {...props}>
+			<div className="flex flex-row items-center justify-between">
+				<Breadcrumbs crumbs={crumbs} />
 
-			<Separator className="my-4" />
+				<Button size="icon" variant="ghost">
+					<UserIcon />
+				</Button>
+			</div>
 
-			{children}
+			<div className="mt-4">
+				<div className="flex flex-row items-center justify-between gap-4">
+					<h1 className="font-medium text-2xl">{title}</h1>
+					{actions && <div>{actions}</div>}
+				</div>
+				<Separator className="mt-2 mb-4" />
+				<div>{children}</div>
+			</div>
 		</div>
 	);
 }
