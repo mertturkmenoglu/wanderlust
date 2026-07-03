5b07d2909c5777de5595249167320a83fdc231ee
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-03 15:38:58 +0300

    update detail renderers

--- apps/admin/src/components/details/renderer.tsx
+++ apps/admin/src/components/details/renderer.tsx
@@ -1,5 +1,11 @@
 import { Link, type LinkOptions } from '@tanstack/react-router';
-import { buttonVariants } from '@wanderlust/ui/components/button';
+import { Badge } from '@wanderlust/ui/components/badge';
+import { Button, buttonVariants } from '@wanderlust/ui/components/button';
+import { ItemGroup } from '@wanderlust/ui/components/item';
+import { cn } from '@wanderlust/ui/lib/utils';
+import { format } from 'date-fns';
+import { ArrowRightIcon, ClockIcon } from 'lucide-react';
+import React from 'react';
 
 export const renderer = {
 	Image: (src: string) => (
@@ -16,9 +22,13 @@ export const renderer = {
 		</a>
 	),
 	Link: (text: string, options: LinkOptions, children: React.ReactNode) => (
-		<div className="flex flex-col gap-2">
-			<Link {...options} className={buttonVariants({ variant: 'link' })}>
-				{text}
+		<div className="flex flex-col items-start">
+			<Link
+				{...options}
+				className={buttonVariants({ variant: 'link', className: 'px-0!' })}
+			>
+				<span>{text}</span>
+				<ArrowRightIcon />
 			</Link>
 			<div className="block">{children}</div>
 		</div>
@@ -26,7 +36,33 @@ export const renderer = {
 	JSON: (data: unknown) => (
 		<pre className="text-wrap">{JSON.stringify(data, null, 2)}</pre>
 	),
-	Date: (date: Date) => date.toISOString(),
+	Multiple: (data: React.ReactNode[]) => (
+		<ItemGroup className="gap-2">
+			{data.map((item, index) => (
+				<React.Fragment key={index}>{item}</React.Fragment>
+			))}
+		</ItemGroup>
+	),
+	Array: (data: React.ReactNode[], vertical = false) => (
+		<ul
+			className={cn('flex flex-wrap gap-1', {
+				'flex-row': !vertical,
+				'flex-col': vertical,
+			})}
+		>
+			{data.map((item, index) => (
+				<li key={index}>
+					<Badge>{item}</Badge>
+				</li>
+			))}
+		</ul>
+	),
+	Date: (date: Date) => (
+		<Button size="sm" variant="outline">
+			<span>{format(date, 'PP p')}</span>
+			<ClockIcon className="size-4" />
+		</Button>
+	),
 	Gallery: (images: string[]) => (
 		<div className="flex w-3xl flex-wrap gap-4">
 			{images.map((image) => (
