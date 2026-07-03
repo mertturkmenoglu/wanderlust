5f16a3af6f4134639bc2b4e39fd72c7c372e29cb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-04 00:19:36 +0300

    add default functions

---
+++ apps/admin/src/lib/defaults.ts
@@ -0,0 +1,41 @@
+import type { QueryClient } from '@tanstack/react-query';
+import type { DataResource, ResourceKey } from './crud';
+
+export function getDefaultStaticData<TKey extends ResourceKey, TOne>(
+	resource: DataResource<TKey, TOne>,
+	path: 'list' | 'details' | 'new' | 'edit',
+) {
+	if (path === 'list') {
+		return {
+			breadcrumbs: () => resource.breadcrumbs.list(),
+		};
+	}
+	if (path === 'details') {
+		return {
+			breadcrumbs: (data: any) =>
+				resource.breadcrumbs.details(resource.extractors.one(data)),
+		};
+	}
+	if (path === 'new') {
+		return {
+			breadcrumbs: () => resource.breadcrumbs.new(),
+		};
+	}
+	if (path === 'edit') {
+		return {
+			breadcrumbs: (data: any) =>
+				resource.breadcrumbs.edit(resource.extractors.one(data)),
+		};
+	}
+
+	throw new Error(`Invalid path: ${path}`);
+}
+
+export function ensureData<TKey extends ResourceKey, TOne>(
+	r: DataResource<TKey, TOne>,
+	qc: QueryClient,
+	opts: Parameters<ReturnType<typeof r.options.one>['queryOptions']>[0],
+) {
+	// @ts-expect-error I wish I could solve this type issue.
+	return qc.ensureQueryData(r.options.one().queryOptions(opts));
+}
