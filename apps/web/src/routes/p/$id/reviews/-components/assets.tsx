3c034ffc09a540bedc3cfb5c5483ef47d98da600
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-04 04:04:19 +0300

    add assets component

---
+++ apps/web/src/routes/p/$id/reviews/-components/assets.tsx
@@ -0,0 +1,46 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useParams } from '@tanstack/react-router';
+import { ScrollArea, ScrollBar } from '@wanderlust/ui/components/scroll-area';
+import { useAssetLightbox } from '@/hooks/use-asset-lightbox';
+import { orpc } from '@/lib/orpc';
+
+export function Assets() {
+	const params = useParams({ from: '/p/$id/reviews/' });
+
+	const query = useSuspenseQuery(
+		orpc.reviews.listAssetsByPlaceId.queryOptions({
+			input: {
+				id: params.id,
+			},
+		}),
+	);
+
+	const assets = query.data.assets;
+
+	const lb = useAssetLightbox(assets);
+
+	if (assets.length === 0) {
+		return null;
+	}
+
+	return (
+		<div className="my-8">
+			<lb.Component />
+
+			<ScrollArea>
+				<div className="mb-4 flex flex-row gap-4">
+					{assets.map((asset, i) => (
+						<button type="button" key={asset.id} onClick={() => lb.openAt(i)}>
+							<img
+								src={asset.url}
+								alt={asset.description ?? ''}
+								className="aspect-video w-32 min-w-32! rounded object-cover"
+							/>
+						</button>
+					))}
+				</div>
+				<ScrollBar orientation="horizontal" />
+			</ScrollArea>
+		</div>
+	);
+}
