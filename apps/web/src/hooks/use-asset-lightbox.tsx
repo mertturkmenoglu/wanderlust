4702fc54d1009b8c1d30f78a6e58218929b59ec2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-05 06:42:51 +0300

    update useAssetLightbox hook

--- apps/web/src/hooks/use-asset-lightbox.tsx
+++ apps/web/src/hooks/use-asset-lightbox.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@wanderlust/ui/components/button';
+import { cn } from '@wanderlust/ui/lib/utils';
+import { ChevronLeftIcon, XIcon } from 'lucide-react';
 import { useState } from 'react';
 import Lightbox, {
 	type LightboxExternalProps,
+	useController,
 } from 'yet-another-react-lightbox';
 import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
 import type { Outputs } from '@/lib/orpc';
@@ -11,6 +15,30 @@ type Options = {
 	props: LightboxExternalProps;
 };
 
+function NavButton({ type }: { type: 'prev' | 'next' }) {
+	const controller = useController();
+
+	return (
+		<Button
+			variant="midnight"
+			size="icon-sm"
+			className={cn('m-4! p-0!', {
+				yarl__navigation_next: type === 'next',
+				yarl__navigation_prev: type === 'prev',
+			})}
+			onClick={() => {
+				if (type === 'prev') {
+					controller.prev();
+				} else {
+					controller.next();
+				}
+			}}
+		>
+			<ChevronLeftIcon className={type === 'next' ? 'rotate-180' : ''} />
+		</Button>
+	);
+}
+
 export function useAssetLightbox(
 	assets: (Asset | { url: string })[],
 	options?: Options,
@@ -19,27 +47,44 @@ export function useAssetLightbox(
 	const [index, setIndex] = useState(0);
 
 	const Component = (
-		<Lightbox
-			open={open}
-			close={() => setOpen(false)}
-			slides={assets.map((m) => ({
-				src: m.url,
-			}))}
-			plugins={[Thumbnails]}
-			carousel={{
-				finite: true,
-			}}
-			controller={{
-				closeOnBackdropClick: true,
-			}}
-			styles={{
-				container: {
-					backgroundColor: 'rgba(0, 0, 0, 0.8)',
-				},
-			}}
-			index={index}
-			{...options?.props}
-		/>
+		<>
+			<Lightbox
+				open={open}
+				close={() => setOpen(false)}
+				slides={assets.map((m) => ({
+					src: m.url,
+				}))}
+				plugins={[Thumbnails]}
+				carousel={{
+					finite: true,
+				}}
+				render={{
+					buttonClose: () => (
+						<Button variant="midnight" size="icon-sm">
+							<XIcon />
+						</Button>
+					),
+					buttonPrev: () => <NavButton type="prev" />,
+					buttonNext: () => <NavButton type="next" />,
+				}}
+				controller={{ closeOnBackdropClick: true }}
+				thumbnails={{
+					vignette: false,
+					imageFit: 'cover',
+					position: 'bottom',
+				}}
+				styles={{
+					container: {
+						backgroundColor: 'rgba(10, 10, 10, 0.8)',
+					},
+					thumbnailsContainer: {
+						backgroundColor: 'rgba(0, 0, 0, 0.8)',
+					},
+				}}
+				index={index}
+				{...options?.props}
+			/>
+		</>
 	);
 
 	return {
