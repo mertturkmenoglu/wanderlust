2c309aa4442efd480f2fb1cd498c1148a3028989
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-05 12:59:49 +0300

    update index.ts
    - add instrumentation import
    - use compress middleware
    - add hono otel instrumentation

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,29 @@
 import 'reflect-metadata';
+import './lib/instrumentation';
 
+import { httpInstrumentationMiddleware } from '@hono/otel';
 import { Hono } from 'hono';
+import { compress } from 'hono/compress';
 import { bootstrapServices } from './bootstrap';
 import { getCorsConfig } from './middlewares/cors';
 import { matchHandler } from './middlewares/match-handler';
+import { requestLogger } from './middlewares/request-logger';
 import { getHandlers } from './routes/handler';
 
 const { cfg, auth } = await bootstrapServices();
 
 const app = new Hono()
+	.use(compress())
+	.use(
+		'*',
+		httpInstrumentationMiddleware({
+			serviceName: 'wl-api',
+			spanNameFactory(c) {
+				return `HTTP ${c.req.method} ${c.req.path}`;
+			},
+		}),
+	)
+	.use('*', requestLogger)
 	.use('/*', getCorsConfig(cfg))
 	.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw))
 	.use('/*', matchHandler(getHandlers()));
