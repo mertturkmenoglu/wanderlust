e24232af733b224e359bbdc8ef6a7bac7d8ef12c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-05 12:58:09 +0300

    refactor error normalization middleware for improved readability

--- apps/api/src/middlewares/with-error-normalization.ts
+++ apps/api/src/middlewares/with-error-normalization.ts
@@ -1,13 +1,20 @@
-import { ORPCError, os } from "@orpc/server";
+import { ORPCError, os } from '@orpc/server';
 
 export const withErrorNormalization = os.middleware(async ({ next }) => {
 	try {
 		return await next();
 	} catch (err) {
-		if (err instanceof ORPCError) throw err;
+		if (err instanceof ORPCError) {
+			throw err;
+		}
+
 		throw new ORPCError('INTERNAL_SERVER_ERROR', {
 			message: 'An unexpected error occurred',
 			cause: err,
 		});
 	}
-})
+});
+
+Object.defineProperty(withErrorNormalization, 'name', {
+	value: 'withErrorNormalization',
+});
