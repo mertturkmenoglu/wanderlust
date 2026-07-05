6c99c9955ab9c088c316ae2b5c9e0e235f79b324
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-05 12:57:37 +0300

    update auth middleware
    - redact email from user info
    - set auth.user on active span
    - define middleware name for better otel integration

--- apps/api/src/middlewares/authn.ts
+++ apps/api/src/middlewares/authn.ts
@@ -1,12 +1,22 @@
+import { redact } from '@arcjet/redact';
+import { trace } from '@opentelemetry/api';
 import { ORPCError, os } from '@orpc/server';
 import type { Context } from '@/lib/context';
 
 export const requireAuth = os
 	.$context<Context>()
 	.middleware(async ({ context, next }) => {
+		const span = trace.getActiveSpan();
+
 		if (!context.session?.user) {
 			throw new ORPCError('UNAUTHORIZED');
 		}
+
+		const [user] = await redact(JSON.stringify(context.session.user), {
+			entities: ['email'],
+		});
+
+		span?.setAttribute('auth.user', user);
 
 		return next({
 			context: {
@@ -15,3 +25,7 @@ export const requireAuth = os
 			},
 		});
 	});
+
+Object.defineProperty(requireAuth, 'name', {
+	value: 'requireAuth',
+});
