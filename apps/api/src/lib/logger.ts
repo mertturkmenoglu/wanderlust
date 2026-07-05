49a6776388536909dba55c4137f92fb36b04fc1d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-05 14:04:39 +0300

    add request logger

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -1,6 +1,6 @@
 import winston from 'winston';
 
-const { combine, timestamp, printf, colorize, json } = winston.format;
+const { combine, timestamp, printf, colorize, json, align } = winston.format;
 
 const devFormat = combine(
 	colorize(),
@@ -18,5 +18,21 @@ export const logger = winston.createLogger({
 			? combine(timestamp(), json())
 			: devFormat,
 	defaultMeta: { service: 'wl-api' },
+	transports: [new winston.transports.Console()],
+});
+
+export const requestLogger = winston.createLogger({
+	level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+	format: combine(
+		colorize({ all: true }),
+		timestamp({ format: 'HH:mm:ss' }),
+		align(),
+		printf(
+			({ level, message, timestamp, ...meta }) => {
+				const metaStr = Object.keys(meta).length ? `\n${JSON.stringify(meta, null, 2)}` : '';
+				return `[${timestamp}] ${level}: ${message}${metaStr}`;
+			}
+		),
+	),
 	transports: [new winston.transports.Console()],
 });
