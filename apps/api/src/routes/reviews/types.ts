3b1aba8f54d85efbcccfa95ea7fd49d8cb0c87bc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-09 08:23:59 +0300

    update CreateReviewParams type

--- apps/api/src/routes/reviews/types.ts
+++ apps/api/src/routes/reviews/types.ts
@@ -3,4 +3,10 @@ import type { reviews as dto } from '@wanderlust/contract';
 export type CreateReviewParams = dto.CreateInput & {
 	detectedLanguage: string | null;
 	urls: string[];
+	facets: {
+		type: string;
+		value: string;
+		start: number;
+		end: number;
+	}[];
 };
