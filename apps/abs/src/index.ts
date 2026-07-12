8189b479b386c2a47bcb42e76392b69193a90a5e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-12 23:48:24 +0300

    update agents initalization

--- apps/abs/src/index.ts
+++ apps/abs/src/index.ts
@@ -1,4 +1,4 @@
-import { createAgentStore as createAgentStoreV2 } from './agent';
+import { Agent } from './agent';
 import { L } from './logger';
 import { waitUntilApiIsHealthy } from './utils';
 import { usernames } from './well-known';
@@ -10,29 +10,13 @@ async function main() {
 
 	await waitUntilApiIsHealthy();
 
-	fireAgents();
+	await startAgents();
 }
 
-function fireAgents() {
-	const agents = usernames.map((u) => createAgentStoreV2(u));
+async function startAgents() {
+	const agents = await Promise.all(usernames.map((u) => Agent.load(u)));
 
-	agents.forEach((agent) => {
-		agent.subscribe((state) => {
-			if (state.state === 'destroyed') {
-				l.info('Agent store v2 completed');
-			}
-
-			if (state.state === 'idle') {
-				// TODO: Choose an action for the agent and start the loop here.
-			}
-		});
-
-		// Start an agent only after it has been hydrated from the persisted state.
-		// Otherwise it will always fire a sign in request.
-		agent.persist.onFinishHydration(() => {
-			agent.getState().init();
-		});
-	});
+	await Promise.all(agents.map((a) => Agent.save(a)));
 }
 
 main();
