8e52572736960b10c8b9059c6bddde0f5edba53a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-12 04:29:38 +0300

    create custom storage for zustand

--- apps/abs/src/persistence.ts
+++ apps/abs/src/persistence.ts
@@ -1,36 +1,21 @@
 import path from 'node:path';
-import type { Auth } from './auth';
 
-export namespace Persistence {
-	const filepath = path.join('tmp', 'state.json');
-
-	export async function load(): Promise<
-		[Map<string, string>, Map<string, Auth.User>]
-	> {
+export const CustomStorage = {
+	getItem: async (name: string) => {
+		const filepath = path.join('tmp', 'agents', name);
 		const file = Bun.file(filepath);
 		const exists = await file.exists();
-
-		if (!exists) {
-			return [new Map(), new Map()];
-		}
-
-		const content = await file.json();
-
-		const tokens = new Map<string, string>(content.tokens);
-		const users = new Map<string, Auth.User>(content.users);
-
-		return [tokens, users];
-	}
-
-	export async function save(
-		tokens: Map<string, string>,
-		users: Map<string, Auth.User>,
-	) {
+		if (!exists) return null;
+		return await file.json();
+	},
+	removeItem: async (name: string) => {
+		const filepath = path.join('tmp', 'agents', name);
 		const file = Bun.file(filepath);
-		const content = {
-			tokens: Array.from(tokens.entries()),
-			users: Array.from(users.entries()),
-		};
-		await Bun.write(file, JSON.stringify(content));
-	}
-}
+		await file.delete();
+	},
+	setItem: async (name: string, value: unknown) => {
+		const filepath = path.join('tmp', 'agents', name);
+		const file = Bun.file(filepath);
+		await file.write(JSON.stringify(value));
+	},
+};
