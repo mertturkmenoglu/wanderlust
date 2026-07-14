1bd996bee951e5506d101912a09105d0d3f85637
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-15 00:29:35 +0300

    add vitest

---
+++ apps/web/vitest.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react';
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		browser: {
+			provider: playwright(),
+			enabled: true,
+			// at least one instance is required
+			instances: [{ browser: 'chromium' }],
+			headless: true,
+		},
+	},
+});
