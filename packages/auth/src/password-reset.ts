f99c7a5fca2bd8d3e6c195330890679ba362afc1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-23 16:54:19 +0300

    rename queue

--- packages/auth/src/password-reset.ts
+++ packages/auth/src/password-reset.ts
@@ -5,7 +5,7 @@ export async function sendResetPassword(
 	data: { user: User; url: string; token: string },
 	jobs: TJobsService,
 ): Promise<void> {
-	await jobs.email.queue.add('password-reset', {
+	await jobs.emails.queue.add('password-reset', {
 		firstName: data.user.name,
 		email: data.user.email,
 		url: data.url,
