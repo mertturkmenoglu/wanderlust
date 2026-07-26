6f9c9d7994ab34a51e728fd490e261a357a71798
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-26 20:46:37 +0300

    delete media page

--- apps/web/src/routes/trips/$id/media/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/trips/$id/media/')({
-	component: RouteComponent,
-});
-
-function RouteComponent() {
-	return <div>Hello "/trips/$id/media/"!</div>;
-}
