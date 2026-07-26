51a915d97e3156a003ec08cd33af0f2eaff278d2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-26 20:46:55 +0300

    show trip description on summary page

--- apps/web/src/routes/trips/$id/index.tsx
+++ apps/web/src/routes/trips/$id/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useLoaderData } from '@tanstack/react-router';
+import { CollapsibleText } from '@/components/collapsible-text';
 import { orpc } from '@/lib/orpc';
 import { useTripSummary } from './-components/hooks';
 import { SummaryCard } from './-components/summary-card';
@@ -17,9 +18,18 @@ export const Route = createFileRoute('/trips/$id/')({
 
 function RouteComponent() {
 	const items = useTripSummary();
+	const { trip } = useLoaderData({ from: '/trips/$id' });
 
 	return (
-		<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
+		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-6">
+			<CollapsibleText
+				className="col-span-full"
+				text={
+					trip.description.length > 0 ? trip.description : 'No description.'
+				}
+				charLimit={500}
+			/>
+
 			{items.map((item) => (
 				<SummaryCard key={`${item.explain}-${item.title}`} {...item} />
 			))}
