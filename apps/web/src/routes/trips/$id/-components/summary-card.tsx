75df08856bb2a9e3e1fc1558ba8338930fe9f54d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-26 20:46:31 +0300

    update summary card

--- apps/web/src/routes/trips/$id/-components/summary-card.tsx
+++ apps/web/src/routes/trips/$id/-components/summary-card.tsx
@@ -1,7 +1,7 @@
-import { Link, type LinkOptions } from '@tanstack/react-router';
-import { Card, CardContent, CardFooter } from '@wanderlust/ui/components/card';
+import type { LinkOptions } from '@tanstack/react-router';
+import { Card, CardContent } from '@wanderlust/ui/components/card';
 import { cn } from '@wanderlust/ui/lib/utils';
-import { ChevronRightIcon } from 'lucide-react';
+import type { ChevronRightIcon } from 'lucide-react';
 
 export type SummaryCardProps = {
 	title: string;
@@ -10,6 +10,7 @@ export type SummaryCardProps = {
 	icon: typeof ChevronRightIcon;
 	explain: string;
 	link: LinkOptions;
+	className?: string;
 };
 
 export function SummaryCard({
@@ -17,30 +18,20 @@ export function SummaryCard({
 	value,
 	color,
 	icon: Icon,
-	explain,
-	link,
+	className,
 }: SummaryCardProps) {
 	return (
-		<Card key="key" className="">
-			<CardContent className="flex flex-row items-start justify-between p-6">
+		<Card size="sm" className={cn(className)}>
+			<CardContent className="flex flex-row items-start justify-between p-4">
 				<div>
 					<dt className="font-medium text-muted-foreground text-sm">{title}</dt>
-					<dd className="lineline-clamp-2 h-16 font-semibold text-3xl text-foreground tabular-nums">
+					<dd className="line-clamp-2 h-16 font-semibold text-3xl text-foreground tabular-nums">
 						{value}
 					</dd>
 				</div>
 
 				<Icon className={cn('size-6', color)} />
 			</CardContent>
-			<CardFooter>
-				<Link
-					{...link}
-					className="flex w-full items-center justify-between gap-1"
-				>
-					<span>{explain}</span>
-					<ChevronRightIcon className="text-primary" />
-				</Link>
-			</CardFooter>
 		</Card>
 	);
 }
