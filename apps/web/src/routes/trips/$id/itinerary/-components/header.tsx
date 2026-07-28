03a233a47a671a8fccff0844bff9083879b439f6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-28 03:11:38 +0300

    use upsert item dialog

--- apps/web/src/routes/trips/$id/itinerary/-components/header.tsx
+++ apps/web/src/routes/trips/$id/itinerary/-components/header.tsx
@@ -1,12 +1,12 @@
 import { useTripIsPrivileged } from '@/hooks/use-trip-is-privileged';
-import { UpsertLocationDialog } from './upsert-location-dialog';
+import { UpsertItemDialog } from './upsert-item-dialog';
 
 export function Header() {
 	const isPrivileged = useTripIsPrivileged();
 
 	return (
 		<div className="flex flex-col gap-2 md:flex-row md:items-start md:gap-4">
-			{isPrivileged && <UpsertLocationDialog />}
+			{isPrivileged && <UpsertItemDialog />}
 		</div>
 	);
 }
