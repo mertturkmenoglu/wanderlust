8f3f264c0ea440e8738424040927f5a7f61b39fe
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-07-28 03:12:50 +0300

    add dialog content

---
+++ apps/web/src/routes/trips/$id/itinerary/-components/upsert-item-dialog/content.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@wanderlust/ui/components/button';
+import {
+	Dialog,
+	DialogContent,
+	DialogTrigger,
+} from '@wanderlust/ui/components/dialog';
+import { PlusIcon } from 'lucide-react';
+import { Footer } from './footer';
+import { Header } from './header';
+import { mux } from './mux';
+
+export function Content() {
+	const InnerContent = mux();
+
+	return (
+		<Dialog>
+			<DialogTrigger
+				render={
+					<Button className="ml-auto" variant="secondary">
+						<PlusIcon />
+						<span>Add Itinerary Item</span>
+					</Button>
+				}
+			/>
+			<DialogContent className="md:min-w-3xl">
+				<Header />
+				<div className="no-scrollbar -mx-4 max-h-[50vh] overflow-y-auto px-4 py-4">
+					<InnerContent />
+				</div>
+				<Footer />
+			</DialogContent>
+		</Dialog>
+	);
+}
