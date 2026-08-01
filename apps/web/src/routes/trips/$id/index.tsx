0eb9f46ca01722f748d1a25bbbcb56956c568bf9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2026-08-01 07:27:45 +0300

    use new components

--- apps/web/src/routes/trips/$id/index.tsx
+++ apps/web/src/routes/trips/$id/index.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute, useLoaderData } from '@tanstack/react-router';
 import { CollapsibleText } from '@/components/collapsible-text';
 import { orpc } from '@/lib/orpc';
+import { CitiesSection } from './-components/cities-section';
 import { useTripSummary } from './-components/hooks';
+import { MapSection } from './-components/map';
 import { SummaryCard } from './-components/summary-card';
 
 export const Route = createFileRoute('/trips/$id/')({
@@ -21,18 +23,24 @@ function RouteComponent() {
 	const { trip } = useLoaderData({ from: '/trips/$id' });
 
 	return (
-		<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-6">
-			<CollapsibleText
-				className="col-span-full"
-				text={
-					trip.description.length > 0 ? trip.description : 'No description.'
-				}
-				charLimit={500}
-			/>
+		<div>
+			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-6">
+				<CollapsibleText
+					className="col-span-full"
+					text={
+						trip.description.length > 0 ? trip.description : 'No description.'
+					}
+					charLimit={500}
+				/>
 
-			{items.map((item) => (
-				<SummaryCard key={`${item.explain}-${item.title}`} {...item} />
-			))}
+				{items.map((item) => (
+					<SummaryCard key={`${item.explain}-${item.title}`} {...item} />
+				))}
+			</div>
+
+			<CitiesSection className="mt-8" />
+
+			<MapSection className="mt-8" />
 		</div>
 	);
 }
